02457ea7cb4769a1085c166fd4931c7dd4217514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 04:03:20 +0000

    Add package codetocad/0.3.1705265326/codetocad-0.3.1705265326.tar.gz

---
+++ packages/codetocad/codetocad-0.3.1705265326.tar.gz/codetocad-0.3.1705265326/codetocad/__init__.py
@@ -0,0 +1,12 @@
+from codetocad.utilities import *
+from codetocad.codetocad_types import *
+from codetocad.interfaces import *
+
+from importlib.util import find_spec
+
+if find_spec("blender_provider"):
+    # this will only be available when the Blender Addon is packaged:
+    from blender_provider import *
+else:
+    # When this python library is bundled, use the providers_sample if the "providers" directory is not copied.
+    from codetocad.providers_sample import *
