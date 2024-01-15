3ed0059ce535e0b76cd09d94cad919e8015e07d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:03:39 +0000

    Add package yt-auto-sys-utils/1.0.0/yt_auto_sys_utils-1.0.0.tar.gz

---
+++ packages/yt-auto-sys-utils/yt_auto_sys_utils-1.0.0.tar.gz/yt_auto_sys_utils-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yt_auto_sys_utils',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Shared library for microservices for yt auto system.',
+)
