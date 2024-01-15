b747ab9bd5f215ad5bc15d4385528df61d9ca68e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:03:36 +0000

    Add package konbinine/0.2.1/konbinine-0.2.1.tar.gz

---
+++ packages/konbinine/konbinine-0.2.1.tar.gz/konbinine-0.2.1/konbinine/fields.py
@@ -0,0 +1,125 @@
+"""
+Opinionated SG entities default fields.
+
+Modify this or use the custom_fields to pass in your preferred
+fields. The models are set up with the default fields in mind.
+
+"""
+
+HUMANUSER_FIELDS = [
+    "login",
+    "name",
+    "firstname",
+    "lastname",
+    "email",
+    "file_access",
+    "image",
+    "sg_status_list",
+    "projects",
+    "groups",
+]
+
+PROJECT_FIELDS = [
+    "name",
+    "type",
+    "archived",
+    "code",
+    "sg_description",
+    "sg_status",
+    "sg_type",
+    "start_date",
+    "end_date",
+    "updated_at",
+    "image",
+    "duration",
+]
+
+TASK_FIELDS = [
+    "content",
+    "entity",
+    "project",
+    "sg_status_list",
+]
+
+ASSET_FIELDS = [
+    "code",
+    "tasks",
+    "notes",
+    "image",
+    "filmstrip_image",
+    "sg_asset_type",
+    "sg_status_list",
+]
+
+SHOT_FIELDS = [
+    "code",
+    "image",
+    "filmstrip_image",
+    "sg_cut_in",
+    "sg_cut_out",
+    "sg_cut_duration",
+    "sg_status_list",
+    "sg_shot_type",
+]
+
+VERSION_FIELDS = [
+    "code",
+    "description",
+    "flagged",
+    "image",
+    "filmstrip_image",
+    "entity",
+    "sg_task",
+    "sg_uploaded_movie",
+    "sg_path_to_frames",
+    "sg_path_to_movie",
+    "sg_status_list",
+    "uploaded_movie_duration",
+    "sg_uploaded_movie_frame_rate",
+    "notes",
+]
+
+ATTACHMENT_FIELDS = [
+    "this_file",
+    "display_name",
+    "description",
+    "image",
+    "filename",
+    "file_extension",
+    "file_size",
+    "filmstrip_image",
+    "processing_status",
+    "original_fname",
+    "open_notes_count",
+    "sg_status_list",
+]
+
+NOTE_FIELDS = [
+    "subject",
+    "content",
+    "project",
+    "user",
+    "attachments",
+    "addressings_cc",
+    "addressings_to",
+    "note_links",
+    "sg_status_list",
+]
+
+BOOKING_FIELDS = [
+    "start_date",
+    "end_date",
+    "vacation",
+    "user",
+    "note",
+    "sg_status_list",
+]
+
+TIMELOG_FIELDS = [
+    "date",
+    "description",
+    "duration",
+    "entity",
+    "project",
+    "user",
+]
---
+++ packages/konbinine/konbinine-0.2.1.tar.gz/konbinine-0.2.1/konbinine/models.py
@@ -0,0 +1,535 @@
+from __future__ import annotations
+
+import datetime
+import inspect
+from dataclasses import asdict, dataclass, field
+from typing import Any, Dict, List, Optional
+
+from konbinine.enums import SgEntity
+from konbinine.exceptions import (
+    InvalidSgDateFormatException,
+)
+from konbinine.types import TSgUploadedMovie
+from konbinine.utils import (
+    get_current_utc_dt,
+    SG_DATE_FORMAT,
+    validate_sg_date_format,
+)
+
+# TODO: Use Python 3.10+ kw_only but that is another headache for maintenance...
+
+
+@dataclass
+class SgIdMixin:
+    id: int = 0
+
+
+@dataclass
+class SgBaseModel:
+    _extra_fields: dict = field(default_factory=dict)
+
+    def to_dict(self, include_extra_fields=False) -> Dict[str, Any]:
+        dict_ = {
+            k: v for k, v in asdict(self).items() if v
+        }
+        dict_.pop("id", None)
+        dict_.pop("type", None)
+        extra_fields = dict_.pop("_extra_fields", None)
+        if include_extra_fields:
+            dict_.update(extra_fields)
+
+        return dict_
+
+    def to_full_dict(self, include_extra_fields=False) -> Dict[str, Any]:
+        dict_ = {
+            k: v for k, v in asdict(self).items() if v
+        }
+        extra_fields = dict_.pop("_extra_fields", None)
+        if include_extra_fields:
+            dict_.update(extra_fields)
+
+        return dict_
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if "." in k:
+                k = k.replace(".", "__")
+            sanitized_dict[k] = v
+
+        _extra_fields = {}
+        for k, v in dict_.items():
+            if k not in params:
+                _extra_fields[k] = v
+
+        sanitized_dict["_extra_fields"] = _extra_fields
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
+
+
+@dataclass
+class SgGenericEntity(SgIdMixin, SgBaseModel):
+    name: str = ""
+    type: str = ""
+
+
+@dataclass
+class SgNote(SgIdMixin, SgBaseModel):
+    subject: str = ""
+    content: str = ""
+    sg_status_list: str = ""
+    project: Optional[SgProject] = None
+    user: Optional[SgGenericEntity] = None
+    addressings_cc: list[SgGenericEntity] = field(default_factory=list)
+    addressings_to: list[SgGenericEntity] = field(default_factory=list)
+    note_links: list[SgGenericEntity] = field(default_factory=list)
+    attachments: list[SgGenericEntity] = field(default_factory=list)
+    type: str = SgEntity.NOTE
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if k == "user" and v:
+                v = SgGenericEntity.from_dict(v)
+
+            if k == "note_links" and v:
+                v = [SgGenericEntity.from_dict(_v) for _v in v]
+
+            if k == "addressings_cc" and v:
+                v = [SgGenericEntity.from_dict(_v) for _v in v]
+
+            if k == "addressings_to" and v:
+                v = [SgGenericEntity.from_dict(_v) for _v in v]
+
+            if k == "attachments" and v:
+                v = [SgGenericEntity.from_dict(_v) for _v in v]
+
+            sanitized_dict[k] = v
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
+
+
+@dataclass
+class SgProject(SgIdMixin, SgBaseModel):
+    name: str = ""
+    type: str = SgEntity.PROJECT
+    archived: bool = False
+    code: Optional[str] = None
+    sg_description: Optional[str] = None
+    sg_status: Optional[str] = None
+    sg_type: Optional[str] = None
+    start_date: Optional[str] = None  # Date uses YYYY-MM-DD format
+    end_date: Optional[str] = None
+    updated_at: Optional[datetime.datetime] = None  # Example 2023-08-07T06:29:35Z
+    image: Optional[str] = None  # When retrieve from SG API, should be the URL path
+    filmstrip_image: Optional[str] = None
+    image_upload: Optional[str] = None  # For uploading to SG (str, bytes or os.PathLike object)
+    duration: Optional[int] = None  # Number of days
+
+    def validate_stale_data(self) -> bool:
+        current_dt = get_current_utc_dt()
+        return self.updated_at > current_dt
+
+
+@dataclass
+class _SgPipelineStep(SgBaseModel):
+    code: str = ""  # The nice name (e.g. Model)
+    short_name: str = ""  # Self-explanatory (e.g. MOD)
+    type: str = SgEntity.STEP
+
+
+@dataclass
+class SgPipelineStep(SgIdMixin, _SgPipelineStep):
+    pass
+
+
+@dataclass
+class _SgVersion(SgBaseModel):
+    code: str = ""  # Version Name
+    description: Optional[str] = None
+    flagged: bool = False
+    image: Optional[str] = None
+    filmstrip_image: Optional[str] = None
+    entity: Optional[SgGenericEntity] = None  # Link
+    sg_task: Optional[SgGenericEntity] = None
+    sg_uploaded_movie: Optional[TSgUploadedMovie] = None
+    sg_uploaded_movie_mp4: Optional[TSgUploadedMovie] = None
+    sg_uploaded_movie_webm: Optional[TSgUploadedMovie] = None
+    sg_path_to_frames: Optional[str] = None
+    sg_path_to_movie: Optional[str] = None
+    sg_status_list: str = ""
+    uploaded_movie_duration: Optional[str] = None
+    sg_uploaded_movie_frame_rate: Optional[str] = None
+    notes: List[SgGenericEntity] = field(default_factory=list)
+    type: str = SgEntity.VERSION
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if "." in k:
+                k = k.replace(".", "__")
+
+            if k == "entity" and v:
+                v = SgGenericEntity.from_dict(v)
+
+            if k == "sg_task" and v:
+                v = SgGenericEntity.from_dict(v)
+
+            if k == "notes" and v:
+                v = [SgGenericEntity.from_dict(_v) for _v in v]
+
+            sanitized_dict[k] = v
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
+
+
+@dataclass
+class SgVersion(SgIdMixin, _SgVersion):
+    pass
+
+
+@dataclass
+class _SgShot(SgBaseModel):
+    code: str = ""  # Shot Code
+    description: Optional[str] = None
+    image: Optional[str] = None
+    filmstrip_image: Optional[str] = None
+    notes: list[SgGenericEntity] = field(default_factory=list)
+    sg_cut_in: Optional[int] = None
+    sg_cut_out: Optional[int] = None
+    sg_cut_duration: Optional[int] = None
+    sg_status_list: str = ""
+    sg_shot_type: str = ""
+    type: str = SgEntity.SHOT
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if "." in k:
+                k = k.replace(".", "__")
+
+            if k == "notes" and v:
+                v = [SgGenericEntity.from_dict(_v) for _v in v]
+
+            sanitized_dict[k] = v
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
+
+
+@dataclass
+class SgShot(SgIdMixin, _SgShot):
+    pass
+
+
+@dataclass
+class _SgTask(SgBaseModel):
+    name: str = ""
+    short_name: str = ""
+    content: str = ""
+    notes: list[SgGenericEntity] = field(default_factory=list)
+    sg_status_list: str = ""
+    entity: Optional[SgGenericEntity] = None
+    project: Optional[SgProject] = None
+    type: str = SgEntity.TASK
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if "." in k:
+                k = k.replace(".", "__")
+
+            if k == "notes" and v:
+                v = [SgGenericEntity.from_dict(_v) for _v in v]
+
+            sanitized_dict[k] = v
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
+
+
+@dataclass
+class SgTask(SgIdMixin, _SgTask):
+    pass
+
+
+@dataclass
+class _SgAsset(SgBaseModel):
+    code: str = ""  # Shot Code
+    tasks: List[SgTask] = field(default_factory=list)
+    notes: list[SgGenericEntity] = field(default_factory=list)
+    image: Optional[str] = None
+    filmstrip_image: Optional[str] = None
+    sg_asset_type: str = ""
+    sg_status_list: str = ""
+    type: str = SgEntity.ASSET
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if "." in k:
+                k = k.replace(".", "__")
+
+            if k == "tasks" and v:
+                v = [SgTask.from_dict(_v) for _v in v]
+
+            if k == "notes" and v:
+                v = [SgGenericEntity.from_dict(_v) for _v in v]
+
+            sanitized_dict[k] = v
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
+
+
+@dataclass
+class SgAsset(SgIdMixin, _SgAsset):
+    pass
+
+
+@dataclass
+class _SgPlaylist(SgBaseModel):
+    code: str = ""  # Playlist name
+    description: str = ""
+    notes: list[SgGenericEntity] = field(default_factory=list)
+    versions: List[SgVersion] = field(default_factory=list)
+    type: str = SgEntity.PLAYLIST
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if k == "versions" and v:
+                v = [SgVersion.from_dict(_v) for _v in v]
+
+            if k == "notes" and v:
+                v = [SgGenericEntity.from_dict(_v) for _v in v]
+
+            sanitized_dict[k] = v
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
+
+
+@dataclass
+class SgPlaylist(SgIdMixin, _SgPlaylist):
+    pass
+
+
+@dataclass
+class _SgHumanUser(SgBaseModel):
+    login: str = ""
+    name: str = ""
+    firstname: str = ""
+    lastname: str = ""
+    email: str = ""
+    sg_status_list: str = ""
+    file_access: bool = False
+    type: str = SgEntity.HUMANUSER
+    image: Optional[str] = None
+    projects: Optional[List[dict]] = None
+    groups: Optional[List[dict]] = None
+
+    def to_dict(self) -> Dict[str, Any]:
+        dict_ = {
+            k: v for k, v in asdict(self).items() if v
+        }
+        dict_.pop("id", None)
+        dict_.pop("type", None)
+
+        return dict_
+
+
+@dataclass
+class SgHumanUser(SgIdMixin, _SgHumanUser):
+    pass
+
+
+@dataclass
+class _SgBooking(SgBaseModel):
+    user: Optional[SgHumanUser] = None
+    start_date: str = ""
+    end_date: str = ""
+    vacation: bool = True
+    note: str = ""
+    sg_status_list: str = ""
+    type: str = SgEntity.BOOKING
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if k == "user" and v:
+                v = SgHumanUser.from_dict(v)
+
+            sanitized_dict[k] = v
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
+
+    def __post_init__(self):
+        valid_start_date = validate_sg_date_format(self.start_date)
+        valid_end_date = validate_sg_date_format(self.end_date)
+
+        if not valid_start_date or not valid_end_date:
+            raise InvalidSgDateFormatException("Date format must be YYYY-MM-DD")
+
+
+@dataclass
+class SgBooking(SgIdMixin, _SgBooking):
+    pass
+
+
+@dataclass
+class _SgTimeLog(SgBaseModel):
+    date: str = ""
+    description: str = ""
+    duration: float = 0.0
+    entity: Optional[SgGenericEntity] = None
+    project: Optional[SgGenericEntity] = None
+    user: Optional[SgGenericEntity] = None
+    type: str = SgEntity.TIMELOG
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if k == "entity" and v:
+                v = SgGenericEntity.from_dict(v)
+
+            if k == "project" and v:
+                v = SgGenericEntity.from_dict(v)
+
+            if k == "user" and v:
+                v = SgGenericEntity.from_dict(v)
+
+            sanitized_dict[k] = v
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
+
+    def __post_init__(self):
+        valid_date = validate_sg_date_format(self.date)
+
+        if not valid_date:
+            raise InvalidSgDateFormatException("Date format must be YYYY-MM-DD")
+
+    def get_date(self) -> datetime.date:
+        timelog_date = datetime.datetime.strptime(self.date, SG_DATE_FORMAT).date()
+        return timelog_date
+
+
+@dataclass
+class SgTimeLog(SgIdMixin, _SgTimeLog):
+    pass
+
+
+@dataclass
+class SgAttachmentFile(SgIdMixin, SgBaseModel):
+    url: str = ""
+    name: str = ""  # the filename
+    content_type: Optional[str] = None
+    link_type: str = ""
+    type: str = SgEntity.ATTACHMENT
+
+
+@dataclass
+class SgAttachment(SgBaseModel):
+    # NOTE: There is no ID attribute on Attachment entity, but it is stored
+    #  in 'this_file' attribute where you can find the file at 'Files' page on SG
+    this_file: Optional[SgAttachmentFile] = None
+    display_name: str = ""  # filename
+    description: Optional[str] = None
+    image: Optional[str] = None
+    filename: Optional[str] = None
+    file_extension: Optional[str] = None
+    file_size: Optional[int] = None
+    filmstrip_image: Optional[str] = None
+    processing_status: Optional[str] = None
+    original_fname: Optional[str] = None
+    open_notes_count: int = 0
+    sg_status_list: str = ""
+
+    @classmethod
+    def from_dict(cls, dict_):
+        params = inspect.signature(cls).parameters
+
+        sanitized_dict = {}
+        for k, v in dict_.items():
+            if "." in k:
+                k = k.replace(".", "__")
+
+            if k == "this_file" and v:
+                v = SgAttachmentFile.from_dict(v)
+
+            sanitized_dict[k] = v
+
+        return cls(
+            **{
+                k: v for k, v in sanitized_dict.items()
+                if k in params
+            }
+        )
