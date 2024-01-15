3546d07347c0729c3866a719ddefcad241ecb1e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:04:43 +0000

    Add package locust/2.20.2.dev37/locust-2.20.2.dev37.tar.gz

---
+++ packages/locust/locust-2.20.2.dev37.tar.gz/locust-2.20.2.dev37/locust/webui/src/redux/api/swarm.ts
@@ -0,0 +1,49 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import {
+  IStatsResponse,
+  ISwarmExceptionsResponse,
+  ISwarmRatios,
+  ILogsResponse,
+} from 'types/ui.types';
+import { createFormData } from 'utils/object';
+import { camelCaseKeys, snakeCaseKeys } from 'utils/string';
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery(),
+  endpoints: builder => ({
+    getStats: builder.query<IStatsResponse, void>({
+      query: () => 'stats/requests',
+      transformResponse: camelCaseKeys<IStatsResponse>,
+    }),
+    getTasks: builder.query<ISwarmRatios, void>({
+      query: () => 'tasks',
+      transformResponse: camelCaseKeys<ISwarmRatios>,
+    }),
+    getExceptions: builder.query<ISwarmExceptionsResponse, void>({
+      query: () => 'exceptions',
+      transformResponse: camelCaseKeys<ISwarmExceptionsResponse>,
+    }),
+    getLogs: builder.query<ILogsResponse, void>({
+      query: () => 'logs',
+      transformResponse: camelCaseKeys<ILogsResponse>,
+    }),
+
+    startSwarm: builder.mutation({
+      query: body => ({
+        url: 'swarm',
+        method: 'POST',
+        body: createFormData(snakeCaseKeys(body)),
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+      }),
+    }),
+  }),
+});
+
+export const {
+  useGetStatsQuery,
+  useGetTasksQuery,
+  useGetExceptionsQuery,
+  useGetLogsQuery,
+  useStartSwarmMutation,
+} = api;
---
+++ packages/locust/locust-2.20.2.dev37.tar.gz/locust-2.20.2.dev37/locust/webui/src/test/mocks/swarmState.mock.ts
@@ -0,0 +1,25 @@
+export const percentilesToChart = [0.5, 0.95];
+
+export const swarmStateMock = {
+  availableShapeClasses: ['Default'],
+  availableUserClasses: ['ExampleUser'],
+  extraOptions: {},
+  history: [],
+  host: 'https://localhost',
+  isDistributed: false,
+  isShape: null,
+  locustfile: 'main.py',
+  numUsers: null,
+  overrideHostWarning: false,
+  percentile1: 0.95,
+  percentile2: 0.99,
+  showUserclassPicker: false,
+  spawnRate: null,
+  state: 'ready',
+  percentilesToChart: percentilesToChart,
+  statsHistoryEnabled: false,
+  tasks: '{}',
+  userCount: 0,
+  version: '2.15.0',
+  workerCount: 0,
+};
