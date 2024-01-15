65ac750d256edc0e4f8e62eb06ae7ddc3d3f95b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:04:06 +0000

    Add package nokoskia/2.0.2/nokoskia-2.0.2-py3-none-any.whl

---
+++ packages/nokoskia/nokoskia-2.0.2-py3-none-any.whl/nokoskia/nokoskia.py
@@ -0,0 +1,53 @@
+import curses 
+from curses import wrapper
+import time
+
+def main(stdscr):
+
+    # Check if the terminal supports colors
+    curses.start_color()
+    # Define color pairs (foreground, background)
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_YELLOW)
+
+    curses.curs_set(0)
+        
+    stdscr.clear();
+    stdscr.refresh();
+        
+    # adding the co-ordinates to be the top-left corner as default.
+    x = 0;
+    y = 0;
+        
+    # splitting the dialogue into two variables making it easy to change the color of the sentences in the sameline.
+    part1 = "I am the One Who ";
+    part2 = "KNOCKS."
+
+    try:
+        # making the loop always true to give it a infinite behaviour.
+        while True:
+            # getting the size of the terminal window, to avoid error like "characters out of the visible area error"
+            height, width = stdscr.getmaxyx();
+            width = width - len(part1)
+            # checking whether the line is within the visible area.
+            if x < height and y < width :
+                stdscr.addstr(x, y, part1, curses.color_pair(1))
+                stdscr.addstr(x, y + len(part1), part2, curses.color_pair(2) | curses.A_BOLD)
+                x = x+1;
+                second = len(part1+part2);
+                stdscr.refresh();
+                # if the line is going to outside the visible area, then:
+            elif y < width :
+                # erase the screen and start from the beginning.
+                stdscr.erase();
+                x = 0;
+                y = (y + second); # Loop horizontally=second
+            else:
+                stdscr.erase();
+                x = 0;
+                y = 0;
+            time.sleep(0.06);
+    except KeyboardInterrupt:
+        pass
+
+wrapper(main);
