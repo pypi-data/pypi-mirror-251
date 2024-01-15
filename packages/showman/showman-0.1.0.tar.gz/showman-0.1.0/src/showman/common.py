7d51ca05a5a938479a99ab720f549aeabe9098d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 04:03:15 +0000

    Add package showman/0.1.0/showman-0.1.0.tar.gz

---
+++ packages/showman/showman-0.1.0.tar.gz/showman-0.1.0/src/showman/common.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+import typing as t
+from pathlib import Path
+
+FilePath = str | Path
+OptionalFilePath = FilePath | None
+StrList = list[str] | str
+OptionalStrList = StrList | None
