fcd2955be37c657499955301a426a8952c1211f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 14:03:09 +0000

    Add package grphpkg/0.0.2/grphpkg-0.0.2.tar.gz

---
+++ packages/grphpkg/grphpkg-0.0.2.tar.gz/grphpkg-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Graph package'
+LONG_DESCRIPTION = """
+ grphpkg is a simple graph library that has DFS and BFS
+ implemented that you can create your own operation."""
+
+# Setting up
+setup(
+    name="grphpkg",
+    version=VERSION,
+    author="Vu Diep",
+    author_email="vudiep411@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/vudiep411/grphpkg",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "networkx",
+        "matplotlib"
+    ],
+    keywords=['python', 'data structure', 'graph', 'visualization'],
+)
+
---
+++ packages/grphpkg/grphpkg-0.0.2.tar.gz/grphpkg-0.0.2/test/test.py
@@ -0,0 +1,47 @@
+# main_script.py
+import sys
+import os
+
+# Add the parent directory to the Python path
+current_path = os.path.abspath(os.path.dirname(__file__))
+project_path = os.path.abspath(os.path.join(current_path, '..'))
+sys.path.append(project_path)
+
+from graph.graph import Graph
+from graph.weighted_graph import WeightedGraph
+
+matrix_representation = [
+    [0, 1, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 1, 0, 0],
+    [1, 0, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 0, 1, 0, 1, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 1, 0],
+    [0, 0, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1, 0]
+]
+
+# g = Graph(matrix_representation)
+# g.print_graph()
+# g.dfs(0, lambda x: print(str(x)+" ", end=""))
+# g.draw_graph()
+
+# g2 = Graph()
+# g2.add_edge(0, 1)
+# g2.print_graph()
+
+matrix = [
+    [0, 2, 3, float('inf')],
+    [2, 0, 1, 4],
+    [3, 1, 0, 5],
+    [float('inf'), 4, 5, 0]
+]
+
+weighted_graph = WeightedGraph(matrix)
+weighted_graph.print_graph()
+print(weighted_graph.prim_mst(0))
+weighted_graph.draw_graph()
+
+
+
