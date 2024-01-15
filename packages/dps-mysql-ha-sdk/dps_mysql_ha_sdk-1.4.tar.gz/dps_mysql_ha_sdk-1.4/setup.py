dae97505df3ad9eedc05aad9a53e084cbde80d30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:04:40 +0000

    Add package dps-mysql-ha-sdk/1.4/dps_mysql_ha_sdk-1.4.tar.gz

---
+++ packages/dps-mysql-ha-sdk/dps_mysql_ha_sdk-1.4.tar.gz/dps_mysql_ha_sdk-1.4/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name='dps_mysql_ha_sdk',
+    version='1.4',
+    packages=setuptools.find_packages(),
+    package_data={'': ['dynamic_pooled_db.py']},  # 包含根目录下的 ab.py 文件
+    # package_dir={'': 'src'},
+    install_requires=[
+        # List your dependencies here
+    ],
+    # Other project metadata
+    author='daijiacong',
+    description='高可用数据库连接',
+    # url='https://github.com/daijiacong/your-sdk',
+    license='MIT',
+)
