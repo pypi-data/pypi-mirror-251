ab69a0a9bde7ca081288b6d71cc91b82273462af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:04 +0000

    Add package earring/1.0.2/earring-1.0.2.tar.gz

---
+++ packages/earring/earring-1.0.2.tar.gz/earring-1.0.2/structures/modules/earring/modules/vibes/clique.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+def clique ():
+	import click
+	@click.group ("safe")
+	def group ():
+		pass
+
+	'''
+		./earring gadget start -np 43123
+	'''
+	import click
+	@group.command ("create-proposal-vibes")
+	#@click.option ('--gadget-port', '-np', default = '43123')
+	def search ():		
+
+		return;
+
+	return group
+
+
+
+
+#
+
+
+
