36347bfca2ac40356e4a6527ee902a43af27153e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 04:03:25 +0000

    Add package calfacto/0.0.2/calfacto-0.0.2.tar.gz

---
+++ packages/calfacto/calfacto-0.0.2.tar.gz/calfacto-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+with open("calfacto/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="calfacto",
+    version="0.0.2",
+    description="Calculate factorial of a number",
+    package_dir={"": "calfacto"},
+    packages=find_packages(where="calfacto"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Subhajit Guha Thakurta",
+    author_email="subhajitguha79@gmail.com",
+    license="MIT",
+    install_requires=[],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ]
+)
