1e9b67d9a65a83683d87eada4ad3e3bbe364626e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 04:03:15 +0000

    Add package gwsnr/0.2.0/gwsnr-0.2.0.tar.gz

---
+++ packages/gwsnr/gwsnr-0.2.0.tar.gz/gwsnr-0.2.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+setup(name='gwsnr',
+      version='0.2.0',
+      description='Fast SNR interpolator',
+      author='Hemantakumar Phurailatpam',
+      license="MIT",
+      author_email='hemantaphurailatpam@gmail.com',
+      url='https://github.com/hemantaph/gwsnr',
+      packages=find_packages(),
+      install_requires=[
+        "setuptools>=61.1.0",
+        "numpy>=1.18",
+        "bilby>=1.0.2",
+        "pycbc>=2.0.4",
+        "scipy>=1.9.0",
+        "tqdm>=4.64.0",
+        "gwpy>=2.1.5",
+      ]
+     )
