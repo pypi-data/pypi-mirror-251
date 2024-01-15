3675faaaeea545fa72c62752180b79b91609ded7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 04:03:20 +0000

    Add package decorator-http/0.0.1/decorator-http-0.0.1.tar.gz

---
+++ packages/decorator-http/decorator-http-0.0.1.tar.gz/decorator-http-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='decorator-http',
+    version='0.0.1',
+    author='Paulo Henrique',
+    author_email='contact@paulohenriquesn.com',
+    description='A easy way to use requests but with decorators',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
