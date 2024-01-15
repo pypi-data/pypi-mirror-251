cf9516c763dbfd339f9451d37519bc46bb048678
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:02:45 +0000

    Add package abcai/1.7/abcai-1.7.tar.gz

---
+++ packages/abcai/abcai-1.7.tar.gz/abcai-1.7/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("./README.md", "r") as f:
+    info = f.read()
+
+setup(
+    name="abcai",
+    version="1.7",
+    packages=find_packages(),
+    description="AI lab just import and run ⚡⚡⚡",
+    long_description=info,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "run = abcai:run",
+        ],
+    },
+)
