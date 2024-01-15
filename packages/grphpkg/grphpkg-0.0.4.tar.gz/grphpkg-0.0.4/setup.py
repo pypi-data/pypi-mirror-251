b50fda52521bff3df9a755d50c060a36cdb583f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:14 +0000

    Add package grphpkg/0.0.4/grphpkg-0.0.4.tar.gz

---
+++ packages/grphpkg/grphpkg-0.0.4.tar.gz/grphpkg-0.0.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import sys
+
+VERSION = '0.0.4'
+DESCRIPTION = 'Graph package'
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+# Setting up
+setup(
+    name="grphpkg",
+    version=VERSION,
+    author="Vu Diep",
+    author_email="vudiep411@gmail.com",
+    description=DESCRIPTION,
+    long_description=readme,
+    url="https://github.com/vudiep411/grphpkg",
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        "networkx",
+        "matplotlib"
+    ],
+    keywords=['python', 'data structure', 'graph', 'visualization'],
+)
+
