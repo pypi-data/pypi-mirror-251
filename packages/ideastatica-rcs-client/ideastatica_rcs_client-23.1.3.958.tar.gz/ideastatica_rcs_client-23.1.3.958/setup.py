9247d6a7d01e0dbcf49b10b9dc7b71837249398b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:50 +0000

    Add package ideastatica-rcs-client/23.1.3.958/ideastatica_rcs_client-23.1.3.958.tar.gz

---
+++ packages/ideastatica-rcs-client/ideastatica_rcs_client-23.1.3.958.tar.gz/ideastatica_rcs_client-23.1.3.958/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+     name='ideastatica_rcs_client',
+    version="23.1.3.958",
+    packages=['ideastatica_rcs_client'],
+    package_dir={'ideastatica_rcs_client': 'ideastatica_rcs_client'},   
+)
