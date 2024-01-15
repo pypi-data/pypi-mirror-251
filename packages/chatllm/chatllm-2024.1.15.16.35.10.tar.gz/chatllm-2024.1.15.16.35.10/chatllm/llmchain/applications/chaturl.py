fb82430f73810525c4f9afc9ca218a63c55be63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:05:00 +0000

    Add package chatllm/2024.1.15.16.35.10/chatllm-2024.1.15.16.35.10.tar.gz

---
+++ packages/chatllm/chatllm-2024.1.15.16.35.10.tar.gz/chatllm-2024.1.15.16.35.10/chatllm/llmchain/applications/chaturl.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : chaturl
+# @Time         : 2023/9/5 16:42
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from meutils.pipe import *
