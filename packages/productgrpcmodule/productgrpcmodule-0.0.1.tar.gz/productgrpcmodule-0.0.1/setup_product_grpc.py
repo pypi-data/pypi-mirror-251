8dcd8a0f5831c20917082667b6d30c858cc2583e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:14 +0000

    Add package productgrpcmodule/0.0.1/productgrpcmodule-0.0.1.tar.gz

---
+++ packages/productgrpcmodule/productgrpcmodule-0.0.1.tar.gz/productgrpcmodule-0.0.1/setup_product_grpc.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Instaperq Product grpc package'
+LONG_DESCRIPTION = 'This package contains the grpc proto generated files'
+
+# Setting up
+setup(
+    # the name must match the folder name 'verysimplemodule'
+    name="productgrpcmodule",
+    version=VERSION,
+    author="Wesam Qaqish",
+    author_email="wq@instaperq.ai",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'product grpc package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
