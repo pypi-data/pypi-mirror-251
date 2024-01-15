ab6ad868c748b4af10c0d376aa18c7282b910268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:19 +0000

    Add package example-pkg-dev-charles/0.0.1/example-pkg-dev-charles-0.0.1.tar.gz

---
+++ packages/example-pkg-dev-charles/example-pkg-dev-charles-0.0.1.tar.gz/example-pkg-dev-charles-0.0.1/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="example-pkg-dev-charles",
+    version="0.0.1",
+    author="charles",
+    author_email="poweryong8@naver.com",
+    description="schooldevops sample lib",
+    long_description='test',
+    url="",
+    project_urls={
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.6",
+)
