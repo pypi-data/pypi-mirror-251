482723a55d326ce863726364753e321b364620d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:58 +0000

    Add package HROCH/1.4.1/HROCH-1.4.1-py3-none-any.whl

---
+++ packages/HROCH/HROCH-1.4.1-py3-none-any.whl/HROCH/__init__.py
@@ -0,0 +1,6 @@
+from HROCH.hroch import *
+from HROCH.regressor import SymbolicRegressor
+from HROCH.fuzzy import FuzzyRegressor
+from HROCH.classifier import NonlinearLogisticRegressor, SymbolicClassifier
+
+__all__ = ['SymbolicRegressor','NonlinearLogisticRegressor','SymbolicClassifier','FuzzyRegressor', 'FuzzyClassifier','RegressorMathModel','ClassifierMathModel']
