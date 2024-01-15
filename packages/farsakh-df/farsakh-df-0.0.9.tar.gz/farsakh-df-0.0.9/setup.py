fc56605a9bece933c230eb7d37b2442849826a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:43 +0000

    Add package farsakh-df/0.0.9/farsakh-df-0.0.9.tar.gz

---
+++ packages/farsakh-df/farsakh-df-0.0.9.tar.gz/farsakh-df-0.0.9/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.9'
+DESCRIPTION = 'test file'
+LONG_DESCRIPTION = 'test file 1111'
+
+# Setting up
+setup(
+    name="farsakh-df",
+    version=VERSION,
+    author="Hamza Mohammed Farsakh",
+    author_email="<farsakh.m.hamza@gmail.com>",
+    description=DESCRIPTION,
+    # long_description_content_type="text/markdown",
+    # long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
