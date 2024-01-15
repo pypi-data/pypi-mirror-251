ec3ba1369ea6738c81498a330920b326d087ac18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:09 +0000

    Add package kwakse-distributions/1.0/kwakse_distributions-1.0.tar.gz

---
+++ packages/kwakse-distributions/kwakse_distributions-1.0.tar.gz/kwakse_distributions-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='kwakse_distributions',
+      version='1.0',
+      packages=['distributions'],
+      author='Joshua Boateng',
+      description='Gaussian and Binomial Distributions',
+      zip_safe=False)
