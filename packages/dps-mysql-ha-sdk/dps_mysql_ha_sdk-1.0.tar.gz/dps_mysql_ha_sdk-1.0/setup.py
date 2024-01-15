530ce194859bc67070125cd2e090935fad719bc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:03:59 +0000

    Add package dps-mysql-ha-sdk/1.0/dps_mysql_ha_sdk-1.0.tar.gz

---
+++ packages/dps-mysql-ha-sdk/dps_mysql_ha_sdk-1.0.tar.gz/dps_mysql_ha_sdk-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dps_mysql_ha_sdk',
+    version='1.0',
+    # packages=find_packages(where='src'),
+    # package_dir={'': 'src'},
+    install_requires=[
+        # List your dependencies here
+    ],
+    # Other project metadata
+    author='daijiacong',
+    description='高可用数据库连接',
+    # url='https://github.com/daijiacong/your-sdk',
+    license='MIT',
+)
