f97d11f933ca977d9d6e767cceaeb535d53201de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:21 +0000

    Add package fluentui/0.0.3/fluentui-0.0.3.tar.gz

---
+++ packages/fluentui/fluentui-0.0.3.tar.gz/fluentui-0.0.3/src/fluentui/components/__init__.py
@@ -0,0 +1,6 @@
+from .app import App
+from .button import Button
+from .combobox import Combobox, MenuCombobox
+from .input import LineEdit, InputEdit, InlineEdit
+from .layout import Row
+from .widget import WidgetMix, Widget
