6af7ca0b743b5f40c3b4c98f4e7fb0c2468467c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:28 +0000

    Add package ellzaf-ml/1.3.0/ellzaf_ml-1.3.0.tar.gz

---
+++ packages/ellzaf-ml/ellzaf_ml-1.3.0.tar.gz/ellzaf_ml-1.3.0/ellzaf_ml/models/__init__.py
@@ -0,0 +1,8 @@
+from .ghostfacenetsv1 import GhostFaceNetsV1
+from .ghostfacenetsv2 import GhostFaceNetsV2
+from .lcff import LBPCNNFeatureFusion
+from .ldnet import LDnet
+from .spectformer import SpectFormer
+from .vitspectral import ViTSpectral
+from .simmim import ViTSpectralForSimMIM, SimMIM
+from .projectordistillation import ProjectorDistillationStudent, ProjectorDistillationTeacher
