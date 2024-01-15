43a73a58e0cd7fe1494da891b7b36cb7eff1ff50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:05 +0000

    Add package mego-first-kiss/1.0/mego-first-kiss-1.0.tar.gz

---
+++ packages/mego-first-kiss/mego-first-kiss-1.0.tar.gz/mego-first-kiss-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='mego-first-kiss',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['mego-first-kiss'],
+      author= 'Yousif Abozid',
+      zip_safe=False)
