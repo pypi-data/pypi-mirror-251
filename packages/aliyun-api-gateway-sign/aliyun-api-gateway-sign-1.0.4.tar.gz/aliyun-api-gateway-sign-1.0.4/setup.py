8940e87ee528a44225bb8d809ed59c659dfd7242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:12 +0000

    Add package aliyun-api-gateway-sign/1.0.4/aliyun-api-gateway-sign-1.0.4.tar.gz

---
+++ packages/aliyun-api-gateway-sign/aliyun-api-gateway-sign-1.0.4.tar.gz/aliyun-api-gateway-sign-1.0.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aliyun-api-gateway-sign",
+    version="1.0.4",
+    keywords=("pip", "aliyun", "gateway", "aliyun-gateway"),
+    description="aliyun api网关调用python3",
+    long_description="aliyun api网关调用python3",
+    license="MIT Licence",
+
+    url="",
+    author="wiggin",
+    author_email="zhangweijian@yigongpin.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[]
+)
