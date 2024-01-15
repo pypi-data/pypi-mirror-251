e454c543ff814a35f0fb470be23fda6416777411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 14:03:04 +0000

    Add package palm2rag-alpha-dev/0.0.1/palm2rag-alpha-dev-0.0.1.tar.gz

---
+++ packages/palm2rag-alpha-dev/palm2rag-alpha-dev-0.0.1.tar.gz/palm2rag-alpha-dev-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='palm2rag-alpha-dev',
+    version='0.0.1',
+    description='test',
+    author='charlesjeon',
+    author_email='poweryong8@gmail.com',
+    url='',
+    install_requires=["",'dotenv', 'pandas','tiktoken','scikit-learn','google-cloud-aiplatform',],
+    packages=find_packages(exclude=[]),
+    keywords=[],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
