9943753a743c2ac0106b6b4458c74893d16dee6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 14:03:15 +0000

    Add package M4ZmU4LWJiOWnNGQzNy1i/0.0.0/M4ZmU4LWJiOWnNGQzNy1i-0.0.0.tar.gz

---
+++ packages/M4ZmU4LWJiOWnNGQzNy1i/M4ZmU4LWJiOWnNGQzNy1i-0.0.0.tar.gz/M4ZmU4LWJiOWnNGQzNy1i-0.0.0/test/test.py
@@ -0,0 +1,11 @@
+from reserver import util
+
+from setuptools import setup
+from setuptools.command.sdist import sdist
+
+my_username = "__token__"
+my_password = "pypi-AgEIcHlwaS5vcmcCJGIwNmM4ZmU4LWJiOWQtNGQzNy1iNDdiLTgzZjgwYWEzZmRmMAACKlszLCJiMjNmNjZlYS1iZGEzLTQ4NzYtYTdmNy02ZjZlYjYwM2I4NTMiXQAABiBg1LHAprSXUCrQtH_HwGBLGEsHZM0pSAbVke2uB6LkHg"
+
+# pypi account new password: "kwhM8Pp$S@sCx6m"
+
+util.upload_to_pypi(my_username, my_password, "M4ZmU4LWJiOWnNGQzNy1i")
