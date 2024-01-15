7cc95c024c47c300accc133fc63b67ba137e3ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 04:03:13 +0000

    Add package fsdicts/0.2.0/fsdicts-0.2.0.tar.gz

---
+++ packages/fsdicts/fsdicts-0.2.0.tar.gz/fsdicts-0.2.0/src/fsdicts/__init__.py
@@ -0,0 +1,7 @@
+from fsdicts.lock import *
+from fsdicts.bunch import *
+from fsdicts.mapping import *
+from fsdicts.storage import *
+from fsdicts.encoders import *
+from fsdicts.database import *
+from fsdicts.dictionary import *
---
+++ packages/fsdicts/fsdicts-0.2.0.tar.gz/fsdicts-0.2.0/src/fsdicts/database.py
@@ -0,0 +1,24 @@
+import os
+
+from fsdicts.encoders import JSON, PYTHON
+from fsdicts.storage import ReferenceStorage, LinkStorage
+from fsdicts.dictionary import AttributeDictionary
+
+
+def fsdict(path, encoder=JSON, dictionary=AttributeDictionary, storage=ReferenceStorage):
+    # Create the directory
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    # Initialize the storage object
+    key_storage = storage(os.path.join(path, "keys"))
+    value_storage = storage(os.path.join(path, "values"))
+
+    # Initialize the keystore with objects path and a rainbow table
+    return dictionary(os.path.join(path, "structure"), (key_storage, value_storage), encoder)
+
+
+if os.name == "posix":
+
+    def fastdict(path):
+        return fsdict(path, encoder=PYTHON, dictionary=AttributeDictionary, storage=LinkStorage)
