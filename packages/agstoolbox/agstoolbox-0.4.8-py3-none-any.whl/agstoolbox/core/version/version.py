fe8f2895524c679f9748337f1b6f315a9db92415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 04:03:25 +0000

    Add package agstoolbox/0.4.8/agstoolbox-0.4.8-py3-none-any.whl

---
+++ packages/agstoolbox/agstoolbox-0.4.8-py3-none-any.whl/agstoolbox/core/version/version.py
@@ -0,0 +1,12 @@
+from __future__ import annotations  # for python 3.8
+
+
+class Version:
+    as_str: str = None
+    as_int: int = None
+    family: str = None
+    family_as_int: int = None
+    major: str = None
+    minor: str = None
+    improv: str = None
+    patch: str = None
