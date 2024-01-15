89b207761059eb13dd852527b92280696a2f39b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:03:38 +0000

    Add package hpglobal/1.20181217/hpglobal-1.20181217.tar.gz

---
+++ packages/hpglobal/hpglobal-1.20181217.tar.gz/hpglobal-1.20181217/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hpglobal',
+    version='1.20181217',
+    author='lyy',
+    author_email='',
+    description='hpformula for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+)
