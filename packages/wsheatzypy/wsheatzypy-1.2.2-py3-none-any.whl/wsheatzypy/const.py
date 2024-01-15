5d82d69743fa4551491e635f1bff7610ad56b1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:16 +0000

    Add package wsheatzypy/1.2.2/wsheatzypy-1.2.2-py3-none-any.whl

---
+++ packages/wsheatzypy/wsheatzypy-1.2.2-py3-none-any.whl/wsheatzypy/const.py
@@ -0,0 +1,12 @@
+"""Constants for the Heatzy component."""
+
+APPLICATION_ID = "c70a66ff039d41b4a220e198b0fcc8b3"
+EU_API_URL = "euapi.gizwits.com"
+US_API_URL = "usapi.gizwits.com"
+DFLT_API_URL = "api.gizwits.com"
+WS_PING_INTERVAL = 30
+WS_HOST = "eusandbox.gizwits.com"
+WS_PORT = 8080
+TIMEOUT = 120
+WSS_PORT = 8880
+RETRY = 3
