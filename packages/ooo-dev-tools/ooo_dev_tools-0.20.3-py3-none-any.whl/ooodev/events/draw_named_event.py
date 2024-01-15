c17d2f4d876919eb70f319f2ce99b4119502b0c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:20 +0000

    Add package ooo-dev-tools/0.20.3/ooo_dev_tools-0.20.3-py3-none-any.whl

---
+++ packages/ooo-dev-tools/ooo_dev_tools-0.20.3-py3-none-any.whl/ooodev/events/draw_named_event.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+"""
+Draw Named Events.
+"""
+from __future__ import annotations
+
+
+class DrawNamedEvent:
+    """
+    Named events for utils.draw.Draw class
+    """
+
+    GET_SHAPES_ERROR = "draw_get_shapes_error"
+    """Draw get_shapes error command see :py:meth:`Draw.get_shapes() <.utils.draw.Draw.get_shapes>`"""
