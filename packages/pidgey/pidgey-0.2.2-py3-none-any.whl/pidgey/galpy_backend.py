9e5b583f1ce3d9238e396ae186732e4d3cd546dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:02 +0000

    Add package pidgey/0.2.2/pidgey-0.2.2-py3-none-any.whl

---
+++ packages/pidgey/pidgey-0.2.2-py3-none-any.whl/pidgey/galpy_backend.py
@@ -0,0 +1,25 @@
+import astropy.coordinates as coord
+import numpy as np
+
+from .base import Backend
+
+
+class GalpyBackend(Backend):
+    def __imports__():
+        from galpy import orbit
+
+    def ORBIT_TYPE(self):
+        return self.orbit.Orbit
+
+    def _compute_orbit(self, skycoord, pot, dt, steps):
+        orbit = self.orbit.Orbit(skycoord)
+        t = np.arange(steps) * dt
+        orbit.integrate(t, pot)
+        return orbit
+
+    def _extract_points(self, orbit):
+        skycoord, pot, dt, steps = self._args
+        t = np.arange(steps) * dt
+        return coord.representation.CartesianRepresentation(
+            orbit.x(t), orbit.y(t), orbit.z(t)
+        )
