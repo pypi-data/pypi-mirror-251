3762f01a387c5b8c1a41706f269c79de63aaf41a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:10 +0000

    Add package movoid-timer/1.0.1/movoid_timer-1.0.1.tar.gz

---
+++ packages/movoid-timer/movoid_timer-1.0.1.tar.gz/movoid_timer-1.0.1/movoid_timer/__init__.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# File          : __init__.py
+# Author        : Sun YiFan-Movoid
+# Time          : 2024/1/11 21:50
+# Description   : 
+"""
+from .timer import Timer
+
+if __name__ == '__main__':
+    pass
