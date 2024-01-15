17f736d9af43f7b6c7faff93d347a8b8b6b9e38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:12 +0000

    Add package varman/0.1.4/varman-0.1.4.tar.gz

---
+++ packages/varman/varman-0.1.4.tar.gz/varman-0.1.4/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding='utf-8') as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="varman",
+  version="0.1.4",
+  python_requires=">=3.6",
+  author="zloss",
+  author_email="zlols@foxmail.com",
+  description="A dict like variable manager, listen for variable changes.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/zlolss/VarMan.py.git",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3 :: Only",
+  "License :: OSI Approved :: MIT License",
+  #"Operating System :: OS Independent",
+  ],
+)
