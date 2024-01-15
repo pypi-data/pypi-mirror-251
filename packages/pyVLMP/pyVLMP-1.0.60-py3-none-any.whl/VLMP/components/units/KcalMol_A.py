28f6b52486f9644c1cc4bbd95ba6c214597fa5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:10 +0000

    Add package pyVLMP/1.0.60/pyVLMP-1.0.60-py3-none-any.whl

---
+++ packages/pyVLMP/pyVLMP-1.0.60-py3-none-any.whl/VLMP/components/units/KcalMol_A.py
@@ -0,0 +1,34 @@
+import sys, os
+
+import logging
+
+from . import unitsBase
+
+class KcalMol_A(unitsBase):
+
+    """
+    Component name: KcalMol_A
+    Component type: units
+
+    Author: Pablo Ibáñez-Freire
+    Date: 13/03/2023
+
+    (Kcal/mol)/A
+
+    """
+
+    def __init__(self,name,**params):
+        super().__init__(_type = self.__class__.__name__,
+                         _name = name,
+                         availableParameters = set(),
+                         requiredParameters  = set(),
+                         **params)
+
+        ############################################################
+        ############################################################
+        ############################################################
+
+        self.setUnitsName("KcalMol_A")
+
+        self.addConstant("KBOLTZ",1.987191E-03)
+        self.addConstant("ELECOEF",332.0716)
