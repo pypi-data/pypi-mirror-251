162022f988335cf604aa11c2bb58d4fbb0f67fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:04:55 +0000

    Add package webshell-generator/0.1.2/webshell_generator-0.1.2.tar.gz

---
+++ packages/webshell-generator/webshell_generator-0.1.2.tar.gz/webshell_generator-0.1.2/tests/test_webshell_generator.py
@@ -0,0 +1,103 @@
+import unittest
+from webshell_generator import get_behinder4_jsp_shell
+from webshell_generator import get_behinder4_php_shell
+from webshell_generator import get_behinder4_aspx_shell
+from webshell_generator import get_godzilla_jsp_shell
+from webshell_generator import get_godzilla_jspx_shell
+from webshell_generator import get_godzilla_php_shell
+from webshell_generator import get_godzilla_aspx_shell
+from webshell_generator import get_godzilla_ashx_shell
+from webshell_generator import get_define_class_shell
+
+
+class TestBehinderWebshell(unittest.TestCase):
+
+    def test_get_jsp_shell(self):
+        webshell = get_behinder4_jsp_shell()
+        self.assertEqual(webshell.tool, "behinder4")
+        self.assertEqual(webshell.type, "jsp")
+        self.assertEqual(webshell.pas, "rebeyond")
+        self.assertEqual(webshell.key, None)
+        self.assertTrue(len(webshell.content) > 0)
+        self.assertTrue(len(webshell.raw_content) > 0)
+
+    def test_get_php_shell(self):
+        webshell = get_behinder4_php_shell()
+        self.assertEqual(webshell.tool, "behinder4")
+        self.assertEqual(webshell.type, "php")
+        self.assertEqual(webshell.pas, "rebeyond")
+        self.assertEqual(webshell.key, None)
+        self.assertTrue(len(webshell.content) > 0)
+        self.assertTrue(len(webshell.raw_content) > 0)
+
+    def test_get_aspx_shell(self):
+        webshell = get_behinder4_aspx_shell()
+        self.assertEqual(webshell.tool, "behinder4")
+        self.assertEqual(webshell.type, "aspx")
+        self.assertEqual(webshell.pas, "rebeyond")
+        self.assertEqual(webshell.key, None)
+        self.assertTrue(len(webshell.content) > 0)
+        self.assertTrue(len(webshell.raw_content) > 0)
+
+
+class TestGodzillaWebshell(unittest.TestCase):
+
+    def test_get_jsp_shell(self):
+        webshell = get_godzilla_jsp_shell()
+        self.assertEqual(webshell.tool, "godzilla")
+        self.assertEqual(webshell.type, "jsp")
+        self.assertEqual(webshell.pas, "pass")
+        self.assertEqual(webshell.key, "key")
+        self.assertTrue(len(webshell.content) > 0)
+        self.assertTrue(len(webshell.raw_content) > 0)
+
+    def test_get_jspx_shell(self):
+        webshell = get_godzilla_jspx_shell()
+        self.assertEqual(webshell.tool, "godzilla")
+        self.assertEqual(webshell.type, "jspx")
+        self.assertEqual(webshell.pas, "pass")
+        self.assertEqual(webshell.key, "key")
+        self.assertTrue(len(webshell.content) > 0)
+        self.assertTrue(len(webshell.raw_content) > 0)
+
+    def test_get_php_shell(self):
+        webshell = get_godzilla_php_shell()
+        self.assertEqual(webshell.tool, "godzilla")
+        self.assertEqual(webshell.type, "php")
+        self.assertEqual(webshell.pas, "pass")
+        self.assertEqual(webshell.key, "key")
+        self.assertTrue(len(webshell.content) > 0)
+        self.assertTrue(len(webshell.raw_content) > 0)
+
+    def test_get_aspx_shell(self):
+        webshell = get_godzilla_aspx_shell()
+        self.assertEqual(webshell.tool, "godzilla")
+        self.assertEqual(webshell.type, "aspx")
+        self.assertEqual(webshell.pas, "pass")
+        self.assertEqual(webshell.key, "key")
+        self.assertTrue(len(webshell.content) > 0)
+        self.assertTrue(len(webshell.raw_content) > 0)
+
+    def test_get_ashx_shell(self):
+        webshell = get_godzilla_ashx_shell()
+        self.assertEqual(webshell.tool, "godzilla")
+        self.assertEqual(webshell.type, "ashx")
+        self.assertEqual(webshell.pas, "pass")
+        self.assertEqual(webshell.key, "key")
+        self.assertTrue(len(webshell.content) > 0)
+        self.assertTrue(len(webshell.raw_content) > 0)
+
+    def test_define_class_shell(self):
+        webshell = get_define_class_shell()
+        self.assertEqual(webshell.tool, "godzilla")
+        self.assertEqual(webshell.type, "jsp")
+        self.assertEqual(webshell.pas, "PaSs")
+        self.assertEqual(webshell.key, "key")
+        self.assertTrue(len(webshell.content) > 0)
+        self.assertTrue(len(webshell.raw_content) > 0)
+        self.assertTrue(len(webshell.shell_class) > 0)
+        self.assertTrue(len(webshell.headers) > 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
