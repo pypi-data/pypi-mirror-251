039ad377ab0e8496fad8db611d33203b1baa70c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:06 +0000

    Add package winter-cloud/0.1.48/winter_cloud-0.1.48.tar.gz

---
+++ packages/winter-cloud/winter_cloud-0.1.48.tar.gz/winter_cloud-0.1.48/src/winter/core/register/logger.py
@@ -0,0 +1,15 @@
+from use_logger import useLogger, useLoggerInterceptUvicorn
+from use_logger.handlers import logstash_handler
+
+
+def register_logger(packages=None, extra=None, handler=None, uvicorn=True):
+    handler = handler or {}
+    useLogger(
+        handlers=[
+            logstash_handler(**handler),
+        ],
+        packages=packages,
+        extra=extra
+    )
+    if uvicorn:
+        useLoggerInterceptUvicorn()
