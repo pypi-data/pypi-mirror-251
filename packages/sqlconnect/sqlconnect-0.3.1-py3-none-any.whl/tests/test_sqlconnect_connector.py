f448e3dfb3c64406b1dc292f23bd0c609ab95456
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:02:46 +0000

    Add package sqlconnect/0.3.1/sqlconnect-0.3.1-py3-none-any.whl

---
+++ packages/sqlconnect/sqlconnect-0.3.1-py3-none-any.whl/tests/test_sqlconnect_connector.py
@@ -0,0 +1,19 @@
+from sqlconnect import Sqlconnector
+
+CONFIG_DICT = {
+    "sqlalchemy_driver": "mssql+pyodbc",
+    "odbc_driver": "SQL+Server",
+    "server": "dev-server.database.com",
+    "database": "DevDB",
+    "options": ["Trusted_Connection=Yes"],
+}
+
+
+def test_Sqlconnector_connection_name_with_config_dict():
+    # Test that Sqlconnector object connection name is correctly initialised
+
+    # Creating an instance of Sqlconnector
+    connector = Sqlconnector("test_connection", config_dict=CONFIG_DICT)
+
+    # Assertions to check if the instance is initialised as expected
+    assert connector.connection_name == "test_connection"
