27d5e42a415d5ce5ab2597be8252d948d4e12831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:02:46 +0000

    Add package haltia/0.0.1/haltia-0.0.1.tar.gz

---
+++ packages/haltia/haltia-0.0.1.tar.gz/haltia-0.0.1/src/haltia/__init__.py
@@ -0,0 +1,9 @@
+# This is free and unencumbered software released into the public domain.
+
+"""Haltia.AI Software Development Kit (SDK) for Python"""
+
+import sys
+
+assert sys.version_info >= (3, 9), "Haltia.AI Software Development Kit (SDK) for Python requires Python 3.9+"
+
+__all__ = []
