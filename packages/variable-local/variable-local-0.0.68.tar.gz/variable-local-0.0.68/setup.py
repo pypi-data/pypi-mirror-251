a90c20dc020850ffc487a3d6810b485f110fdde6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:43 +0000

    Add package variable-local/0.0.68/variable-local-0.0.68.tar.gz

---
+++ packages/variable-local/variable-local-0.0.68.tar.gz/variable-local-0.0.68/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+PACKAGE_NAME = 'variable-local'
+package_dir = PACKAGE_NAME.replace('-', '_')
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version='0.0.68',  # https://pypi.org/project/variable-local
+    author="Circles",
+    author_email="info@circles.life",
+    description="PyPI Package for Circles <project-name> Local/Remote Python",
+    long_description="This is a package for sharing common XXX function used in different repositories",
+    long_description_content_type="text/markdown",
+    url="https://github.com/circles",
+    packages=[package_dir],
+    package_dir={package_dir: f'{package_dir}/src'},
+    package_data={package_dir: ['*.py']},
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'logger-local',
+        'language_local',
+        'database-mysql-local',
+        'smartlink-local'
+    ],
+)
