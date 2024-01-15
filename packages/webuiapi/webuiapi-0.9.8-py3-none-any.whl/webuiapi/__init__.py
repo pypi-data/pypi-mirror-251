8021dfdb1b7abb0785b628357f3e89ad0a968263
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:04 +0000

    Add package webuiapi/0.9.8/webuiapi-0.9.8-py3-none-any.whl

---
+++ packages/webuiapi/webuiapi-0.9.8-py3-none-any.whl/webuiapi/__init__.py
@@ -0,0 +1,53 @@
+from .webuiapi import (
+    WebUIApi,
+    WebUIApiResult,
+    Upscaler,
+    HiResUpscaler,
+    b64_img,
+    raw_b64_img,
+    ModelKeywordResult,
+    ModelKeywordInterface,
+    InstructPix2PixInterface,
+    ControlNetInterface,
+    ControlNetUnit,
+    RemBGInterface,
+    ADetailer,
+    Roop,
+    ReActor,
+    ADetailer,    
+    Roop,
+    SegmentAnythingInterface,
+    SegmentAnythingSamResult,
+    SegmentAnythingDilationResult,
+    SegmentAnythingGinoResult,
+    SegmentAnythingControlNetSegRandomResult,
+    SegmentAnythingControlNetSegNotRandomResult,
+    SegmentAnythingSemanticSegWithCatIdResult
+)
+
+__version__ = "0.9.8"
+
+__all__ = [
+    "__version__",
+    "WebUIApi",
+    "WebUIApiResult",
+    "Upscaler",
+    "HiResUpscaler",
+    "b64_img",
+    "ModelKeywordResult",
+    "ModelKeywordInterface",
+    "InstructPix2PixInterface",
+    "ControlNetInterface",
+    "ControlNetUnit",
+    "RemBGInterface",
+    "ADetailer",
+    "Roop",
+    "ReActor",
+    "SegmentAnythingInterface",
+    "SegmentAnythingSamResult",
+    "SegmentAnythingDilationResult",
+    "SegmentAnythingGinoResult",
+    "SegmentAnythingControlNetSegRandomResult",
+    "SegmentAnythingControlNetSegNotRandomResult",
+    "SegmentAnythingSemanticSegWithCatIdResult"
+]
