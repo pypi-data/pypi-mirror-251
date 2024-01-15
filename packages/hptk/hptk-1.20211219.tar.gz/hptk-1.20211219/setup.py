54f4e1f26710ef3986d79e490f74de34f16e9a7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:03:45 +0000

    Add package hptk/1.20211219/hptk-1.20211219.tar.gz

---
+++ packages/hptk/hptk-1.20211219.tar.gz/hptk-1.20211219/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='hptk',
+    version='1.20211219',
+    author='lyy',
+    author_email='',
+    description='hpformula for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+)
