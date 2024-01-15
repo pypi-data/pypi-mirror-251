c0fd6d3c2db8e30a132929ac7dd6eaa19f935aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:02:56 +0000

    Add package mykit/0.20240114b131235/mykit-0.20240114b131235.tar.gz

---
+++ packages/mykit/mykit-0.20240114b131235.tar.gz/mykit-0.20240114b131235/mykit/__main__.py
@@ -0,0 +1,8 @@
+
+
+def main():
+    print('hello 123 abc')
+
+
+# if __name__ == '__main__':  # not needed, since the binary `mykit` will execute `main` regardless this line exist or not
+#     main()  # not needed, since the binary `mykit` will execute `main` regardless this line exist or not
