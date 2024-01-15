9c3cf32acc292118f7f8a8b3c44a9f8cefbbbe68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:22 +0000

    Add package distributionshajimete/1.0/distributionshajimete-1.0.tar.gz

---
+++ packages/distributionshajimete/distributionshajimete-1.0.tar.gz/distributionshajimete-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributionshajimete',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['distributionshajimete'],
+      author = 'Miki Kaneko',
+      author_email = 'novmikicity@gmail.com',
+      zip_safe=False)
