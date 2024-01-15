6032ad057119bb85adbd7369f613bcedce14bb67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 15:03:45 +0000

    Add package webshell-generator/0.1.1/webshell_generator-0.1.1.tar.gz

---
+++ packages/webshell-generator/webshell_generator-0.1.1.tar.gz/webshell_generator-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="webshell_generator",
+    version="0.1.1",
+    packages=find_packages(include=['webshell_generator', 'webshell_generator.*']),
+    include_package_data=True,
+    install_requires=[
+        "requests"
+    ],
+    author="haruka",
+    author_email="admin@haruka.com",
+    description="A library to generate processed webshell instances",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+)
