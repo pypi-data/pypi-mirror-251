64186e22f71bf5ec8c41c9817275696b6a096224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 03:03:27 +0000

    Add package nokoskia/2.0/nokoskia-2.0.tar.gz

---
+++ packages/nokoskia/nokoskia-2.0.tar.gz/nokoskia-2.0/README.md
@@ -0,0 +1,9 @@
+Installation 
+
+nokoskia can be installed from PyPi.
+
+To install with pip:
+ pip install nokoskia
+
+
+The command to run the program is ' knock '.
---
+++ packages/nokoskia/nokoskia-2.0.tar.gz/nokoskia-2.0/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '2.0'
+DESCRIPTION = 'simply fill the terminal with famous breaking bad dialogue'
+LONG_DESCRIPTION = """funny command line tool.
+Installation 
+nokoskia can be installed from PyPi.
+To install with pip: pip install nokoskia
+
+The command to run the program is ' knock '.
+
+"""
+
+# Setting up
+setup(
+    name="nokoskia",
+    version=VERSION,
+    author="PranavSV",
+    author_email="pranavsv2004@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'breakingbad', 'movie', 'fun', 'funny tools', 'command line tool'],
+    classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: End Users/Desktop',
+    'Programming Language :: Python :: 3',
+    'Operating System :: Unix',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+],
+    entry_points={
+    "console_scripts":[
+        "knock = nokoskia:knock"
+    ],
+},
+
+)
