1ee84e6f8beede7da9a38a40235aed7f65e5567c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-15 16:04:24 +0000

    Add package matfree/0.0.14/matfree-0.0.14.tar.gz

---
+++ packages/matfree/matfree-0.0.14.tar.gz/matfree-0.0.14/matfree/backend/typing.py
@@ -0,0 +1,8 @@
+"""Types."""
+# fmt: off
+from collections.abc import Callable  # noqa: F401
+from typing import Any, Generic, Iterable, Sequence, Tuple, TypeVar  # noqa: F401, UP035
+
+from jax import Array  # noqa: F401
+
+# fmt: on
