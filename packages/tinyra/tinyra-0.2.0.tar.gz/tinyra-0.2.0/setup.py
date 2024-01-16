66131d2cbc11596849cfd661bb4e1279ec97ac5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:04:24 +0000

    Add package tinyra/0.2.0/tinyra-0.2.0.tar.gz

---
+++ packages/tinyra/tinyra-0.2.0.tar.gz/tinyra-0.2.0/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+from setuptools import setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="tinyra",
+    version="0.2.0",
+    description="A minimalistic research assistant built with AutoGen.",
+    py_modules=["tui", "run_tab"],
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "tinyra = tui:run_tinyra",
+        ],
+    },
+)
