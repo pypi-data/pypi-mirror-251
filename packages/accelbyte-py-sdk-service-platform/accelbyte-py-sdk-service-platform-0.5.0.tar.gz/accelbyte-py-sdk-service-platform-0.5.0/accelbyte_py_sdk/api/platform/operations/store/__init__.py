2310295fe4462745df7dcbb9a384582345041d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:03:45 +0000

    Add package accelbyte-py-sdk-service-platform/0.5.0/accelbyte-py-sdk-service-platform-0.5.0.tar.gz

---
+++ packages/accelbyte-py-sdk-service-platform/accelbyte-py-sdk-service-platform-0.5.0.tar.gz/accelbyte-py-sdk-service-platform-0.5.0/accelbyte_py_sdk/api/platform/operations/store/__init__.py
@@ -0,0 +1,31 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: operation-init.j2
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Platform Service."""
+
+__version__ = "4.44.0"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .clone_store import CloneStore
+from .create_store import CreateStore
+from .delete_published_store import DeletePublishedStore
+from .delete_store import DeleteStore
+from .export_store import ExportStore
+from .export_store_1 import ExportStore1
+from .get_published_store import GetPublishedStore
+from .get_published_store_backup import GetPublishedStoreBackup
+from .get_store import GetStore
+from .import_store import ImportStore
+from .import_store_1 import ImportStore1
+from .list_stores import ListStores
+from .public_list_stores import PublicListStores
+from .rollback_published_store import RollbackPublishedStore
+from .update_store import UpdateStore
