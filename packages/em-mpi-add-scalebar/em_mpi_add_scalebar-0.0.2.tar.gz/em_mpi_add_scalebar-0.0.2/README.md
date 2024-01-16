e85ea5770c54abed65549e8b7fdd57213eca3260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:58 +0000

    Add package em-mpi-add-scalebar/0.0.2/em_mpi_add_scalebar-0.0.2.tar.gz

---
+++ packages/em-mpi-add-scalebar/em_mpi_add_scalebar-0.0.2.tar.gz/em_mpi_add_scalebar-0.0.2/README.md
@@ -0,0 +1,4 @@
+# AddScaleBar Package
+
+Here is a walkthrough on how to use the addScaleBar Package 
+
---
+++ packages/em-mpi-add-scalebar/em_mpi_add_scalebar-0.0.2.tar.gz/em_mpi_add_scalebar-0.0.2/add_scalebar/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 15 15:06:38 2024
+
+@author: vega
+"""
+if __name__ == "__main__":
+	from add_scalebar import addScaleBarGUI
+else:
+    from .add_scalebar import addScaleBarGUI
+
+addScaleBarGUI.main()
+
