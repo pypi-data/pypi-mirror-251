91f6c7f08e6b54fa12d91c151a5be83d2e59ce58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:04:03 +0000

    Add package bolton-logrus/0.1.1/bolton_logrus-0.1.1-py3-none-any.whl

---
+++ packages/bolton-logrus/bolton_logrus-0.1.1-py3-none-any.whl/logrus/__init__.py
@@ -0,0 +1,34 @@
+"""logrus package
+
+Better logging made easy with support for structlog and the standard logging
+module.
+"""
+
+import logging as _logging
+
+from ._core import (
+    BetterBoundLogger,
+    Log,
+    LogFormat,
+    Logger,
+    LogLevel,
+    get_default_logfile,
+    init_logging,
+)
+
+
+__all__ = [
+    "BetterBoundLogger",
+    "Log",
+    "LogFormat",
+    "Logger",
+    "LogLevel",
+    "get_default_logfile",
+    "init_logging",
+]
+
+__author__ = "Bryan M Bugyi"
+__email__ = "bryanbugyi34@gmail.com"
+__version__ = "0.1.1"
+
+_logging.getLogger(__name__).addHandler(_logging.NullHandler())
