13180f7a14d186f484bc8cd759ad6dbc83d17e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 15:04:22 +0000

    Add package ebtorch/0.20.0/ebtorch-0.20.0-py3-none-any.whl

---
+++ packages/ebtorch/ebtorch-0.20.0-py3-none-any.whl/ebtorch/nn/functional/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#  Copyright (c) 2020-2023 Emanuele Ballarin <emanuele@ballarin.cc>
+#  Released under the terms of the MIT License
+#  (see: https://url.ballarin.cc/mitlicense)
+#
+# ------------------------------------------------------------------------------
+#
+# SPDX-License-Identifier: MIT
+#
+# ------------------------------------------------------------------------------
+# Imports (specific)
+from .inner_functional import cummatmul
+from .inner_functional import field_transform
+from .inner_functional import mish
+from .inner_functional import oldtranspose
+from .inner_functional import serf
+from .inner_functional import serlu
+from .inner_functional import silhouette_score
+from .inner_functional import smelu
+
+# Deletions (from .)
+del inner_functional
