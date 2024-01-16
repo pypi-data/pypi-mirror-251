0b6742b5cda99a64175cdcf5013f3ecaf7da571f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:30 +0000

    Add package pure-cv/0.1.4/pure_cv-0.1.4-py3-none-any.whl

---
+++ packages/pure-cv/pure_cv-0.1.4-py3-none-any.whl/pure_cv/__init__.py
@@ -0,0 +1,7 @@
+from .opencv import imread, grayscale, threshold, dilate, erode, cvtColor, imdecode
+from .plot import show
+from .encoding import b64encode, b64decode, encode, decode
+from .colors import mod_color
+from .misc import black
+from .rescaling import descale_h, rescale_h
+from . import draw
