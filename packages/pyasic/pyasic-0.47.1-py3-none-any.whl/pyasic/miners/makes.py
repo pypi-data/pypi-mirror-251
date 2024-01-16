4aed080e5332629d158ffc65517cef3ecb282e41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 06:03:52 +0000

    Add package pyasic/0.47.1/pyasic-0.47.1-py3-none-any.whl

---
+++ packages/pyasic/pyasic-0.47.1-py3-none-any.whl/pyasic/miners/makes.py
@@ -0,0 +1,47 @@
+# ------------------------------------------------------------------------------
+#  Copyright 2022 Upstream Data Inc                                            -
+#                                                                              -
+#  Licensed under the Apache License, Version 2.0 (the "License");             -
+#  you may not use this file except in compliance with the License.            -
+#  You may obtain a copy of the License at                                     -
+#                                                                              -
+#      http://www.apache.org/licenses/LICENSE-2.0                              -
+#                                                                              -
+#  Unless required by applicable law or agreed to in writing, software         -
+#  distributed under the License is distributed on an "AS IS" BASIS,           -
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.    -
+#  See the License for the specific language governing permissions and         -
+#  limitations under the License.                                              -
+# ------------------------------------------------------------------------------
+
+from pyasic.miners.base import BaseMiner
+
+
+class WhatsMiner(BaseMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.make = "WhatsMiner"
+
+
+class AntMiner(BaseMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.make = "AntMiner"
+
+
+class AvalonMiner(BaseMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.make = "AvalonMiner"
+
+
+class InnosiliconMiner(BaseMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.make = "Innosilicon"
+
+
+class GoldshellMiner(BaseMiner):  # noqa - ignore ABC method implementation
+    def __init__(self, ip: str, api_ver: str = "0.0.0"):
+        super().__init__(ip, api_ver)
+        self.make = "Goldshell"
