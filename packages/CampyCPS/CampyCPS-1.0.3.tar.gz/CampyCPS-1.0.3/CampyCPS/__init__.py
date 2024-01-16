60e463cf637cc5bb29658c8a8b6f030037ba5013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:04:45 +0000

    Add package CampyCPS/1.0.3/CampyCPS-1.0.3.tar.gz

---
+++ packages/CampyCPS/CampyCPS-1.0.3.tar.gz/CampyCPS-1.0.3/CampyCPS/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+__title__ = 'CampyCPS'
+__description__ = 'Campylobacter CPS relevant gene analysis'
+__url__ = 'https://github.com/hbucqp/CampyCPS'
+__version__ = "1.0.3"
+__author__ = 'Qingpo Cui'
+__author_email__ = 'cqp@cau.edu.cn'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2024 Qingpo Cui'
