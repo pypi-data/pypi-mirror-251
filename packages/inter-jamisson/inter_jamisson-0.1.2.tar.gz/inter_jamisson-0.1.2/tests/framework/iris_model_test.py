d216c3471cb0d369516b7463192fc50868f8dcfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:39 +0000

    Add package inter-jamisson/0.1.2/inter_jamisson-0.1.2.tar.gz

---
+++ packages/inter-jamisson/inter_jamisson-0.1.2.tar.gz/inter_jamisson-0.1.2/tests/framework/iris_model_test.py
@@ -0,0 +1,67 @@
+import pytest
+from unittest.mock import Mock
+from inter_jamisson.use_cases.base import UseCase
+from inter_jamisson.framework.iris_model import IrisModel
+
+@pytest.fixture
+def mock_train_model_usecase():
+    return Mock(spec=UseCase)
+
+@pytest.fixture
+def mock_evaluate_model_usecase():
+    return Mock(spec=UseCase)
+
+@pytest.fixture
+def mock_save_model_usecase():
+    return Mock(spec=UseCase)
+
+def test_train_model_calls_execute_on_train_model_usecase(
+        mock_train_model_usecase,
+        mock_evaluate_model_usecase,
+        mock_save_model_usecase
+    ):
+    
+    iris_model = IrisModel(
+        mock_train_model_usecase,
+        mock_evaluate_model_usecase,
+        mock_save_model_usecase
+    )
+
+    iris_model.train_model()
+
+    mock_train_model_usecase.execute.assert_called_once()
+
+def test_evaluate_model_calls_execute_on_evaluate_model_usecase(
+        mock_train_model_usecase,
+        mock_evaluate_model_usecase,
+        mock_save_model_usecase
+    ):
+    
+    iris_model = IrisModel(
+        mock_train_model_usecase,
+        mock_evaluate_model_usecase,
+        mock_save_model_usecase
+    )
+
+    iris_model.evaluate_model()
+
+    mock_evaluate_model_usecase.execute.assert_called_once()
+
+def test_save_model_calls_execute_on_save_model_usecase(
+        mock_train_model_usecase,
+        mock_evaluate_model_usecase,
+        mock_save_model_usecase
+    ):
+    
+    iris_model = IrisModel(
+        mock_train_model_usecase,
+        mock_evaluate_model_usecase,
+        mock_save_model_usecase
+    )
+
+    iris_model.save_model()
+
+    mock_save_model_usecase.execute.assert_called_once()
+
+if __name__ == '__main__':
+    pytest.main()
---
+++ packages/inter-jamisson/inter_jamisson-0.1.2.tar.gz/inter_jamisson-0.1.2/tests/use_cases/save_model_test.py
@@ -0,0 +1,21 @@
+import pytest
+from unittest.mock import Mock
+from inter_jamisson.use_cases.port.model import Model
+
+from inter_jamisson.use_cases.save_model import SaveModel  
+
+@pytest.fixture
+def mock_model():
+    
+    return Mock(spec=Model)
+
+def test_execute_calls_save_method(mock_model):
+    file_path='test.pkl'
+    save_model = SaveModel(model=mock_model, file_path=file_path)
+
+    save_model.execute()
+
+    mock_model.save.assert_called_once_with(file_path)
+
+if __name__ == '__main__':
+    pytest.main()
