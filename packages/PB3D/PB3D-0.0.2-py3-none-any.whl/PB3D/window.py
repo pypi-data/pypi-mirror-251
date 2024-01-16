a8444961f740df9eb97488fa6092457cbd7e5c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:04:02 +0000

    Add package PB3D/0.0.2/PB3D-0.0.2-py3-none-any.whl

---
+++ packages/PB3D/PB3D-0.0.2-py3-none-any.whl/PB3D/window.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.locals import *
+from OpenGL.GL import glTranslatef, glClear, glRotatef, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, glOrtho, glClearColor
+from OpenGL.GLU import gluPerspective
+
+def init(size: tuple[int, int], color: tuple[int, int, int, int]):
+    pygame.init()
+    pygame.display.set_mode(size, DOUBLEBUF | OPENGL)
+    gluPerspective(45, (size[0] / size[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+    glClearColor(*color)
+
+def init_2d(size: tuple[int, int]):
+    pygame.init()
+    pygame.display.set_mode(size, DOUBLEBUF | OPENGL)
+    glOrtho(0, size[0], size[1], 0, -1, 1)
+
+def update():
+    pygame.display.flip()
+
+def clean():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+def turn(angle, x, y, z):
+    glRotatef(angle, x, y, z)
+
+
+def loop(func1=None, func2=None):
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if func2 != None:
+                func2()
+
+        if func1 != None:
+            func1()
+
+Event = pygame.event.Event
