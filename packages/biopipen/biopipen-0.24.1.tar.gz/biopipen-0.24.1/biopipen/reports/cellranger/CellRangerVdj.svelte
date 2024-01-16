34f662c7584bbe136540f170279ff9f2e242137c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:35 +0000

    Add package biopipen/0.24.1/biopipen-0.24.1.tar.gz

---
+++ packages/biopipen/biopipen-0.24.1.tar.gz/biopipen-0.24.1/biopipen/reports/cellranger/CellRangerVdj.svelte
@@ -0,0 +1,15 @@
+{% from "utils/misc.liq" import report_jobs, table_of_images -%}
+
+{%- macro report_job(job, h=1) -%}
+    <iframe
+        src="{{job.out.outdir}}/outs/web_summary.html"
+        width="100%"
+        frameborder="0"
+        style="min-height: 60vh"></iframe>
+{%- endmacro -%}
+
+{%- macro head_job(job) -%}
+    <h1>{{job.out.outdir | basename | escape}}</h1>
+{%- endmacro -%}
+
+{{ report_jobs(jobs, head_job, report_job) }}
