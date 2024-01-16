9920c8ae6da66d3c013e838ddc768cd26793e29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:06 +0000

    Add package pyopal/0.5.0a2/pyopal-0.5.0a2-pp310-pypy310_pp73-win_amd64.whl

---
+++ packages/pyopal/pyopal-0.5.0a2-pp310-pypy310_pp73-win_amd64.whl/pyopal/tests/__init__.py
@@ -0,0 +1,16 @@
+# noqa: D104
+
+from . import (
+    test_alphabet,
+    test_database,
+    test_doctest,
+    test_search,
+)
+
+
+def load_tests(loader, suite, pattern):
+    test_doctest.load_tests(loader, suite, pattern)
+    suite.addTests(loader.loadTestsFromModule(test_alphabet))
+    suite.addTests(loader.loadTestsFromModule(test_database))
+    suite.addTests(loader.loadTestsFromModule(test_search))
+    return suite
