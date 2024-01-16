338b9e12c5522728ad0cb425f00e2ea003c2f1c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:31 +0000

    Add package semper/2.4/semper-2.4.tar.gz

---
+++ packages/semper/semper-2.4.tar.gz/semper-2.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_namespace_packages
+from os import listdir
+setup(name='semper',
+
+      version='2.4',
+
+      url='https://github.com',
+
+      license='MIT',
+
+      author='Levap Vobayr',
+
+      author_email='pppf@hmail.ri',
+
+      description='',
+      packages=find_namespace_packages(where="src"),
+      package_dir={"": "src"},
+      package_data={**{
+        f"semper.files.q1_1.{x}": ["*.jpg"] for x in listdir('src/semper/files/q1_1/')
+        },**{
+        f"semper.files.q2_1.{x}": ["*.jpg"] for x in listdir('src/semper/files/q2_1/')
+        },**{
+        f"semper.files.q3_1.{x}": ["*.jpg"] for x in listdir('src/semper/files/q3_1/')
+        },},
+
+
+      zip_safe=False)
