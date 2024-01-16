e406e8042559dba964a325a7d2c4f1faf8a05997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:40 +0000

    Add package pyfritzhome/0.6.10/pyfritzhome-0.6.10.tar.gz

---
+++ packages/pyfritzhome/pyfritzhome-0.6.10.tar.gz/pyfritzhome-0.6.10/pyfritzhome/__init__.py
@@ -0,0 +1,12 @@
+"""Init file for pyfritzhome."""
+
+from .errors import InvalidError, LoginError
+from .fritzhome import Fritzhome
+from .fritzhomedevice import FritzhomeDevice
+
+__all__ = (
+    "Fritzhome",
+    "FritzhomeDevice",
+    "InvalidError",
+    "LoginError",
+)
