0c9f45794429029c14a4f984e656c58ccc06ebbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:49 +0000

    Add package med-metrics/0.0.3/med_metrics-0.0.3-py3-none-any.whl

---
+++ packages/med-metrics/med_metrics-0.0.3-py3-none-any.whl/med_metrics/__init__.py
@@ -0,0 +1,27 @@
+"""
+Python tools for evaluating medical ML models
+=============================================
+
+This package provides tools for evaluating machine learning models, 
+specifically designed for use in medical applications. It includes 
+functions for calculating various metrics, performing bootstrap evaluations, 
+and utility functions to support these tasks.
+
+"""
+
+# Author: Erkin Ötleş
+# Email: hi@eotles.com
+'''
+from .metrics import *
+from .curves import *
+from .compatibility_metrics import backwards_trust_compatibility, backwards_error_compatibility, rank_based_compatibility
+from .bootstrap import *
+
+__all__ = [
+    'metrics',
+    'curves',
+    'compatibility_metrics',
+    'bootstrap'
+]
+'''
+
