ae4c518d0af2342adc2c8909bcf387fc4d6ee171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:42 +0000

    Add package distribut/0.1/distribut-0.1.tar.gz

---
+++ packages/distribut/distribut-0.1.tar.gz/distribut-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distribut',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distribut'],
+      author = 'Trav',
+      author_email = 'nduas77@gmail.com',
+      zip_safe=False)
