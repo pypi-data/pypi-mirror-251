2338f7656df87acb74034c0cd213f752868b807a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:03:39 +0000

    Add package onnxscript/0.1.0.dev20240116/onnxscript-0.1.0.dev20240116-py3-none-any.whl

---
+++ packages/onnxscript/onnxscript-0.1.0.dev20240116-py3-none-any.whl/onnxscript/function_libs/torch_lib/ops/__init__.py
@@ -0,0 +1,12 @@
+__all__ = [
+    "core",
+    "fft",
+    "linalg",
+    "nested",
+    "nn",
+    "prims",
+    "sparse",
+    "special",
+]
+
+from . import core, fft, linalg, nested, nn, prims, sparse, special
