6a393655a7879dbb66bf6d0608e4059a41ded3f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:28 +0000

    Add package minivectordb/1.1.5/minivectordb-1.1.5.tar.gz

---
+++ packages/minivectordb/minivectordb-1.1.5.tar.gz/minivectordb-1.1.5/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minivectordb',
+    version='1.1.5',
+    author='Carlo Moro',
+    author_email='cnmoro@gmail.com',
+    description="This is a Python project aimed at extracting embeddings from textual data and performing semantic search.",
+    packages=find_packages(),
+    package_data={
+        'minivectordb': ['resources/embedding_model_quantized.onnx']
+    },
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "onnx",
+        "onnxruntime",
+        "onnxruntime-extensions",
+        "transformers",
+        "faiss-cpu",
+        "torch",
+        "pytest",
+        "pytest-cov",
+        "rank-bm25",
+        "thefuzz[speedup]"
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
