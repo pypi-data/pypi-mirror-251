41a44025a9157f4733932b1b86a3c83ec72eaa57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:34 +0000

    Add package mocodo/4.1.3b1/mocodo-4.1.3b1-py3-none-any.whl

---
+++ packages/mocodo/mocodo-4.1.3b1-py3-none-any.whl/mocodo/__init__.py
@@ -0,0 +1,14 @@
+import importlib
+
+def load_ipython_extension(ipython):
+    # This function is called when the extension is loaded in a notebook
+    # with %load_ext mocodo or %reload_ext mocodo.
+
+    importlib.import_module("mocodo.magic_command")
+    pkg_resources = importlib.import_module("pkg_resources")
+
+    try:
+        MOCODO_VERSION = pkg_resources.get_distribution("mocodo").version
+    except pkg_resources.DistributionNotFound:
+        MOCODO_VERSION = "(unknown version)"  # For tests during CI
+    print(f"Mocodo {MOCODO_VERSION} loaded.")
