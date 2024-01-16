daa33715a624f8fde449db96481401bd89a459fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:03:29 +0000

    Add package sustech-ncs/0.0.6/sustech_ncs-0.0.6.tar.gz

---
+++ packages/sustech-ncs/sustech_ncs-0.0.6.tar.gz/sustech_ncs-0.0.6/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="sustech_ncs",
+  version="0.0.6",
+  author="Yibo Yang, Jimao Shi",
+  author_email="12112222@mail.sustech.edu.cn, 12112218@mail.sustech.edu.cn",
+  description="A python impletation of NCS-C.",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/Joy-Mine/sustech_ncs",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
---
+++ packages/sustech-ncs/sustech_ncs-0.0.6.tar.gz/sustech_ncs-0.0.6/sustech_ncs/__init__.py
@@ -0,0 +1,7 @@
+from .NCS_mul_pack import NCS
+from .NCS_noMul_pack import NCS as NCS_noMul
+from .NCS_mul_asym import NCS as NCS_asym
+
+__version__ = "0.0.6"
+
+name = "sustech_ncs"
