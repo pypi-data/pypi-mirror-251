cd605d4cac2feeb1abf3c17b34c7ec2f5b56cd89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:57 +0000

    Add package Pylogram/0.1.0/Pylogram-0.1.0-py3-none-any.whl

---
+++ packages/Pylogram/Pylogram-0.1.0-py3-none-any.whl/pylogram/raw/types/message_interaction_counters.py
@@ -0,0 +1,89 @@
+#  Pylogram - Telegram MTProto API Client Library for Python
+#  Copyright (C) 2017-2023 Dan <https://github.com/delivrance>
+#  Copyright (C) 2023-2024 Pylakey <https://github.com/pylakey>
+#
+#  This file is part of Pylogram.
+#
+#  Pylogram is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Lesser General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Pylogram is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Lesser General Public License for more details.
+#
+#  You should have received a copy of the GNU Lesser General Public License
+#  along with Pylogram.  If not, see <http://www.gnu.org/licenses/>.
+
+from io import BytesIO
+
+from pylogram.raw.core.primitives import Int, Long, Int128, Int256, Bool, Bytes, String, Double, Vector
+from pylogram.raw.core import TLObject
+from pylogram import raw
+from typing import List, Optional, Any
+
+# # # # # # # # # # # # # # # # # # # # # # # #
+#               !!! WARNING !!!               #
+#          This is a generated file!          #
+# All changes made in this file will be lost! #
+# # # # # # # # # # # # # # # # # # # # # # # #
+
+
+class MessageInteractionCounters(TLObject):  # type: ignore
+    """Telegram API type.
+
+    Constructor of :obj:`~pylogram.raw.base.MessageInteractionCounters`.
+
+    Details:
+        - Layer: ``166``
+        - ID: ``AD4FC9BD``
+
+    Parameters:
+        msg_id (``int`` ``32-bit``):
+            N/A
+
+        views (``int`` ``32-bit``):
+            N/A
+
+        forwards (``int`` ``32-bit``):
+            N/A
+
+    """
+
+    __slots__: List[str] = ["msg_id", "views", "forwards"]
+
+    ID = 0xad4fc9bd
+    QUALNAME = "types.MessageInteractionCounters"
+
+    def __init__(self, *, msg_id: int, views: int, forwards: int) -> None:
+        self.msg_id = msg_id  # int
+        self.views = views  # int
+        self.forwards = forwards  # int
+
+    @staticmethod
+    def read(b: BytesIO, *args: Any) -> "MessageInteractionCounters":
+        # No flags
+        
+        msg_id = Int.read(b)
+        
+        views = Int.read(b)
+        
+        forwards = Int.read(b)
+        
+        return MessageInteractionCounters(msg_id=msg_id, views=views, forwards=forwards)
+
+    def write(self, *args) -> bytes:
+        b = BytesIO()
+        b.write(Int(self.ID, False))
+
+        # No flags
+        
+        b.write(Int(self.msg_id))
+        
+        b.write(Int(self.views))
+        
+        b.write(Int(self.forwards))
+        
+        return b.getvalue()
