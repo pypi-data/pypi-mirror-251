be5514e21bc437499ede538be0d613662d3e85f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:05 +0000

    Add package jobbergate-api/4.2.1/jobbergate_api-4.2.1-py3-none-any.whl

---
+++ packages/jobbergate-api/jobbergate_api-4.2.1-py3-none-any.whl/jobbergate_api/apps/job_scripts/tools.py
@@ -0,0 +1,31 @@
+"""Provide a convenience class for managing job-script files."""
+
+from loguru import logger
+
+
+def inject_sbatch_params(job_script_data_as_string: str, sbatch_params: list[str]) -> str:
+    """
+    Inject sbatch params into job script.
+
+    Given the job script as job_script_data_as_string, inject the sbatch params in the correct location.
+    """
+    logger.debug("Preparing to inject sbatch params into job script")
+
+    if not sbatch_params:
+        logger.warning("Sbatch param list is empty")
+        return job_script_data_as_string
+
+    first_sbatch_index = job_script_data_as_string.find("#SBATCH")
+    string_slice = job_script_data_as_string[first_sbatch_index:]
+    line_end = string_slice.find("\n") + first_sbatch_index + 1
+
+    inner_string = ""
+    for parameter in sbatch_params:
+        inner_string += "#SBATCH " + parameter + "\n"
+
+    new_job_script_data_as_string = (
+        job_script_data_as_string[:line_end] + inner_string + job_script_data_as_string[line_end:]
+    )
+
+    logger.debug("Done injecting sbatch params into job script")
+    return new_job_script_data_as_string
