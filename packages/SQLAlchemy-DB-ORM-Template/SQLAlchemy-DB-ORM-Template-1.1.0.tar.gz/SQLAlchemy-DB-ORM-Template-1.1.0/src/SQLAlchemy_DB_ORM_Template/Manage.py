73743fe29bbbc0387be3d38bb63f1b9b087c1fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 15:04:22 +0000

    Add package SQLAlchemy-DB-ORM-Template/1.1.0/SQLAlchemy-DB-ORM-Template-1.1.0.tar.gz

---
+++ packages/SQLAlchemy-DB-ORM-Template/SQLAlchemy-DB-ORM-Template-1.1.0.tar.gz/SQLAlchemy-DB-ORM-Template-1.1.0/src/SQLAlchemy_DB_ORM_Template/Manage.py
@@ -0,0 +1,12 @@
+from App import db, app
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
+
