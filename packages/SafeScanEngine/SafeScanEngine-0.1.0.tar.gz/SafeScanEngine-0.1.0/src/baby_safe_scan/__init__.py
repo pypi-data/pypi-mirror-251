1ef9ec2bfa78daf54151f3a0dc238bed198af305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 15:04:26 +0000

    Add package SafeScanEngine/0.1.0/SafeScanEngine-0.1.0.tar.gz

---
+++ packages/SafeScanEngine/SafeScanEngine-0.1.0.tar.gz/SafeScanEngine-0.1.0/src/baby_safe_scan/__init__.py
@@ -0,0 +1,3 @@
+# ruff: noqa: F401
+
+from .resolver_yolov8 import YoloV8Resolver
---
+++ packages/SafeScanEngine/SafeScanEngine-0.1.0.tar.gz/SafeScanEngine-0.1.0/src/baby_safe_scan/encoders.py
@@ -0,0 +1,18 @@
+import base64
+import io
+
+from PIL.Image import Image
+
+
+class Base64Encoder:
+    """
+    Class to handle encoding from image to ASCI string.
+    """
+
+    @staticmethod
+    def encode_image_to_base64(img: Image) -> str:
+        # Open the image file and convert it to binary data
+        buffered = io.BytesIO()
+        img.save(buffered, format="png")
+        encoded_image = base64.b64encode(buffered.getvalue())
+        return encoded_image.decode("utf-8")
