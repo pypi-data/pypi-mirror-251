8782ab95c056df8294adb8667fa951e84d55e8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:11 +0000

    Add package mazeforge/0.1.0/mazeforge-0.1.0-py3-none-any.whl

---
+++ packages/mazeforge/mazeforge-0.1.0-py3-none-any.whl/mazeforge/__init__.py
@@ -0,0 +1,16 @@
+"""MazeForge
+
+Contact:
+--------
+
+- oskar.meyenburg@gmail.com
+
+More information is available at:
+
+- https://pypi.org/project/mazeforge/
+- https://github.com/oskarmeyenburg/mazeforge
+"""
+from .generator import generate
+
+
+__version__ = "0.1.0"
