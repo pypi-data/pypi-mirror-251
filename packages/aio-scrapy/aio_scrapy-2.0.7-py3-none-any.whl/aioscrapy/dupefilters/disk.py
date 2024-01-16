bcfb0ab0a0f5286d6f253420f688f63ae61abe80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:03:58 +0000

    Add package aio-scrapy/2.0.7/aio_scrapy-2.0.7-py3-none-any.whl

---
+++ packages/aio-scrapy/aio_scrapy-2.0.7-py3-none-any.whl/aioscrapy/dupefilters/disk.py
@@ -0,0 +1,58 @@
+import os
+from typing import Optional, Set
+
+from aioscrapy import Request, Spider
+from aioscrapy.dupefilters import DupeFilterBase
+from aioscrapy.utils.log import logger
+from aioscrapy.utils.request import referer_str
+
+
+class DiskRFPDupeFilter(DupeFilterBase):
+    """Request Fingerprint duplicates filter built with Disk storage"""
+
+    def __init__(self, path: Optional[str] = None, debug: bool = False):
+        self.file: Optional["File object"] = None
+        self.debug = debug
+        self.fingerprints: Set = set()
+        self.logdupes: bool = True
+        if path:
+            self.file = open(os.path.join(path, 'requests.seen'), 'a+')
+            self.file.seek(0)
+            self.fingerprints.update(x.rstrip() for x in self.file)
+
+    @classmethod
+    def from_crawler(cls, crawler: "aioscrapy.crawler.Crawler"):
+        debug = crawler.settings.getbool('DUPEFILTER_DEBUG')
+        path = crawler.settings.get('JOBDIR', './job_dir')
+        if path and not os.path.exists(path):
+            os.makedirs(path)
+        return cls(path, debug)
+
+    async def request_seen(self, request: Request) -> bool:
+        if request.fingerprint in self.fingerprints:
+            return True
+        self.fingerprints.add(request.fingerprint)
+        if self.file:
+            self.file.write(request.fingerprint + '\n')
+        return False
+
+    async def close(self, reason: str = '') -> None:
+        if self.file:
+            self.file.close()
+
+    def log(self, request: Request, spider: Spider):
+        if self.debug:
+            logger.debug("Filtered duplicate request: %(request)s (referer: %(referer)s)" % {
+                'request': request, 'referer': referer_str(request)
+            })
+        elif self.logdupes:
+            msg = ("Filtered duplicate request: %(request)s"
+                   " - no more duplicates will be shown"
+                   " (see DUPEFILTER_DEBUG to show all duplicates)")
+            logger.debug(msg % {'request': request})
+            self.logdupes = False
+
+        spider.crawler.stats.inc_value('dupefilter/filtered', spider=spider)
+
+
+RFPDupeFilter = DiskRFPDupeFilter
