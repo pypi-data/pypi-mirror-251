c899235e4884428e8aa757ade6bb3c5fddd20a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 15:03:54 +0000

    Add package BUAIP-Distributions/0.1/BUAIP_Distributions-0.1.tar.gz

---
+++ packages/BUAIP-Distributions/BUAIP_Distributions-0.1.tar.gz/BUAIP_Distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='BUAIP_Distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['BUAIP_Distributions'],
+      zip_safe=False)
