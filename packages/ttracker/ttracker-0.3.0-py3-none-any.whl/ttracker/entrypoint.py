8380439e5937e60a9db37cb5c5361e0555951cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:03:51 +0000

    Add package ttracker/0.3.0/ttracker-0.3.0-py3-none-any.whl

---
+++ packages/ttracker/ttracker-0.3.0-py3-none-any.whl/ttracker/entrypoint.py
@@ -0,0 +1,10 @@
+import click
+from ttracker.cli_factory import command_factory
+
+
+def main():
+    c = command_factory()
+    try:
+        c()
+    except Exception as e:
+        click.secho(f"Error: {str(e)}", fg="red")
