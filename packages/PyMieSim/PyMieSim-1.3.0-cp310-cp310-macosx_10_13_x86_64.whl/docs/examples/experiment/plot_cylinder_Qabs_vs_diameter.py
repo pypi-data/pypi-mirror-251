f740a036727e93ec754a24717cc7ab3af30c7895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:04:07 +0000

    Add package PyMieSim/1.3.0/PyMieSim-1.3.0-cp310-cp310-macosx_10_13_x86_64.whl

---
+++ packages/PyMieSim/PyMieSim-1.3.0-cp310-cp310-macosx_10_13_x86_64.whl/docs/examples/experiment/plot_cylinder_Qabs_vs_diameter.py
@@ -0,0 +1,49 @@
+"""
+Cylinder: Qsca vs diameter
+==========================
+
+"""
+
+# %%
+# Importing the package dependencies: numpy, PyMieSim
+import numpy as np
+from PyMieSim.experiment import CylinderSet, SourceSet, Setup
+from PyMieSim.materials import Gold, Silver, Aluminium
+from PyMieSim import measure
+
+# %%
+# Defining the ranging parameters for the scatterer distribution
+scatterer_set = CylinderSet(
+    diameter=np.linspace(1e-09, 800e-9, 300),
+    material=[Silver, Gold, Aluminium],
+    n_medium=1
+)
+
+# %%
+# Defining the source to be employed.
+source_set = SourceSet(
+    wavelength=400e-9,
+    linear_polarization=0,
+    optical_power=1e-3,
+    NA=0.2
+)
+
+# %%
+# Defining the experiment setup
+experiment = Setup(
+    scatterer_set=scatterer_set,
+    source_set=source_set
+)
+
+# %%
+# Measuring the properties
+data = experiment.Get(measure.Qabs)
+
+# %%
+# Plotting the results
+figure = data.plot(
+    x=scatterer_set.diameter,
+    y_scale="linear"
+)
+
+_ = figure.show()
