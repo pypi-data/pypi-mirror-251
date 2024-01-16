1098c53138733b3aea21c98c5f0043e85b5061ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:03:34 +0000

    Add package tvms/0.0.28/tvms-0.0.28.tar.gz

---
+++ packages/tvms/tvms-0.0.28.tar.gz/tvms-0.0.28/setup.py
@@ -0,0 +1,23 @@
+from io import open
+from setuptools import setup
+
+version = '0.0.28'
+
+setup(
+    name = 'tvms',
+    version=version,
+
+    author='Boo4kin',
+    author_email='bkolyabi@gmail.com',
+
+    license='Free' ,
+
+    include_package_data=True,  # Include data files specified in MANIFEST.in
+    
+    package_data={
+        '': ['pic/*.jpeg'],  # Include all PNG files in the 'pic' directory
+    },
+    
+    packages= ['tvms']
+)
+
