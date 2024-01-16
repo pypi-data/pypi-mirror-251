098b0ab3d0b5e5b1f84678873f4c1270fc5f4f3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:49 +0000

    Add package hassn-distributions/0.1/hassn_distributions-0.1.tar.gz

---
+++ packages/hassn-distributions/hassn_distributions-0.1.tar.gz/hassn_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='hassn_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=find_packages(),
+      author='Hassn Hamada',
+      author_email='hassneltokhy4@gmail.com',
+      zip_safe=False)
