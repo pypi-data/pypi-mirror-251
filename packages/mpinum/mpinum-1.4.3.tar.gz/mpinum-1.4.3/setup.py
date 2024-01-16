d46f0a17ae5c71e11baa4fc1431896630a870aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:03:29 +0000

    Add package mpinum/1.4.3/mpinum-1.4.3.tar.gz

---
+++ packages/mpinum/mpinum-1.4.3.tar.gz/mpinum-1.4.3/setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+
+setup(name = 'mpinum',
+      version = '1.4.3',
+      description = 'A very lightweight implementation of distributed arrays',
+      long_description = README,
+      long_description_content_type="text/markdown",
+      author = 'Alexander Pletzer',
+      author_email = 'alexander@gokliya.net',
+      url = 'https://github.com/pletzer/mpinum.git',
+      install_requires=['numpy', 'mpi4py'],
+      package_dir={'mpinum': 'src'}, # the present directory maps to src 
+      packages = ['mpinum'],
+)
