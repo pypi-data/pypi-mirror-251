f005ccc3fb91baae489b72e5f2fd1d80f19f3a7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:22 +0000

    Add package ibm-generative-ai/2.0.0/ibm_generative_ai-2.0.0-py3-none-any.whl

---
+++ packages/ibm-generative-ai/ibm_generative_ai-2.0.0-py3-none-any.whl/genai/__init__.py
@@ -0,0 +1,14 @@
+import logging
+
+from genai._utils.api_client import ApiClient
+from genai._utils.shared_loop import handle_shutdown_event
+from genai._version import __version__ as v
+from genai.client import Client
+from genai.credentials import Credentials
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__version__ = v
+
+
+__all__ = ["Client", "ApiClient", "Credentials", "handle_shutdown_event", "__version__"]
