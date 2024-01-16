9b91665a9a1409af832e5dfa20b1a381d137e255
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:04:45 +0000

    Add package PB3D/0.0.2a1/PB3D-0.0.2a1.tar.gz

---
+++ packages/PB3D/PB3D-0.0.2a1.tar.gz/PB3D-0.0.2a1/PB3D/math/vector.py
@@ -0,0 +1,21 @@
+class Vec3:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, scalar):
+        return Vec3(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def __truediv__(self, scalar):
+        if scalar != 0:
+            return Vec3(self.x / scalar, self.y / scalar, self.z / scalar)
+        else:
+            raise ValueError("Cannot divide by zero.")
+
