a12141efa6adb4eb61babc20b68c945f86f6d739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:16 +0000

    Add package leandesk/1.6.0/leandesk-1.6.0.tar.gz

---
+++ packages/leandesk/leandesk-1.6.0.tar.gz/leandesk-1.6.0/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='leandesk',
+    version='1.6.0',
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies your module may have
+    ],
+    entry_points={
+        'console_scripts': [
+            'clean = leandesk.cli:main',
+        ],
+    },
+)
