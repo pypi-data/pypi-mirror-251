3ec81722f459e19a0748fc094cf778cccbae4990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:02:55 +0000

    Add package genomeshader/0.1.11/genomeshader-0.1.11-cp37-abi3-macosx_10_12_x86_64.whl

---
+++ packages/genomeshader/genomeshader-0.1.11-cp37-abi3-macosx_10_12_x86_64.whl/genomeshader/__init__.py
@@ -0,0 +1,11 @@
+# __init__.py: genomeshader
+# Contact: Kiran V Garimella <kiran@broadinstitute.org>
+
+
+"""
+For detailed documentation and examples, see the README.
+"""
+
+__version__ = '0.1.11'
+
+from .view import *
