5f364833b9df61c1734cc72577960130cd731a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:03:56 +0000

    Add package cg/55.3.4/cg-55.3.4.tar.gz

---
+++ packages/cg/cg-55.3.4.tar.gz/cg-55.3.4/cg/cli/workflow/fastq/base.py
@@ -0,0 +1,51 @@
+import datetime as dt
+import logging
+
+import click
+
+from cg.cli.workflow.commands import ARGUMENT_CASE_ID
+from cg.constants.constants import DRY_RUN, Pipeline
+from cg.meta.workflow.analysis import AnalysisAPI
+from cg.store import Store
+from cg.store.models import Analysis, Case
+
+LOG = logging.getLogger(__name__)
+
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def fastq(context: click.Context):
+    """Function for storing fastq-cases"""
+    AnalysisAPI.get_help(context)
+
+
+@fastq.command("store")
+@DRY_RUN
+@ARGUMENT_CASE_ID
+@click.pass_context
+def store_fastq_analysis(context: click.Context, case_id: str, dry_run: bool = False):
+    """Creates an analysis object in status-db for the given fast case"""
+    LOG.info(f"Creating an analysis for case {case_id}")
+    status_db: Store = context.obj.status_db
+    case_obj: Case = status_db.get_case_by_internal_id(internal_id=case_id)
+    new_analysis: Analysis = status_db.add_analysis(
+        pipeline=Pipeline.FASTQ,
+        completed_at=dt.datetime.now(),
+        primary=True,
+        started_at=dt.datetime.now(),
+        case_id=case_obj.id,
+    )
+    if dry_run:
+        return
+    status_db.session.add(new_analysis)
+    status_db.session.commit()
+
+
+@fastq.command("store-available")
+@DRY_RUN
+@click.pass_context
+def store_available_fastq_analysis(context: click.Context, dry_run: bool = False):
+    """Creates an analysis object in status-db for all fastq cases to be delivered"""
+    status_db: Store = context.obj.status_db
+    for case in status_db.cases_to_analyze(pipeline=Pipeline.FASTQ, threshold=False):
+        context.invoke(store_fastq_analysis, case_id=case.internal_id, dry_run=dry_run)
