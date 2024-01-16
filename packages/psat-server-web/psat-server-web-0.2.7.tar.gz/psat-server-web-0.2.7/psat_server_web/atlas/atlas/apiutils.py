77fc9a6f4272d5e39e7c8a1acd52e77b7fbbe2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:33 +0000

    Add package psat-server-web/0.2.7/psat-server-web-0.2.7.tar.gz

---
+++ packages/psat-server-web/psat-server-web-0.2.7.tar.gz/psat-server-web-0.2.7/psat_server_web/atlas/atlas/apiutils.py
@@ -0,0 +1,128 @@
+from requests.exceptions import ConnectionError as RequestsConnectionError
+from requests.exceptions import Timeout as RequestsConnectionTimeoutError
+from django.conf import settings
+from atlas.models import AtlasDiffObjects
+from atlas.models import AtlasForcedPhotometry
+from atlas.models import TcsGravityEventAnnotations
+from atlas.models import SherlockClassifications
+from atlas.models import SherlockCrossmatches
+from atlas.models import TcsObjectComments
+from atlas.models import TcsCrossMatchesExternal
+from atlas.models import AtlasDetectionsddc
+from django.forms.models import model_to_dict
+from .lightcurvequeries import *
+from .views import followupClassList
+from .views import WebViewUserDefined
+from .views import AtlasDetectionsddcTable
+from .commonqueries import getNonDetectionsUsingATLASFootprint, ATLAS_METADATADDC, filterWhereClauseddc, LC_POINTS_QUERY_ATLAS_DDC, FILTERS, getLightcurvePoints, getLightcurvePointsDDCAPI, getNonDetectionsUsingATLASFootprintAPI
+from .dbviews import CustomLCBlanks, CustomLCPoints, CustomLCPoints2, CustomLCBlanks2
+
+from .views import LC_LIMITS, LC_LIMITS_MD
+import numpy as np
+
+def candidateddcApi(request, atlas_diff_objects_id, mjdThreshold = None):
+    """candidateddcApi.
+
+    Args:
+        request:
+        atlas_diff_objects_id:
+        mjdThreshold:
+    """
+
+    from django.db import connection
+    import sys
+
+    # 2021-10-21 KWS Use the Lasair API to do a cone search so we can check for nearby ZTF objects
+    from lasair import LasairError, lasair_client as lasair
+
+    transient = AtlasDiffObjects.objects.get(pk=atlas_diff_objects_id)
+
+    # 2017-03-21 KWS Get Gravity Wave annotations and Sherlock Classifications
+    sc = SherlockClassifications.objects.filter(transient_object_id_id = transient.id)
+    # 2017-11-07 KWS Get Sherlock Crossmatches
+    sx = SherlockCrossmatches.objects.filter(transient_object_id_id = transient.id)
+    gw = TcsGravityEventAnnotations.objects.filter(transient_object_id_id = transient.id).filter(enclosing_contour__lt=100)
+    #existingComments = TcsObjectComments.objects.filter(transient_object_id = transient.id).order_by('date_inserted')
+
+    # 2013-10-30 KWS Get external crossmatches if they exist
+    externalXMs = TcsCrossMatchesExternal.objects.filter(transient_object_id = transient.id).exclude(matched_list = 'Transient Name Server').order_by('external_designation')
+
+    # 2019-10-18 KWS Get TNS crossmatch if it exists. Yes - another unnecessary hit to the database, but quick.
+    tnsXMs = TcsCrossMatchesExternal.objects.filter(transient_object_id = transient.id, matched_list = 'Transient Name Server')
+
+    dbName = settings.DATABASES['default']['NAME'].replace('_django', '')
+
+    try:
+        detectionLimits = LC_LIMITS[dbName]
+    except KeyError as e:
+        # Default detections limits for medium deep
+        detectionLimits = LC_LIMITS_MD
+
+    if mjdThreshold is not None:
+        forcedPhotometry = AtlasForcedPhotometry.objects.filter(atlas_object_id = transient.id).filter(mjd_obs__gte=mjdThreshold).order_by('mjd_obs')
+    else:
+        forcedPhotometry = AtlasForcedPhotometry.objects.filter(atlas_object_id = transient.id).order_by('mjd_obs')
+
+
+    recsExperiment = getLightcurvePointsDDCAPI(transient.id, djangoRawObject = CustomLCPoints2, lcQuery=LC_POINTS_QUERY_ATLAS_DDC + filterWhereClauseddc(FILTERS), conn = connection, mjdThreshold = mjdThreshold)
+    blanksExperiment = getNonDetectionsUsingATLASFootprintAPI(recsExperiment, djangoRawObject = CustomLCBlanks2, ndQuery=ATLAS_METADATADDC, filterWhereClause = filterWhereClauseddc, catalogueName = 'atlas_metadataddc', conn = connection, mjdThreshold = mjdThreshold, transient = transient)
+
+#    dets = AtlasDetectionsddc.objects.filter(atlas_object_id=transient.id)
+#    detTable = AtlasDetectionsddcTable(dets)
+
+#    p, recurrences = getLightcurvePoints(transient.id, djangoRawObject = CustomLCPoints, lcQuery=LC_POINTS_QUERY_ATLAS_DDC + filterWhereClauseddc(FILTERS), conn = connection)
+
+    # The first variable below is the list of nondetections for each filter defined by the FILTERS variable in commonqueries.py. Ken to find out what blanks actually contains, since this is actually the raw query object. Why does it need to return the rawQuery object at all???
+#    nondets, blanks, lastNonDetection = getNonDetectionsUsingATLASFootprint(recurrences, djangoRawObject = CustomLCBlanks, ndQuery=ATLAS_METADATADDC, filterWhereClause = filterWhereClauseddc, catalogueName = 'atlas_metadataddc', conn = connection)
+
+#    tableHeader = ['id','Ra','Dec','Mag','Dmag','X','Y','Major','Minor','Phi','Det','Chin','Pvr','Ptr','Pmv','Pkn','Pno','Pbn','Pcr','Pxt','Psc','Dup','Wpflx','Dflx','Image_group_id','Mjd','Obs','Mag5sig','Atlas_metadata_id','Texp','Filt','Obj','fpRA','fpDec']
+
+#    django_rows = [r for r in detTable.rows]
+
+#    rows = []
+#    for row in django_rows:
+#        rows.append([e for e in row])
+
+#    data = {'detection_table': {k: v for k,v in zip(tableHeader, np.array(rows).T)},
+#            'lcnondets': {k: np.array(v).T for k,v in zip(FILTERS, nondets)},
+#            'fp': [model_to_dict(f) for f in forcedPhotometry],
+#            'object': model_to_dict(transient),
+#            'sherlock_crossmatches': [model_to_dict(s) for s in sx],
+#            'sherlock_classifications': [model_to_dict(s) for s in sc],
+#            'tns_crossmatches': [model_to_dict(t) for t in tnsXMs],
+#            'external_crossmatches': [model_to_dict(e) for e in externalXMs]
+#           }
+
+    data = {
+            'object': model_to_dict(transient),
+            'lc': [model_to_dict(r) for r in recsExperiment],
+            'lcnondets': [model_to_dict(b) for b in blanksExperiment],
+            'fp': [model_to_dict(f) for f in forcedPhotometry],
+            'sherlock_crossmatches': [model_to_dict(s) for s in sx],
+            'sherlock_classifications': [model_to_dict(s) for s in sc],
+            'tns_crossmatches': [model_to_dict(t) for t in tnsXMs],
+            'external_crossmatches': [model_to_dict(e) for e in externalXMs]
+           }
+
+    return data
+
+
+def getObjectList(request, listId, getCustomList = False):
+
+    querySet = None
+
+    if getCustomList:
+        if listId > 0 and listId <= 100:
+            querySet = WebViewUserDefined.objects.filter(object_group_id = listId)
+    else:
+        # There are currently 11 valid lists.
+        if listId >= 0 and listId <= 11:
+            querySet = followupClassList[int(listId)].objects.all()
+
+    objectList = []
+
+    if querySet is not None:
+        for row in querySet:
+            objectList.append(model_to_dict(row))
+
+    return objectList
---
+++ packages/psat-server-web/psat-server-web-0.2.7.tar.gz/psat-server-web-0.2.7/psat_server_web/atlas/atlasapi/serializers.py
@@ -0,0 +1,116 @@
+import datetime
+import re
+import json
+import requests
+from django.db import IntegrityError
+from django.db import connection
+from datetime import datetime
+from gkutils.commonutils import coneSearchHTM, FULL, QUICK, COUNT, CAT_ID_RA_DEC_COLS, base26, Struct
+from rest_framework import serializers
+import sys
+from atlas.apiutils import candidateddcApi, getObjectList
+
+#CAT_ID_RA_DEC_COLS['objects'] = [['objectId', 'ramean', 'decmean'], 1018]
+
+REQUEST_TYPE_CHOICES = (
+    ('count', 'Count'),
+    ('all', 'All'),
+    ('nearest', 'Nearest'),
+)
+
+
+class ConeSerializer(serializers.Serializer):
+    ra = serializers.FloatField(required=True)
+    dec = serializers.FloatField(required=True)
+    radius = serializers.FloatField(required=True)
+    requestType = serializers.ChoiceField(choices=REQUEST_TYPE_CHOICES)
+
+    def save(self):
+
+        ra = self.validated_data['ra']
+        dec = self.validated_data['dec']
+        radius = self.validated_data['radius']
+        requestType = self.validated_data['requestType']
+
+        # Get the authenticated user, if it exists.
+        userId = 'unknown'
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            userId = request.user
+
+        if radius > 1000:
+            replyMessage = "Max radius is 1000 arcsec."
+            info = {"error": replyMessage}
+            return info
+
+        replyMessage = 'No object found ra=%.5f dec=%.5f radius=%.2f' % (ra, dec, radius)
+        info = {"error": replyMessage}
+
+        # Is there an object within RADIUS arcsec of this object? - KWS - need to fix the gkhtm code!!
+        # For ATLAS QUICK does not work because of the `dec` syntax error problem. Use FULL until I figure out what
+        # needs to be fixed.
+        message, results = coneSearchHTM(ra, dec, radius, 'atlas_diff_objects', queryType=FULL, conn=connection, django=True)
+
+        obj = None
+        separation = None
+
+        objectList = []
+        if len(results) > 0:
+            if requestType == "nearest":
+                obj = results[0][1]['id']
+                separation = results[0][0]
+                info = {"object": obj, "separation": separation}
+            elif requestType == "all":
+                for row in results:
+                    objectList.append({"object": row[1]["id"], "separation": row[0]})
+                info = objectList
+            elif requestType == "count":
+                info = {'count': len(results)}
+            else:
+                info = {"error": "Invalid request type"}
+
+        return info
+
+
+class ObjectsSerializer(serializers.Serializer):
+    objects = serializers.CharField(required=True)
+    mjd = serializers.FloatField(required=False, default=None)
+
+    def save(self):
+        objects = self.validated_data['objects']
+        mjd = self.validated_data['mjd']
+
+        olist = []
+        for tok in objects.split(','):
+            olist.append(tok.strip())
+
+        if len(olist) > 100:
+            return {"info": "Max number of objects for each requests is 100"}
+#        olist = olist[:10] # restrict to 10
+
+        # Get the authenticated user, if it exists.
+        userId = 'unknown'
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            userId = request.user
+
+
+        result = []
+        for candidate in olist:
+            result.append(candidateddcApi(request, candidate, mjdThreshold=mjd))
+        return result
+
+
+class ObjectListSerializer(serializers.Serializer):
+    objectlistid = serializers.IntegerField(required=True)
+    getcustomlist = serializers.BooleanField(required=False, default = False)
+
+    def save(self):
+        objectlistid = self.validated_data['objectlistid']
+        getcustomlist = self.validated_data['getcustomlist']
+        request = self.context.get("request")
+
+        objectList = getObjectList(request, objectlistid, getCustomList = getcustomlist)
+        return objectList
+
+
