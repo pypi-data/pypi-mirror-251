8b9ca5aec5124a68ab58c77e1c523a657d87d13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:29 +0000

    Add package jsz.rapidbuild/0.5.2/jsz.rapidbuild-0.5.2-py3-none-any.whl

---
+++ packages/jsz.rapidbuild/jsz.rapidbuild-0.5.2-py3-none-any.whl/jsz/rapidbuild/__init__.py
@@ -0,0 +1,26 @@
+__all__ = [
+    "base",
+
+    "compiler_base",
+    "compiler_llvm",
+    "compiler_msvc",
+
+    "librarian_base",
+    "librarian_llvm",
+    "librarian_msvc",
+
+    "linker_base",
+    "linker_llvm",
+    "linker_msvc",
+
+    "toolchain",
+
+    "conan",
+    "fastbuild",
+
+    "graph",
+    "target_graph",
+
+    "workspace_def",
+    "workspace",
+]
