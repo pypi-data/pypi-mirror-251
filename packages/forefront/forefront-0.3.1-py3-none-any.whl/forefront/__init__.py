28e7f3793d0a445cd221f37393a77f4b2f0e7ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:35 +0000

    Add package forefront/0.3.1/forefront-0.3.1-py3-none-any.whl

---
+++ packages/forefront/forefront-0.3.1-py3-none-any.whl/forefront/__init__.py
@@ -0,0 +1,6 @@
+from .forefront_client import ForefrontClient
+from .workflows import Workflow
+from .pipelines import Pipeline
+from .chat import Chat
+from .fine_tunes import FineTunes
+from .datasets import Datasets
