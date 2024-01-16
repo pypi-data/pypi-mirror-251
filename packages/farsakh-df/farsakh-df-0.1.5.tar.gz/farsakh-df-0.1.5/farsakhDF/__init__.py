ee6ac436271a060df1de2b4e1fff40f7b40c0c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:04:56 +0000

    Add package farsakh-df/0.1.5/farsakh-df-0.1.5.tar.gz

---
+++ packages/farsakh-df/farsakh-df-0.1.5.tar.gz/farsakh-df-0.1.5/farsakhDF/__init__.py
@@ -0,0 +1,15 @@
+
+
+
+from .preprocessor_maker_file import preprocessor_maker
+from .neural_networks_builder_file import neural_networks_builder
+from .KMeansCoff_file import KMeansCoff
+from .evaluate_classification_file import evaluate_classification
+from .evaluate_regression_file import evaluate_regression
+from .explore_categorical_file import explore_categorical
+from .plot_categorical_vs_target_file import plot_categorical_vs_target
+from .explore_numeric_file import explore_numeric
+from .modelSearch_file import modelSearch
+from .plot_numeric_vs_target_file import plot_numeric_vs_target
+from .plot_PCA_file import plot_PCA
+# from .all_imports_file import all_imports
---
+++ packages/farsakh-df/farsakh-df-0.1.5.tar.gz/farsakh-df-0.1.5/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.1.5'
+DESCRIPTION = 'Data Science'
+LONG_DESCRIPTION = 'some helpfull methods to use'
+
+# Setting up
+setup(
+    name="farsakh-df",
+    version=VERSION,
+    author="Hamza Mohammed Farsakh",
+    author_email="<farsakh.m.hamza@gmail.com>",
+    description=DESCRIPTION,
+    # long_description_content_type="text/markdown",
+    # long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'sklearn'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
