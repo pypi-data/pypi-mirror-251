5ec09aaf7ed5c0189cf2786231d9542ab822f5fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:03:41 +0000

    Add package accelbyte-py-sdk/0.58.0/accelbyte_py_sdk-0.58.0.tar.gz

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.58.0.tar.gz/accelbyte_py_sdk-0.58.0/accelbyte_py_sdk/api/basic/operations/namespace/__init__.py
@@ -0,0 +1,26 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: ags_py_codegen
+
+"""Auto-generated package that contains models used by the AccelByte Gaming Services Basic Service."""
+
+__version__ = "2.15.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .change_namespace_status import ChangeNamespaceStatus
+from .create_namespace import CreateNamespace
+from .delete_namespace import DeleteNamespace
+from .get_game_namespaces import GetGameNamespaces
+from .get_namespace import GetNamespace
+from .get_namespace_publisher import GetNamespacePublisher
+from .get_namespaces import GetNamespaces
+from .public_get_namespace_publisher import PublicGetNamespacePublisher
+from .public_get_namespaces import PublicGetNamespaces
+from .update_namespace import UpdateNamespace
