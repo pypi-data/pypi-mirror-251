7fa477f86e55eaa4cc7b945e79cd1b36551e8608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:40 +0000

    Add package tvp/9/tvp-9.tar.gz

---
+++ packages/tvp/tvp-9.tar.gz/tvp-9/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='tvp',
+      version='9',
+      description='none',
+      packages=['tvp'],
+      author_email='antoha123_3@mail.ru',
+      zip_safe=False)
