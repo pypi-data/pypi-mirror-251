c87b8d966da31813d1342721d88a9616e47d5d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:48 +0000

    Add package logngo/0.2/logngo-0.2.tar.gz

---
+++ packages/logngo/logngo-0.2.tar.gz/logngo-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='logngo',
+    version='0.2',
+    packages=find_packages(),
+    description='Log chains for webserver',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Cong Hoang Tran',
+    author_email='trconghoangg@gmail.com',
+    url='https://github.com/hoangtc125/logtc',
+    install_requires=[
+        'requests',
+        'python-socketio',
+        'websocket-client',
+        'contextvars'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
