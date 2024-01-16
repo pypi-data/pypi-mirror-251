07232a2d4494c7849df6dcb80b1be506c329f0f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 15:03:54 +0000

    Add package tkchart/1.0.3/tkchart-1.0.3-py3-none-any.whl

---
+++ packages/tkchart/tkchart-1.0.3-py3-none-any.whl/tkchart/Line.py
@@ -0,0 +1,26 @@
+         
+class Line():
+   def __init__(self ,*args ,master=None ,color="#909090" ,size=1):
+      try :
+         self.__chart = args[0]
+      except:
+         self.__chart = master
+
+      self.__color = color
+      self.__size = size
+      self.__y_end = 0
+      self.__x_end  = self.__chart._LineChart__line_width* -1
+      self.__data = []
+      self.__temp_data = []
+   
+
+   def configure(self , color=None ,size=None) :
+      if color != None:
+         self.__color = color
+      if size != None:
+         self.__size = size
+   
+   def __reset(self):
+      self.__y_end = 0
+      self.__x_end  = self.__chart._LineChart__line_width* -1
+      self.__data = []
