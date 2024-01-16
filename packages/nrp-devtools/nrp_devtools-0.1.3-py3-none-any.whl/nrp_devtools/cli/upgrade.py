64ae5ef2e0bf32e2678c9d5401972cad9be556fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 15:03:44 +0000

    Add package nrp-devtools/0.1.3/nrp_devtools-0.1.3-py3-none-any.whl

---
+++ packages/nrp-devtools/nrp_devtools-0.1.3-py3-none-any.whl/nrp_devtools/cli/upgrade.py
@@ -0,0 +1,16 @@
+from .build import build_command_internal
+from ..commands.pdm import build_requirements
+from ..config import OARepoConfig
+from .base import command_sequence, nrp_command
+
+
+@nrp_command.command(name="upgrade")
+@command_sequence()
+def upgrade_command(*, config: OARepoConfig, **kwargs):
+    """Upgrades the repository.
+
+    Resolves the newest applicable packages, downloads them and rebuilds the repository.
+    """
+    return (
+        build_requirements,
+    ) + build_command_internal(config=config)
