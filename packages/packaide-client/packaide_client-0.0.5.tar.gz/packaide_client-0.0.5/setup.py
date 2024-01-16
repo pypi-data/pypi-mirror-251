3fbd78d9bb80256f50447fcb7e7323b7d1f06b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:34 +0000

    Add package packaide-client/0.0.5/packaide_client-0.0.5.tar.gz

---
+++ packages/packaide-client/packaide_client-0.0.5.tar.gz/packaide_client-0.0.5/setup.py
@@ -0,0 +1,25 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+setup(
+    name="packaide_client",  # Required
+    version="0.0.5",  # Required
+    description="A client for accessing a packaideServer instance",  # Optional
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),  # Required
+    python_requires=">=3.7, <4",
+    install_requires=[
+        "pydantic",
+        "requests"
+    ],
+)
