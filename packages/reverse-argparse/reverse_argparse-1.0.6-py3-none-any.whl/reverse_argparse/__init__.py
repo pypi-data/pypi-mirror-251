9e3b12de80f61e020227b1070e25600fe4de091b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:39 +0000

    Add package reverse-argparse/1.0.6/reverse_argparse-1.0.6-py3-none-any.whl

---
+++ packages/reverse-argparse/reverse_argparse-1.0.6-py3-none-any.whl/reverse_argparse/__init__.py
@@ -0,0 +1,10 @@
+"""
+The ``reverse_argparse`` package.
+
+Provide the :class:`ReverseArgumentParser` class and
+:func:`quote_arg_if_necessary` helper function.
+"""
+from .reverse_argparse import ReverseArgumentParser, quote_arg_if_necessary
+
+__all__ = ["ReverseArgumentParser", "quote_arg_if_necessary"]
+__version__ = "1.0.6"
