2cb237a6bfac99bd6b92a8b7e47f2ba9ad75719c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:52 +0000

    Add package jsonschema-gentypes/2.4.0/jsonschema_gentypes-2.4.0-py3-none-any.whl

---
+++ packages/jsonschema-gentypes/jsonschema_gentypes-2.4.0-py3-none-any.whl/jsonschema_gentypes/jsonschema_draft_2019_09_content.py
@@ -0,0 +1,16 @@
+"""
+Automatically generated file from a JSON schema.
+"""
+
+
+from typing import TypedDict, Union
+
+ContentVocabularyMetaSchema = Union["_ContentVocabularyMetaSchemaObject", bool]
+""" Content vocabulary meta-schema. """
+
+
+class _ContentVocabularyMetaSchemaObject(TypedDict, total=False):
+    contentMediaType: str
+    contentEncoding: str
+    contentSchema: "ContentVocabularyMetaSchema"
+    """ Content vocabulary meta-schema. """
