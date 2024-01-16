7d243e08823acb6bbddd2a7658c777e25f7daa62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:03:53 +0000

    Add package p8n-importer/1.0.0/p8n-importer-1.0.0.tar.gz

---
+++ packages/p8n-importer/p8n-importer-1.0.0.tar.gz/p8n-importer-1.0.0/P8nImporter/utilities/importer.py
@@ -0,0 +1,15 @@
+from ..formats import COCOImporter, VOCImporter, YOLOv8Importer, ImageClassificationImporter, TabularImporter
+
+def setup_importer(format, source_folder, output_folder):
+    if format == "coco_json":
+        return COCOImporter(source_folder, output_folder)
+    elif format == "voc":
+        return VOCImporter(source_folder, output_folder)
+    elif format == "yolov8":
+        return YOLOv8Importer(source_folder, output_folder)
+    elif format == "image_classification":
+        return ImageClassificationImporter(source_folder, output_folder)
+    elif format == "tabular":
+        return TabularImporter(source_folder, output_folder)
+    else:
+        raise ValueError("Format not supported")
