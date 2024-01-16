3ac04cdf9b07996a4599219b66dc5f3e36f3d90c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:04:12 +0000

    Add package Geode-Common/30.0.11rc1/Geode_Common-30.0.11rc1-cp38-cp38-win_amd64.whl

---
+++ packages/Geode-Common/Geode_Common-30.0.11rc1-cp38-cp38-win_amd64.whl/geode_common/modifier_solid.py
@@ -0,0 +1,8 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+
+from .bin.geode_common_py_modifier_solid import *
+CommonModifierSolidLibrary.initialize()
