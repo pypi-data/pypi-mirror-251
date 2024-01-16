8c74516d828def5a380611eaf5d956cddded7a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:03:52 +0000

    Add package circadian_scp_upload/0.4.4/circadian_scp_upload-0.4.4.tar.gz

---
+++ packages/circadian_scp_upload/circadian_scp_upload-0.4.4.tar.gz/circadian_scp_upload-0.4.4/circadian_scp_upload/client.py
@@ -0,0 +1,302 @@
+from __future__ import annotations
+from typing import Any, Callable, Literal
+import datetime
+import glob
+import math
+import fabric.connection
+import fabric.transfer
+import os
+import shutil
+import filelock
+import invoke
+import re
+import circadian_scp_upload
+
+
+class RemoteConnection:
+    def __init__(self, host: str, username: str, password: str) -> None:
+        self.host = host
+        self.username = username
+        self.password = password
+        self.connection = fabric.connection.Connection(
+            f"{self.username}@{self.host}",
+            connect_kwargs={"password": self.password},
+            connect_timeout=5,
+        )
+        self.transfer_process = fabric.transfer.Transfer(self.connection)
+
+    def __enter__(self) -> RemoteConnection:
+        self.connection.open()
+        assert self.connection.is_connected, "could not open the ssh connection"
+        return self
+
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        self.connection.close()
+
+
+class DailyTransferClient:
+    def __init__(
+        self,
+        remote_connection: RemoteConnection,
+        src_path: str,
+        dst_path: str,
+        remove_files_after_upload: bool,
+        variant: Literal["directories", "files"],
+        callbacks: circadian_scp_upload.
+        UploadClientCallbacks = circadian_scp_upload.UploadClientCallbacks(),
+    ) -> None:
+        self.src_path = src_path.rstrip("/")
+        self.dst_path = dst_path.rstrip("/")
+        self.remove_files_after_upload = remove_files_after_upload
+        self.remote_connection = remote_connection
+        assert self.remote_connection.transfer_process.is_remote_dir(
+            self.dst_path
+        ), f'remote "{self.dst_path}" is not a directory'
+        self.variant = variant
+        self.callbacks = callbacks
+
+    def __directory_checksums_match(self, dir_name: str) -> bool:
+        """Use `hashlib` to generate a checksum for the local and the
+        remote directory. The remote checksum will be calculated by
+        copying a script to the remote server and executing it there.
+
+        This script requires the server to have Python 3.10 installed
+        and will raise an exception if its not present."""
+
+        file_regex = "^.*$"
+        local_checksum = circadian_scp_upload.checksum.get_dir_checksum(
+            os.path.join(self.src_path, dir_name), file_regex
+        )
+        local_script_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "checksum.py"
+        )
+        remote_dir_path = f"{self.dst_path}/{dir_name}"
+        remote_script_path = f"{self.dst_path}/checksum.py"
+        self.callbacks.log_info(
+            f'Copying checksum script to remote server at path "{remote_script_path}"'
+        )
+        self.remote_connection.transfer_process.put(
+            local_script_path, remote_script_path
+        )
+
+        try:
+            self.remote_connection.connection.run(
+                "python3.10 --version", hide=True, in_stream=False
+            )
+        except invoke.exceptions.UnexpectedExit:
+            raise Exception("python3.10 is not installed on the server")
+
+        try:
+            remote_command = f'python3.10 {remote_script_path} --file_regex "{file_regex}" "{remote_dir_path}"'
+            a: invoke.runners.Result = self.remote_connection.connection.run(
+                remote_command, hide=True, in_stream=False
+            )
+            assert a.exited == 0
+            remote_checksum = a.stdout.strip()
+            assert isinstance(remote_checksum, str)
+        except (invoke.exceptions.UnexpectedExit, AssertionError) as e:
+            raise Exception(
+                f"could not execute remote command on server ({remote_command}): {e}"
+            )
+
+        return local_checksum == remote_checksum
+
+    def __upload_date_directory(
+        self, date: datetime.date, dir_name: str
+    ) -> Literal["successful", "failed", "aborted", "no files found"]:
+        """Perform the whole upload process for a given directory.
+
+        1. If the respective remote directory doesn't exist, create it
+        2. Determine which files have not been uploaded yet
+        4. Upload every file that is found locally but has not been uploaded yet
+        5. Test whether the checksums of "files on server" and "local ifgs"
+           are equal, raise an exception (and end the function) if they differ
+        6. Remove the remote meta file
+        7. Optionally remove local ifgs"""
+
+        log_info: Callable[
+            [str],
+            None] = lambda msg: self.callbacks.log_info(f"{date}: {msg}")
+        log_error: Callable[
+            [str],
+            None] = lambda msg: self.callbacks.log_error(f"{date}: {msg}")
+
+        src_dir_path = os.path.join(self.src_path, dir_name)
+        dst_dir_path = f"{self.dst_path}/{dir_name}"
+        log_info(
+            f"starting to upload local directory '{src_dir_path}'" +
+            f" to remote directory '{dst_dir_path}'"
+        )
+
+        meta = circadian_scp_upload.utils.UploadMeta.init(
+            src_dir_path=src_dir_path
+        )
+
+        # determine files present in src and dst directory
+        # files should be named like "<anything>YYYYMMDD<anything>"
+        src_files = set([
+            f for f in os.listdir(src_dir_path)
+            if f not in ["upload-meta.json", ".do-not-touch"]
+        ])
+        log_info(f"found {len(src_files)} files in src directory")
+
+        # quit if no src files are found
+        if len(src_files) == 0:
+            log_info("directory is empty")
+            if self.remove_files_after_upload:
+                shutil.rmtree(src_dir_path)
+                log_info("finished removing source")
+            else:
+                log_info("skipped removal of source")
+            return "no files found"
+
+        # determine file differences between src and dst
+        files_missing_in_dst = src_files.difference(set(meta.uploaded_files))
+        log_info(f"found {len(files_missing_in_dst)} files for this date")
+
+        # possibly create remote directory
+        if not self.remote_connection.transfer_process.is_remote_dir(
+            dst_dir_path
+        ):
+            self.remote_connection.connection.run(f"mkdir -p {dst_dir_path}")
+            assert self.remote_connection.transfer_process.is_remote_dir(
+                dst_dir_path
+            )
+            log_info(f"created remote directory")
+
+        # logging progress every 10%
+        max_file_count_characters = len(str(len(src_files)))
+        log_progress: Callable[[float], None] = lambda fraction: log_info(
+            f"{int(fraction * 100):3d} % " +
+            f"({len(meta.uploaded_files):{max_file_count_characters}d}/{len(src_files)})"
+            + f" uploaded {'(finished)' if fraction == 1 else ''}"
+        )
+
+        # locking the directory both locally and remote
+        with circadian_scp_upload.utils.TwinFileLock(
+            src_dir_path,
+            dst_dir_path,
+            self.remote_connection.connection,
+            log_info=self.callbacks.log_info
+        ):
+            progress: float = len(meta.uploaded_files) / len(src_files)
+
+            # upload every file that is missing in the remote
+            # meta but present in the local directory
+            for f in sorted(files_missing_in_dst):
+                self.remote_connection.transfer_process.put(
+                    os.path.join(src_dir_path, f), f"{dst_dir_path}/{f}"
+                )
+                meta.uploaded_files.append(f)
+                meta.dump()
+                new_progress = len(meta.uploaded_files) / len(src_files)
+                if math.floor(new_progress * 10) != math.floor(progress * 10):
+                    log_progress(progress)
+                    if self.callbacks.should_abort_upload():
+                        return "aborted"
+                progress = new_progress
+
+            log_progress(1)
+
+            # raise an exception if the checksums do not match
+            if not self.__directory_checksums_match(dir_name):
+                log_error("checksums do not match")
+                return "failed"
+            else:
+                log_info("checksums match")
+
+        # only remove src if configured and checksums match
+        if self.remove_files_after_upload:
+            shutil.rmtree(src_dir_path)
+            log_info("finished removing source")
+        else:
+            log_info("skipped removal of source")
+
+        return "successful"
+
+    def __upload_date_files(
+        self, date: datetime.date
+    ) -> Literal["successful", "failed"]:
+        meta = circadian_scp_upload.utils.UploadMeta.init(
+            src_dir_path=self.src_path
+        )
+
+        # determine file differences between src and dst
+        file_regex = date.strftime(self.callbacks.dated_regex)
+        src_files = set([
+            f for f in os.listdir(self.src_path) if re.match(file_regex, f)
+        ])
+        files_missing_in_dst = src_files.difference(set(meta.uploaded_files))
+        self.callbacks.log_info(
+            f"{date}: found {len(files_missing_in_dst)} files for this date"
+        )
+
+        # locking the directory both locally and remote
+        with circadian_scp_upload.utils.TwinFileLock(
+            self.src_path,
+            self.dst_path,
+            self.remote_connection.connection,
+            log_info=self.callbacks.log_info
+        ):
+            # upload every file that is missing in the remote
+            # meta but present in the local directory
+            for f in sorted(files_missing_in_dst):
+                self.remote_connection.transfer_process.put(
+                    os.path.join(self.src_path, f),
+                    f"{self.dst_path}/{f}",
+                )
+                if self.remove_files_after_upload:
+                    os.remove(os.path.join(self.src_path, f))
+                meta.uploaded_files.append(f)
+                meta.dump()
+
+        return "successful"
+
+    def block_if_process_is_already_running(self) -> None:
+        """Checks whether any filelock is locked in the source directory.
+        Raises an exception if this is the case because this would mean
+        that another upload process is currently running on that source
+        directory."""
+
+        for do_not_touch_filepath in glob.glob(
+            os.path.join(self.src_path, "**", ".do-not-touch"), recursive=True
+        ) + glob.glob(os.path.join(self.src_path, ".do-not-touch")):
+            if filelock.FileLock(do_not_touch_filepath).is_locked:
+                raise Exception(
+                    f"path is used by another upload process: " +
+                    f"filelock at {do_not_touch_filepath} is locked"
+                )
+
+    def run(self) -> None:
+        self.block_if_process_is_already_running()
+        src_dates = circadian_scp_upload.utils.get_src_dates(
+            self.src_path, self.variant, self.callbacks.dated_regex
+        )
+        self.callbacks.log_info(
+            f"Searching for dates in {self.src_path} using " +
+            f"the regex {self.callbacks.dated_regex}"
+        )
+        self.callbacks.log_info(
+            f"Found {len(src_dates)} date(s) to be uploaded: {src_dates}"
+        )
+
+        for date, paths in src_dates.items():
+            if self.variant == "directories":
+                self.callbacks.log_info(
+                    f"{date}: found {len(paths)} paths for this date: {paths}"
+                )
+                for path in paths:
+                    result = self.__upload_date_directory(
+                        date, os.path.basename(path)
+                    )
+                    self.callbacks.log_info(f"{date}: done ({result})")
+                    if result == "aborted":
+                        break
+
+            elif self.variant == "files":
+                result = self.__upload_date_files(date)
+                self.callbacks.log_info(f"{date}: done ({result})")
+
+            if self.callbacks.should_abort_upload():
+                self.callbacks.log_info("Aborting upload")
+                break
---
+++ packages/circadian_scp_upload/circadian_scp_upload-0.4.4.tar.gz/circadian_scp_upload-0.4.4/circadian_scp_upload/utils.py
@@ -0,0 +1,242 @@
+from __future__ import annotations
+from typing import Any, Callable, List, Literal, Optional
+import json
+import os
+import re
+import datetime
+import pydantic
+import filelock
+import fabric.connection
+
+
+def filename_is_ambiguous_for_dated_regex(regex: str, filename: str) -> bool:
+    """Returns true if the filename matches the dated regex more than once.
+
+    This happens when this filename could have been produced on more than one
+    date with respect to the dated regex. For example, if the dated regex is
+    `^.*%Y%m%d.*$`, the filename `log-2020111111.txt` is ambiguous because
+    it could have been produced on 2020-11-11 or 2011-11-11."""
+
+    substrings = ([filename[0 : i] for i in range(1, len(filename))] +
+                  [filename[i :] for i in range(1, len(filename))] + [filename])
+    trimmed_regex = regex[regex.index("("): regex.rindex(")") + 1]
+    trimmed_regex = trimmed_regex.replace("(", "").replace(")", "")
+
+    matches: list[str] = []
+
+    for substring in substrings:
+        new_matches = re.findall(trimmed_regex, substring)
+        assert isinstance(new_matches, list)
+        assert all(isinstance(m, str) for m in new_matches)
+        matches += new_matches
+
+    return len(set(matches)) > 1
+
+
+def file_or_dir_name_to_date(
+    file_or_dir_name: str,
+    dated_regex: str,
+) -> Optional[datetime.date]:
+    """Converts a string to a date based on a dated regex.
+
+    Sample input for `string`: "2021-01-01"
+    Sample input for `dated_regex`: "%Y-%m-%d" """
+
+    # only consider dates after at least 1
+    # hour of the following day has passed
+    now = datetime.datetime.now()
+    latest_date = ((now - datetime.timedelta(days=1)) if (now.hour > 0) else
+                   (now - datetime.timedelta(days=2))).date()
+
+    keys = list(sorted(["%Y", "%m", "%d"], key=lambda x: dated_regex.index(x)))
+
+    regex = dated_regex
+    for old, new in {
+        "%Y": r"(\d{4})",
+        "%m": r"(\d{2})",
+        "%d": r"(\d{2})",
+    }.items():
+        regex = regex.replace(old, new)
+
+    if filename_is_ambiguous_for_dated_regex(regex, file_or_dir_name):
+        raise ValueError()
+
+    matches = re.findall(regex, file_or_dir_name)
+    if len(matches) == 0:
+        return None
+    assert len(matches) == 1
+    match = matches[0]
+    assert isinstance(match, tuple)
+    assert len(match) == 3
+    try:
+        date = datetime.datetime.strptime(
+            f"{match[0]}-{match[1]}-{match[2]}", "-".join(keys)
+        ).date()
+        return date if (date <= latest_date) else None
+    except ValueError:
+        return None
+
+
+def get_src_dates(
+    src_path: str,
+    variant: Literal["directories", "files"],
+    dated_regex: str,
+) -> dict[datetime.date, list[str]]:
+    """For a given src path and dated regex,returns"""
+
+    dates: dict[datetime.date, list[str]] = {}
+
+    if not os.path.isdir(src_path):
+        raise Exception(f'path "{src_path}" is not a directory')
+
+    ambiguous_paths: list[str] = []
+
+    for file_or_dir_name in os.listdir(src_path):
+        file_or_dir_path = os.path.join(src_path, file_or_dir_name)
+
+        try:
+            if variant == "directories":
+                consider_path = os.path.isdir(file_or_dir_path)
+            else:
+                consider_path = os.path.isfile(file_or_dir_path)
+            if consider_path:
+                date = file_or_dir_name_to_date(file_or_dir_name, dated_regex)
+                if date is not None:
+                    if date not in dates:
+                        dates[date] = []
+                    dates[date].append(file_or_dir_path)
+        except ValueError:
+            ambiguous_paths.append(file_or_dir_path)
+
+    if len(ambiguous_paths) > 0:
+        raise ValueError(
+            f"The following {variant} match the regex `{repr(dated_regex)}` but cannot be "
+            +
+            f"parsed with parsed into a valid date because the result is ambiguous (can "
+            + "be produced on more than one date): [\n" +
+            ",\n".join(ambiguous_paths) + "\n]"
+        )
+
+    return dates
+
+
+class UploadMeta(pydantic.BaseModel):
+    src_dir_path: str
+    uploaded_files: List[str]
+
+    def dump(self) -> None:
+        """dumps the meta object to a JSON file"""
+        with open(
+            os.path.join(self.src_dir_path, "upload-meta.json"), "w"
+        ) as f:
+            json.dump(self.model_dump(exclude={"src_dir_path"}), f, indent=4)
+
+    @staticmethod
+    def init(src_dir_path: str) -> UploadMeta:
+        src_dir_path = src_dir_path.rstrip("/")
+
+        if os.path.isfile(os.path.join(src_dir_path, "upload-meta.json")):
+            try:
+                with open(
+                    os.path.join(src_dir_path, "upload-meta.json"), "r"
+                ) as f:
+                    meta = UploadMeta(src_dir_path=src_dir_path, **json.load(f))
+            except (
+                FileNotFoundError,
+                TypeError,
+                json.JSONDecodeError,
+                pydantic.ValidationError,
+            ):
+                raise Exception("could not load local upload-meta.json")
+        else:
+            meta = UploadMeta(src_dir_path=src_dir_path, uploaded_files=[])
+
+        return meta
+
+
+class UploadClientCallbacks(pydantic.BaseModel):
+    """A collection of callbacks passed to the upload client."""
+
+    model_config = pydantic.ConfigDict(extra="forbid")
+
+    dated_regex: str = pydantic.Field(
+        default=r"^.*" + "%Y%m%d" + r".*$",
+        description=
+        "Which files/directories to consider in the upload process. The patterns `%Y`/`%m`/`%d` represent the date at which the file was generated. Any string containing some pattern except for these four will raise an exception. `%Y` = 4-digit year, `%m` = 2-digit month, `%d` = 2-digit day (all fixed width, i.e. zero-padded).",
+    )
+
+    @pydantic.field_validator("dated_regex", mode="before")
+    @classmethod
+    def _validate_dated_regex(cls, v: str) -> str:
+        checks: list[tuple[bool, str]] = [
+            ("%Y" in v, "string must contain `%Y`"),
+            ("%m" in v, "string must contain `%m`"),
+            ("%d" in v, "string must contain `%d`"),
+            (v.count("%") == 3, "string must contain exactly 3 `%` characters"),
+            ("(" not in v, "string must not contain `(`"),
+            (")" not in v, "string must not contain `)`"),
+            (v.startswith("^"), "string must start with `^`"),
+            (v.endswith("$"), "string must end with `$`"),
+        ]
+        error_message = "; ".join([m for (c, m) in checks if not c])
+        if len(error_message) > 0:
+            raise ValueError(f"value `{repr(v)}` is invalid: {error_message}")
+        return v
+
+    log_info: Callable[[str], None] = pydantic.Field(
+        default=(lambda msg: print(f"INFO - {msg}")),
+        description="Function to be called when logging a message or type INFO.",
+    )
+    log_error: Callable[[str], None] = pydantic.Field(
+        default=(lambda msg: print(f"ERROR - {msg}")),
+        description=
+        "Function to be called when logging a message or type ERROR.",
+    )
+    should_abort_upload: Callable[[], bool] = pydantic.Field(
+        default=(lambda: False),
+        description=
+        "Can be used to interrupt the upload process. This function will be run between each file or directory and (when uploading large directories), after every 25 files. If it returns true, then the upload process will be aborted.",
+    )
+
+
+class TwinFileLock:
+    def __init__(
+        self,
+        local_dir_path: str,
+        remote_dir_path: str,
+        remote_connection: fabric.connection.Connection,
+        log_info: Optional[Callable[[str], None]] = None,
+    ):
+        self.src_filepath = os.path.join(local_dir_path, ".do-not-touch")
+        self.dst_filepath = f"{remote_dir_path}/.do-not-touch"
+
+        self.src_filelock = filelock.FileLock(self.src_filepath)
+        self.remote_connection = remote_connection
+        self.log_info = log_info
+
+    def __enter__(self) -> None:
+        if self.log_info is not None:
+            self.log_info(
+                f'acquiring lock on local machine at "{self.src_filepath}"'
+            )
+        self.src_filelock.acquire(timeout=0)
+        if self.log_info is not None:
+            self.log_info(
+                f'acquiring lock on remote server at "{self.dst_filepath}"'
+            )
+        self.remote_connection.run(f"touch {self.dst_filepath}")
+
+    def __exit__(self, exc_type: Any, exc_value: Any, traceback: Any) -> None:
+        if self.log_info is not None:
+            self.log_info(
+                f'releasing lock on remote server at "{self.dst_filepath}"'
+            )
+        self.remote_connection.run(f"rm -rf {self.dst_filepath}")
+
+        if self.log_info is not None:
+            self.log_info(
+                f'releasing lock on local machine at "{self.src_filepath}"'
+            )
+        self.src_filelock.release()
+        if os.path.isfile(self.src_filepath):
+            os.remove(self.src_filepath)
