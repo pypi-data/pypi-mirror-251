53391caefc237e85b082eb4637720a0baa160877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 15:04:32 +0000

    Add package pycaptcha-guard/0.1.7/pycaptcha-guard-0.1.7.tar.gz

---
+++ packages/pycaptcha-guard/pycaptcha-guard-0.1.7.tar.gz/pycaptcha-guard-0.1.7/pycaptcha_guard/common_components/constants.py
@@ -0,0 +1,8 @@
+WAIT_TIMEOUT = 5
+
+NOPECHA_API_KEY = "NOPECHA_API_KEY"
+GOOGLE_RECAPTCHA_PAGE_URL = "le.com/recaptcha/api2/demo"
+
+
+CAPTCHA_TYPE_RECAPTCHA = 'recaptcha'
+CAPTCHA_TYPE_TEXTCAPTCHA = 'textcaptcha'
