821e1623d672d95672c5c94a8cd590c4f5406277
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:42 +0000

    Add package data-helpers/0.1.20/data_helpers-0.1.20.tar.gz

---
+++ packages/data-helpers/data_helpers-0.1.20.tar.gz/data_helpers-0.1.20/data_helpers/meta_type.py
@@ -0,0 +1,12 @@
+"""A meta type for adding additional info in the type of a variable."""
+
+from dataclasses import dataclass
+
+@dataclass
+class FieldType:
+    label: str
+    type: type
+    default: None
+
+
+
