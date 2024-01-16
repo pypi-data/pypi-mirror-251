1493c094576a4e49f71ec311a6ac8cc099e9fe00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:13 +0000

    Add package libwebview/1.0.3/libwebview-1.0.3-cp311-cp311-win_amd64.whl

---
+++ packages/libwebview/libwebview-1.0.3-cp311-cp311-win_amd64.whl/libwebview/tests/test_app.py
@@ -0,0 +1,21 @@
+from libwebview import App
+from datetime import datetime
+
+app = App(
+    app_name="test_app_python",
+    title="Test App", 
+    size=(800, 600), 
+    resizeable=False
+)
+app.set_max_size((800, 600))
+
+@app.route
+def app_close():
+    app.quit()
+
+@app.route
+def get_message():
+    app.emit('update_time', datetime.now().strftime('%d-%m-%Y'))
+    return "Привет из libwebview!"
+
+app.run("resources/index.html")
