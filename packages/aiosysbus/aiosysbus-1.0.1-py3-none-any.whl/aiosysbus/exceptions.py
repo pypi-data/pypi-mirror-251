9cf50e7fe1878620851dd49148a315013a79579f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:03:54 +0000

    Add package aiosysbus/1.0.1/aiosysbus-1.0.1-py3-none-any.whl

---
+++ packages/aiosysbus/aiosysbus-1.0.1-py3-none-any.whl/aiosysbus/exceptions.py
@@ -0,0 +1,33 @@
+"""Exceptions for livebox."""
+
+
+class AiosysbusException(Exception):
+    """General exception."""
+
+
+class NotOpenError(AiosysbusException):
+    """Not open url."""
+
+
+class AuthenticationFailed(AiosysbusException):
+    """Authentication error."""
+
+
+class TimeoutExceededError(AiosysbusException):
+    """Timeout exceeded."""
+
+
+class HttpRequestFailed(AiosysbusException):
+    """Requests error."""
+
+
+class InsufficientPermissionsError(HttpRequestFailed):
+    """Insufficient Permissions."""
+
+
+class RetrieveFailed(HttpRequestFailed):
+    """Retrieve exception."""
+
+
+class UnexpectedResponse(HttpRequestFailed):
+    """Unexpected response."""
