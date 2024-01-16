713fbbd0a58726f89524a85efd6614e98c37f737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:04:41 +0000

    Add package ert/8.4.0b0/ert-8.4.0b0-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/ert/ert-8.4.0b0-cp311-cp311-macosx_11_0_arm64.whl/ert/config/summary_config.py
@@ -0,0 +1,50 @@
+from __future__ import annotations
+
+import logging
+from dataclasses import dataclass
+from datetime import datetime
+from typing import TYPE_CHECKING, Set, Union
+
+import xarray as xr
+
+from ._read_summary import read_summary
+from .response_config import ResponseConfig
+
+if TYPE_CHECKING:
+    from typing import List
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class SummaryConfig(ResponseConfig):
+    input_file: str
+    keys: List[str]
+    refcase: Union[Set[datetime], List[str], None] = None
+
+    def __post_init__(self) -> None:
+        if isinstance(self.refcase, list):
+            self.refcase = {datetime.fromisoformat(val) for val in self.refcase}
+
+    def read_from_file(self, run_path: str, iens: int) -> xr.Dataset:
+        filename = self.input_file.replace("<IENS>", str(iens))
+        keys, time_map, data = read_summary(f"{run_path}/{filename}", self.keys)
+
+        if self.refcase:
+            assert isinstance(self.refcase, set)
+            missing = self.refcase.difference(time_map)
+            if missing:
+                first, last = min(missing), max(missing)
+                logger.warning(
+                    f"Realization: {iens}, load warning: {len(missing)} "
+                    f"inconsistencies in time map, first: Time mismatch for response "
+                    f"time: {first}, last: Time mismatch for response time: "
+                    f"{last} from: {run_path}/{filename}.UNSMRY"
+                )
+
+        ds = xr.Dataset(
+            {"values": (["name", "time"], data)},
+            coords={"time": time_map, "name": keys},
+        )
+        return ds.drop_duplicates("time")
