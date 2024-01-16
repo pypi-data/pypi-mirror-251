7183686e0ea0cca089df13d67e5c53d4de84b2fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:04:25 +0000

    Add package PyMieSim/1.3.0/PyMieSim-1.3.0-cp312-cp312-win_amd64.whl

---
+++ packages/PyMieSim/PyMieSim-1.3.0-cp312-cp312-win_amd64.whl/docs/examples/experiment/plot_sphere_a1_vs_diameter.py
@@ -0,0 +1,48 @@
+"""
+Sphere: A1 scattering coefficient
+===================================
+
+"""
+
+
+# %%
+# Importing the package dependencies: numpy, PyMieSim
+import numpy as np
+from PyMieSim.experiment import SphereSet, SourceSet, Setup
+from PyMieSim import measure
+
+# %%
+# Defining the ranging parameters for the scatterer distribution
+scatterer_set = SphereSet(
+    diameter=np.linspace(100e-9, 10000e-9, 1000),
+    index=1.4,
+    n_medium=1
+)
+
+# %%
+# Defining the source to be employed.
+source_set = SourceSet(
+    wavelength=400e-9,
+    linear_polarization=0,
+    optical_power=1e-3,
+    NA=0.2
+)
+
+# %%
+# Defining the experiment setup
+experiment = Setup(
+    scatterer_set=scatterer_set,
+    source_set=source_set
+)
+
+# %%
+# Measuring the properties
+data = experiment.Get(measure.a1)
+
+# %%
+# Plotting the results
+figure = data.plot(
+    x=scatterer_set.diameter
+)
+
+_ = figure.show()
