41c3dc055df33740af8ed6caa88192d11be03ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:02:59 +0000

    Add package rps-milea-users/0.1.2/rps-milea-users-0.1.2.tar.gz

---
+++ packages/rps-milea-users/rps-milea-users-0.1.2.tar.gz/rps-milea-users-0.1.2/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys
+
+from setuptools import setup
+
+with open('README.rst', 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name='rps-milea-users',
+    version='0.1.2',
+    author='red-pepper-services',
+    author_email='pypi@schiegg.at',
+    url='https://github.com/milea-framework/milea-users',
+    license='MIT',
+    description="Django rps.Milea Framework - User Module",
+    long_description=long_description,
+    include_package_data=True,
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
