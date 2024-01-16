e373b3badba04afe3bb85d1d9535bda23729a5f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:03:33 +0000

    Add package cloudpss/4.1.1a10/cloudpss-4.1.1a10.tar.gz

---
+++ packages/cloudpss/cloudpss-4.1.1a10.tar.gz/cloudpss-4.1.1a10/test/testSend.py
@@ -0,0 +1,39 @@
+import os,sys
+import time
+sys.path.append(os.path.join(os.path.dirname(__file__), '..\\'))
+import cloudpss 
+import logging
+
+def next(m):
+    m.write({'type': 'debug', 'step': '-1'})
+def goto(m,step):
+    m.write({'type': 'debug', 'step': step})
+def pause(m):
+    m.write({'type': 'debug', 'step': 10})
+
+
+def memory(m,path,buffer,offset):
+    m.write({'type': 'memory', 'path': path,'value':buffer,'offset':offset})
+    
+
+    
+if __name__ == '__main__':
+    # logging.basicConfig(level=logging.DEBUG)
+    os.environ['CLOUDPSS_API_URL'] = 'http://10.101.10.45/'
+    cloudpss.setToken('eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJhZG1pbiIsInNjb3BlcyI6WyJtb2RlbDo5ODMzNSIsImZ1bmN0aW9uOjk4MzM1IiwiYXBwbGljYXRpb246OTgzMzUiXSwicm9sZXMiOlsiYWRtaW4iXSwidHlwZSI6ImFwcGx5IiwiZXhwIjoxNzI2MjA4NjM5LCJub3RlIjoidGVzdDIiLCJpYXQiOjE2OTUxMDQ2Mzl9.ZlFSPCH3u4OSBlDBJWve4GZkWIZAku_DI1j-or-uDtAtyDy-RZecn7RBhymsILNPsmKnwkXcx1UnsjRNiAf5yA')
+    # job = cloudpss.Job.fetch()
+    
+    model = cloudpss.Model.fetch('model/admin/shm_test_2')
+    
+    jobConfig = model.jobs[0]
+    jobConfig['args']['stop_on_entry']='1'
+    modelJob =model.run(job=jobConfig)
+    
+    view =modelJob.result
+    i=0
+    while not view.end:
+        i+=1
+        print(view.getLogs())
+        view.next()
+        view.writeShm('flag2',float(i),0)
+        time.sleep(0.01)
