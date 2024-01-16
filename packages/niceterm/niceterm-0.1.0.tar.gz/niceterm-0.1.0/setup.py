979bbee28793043ead81e09df302f6c46cfc58fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:03:03 +0000

    Add package niceterm/0.1.0/niceterm-0.1.0.tar.gz

---
+++ packages/niceterm/niceterm-0.1.0.tar.gz/niceterm-0.1.0/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name='niceterm',
+    version='0.1.0',
+    py_modules=['niceterm'],
+    install_requires=[
+        # List any dependencies your package may have
+    ],
+)
