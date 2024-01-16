58b1cc35f776aed76626a96cf04b3588a5e479ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:04:05 +0000

    Add package suffersync/1.4.4/suffersync-1.4.4.tar.gz

---
+++ packages/suffersync/suffersync-1.4.4.tar.gz/suffersync-1.4.4/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='suffersync',
+    version='1.4.4',
+    description='Syncs workouts from Wahoo SYSTM to intervals.icu',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    url='https://github.com/bakermat/suffersync',
+    author='Bakermat',
+    author_email='',
+    license='MIT',
+    py_modules=['suffersync'],
+    install_requires=[
+        'requests>=2.26'
+    ],
+    entry_points={
+        'console_scripts': [
+            'suffersync=suffersync:main',
+        ],
+    },
+)
