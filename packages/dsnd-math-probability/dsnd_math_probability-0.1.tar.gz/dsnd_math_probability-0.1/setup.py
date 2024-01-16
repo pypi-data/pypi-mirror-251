24d22311ea630d3242289d9d985abaac237b15a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 03:03:28 +0000

    Add package dsnd-math-probability/0.1/dsnd_math_probability-0.1.tar.gz

---
+++ packages/dsnd-math-probability/dsnd_math_probability-0.1.tar.gz/dsnd_math_probability-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dsnd_math_probability',
+      version='0.1',
+      description='Gaussian and Binomial Distributions',
+      packages=['dsnd_math_probability'],
+      zip_safe=False,
+      author = 'Kawalpreet Deol',
+      author_email = ''
+     )
