f1fc96da204534c54a159ed3ba21832fa7b8b2dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 15:03:42 +0000

    Add package python-homeassistant-analytics/0.4.0/python_homeassistant_analytics-0.4.0-py3-none-any.whl

---
+++ packages/python-homeassistant-analytics/python_homeassistant_analytics-0.4.0-py3-none-any.whl/python_homeassistant_analytics/models.py
@@ -0,0 +1,28 @@
+"""Models for Homeassistant Analytics."""
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+from mashumaro import field_options
+from mashumaro.mixins.orjson import DataClassORJSONMixin
+
+
+@dataclass
+class CurrentAnalytics(DataClassORJSONMixin):
+    """CurrentAnalytics model."""
+
+    integrations: dict[str, int]
+
+
+@dataclass
+class Analytics(DataClassORJSONMixin):
+    """Analytics model."""
+
+    current: CurrentAnalytics = field(metadata=field_options(alias="current"))
+
+
+@dataclass
+class Integration(DataClassORJSONMixin):
+    """Integration model."""
+
+    title: str
