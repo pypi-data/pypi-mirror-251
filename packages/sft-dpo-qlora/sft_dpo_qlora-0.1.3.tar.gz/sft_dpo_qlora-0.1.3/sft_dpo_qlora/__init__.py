465f1b6012ca08c4667602828ccfb30d2594b254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:04:17 +0000

    Add package sft-dpo-qlora/0.1.3/sft_dpo_qlora-0.1.3.tar.gz

---
+++ packages/sft-dpo-qlora/sft_dpo_qlora-0.1.3.tar.gz/sft_dpo_qlora-0.1.3/sft_dpo_qlora/__init__.py
@@ -0,0 +1,5 @@
+from sft_dpo_qlora.configSFT import sftConfig
+from sft_dpo_qlora.configDPO import dpoConfig
+from sft_dpo_qlora.trainerSFT import sftTrainer
+from sft_dpo_qlora.trainerDPO import dpoTrainer
+
