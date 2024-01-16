953c9f0a1f35e533484ce4380a6e42e88030b8ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 04:03:26 +0000

    Add package magicsoup/1.0.5/magicsoup-1.0.5-cp312-none-win_amd64.whl

---
+++ packages/magicsoup/magicsoup-1.0.5-cp312-none-win_amd64.whl/magicsoup/examples/wood_ljungdahl.py
@@ -0,0 +1,61 @@
+"""
+using https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2646786/
+
+Methyl (Eastern) Branch:
+formate dehydrogenase (1.2.1.43): CO2 + NADPH -> formiat + NADP (- 21.5 kJ/mol)
+formate-tetrahydrofolate ligase (6.3.4.3): formiat + FH4 + ATP -> formyl-FH4 + ADP + Pi (-8.4 kJ/mol)
+methenyltetrahydrofolate cyclohydrolase (3.5.4.9): formyl-FH4 + NADPH -> methylen-FH4 + NADP (-40.2 kJ/mol)
+methylenetetrahydrofolate reductase (1.1.99.15): methylen-FH4 + NADPH -> methyl-FH4 + NADP (-39.2 kJ/mol)
+
+Carbonyl (Western) Branch:
+CFeSP methyltransferase (2.1.1.X): methyl-FH4 + Ni-ACS -> FH4 + methyl-Ni-ACS
+CO dehydrogenase / acetyl-CoA synthetase: CO2 + HS-CoA + methyl-Ni-ACS -> Ni-ACS + acetyl-CoA
+(skipped Co step)
+"""
+from magicsoup.containers import Molecule, Chemistry
+
+NADPH = Molecule("NADPH", 200.0 * 1e3)
+NADP = Molecule("NADP", 100.0 * 1e3)
+ATP = Molecule("ATP", 100.0 * 1e3)
+ADP = Molecule("ADP", 70.0 * 1e3)
+
+methylFH4 = Molecule("methyl-FH4", 360.0 * 1e3)
+methylenFH4 = Molecule("methylen-FH4", 300.0 * 1e3)
+formylFH4 = Molecule("formyl-FH4", 240.0 * 1e3)
+FH4 = Molecule("FH4", 200.0 * 1e3)
+formiat = Molecule("formiat", 20.0 * 1e3)
+co2 = Molecule("CO2", 10.0 * 1e3, diffusivity=1.0, permeability=1.0)
+
+NiACS = Molecule("Ni-ACS", 200.0 * 1e3)
+methylNiACS = Molecule("methyl-Ni-ACS", 300.0 * 1e3)
+HSCoA = Molecule("HS-CoA", 200.0 * 1e3)
+acetylCoA = Molecule("acetyl-CoA", 260.0 * 1e3)
+
+
+MOLECULES = [
+    NADPH,
+    NADP,
+    ATP,
+    ADP,
+    methylFH4,
+    methylenFH4,
+    formylFH4,
+    FH4,
+    formiat,
+    co2,
+    NiACS,
+    methylNiACS,
+    HSCoA,
+    acetylCoA,
+]
+
+REACTIONS = [
+    ([co2, NADPH], [formiat, NADP]),  # -90k
+    ([formiat, FH4, ATP], [formylFH4, ADP]),  # -10k
+    ([formylFH4, NADPH], [methylenFH4, NADP]),  # -40k
+    ([methylenFH4, NADPH], [methylFH4, NADP]),  # -40k
+    ([methylFH4, NiACS], [FH4, methylNiACS]),  # -60k
+    ([methylNiACS, co2, HSCoA], [NiACS, acetylCoA]),  # -50k
+]
+
+CHEMISTRY = Chemistry(molecules=MOLECULES, reactions=REACTIONS)
---
+++ packages/magicsoup/magicsoup-1.0.5-cp312-none-win_amd64.whl/magicsoup/mutations.py
@@ -0,0 +1,51 @@
+from magicsoup import _lib  # type: ignore
+
+
+def point_mutations(
+    seqs: list[str], p: float = 1e-6, p_indel: float = 0.4, p_del: float = 0.66
+) -> list[tuple[str, int]]:
+    """
+    Add point mutations to a list of base pair sequences.
+    Mutations are substitutions and indels.
+
+    Arguments:
+        seqs: base pair sequences
+        p: probability of a mutation per base pair
+        p_indel: probability of any point mutation being an indel
+                 (inverse probability of it being a substitution)
+        p_del: probability of any indel being a deletion
+               (inverse probability of it being an insertion)
+
+    Returns:
+        List of mutated sequences and their indices from `seqs`.
+
+    The returned list only contains sequences which experienced at least one mutation.
+    The new sequences are returned together with their `seqs` index.
+    E.g. `("...", 5)` means `seqs[5]` was mutated and the resulting sequence is
+    in the tuple.
+    """
+    return _lib.point_mutations(seqs, p, p_indel, p_del)
+
+
+def recombinations(
+    seq_pairs: list[tuple[str, str]], p: float = 1e-7
+) -> list[tuple[str, str, int]]:
+    """
+    Add random recombinations to pairs of base pair sequences.
+    The recombination happens by creating random strand breaks in the input sequence pairs
+    and randomly re-joining them.
+
+    Arguments:
+        seq_pairs: base pair sequence pairs
+        p: probability of a strand break per base pair
+
+    Returns:
+        List of mutated sequence pairs and their indices.
+
+    The returned list only contains sequence pairs that experienced a recombination.
+    The new sequence paris are returned and in addition to that their `seq_pairs` index.
+    E.g. if it's 5, it means `seq_pairs[5]` was mutated and the resulting sequences are in this 3-tuple.
+    """
+    if len(seq_pairs) == 0:
+        return []
+    return _lib.recombinations(seq_pairs, p)
