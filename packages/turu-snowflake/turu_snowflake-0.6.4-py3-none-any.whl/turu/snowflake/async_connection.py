3d919cc7e8350bc3f3c1d2849f2404ca20e268fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:04:25 +0000

    Add package turu-snowflake/0.6.4/turu_snowflake-0.6.4-py3-none-any.whl

---
+++ packages/turu-snowflake/turu_snowflake-0.6.4-py3-none-any.whl/turu/snowflake/async_connection.py
@@ -0,0 +1,133 @@
+import os
+from pathlib import Path
+from typing import Any, Optional, Sequence, Tuple, Type
+
+import turu.core.async_connection
+import turu.core.cursor
+import turu.core.mock
+from typing_extensions import Never, Unpack, override
+
+import snowflake.connector
+
+from .async_cursor import AsyncCursor, ExecuteOptions, MockAsyncCursor
+
+
+class AsyncConnection(turu.core.async_connection.AsyncConnection):
+    def __init__(self, connection: snowflake.connector.SnowflakeConnection):
+        self._raw_connection = connection
+
+    async def close(self) -> None:
+        self._raw_connection.close()
+
+    async def commit(self) -> None:
+        self._raw_connection.commit()
+
+    async def rollback(self) -> None:
+        self._raw_connection.rollback()
+
+    async def cursor(self) -> AsyncCursor[Never]:
+        return AsyncCursor(self._raw_connection.cursor())
+
+    async def execute(
+        self,
+        operation: str,
+        parameters: Optional[Any] = None,
+        /,
+        **options: Unpack[ExecuteOptions],
+    ) -> AsyncCursor[Tuple[Any]]:
+        return await (await self.cursor()).execute(operation, parameters, **options)
+
+    async def executemany(
+        self,
+        operation: str,
+        seq_of_parameters: Sequence[Any],
+        /,
+        **options: Unpack[ExecuteOptions],
+    ) -> AsyncCursor[Tuple[Any]]:
+        return await (await self.cursor()).executemany(
+            operation, seq_of_parameters, **options
+        )
+
+    async def execute_map(
+        self,
+        row_type: Type[turu.core.cursor.GenericNewRowType],
+        operation: str,
+        parameters: Optional[Any] = None,
+        /,
+        **options: Unpack[ExecuteOptions],
+    ) -> AsyncCursor[turu.core.cursor.GenericNewRowType]:
+        return await (await self.cursor()).execute_map(
+            row_type,
+            operation,
+            parameters,
+            **options,
+        )
+
+    async def executemany_map(
+        self,
+        row_type: Type[turu.core.cursor.GenericNewRowType],
+        operation: str,
+        seq_of_parameters: Sequence[Any],
+        /,
+        **options: Unpack[ExecuteOptions],
+    ) -> AsyncCursor[turu.core.cursor.GenericNewRowType]:
+        return await (await self.cursor()).executemany_map(
+            row_type,
+            operation,
+            seq_of_parameters,
+            **options,
+        )
+
+
+class MockAsyncConnection(AsyncConnection, turu.core.mock.MockAsyncConnection):
+    def __init__(self, *args, **kwargs):
+        turu.core.mock.MockAsyncConnection.__init__(self)
+
+    @override
+    async def cursor(self) -> "MockAsyncCursor[Never]":
+        return MockAsyncCursor(self._turu_mock_store)
+
+
+async def connect_async(
+    connection_name: Optional[str] = None,
+    connections_file_path: Optional[Path] = None,
+    **kwargs,
+) -> AsyncConnection:
+    return AsyncConnection(
+        snowflake.connector.SnowflakeConnection(
+            connection_name,
+            connections_file_path,
+            **kwargs,
+        )
+    )
+
+
+async def connect_async_from_env(
+    connection_name: Optional[str] = None,
+    connections_file_path: Optional[Path] = None,
+    user_envname: str = "SNOWFLAKE_USER",
+    password_envname: str = "SNOWFLAKE_PASSWORD",
+    account_envname: str = "SNOWFLAKE_ACCOUNT",
+    database_envname: str = "SNOWFLAKE_DATABASE",
+    schema_envname: str = "SNOWFLAKE_SCHEMA",
+    warehouse_envname: str = "SNOWFLAKE_WAREHOUSE",
+    role_envname: str = "SNOWFLAKE_ROLE",
+    **kwargs,
+) -> AsyncConnection:
+    if (
+        authenticator := os.environ.get("SNOWFLAKE_AUTHENTICATOR")
+    ) and "authenticator" not in kwargs:
+        kwargs["authenticator"] = authenticator
+
+    return await connect_async(
+        connection_name,
+        connections_file_path,
+        user=os.environ.get(user_envname, kwargs.get("user")),
+        password=os.environ.get(password_envname, kwargs.get("password")),
+        account=os.environ.get(account_envname, kwargs.get("account")),
+        database=os.environ.get(database_envname, kwargs.get("database")),
+        schema=os.environ.get(schema_envname, kwargs.get("schema")),
+        warehouse=os.environ.get(warehouse_envname, kwargs.get("warehouse")),
+        role=os.environ.get(role_envname, kwargs.get("role")),
+        **kwargs,
+    )
