712d9745e3b55b229d0bf9c2423b2c53310b89d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 14:03:39 +0000

    Add package envdatareader/1.3/envdatareader-1.3-py3-none-any.whl

---
+++ packages/envdatareader/envdatareader-1.3-py3-none-any.whl/envdatareader/envdatareader.py
@@ -0,0 +1,17 @@
+from dotenv import dotenv_values
+
+class EnvDataReader:
+	def __init__(self, *, file_path=".env"):
+		self.file_path = file_path
+		self.variables = self.load_variables()
+
+	def load_variables(self):
+		return dotenv_values(self.file_path)			
+
+	def get_value(self, key, *, default=None) -> str:
+		value = self.variables.get(key)
+		return value if value is not None and value != '' else default
+	
+	def group_get(self, keys)->dict:
+		values = {key: self.get_value(key) for key in keys}
+		return values
