e7b0fb35f90a2d2ee9902b44172d083b1ba5b4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 05:02:56 +0000

    Add package ottoman/0.0.1/ottoman-0.0.1.tar.gz

---
+++ packages/ottoman/ottoman-0.0.1.tar.gz/ottoman-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='ottoman',
+    version='0.0.1',
+    description='ottoman',
+    author='LM Studio',
+    author_email='team@lmstudio.ai',
+    zip_safe=False
+)
