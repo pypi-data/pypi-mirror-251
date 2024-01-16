583066bc33500b475539d0c4f8a99d8c05f21294
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-16 15:04:31 +0000

    Add package herbarium/0.23.2/herbarium-0.23.2-py3-none-any.whl

---
+++ packages/herbarium/herbarium-0.23.2-py3-none-any.whl/herbarium/queue_navigator.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from typing import Protocol
+
+from rich import box, print
+from rich.panel import Panel
+
+from .git_stager import StagingMigrater
+from .subprocess_utils import interactive_cmd, shell_output
+
+
+class QueueNavigator(Protocol):
+    def go_to_front(self):
+        ...
+
+    def go_to_back(self):
+        ...
+
+    def move_forward_one(self):
+        ...
+
+    def move_back_one(self):
+        ...
+
+    def status(self):
+        ...
+
+
+@dataclass(frozen=True)
+class GraphiteNavigator(QueueNavigator):
+    def go_to_front(self):
+        with StagingMigrater():
+            interactive_cmd("gt trunk")
+
+    def go_to_back(self):
+        with StagingMigrater():
+            interactive_cmd("gt top")
+
+    def move_forward_one(self):
+        interactive_cmd("gt down")
+
+    def move_back_one(self):
+        interactive_cmd("gt up")
+
+    def status(self):
+        result: str = shell_output("gt log short")  # type: ignore
+        print(
+            "\n",
+            Panel(result, title="Back", subtitle="Front", expand=False, box=box.HORIZONTALS),
+            "\n",
+        )
