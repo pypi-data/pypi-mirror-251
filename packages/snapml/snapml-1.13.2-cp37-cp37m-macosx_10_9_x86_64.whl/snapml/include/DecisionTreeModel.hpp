8bb6e777de0e502edf4102e8cbcf5e279d1fb35b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:04:49 +0000

    Add package snapml/1.13.2/snapml-1.13.2-cp37-cp37m-macosx_10_9_x86_64.whl

---
+++ packages/snapml/snapml-1.13.2-cp37-cp37m-macosx_10_9_x86_64.whl/snapml/include/DecisionTreeModel.hpp
@@ -0,0 +1,35 @@
+/*********************************************************************
+ *
+ * Licensed Materials - Property of IBM
+ *
+ * (C) Copyright IBM Corp. 2022, 2023. All Rights Reserved.
+
+ * US Government Users Restricted Rights - Use, duplication or
+ * disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+ *
+ ********************************************************************/
+
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <stdint.h>
+
+namespace tree {
+struct TreeModel;
+}
+
+namespace snapml {
+
+//! @ingroup c-api
+class DecisionTreeModel {
+public:
+    DecisionTreeModel();
+    void get(std::vector<uint8_t>& vec);
+    void put(const std::vector<uint8_t>& vec);
+
+protected:
+    std::shared_ptr<tree::TreeModel> model_;
+};
+
+}
