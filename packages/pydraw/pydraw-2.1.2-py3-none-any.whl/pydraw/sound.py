63c43f8363f65911a52bf43ca58cca2a733e9c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 15:05:19 +0000

    Add package pydraw/2.1.2/pydraw-2.1.2-py3-none-any.whl

---
+++ packages/pydraw/pydraw-2.1.2-py3-none-any.whl/pydraw/sound.py
@@ -0,0 +1,13 @@
+from pydraw.errors import PydrawError
+class Sound:
+    def __int__(self, path: str, autoplay: bool = False, loop: bool = False):
+        raise PydrawError('Sound class is not yet implemented due to issues with Python!')
+
+    def play(self) -> None:
+        """
+        Plays the sound file at the path registered
+        :return: None
+        """
+        raise PydrawError('Sound class is not yet implemented due to issues with Python!')
+
+
