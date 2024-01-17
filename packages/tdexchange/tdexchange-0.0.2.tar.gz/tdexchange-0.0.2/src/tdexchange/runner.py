b4aea9ce4ea3a4cd94c36c61447f6f45963dee39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:04:20 +0000

    Add package tdexchange/0.0.2/tdexchange-0.0.2.tar.gz

---
+++ packages/tdexchange/tdexchange-0.0.2.tar.gz/tdexchange-0.0.2/src/tdexchange/runner.py
@@ -0,0 +1,25 @@
+import time
+
+from tdexchange.bot import RegularBot, MarketPrice, PairPrice
+
+
+def typical():
+    bots = []
+    price = MarketPrice()
+    pair = PairPrice(price)
+    for i in range(1, 5):
+        name = f'bot-0{i}'
+        bot = RegularBot(name, name, price, pair)
+        bot.run()
+        bots.append(bot)
+
+    try:
+        while True:
+            time.sleep(1 / 25)
+    except KeyboardInterrupt:
+        print('ctrl-c encountered, exiting')
+    finally:
+        for bot in bots:
+            bot.stop()
+
+        exit()
