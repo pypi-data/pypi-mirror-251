c374dc1d2261ba52382d532ebacdd8e4f400fc72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 14:04:50 +0000

    Add package TheSilent/0.0.869/TheSilent-0.0.869.tar.gz

---
+++ packages/TheSilent/TheSilent-0.0.869.tar.gz/TheSilent-0.0.869/src/TheSilent/dolphin.py
@@ -0,0 +1,25 @@
+import asyncio
+
+ports = []
+
+async def bottlenose(host, port):
+    global ports
+
+    try:
+        reader, writer = await asyncio.wait_for(asyncio.open_connection(host, port),10)
+        ports.append(port)
+        writer.close()
+
+    except:
+        pass
+
+async def pink_dolphin(host):
+    tasks = []
+    for port in range(1,65535):
+        tasks.append(bottlenose(host, port))
+
+    await asyncio.gather(*tasks)
+
+def dolphin(host):
+    asyncio.run(pink_dolphin(host))
+    return ports
