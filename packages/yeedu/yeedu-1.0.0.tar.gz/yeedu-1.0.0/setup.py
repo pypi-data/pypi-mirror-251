81eb6362df29c2a55b41e6ebc2f9aec24b2e5c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:39 +0000

    Add package yeedu/1.0.0/yeedu-1.0.0.tar.gz

---
+++ packages/yeedu/yeedu-1.0.0.tar.gz/yeedu-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yeedu',
+    packages=find_packages(),
+    install_requires=[
+        'argparse==1.4.0',
+        'requests==2.28.1',
+        'python-dotenv==1.0.0',
+        'PyYAML==6.0',
+        'setuptools==59.6.0'
+    ],
+    version='1.0.0',
+    entry_points='''
+    [console_scripts]
+    yeedu=yeedu:yeedu
+    '''
+)
