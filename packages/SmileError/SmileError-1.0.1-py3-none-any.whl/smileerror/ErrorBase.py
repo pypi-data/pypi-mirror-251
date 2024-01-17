d7e23af191ed25faac8b7da8e49e10057f142d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 15:05:49 +0000

    Add package SmileError/1.0.1/SmileError-1.0.1-py3-none-any.whl

---
+++ packages/SmileError/SmileError-1.0.1-py3-none-any.whl/smileerror/ErrorBase.py
@@ -0,0 +1,73 @@
+"""
+Author: masakokh
+Year: 2022
+Version: 1.0.1
+Note:
+Package: base class
+"""
+# built-in
+# internal
+
+
+class ErrorBase:
+	"""
+
+	"""
+
+	def __init__(self, defaultValue: bool= False):
+		"""
+
+		"""
+		self.__code     = 0
+		self.__found	= defaultValue
+		self.__message	= ''
+
+	def findCode(self, code: int) -> bool:
+		"""
+
+		:param code:
+		:return:
+		"""
+		return bool(self.__code == code)
+
+	def getCode(self) -> int:
+		"""
+
+		:return:
+		"""
+		return self.__code
+
+	def getMessage(self) -> str:
+		"""
+
+		:return:
+		"""
+		return self.__message
+
+	def isTrue(self) -> bool:
+		"""
+
+		:return:
+		"""
+		return self.__found
+
+	def setFalse(self) -> None:
+		"""
+
+		:return:
+		"""
+		self.__code     = 0
+		self.__found	= False
+		self.__message	= ''
+
+	def setTrue(self, code: int= 0, message: str= '') -> None:
+		"""
+
+		:param code:
+		:param message:
+		:return:
+		"""
+		self.__code	= code
+		self.__found	= True
+		self.__message	= message
+
