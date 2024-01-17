e2a965b1068e1c5c440f78c5d8a753c5be04ddb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:05:52 +0000

    Add package orchestrator-core/2.0.1rc1/orchestrator_core-2.0.1rc1-py3-none-any.whl

---
+++ packages/orchestrator-core/orchestrator_core-2.0.1rc1-py3-none-any.whl/orchestrator/services/products.py
@@ -0,0 +1,52 @@
+# Copyright 2019-2020 SURF.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from typing import List, Union
+from uuid import UUID
+
+from more_itertools.more import one
+from sqlalchemy.orm import joinedload
+
+from orchestrator.db import ProductTable
+from orchestrator.types import UUIDstr
+
+
+def get_product_by_id(product_id: Union[UUID, UUIDstr], join_fixed_inputs: bool = True) -> ProductTable:
+    """Get product by id.
+
+    Args:
+        product_id: ProductTable id uuid
+        join_fixed_inputs: whether to join the fixed inputs in the same query or not
+
+    Returns: ProductTable object
+
+    """
+    if not join_fixed_inputs:
+        return ProductTable.query.get(product_id)
+
+    return ProductTable.query.options(joinedload(ProductTable.fixed_inputs)).get(product_id)
+
+
+def get_product_by_name(name: str) -> ProductTable:
+    return ProductTable.query.options(joinedload(ProductTable.fixed_inputs)).filter(ProductTable.name == name).one()
+
+
+def get_types() -> List[str]:
+    return list(
+        map(one, ProductTable.query.distinct(ProductTable.product_type).with_entities(ProductTable.product_type))
+    )
+
+
+def get_tags() -> List[str]:
+    return list(map(one, ProductTable.query.distinct(ProductTable.tag).with_entities(ProductTable.tag)))
