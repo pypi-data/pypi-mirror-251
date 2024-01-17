8856a685be9d4ec777ff104de64e36be0263e2e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:14 +0000

    Add package rpsl-lexer/0.2.0/rpsl_lexer-0.2.0.tar.gz

---
+++ packages/rpsl-lexer/rpsl_lexer-0.2.0.tar.gz/rpsl_lexer-0.2.0/rpsl_lexer/aut_num.py
@@ -0,0 +1,32 @@
+import sys
+
+from .dump import parse_mp_import
+from .lines import expressions
+from .piped import stdin_lines, write_obj
+from .rpsl_object import AutNum
+
+
+def parse_aut_num():
+    imports: dict[str, dict[str, list[dict]]] = {}
+    exports: dict[str, dict[str, list[dict]]] = {}
+    for key, expr in expressions(stdin_lines()):
+        if key == "import":
+            parse_mp_import(expr, imports)
+        elif key == "mp-import":
+            parse_mp_import(expr, imports, is_mp=True)
+        elif key == "export":
+            parse_mp_import(expr, exports)
+        elif key == "mp-export":
+            parse_mp_import(expr, exports, is_mp=True)
+    return AutNum("", "", imports, exports).__dict__
+
+
+def main():
+    print("Launching aut_num lexer.", file=sys.stderr)
+
+    while True:
+        aut_num = parse_aut_num()
+        write_obj(aut_num)
+
+
+main() if __name__ == "__main__" else None
---
+++ packages/rpsl-lexer/rpsl_lexer-0.2.0.tar.gz/rpsl_lexer-0.2.0/rpsl_lexer/rpsl_object.py
@@ -0,0 +1,55 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class RPSLObject:
+    closs: str
+    name: str
+    body: str
+
+
+@dataclass
+class AutNum:
+    """aut-num class."""
+
+    name: str
+    body: str
+    imports: dict[str, dict[str, list[dict]]]
+    exports: dict[str, dict[str, list[dict]]]
+
+
+@dataclass
+class AsSet:
+    """as-set class."""
+
+    name: str
+    body: str
+    members: list[str]
+
+
+@dataclass
+class RouteSet:
+    """route-set class."""
+
+    name: str
+    body: str
+    members: list[str]
+    """members and mp-members."""
+
+
+@dataclass
+class PeeringSet:
+    """peering-set class."""
+
+    name: str
+    body: str
+    peerings: list[dict]
+
+
+@dataclass
+class FilterSet:
+    """filter-set class."""
+
+    name: str
+    body: str
+    filters: list[dict]
