bb8b3a59e4c09610f2b85b3ce1bf966b6feea230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 07:03:29 +0000

    Add package musify/2024.1.2/musify-2024.1.2-py3-none-any.whl

---
+++ packages/musify/musify-2024.1.2-py3-none-any.whl/musify/shared/remote/types.py
@@ -0,0 +1,6 @@
+from collections.abc import MutableMapping
+from typing import Any
+
+from musify.shared.types import UnitMutableSequence
+
+APIMethodInputType = UnitMutableSequence[str] | UnitMutableSequence[MutableMapping[str, Any]]
