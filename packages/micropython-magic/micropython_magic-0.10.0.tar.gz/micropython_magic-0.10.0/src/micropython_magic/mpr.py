d8cf2f4125c6d5c48146c1d91216a9f91251562a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:09 +0000

    Add package micropython-magic/0.10.0/micropython_magic-0.10.0.tar.gz

---
+++ packages/micropython-magic/micropython_magic-0.10.0.tar.gz/micropython_magic-0.10.0/src/micropython_magic/mpr.py
@@ -0,0 +1,213 @@
+""" Micropython Remote (MPR) magic for Jupyter Notebooks
+"""
+
+import contextlib
+import json
+import sys
+import tempfile
+from pathlib import Path
+from typing import List, Optional, Union
+
+from IPython.core.interactiveshell import InteractiveShell
+from loguru import logger as log
+
+from micropython_magic.script_access import path_for_script
+
+from .interactive import ipython_run
+
+JSON_START = "<json~"
+JSON_END = "~json>"
+DONT_KNOW = "<~?~>"
+
+import os
+
+from .interactive import TIMEOUT
+
+
+class MPRemote2:
+    def __init__(
+        self,
+        shell: InteractiveShell,
+        port: str = "auto",
+        resume: bool = True,
+    ):
+        self.shell: InteractiveShell = shell
+        self.port: str = port  # by default connect to the first device
+        self.resume = resume  # by default resume the device to maintain state
+        self.timeout = TIMEOUT
+
+    @property
+    def cmd_prefix(self) -> List[str]:
+        """mpremote command prefix including port and resume according to options"""
+        prefix = [sys.executable, "-m", "mpremote"] + self.connect_to
+        if self.resume:
+            prefix.append("resume")
+        return prefix
+
+    @property
+    def connect_to(self) -> List[str]:
+        "Creates mpremote 'connect to string' if port is specified."
+        c = ["connect"]
+        if self.port:
+            c.append(self.port)
+        return c
+
+    def run_cmd(
+        self,
+        cmd: List[str],
+        *,
+        auto_connect: bool = True,
+        stream_out: bool = True,
+        shell=True,
+        timeout: Union[int, float] = 0,
+        follow: bool = True,
+    ):
+        """run a command on the device and return the output"""
+        assert isinstance(cmd, list)
+        if auto_connect:
+            cmd = self.cmd_prefix + cmd
+            # if isinstance(cmd, str):
+            #     cmd = f"""{self.cmd_prefix} {cmd}"""
+            # else:
+            #     log.warning(f"cmd is not a string: {cmd}")
+        log.debug(cmd)
+        return ipython_run(
+            cmd, stream_out=stream_out, shell=shell, timeout=timeout or self.timeout, follow=follow
+        )
+
+        # output = self.shell.getoutput(cmd, split=True)
+        # assert isinstance(output, SList)
+        # if len(output) > 0 and output[0].strip() == "no device found":
+        #     raise ConnectionError("no device found")
+        # return output
+
+    def select_device(self, port: Optional[str], verify: bool = False):
+        """try to select the device to connect to by specifying the serial port name."""
+        _port = port.strip() if port else "auto"
+        if not verify:
+            self.port = _port
+            return _port
+        # cmd = f"""eval \"'{_port}'\""""
+        cmd = ["eval", f"\"'{_port}'\""]
+        try:
+            output = self.run_cmd(cmd)
+            self.port = _port
+        except Exception as e:
+            output = e
+        return output
+
+    def run_cell(
+        self,
+        cell: str,
+        *,
+        timeout: Union[int, float] = TIMEOUT,
+        follow: bool = True,
+        mount: Optional[str] = None,
+    ):
+        """run a codeblock on the device and return the output"""
+        """copy cell to a file and run it on the MCU"""
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".py", delete=False) as f:
+            f.write(
+                "# Jupyter cell\n"
+            )  # add a line to replace the cell magic to keep the line numbers aligned
+            f.write(cell)
+            f.close()
+            # copy the file to the device
+            log.trace(f"copied cell to {f.name}")
+            file_attributes = os.stat(f.name)
+            log.trace(f"{file_attributes=}")
+            run_cmd = ["run", f.name]  # TODO: may need to add quotes around f.name
+            if mount:
+                # prefix the run command with a mount command
+                # run_cmd = f'mount "{Path(mount).as_posix()}" ' + run_cmd
+                run_cmd = ["mount", mount] + run_cmd
+
+            # TODO: detect / retry / report errors copying the file
+            log.trace(f"running {run_cmd}")
+            try:
+                result = self.run_cmd(
+                    run_cmd,
+                    stream_out=True,
+                    timeout=timeout,
+                    follow=follow,
+                )
+                if result:
+                    log.debug(f"result: {result}")
+            except Exception as e:
+                result = e
+
+            finally:
+                Path(f.name).unlink()
+        return result
+
+    def run_mcu_file(
+        self,
+        filename: str,
+        *,
+        stream_out: bool = True,
+        timeout: Union[int, float] = 0,
+        follow: bool = True,
+        mount: Optional[str] = None,
+    ):
+        """run a file on the device and return the output"""
+        exec_cmd = []
+        if mount:
+            exec_cmd = ["mount" + f'"{mount}"']
+        exec_cmd += ["exec", f"\"exec( open('{filename}').read() , globals() )\""]
+        return self.run_cmd(exec_cmd, stream_out=stream_out, timeout=timeout, follow=follow)
+
+    def copy_cell_to_mcu(self, cell, *, filename: str):
+        """copy cell to a file to the MCU"""
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".py", delete=False) as f:
+            f.write(
+                "# Jupyter cell\n"
+            )  # add a line to replace the cell magic to keep the line numbers aligned
+            f.write(cell)
+            f.close()
+            # copy the file to the device
+            copy_cmd = ["cp", f.name, f":{filename}"]
+            # TODO: detect / retry / report errors copying the file
+            _ = self.run_cmd(copy_cmd, stream_out=False, timeout=60)
+            # log.info(_)
+            # log.info(f.name, "copied to device")
+            Path(f.name).unlink()
+
+    def cell_from_mcu_file(self, filename):
+        """read a file from the device and return the contents"""
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".py", delete=False) as f:
+            # copy_cmd = f"cp :{filename} {f.name}"
+            copy_cmd = ["cp", f":{filename}", f.name]
+            # TODO: detect / retry / report errors copying the file
+            _ = self.run_cmd(copy_cmd, stream_out=False, timeout=60)
+
+            return Path(f.name).read_text()
+
+    @staticmethod
+    def load_json_from_MCU(line: str):
+        """try to load the output from the MCU transferred as json"""
+        result = DONT_KNOW
+        if line.startswith(JSON_START) and line.endswith(JSON_END):
+            # remove the json wrapper
+            line = line[7:-7]
+            if line == "none":
+                return None
+            try:
+                result = json.loads(line)
+            except json.JSONDecodeError as e:
+                with contextlib.suppress(Exception):
+                    result = eval(line)
+            except Exception as e:
+                # result = None
+                pass
+        return result
+
+    def get_fw_info(self, timeout: float):
+        fw_info = {}
+        #  load datafile from installed package
+        cmd = ["run", str(path_for_script("fw_info.py"))]
+        if out := self.run_cmd(cmd, stream_out=False, timeout=timeout):
+            if not out[0].startswith("{"):
+                return out
+            fw_info = eval(out[0])
+            fw_info["serial_port"] = self.port
+        return fw_info
---
+++ packages/micropython-magic/micropython_magic-0.10.0.tar.gz/micropython_magic-0.10.0/src/micropython_magic/octarine.py
@@ -0,0 +1,392 @@
+"""
+Run micropython code from a a notebook
+ - connects to the first MCU connected on serial
+ - uses resume to avoid soft-resetting the MCU
+"""
+
+import argparse
+import re
+import sys
+from typing import List, Optional
+
+from colorama import Style
+from IPython.core.error import UsageError
+from IPython.core.interactiveshell import InteractiveShell
+from IPython.core.magic import Magics, cell_magic, line_magic, magics_class, output_can_be_silenced
+from IPython.core.magic_arguments import argument, argument_group, magic_arguments, parse_argstring
+from IPython.utils.text import LSString, SList
+from loguru import logger as log  # type: ignore
+
+from micropython_magic.interactive import TIMEOUT
+from micropython_magic.param_fixup import get_code
+
+from .mpr import DONT_KNOW, JSON_END, JSON_START, MPRemote2
+
+# https://ipython.readthedocs.io/en/stable/config/custommagics.html
+# https://ipython.readthedocs.io/en/stable/api/generated/IPython.core.magic.html#IPython.core.magic.Magics
+# https://nbviewer.org/github/rossant/ipython-minibook/blob/master/chapter6/602-cpp.ipynb
+
+
+class MCUException(Exception):
+    """Exception raised for errors on the MCU."""
+
+    pass
+
+
+def set_log_level(llevel: str):
+    # format_str = "<level>{level: <8}</level> | <cyan>{module: <18}</cyan> - <level>{message}</level>"
+    # format_str = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+    format_str = "<level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
+    log.remove()
+    log.add(sys.stdout, format=format_str, level=llevel, colorize=True)
+
+
+set_log_level("WARNING")
+
+
+class PrettyOutput(object):
+    """"""
+
+    def __init__(self, data):
+        self.data = data
+
+    def __getattr__(self, item):
+        return getattr(self.data, item)
+
+    def __repr__(self):
+        return repr(self.data)
+
+    def _str_(self):
+        return "\n".join(self.data.list)
+
+    def _repr_json_(self):
+        return self.data
+        # return json.dumps(self.data, indent=2)
+
+
+def just_text(output) -> str:
+    """returns the text output of the command"""
+    if isinstance(output, SList):
+        return "\n".join(output.list)
+    elif isinstance(output, LSString):
+        return output
+    else:
+        return str(output)
+
+
+import enum
+
+from traitlets import All
+from traitlets import Bool as Bool_
+from traitlets import Enum as Enum_
+from traitlets import Float as Float_
+from traitlets import HasTraits, Unicode, UseEnum, default, observe, validate
+from traitlets.config.configurable import Configurable
+
+
+class LogLevel(str, enum.Enum):
+    """Log level"""
+
+    TRACE = "TRACE"
+    DEBUG = "DEBUG"
+    INFO = "INFO"
+    WARNING = "WARNING"
+    ERROR = "ERROR"
+
+
+@magics_class
+class MicroPythonMagic(Magics):
+    """A class to define the magic functions for MicroPython."""
+
+    # The default timeout
+    timeout = Float_(TIMEOUT).tag(config=True, sync=True)  # type: ignore
+    # verbose = Bool_(False).tag(config=True, sync=True)  # type: ignore
+    loglevel = UseEnum(LogLevel, default_value=LogLevel.WARNING).tag(config=True)
+    # .tag(config=True, sync=True)  # type: ignore
+
+    def __init__(self, shell: InteractiveShell):
+        # first call the parent constructor
+        super(MicroPythonMagic, self).__init__(shell)
+        self.shell: InteractiveShell
+        self._MCUs: list[MPRemote2] = [MPRemote2(shell)]
+        # self.port: str = "auto"  # by default connect to the first device
+        # self.resume = True  # by default resume the device to maintain state
+
+    @observe("loglevel")
+    def _verbose_changed(self, change):
+        # print(f"{change=}")
+        if change["new"]:
+            set_log_level(change["new"])
+        else:
+            set_log_level(LogLevel.WARNING)
+
+    @property
+    def port(self) -> None:
+        return None
+
+    @property
+    def MCU(self) -> MPRemote2:
+        """Return the first/current/only MCU"""
+        # to allow expansion to multiple MCUs in the future
+        return self._MCUs[0]
+
+    # -------------------------------------------------------------------------
+    # cell magics
+    # -------------------------------------------------------------------------
+
+    @cell_magic("micropython")
+    @cell_magic("mpy")
+    @magic_arguments("%micropython")  # add additional % to display two %% in help
+    #
+    @argument_group("Mount")
+    @argument(
+        "--mount",
+        type=str,
+        help="Host folder to mount on the MCU",
+        metavar="A/FOLDER",
+    )
+    @argument_group("Code execution")
+    @argument(
+        "--writefile",
+        "--save",
+        "-wf",
+        type=str,
+        help="MCU [path/]filename to write to",
+        metavar="PATH/FILE.PY",
+    )
+    @argument(
+        "--readfile",
+        "--load",
+        "-rf",
+        type=str,
+        help="MCU [path/]filename to read from",
+        metavar="PATH/FILE.PY",
+    )
+    @argument(
+        "--new",
+        action="store_true",
+        help="new cell is added after the current cell instead of replacing it",
+    )
+    @argument("--timeout", default=-1, help="maximum timeout for the cell to run")
+    @argument(
+        "--follow",
+        action=argparse.BooleanOptionalAction,
+        default=True,
+        help="follow the output",
+    )
+    #
+    @argument_group("Devices")
+    @argument("--select", nargs="+", help="serial port to connect to", metavar="PORT")
+    @argument(
+        "--reset", "--soft-reset", action="store_true", help="Reset device (before running cell)."
+    )
+    @argument("--hard-reset", action="store_true", help="reset device.")
+    def micropython(self, line: str, cell: str = ""):
+        """
+        Run Micropython code on an attached device using mpremote.
+        """
+        args = parse_argstring(self.micropython, line or "")
+
+        if args.timeout == -1:
+            args.timeout = self.timeout
+        assert isinstance(args.timeout, float)
+
+        if args.select:
+            if len(args.select) > 1:
+                # allow a list of ports to be specified
+                log.warning(f"{args.select=} not yet implemented")
+                return
+            else:
+                self.select(args.select[0])
+
+        if line:
+            log.debug(f"{args=}")
+        # pre processing - these can be combined with the main processing
+
+        if args.hard_reset:
+            self.hard_reset()
+        elif args.reset:
+            self.soft_reset()
+
+        if args.writefile:
+            log.debug(f"{args.writefile=}")
+            if args.new:
+                log.warning(f"{args.new=} not implemented")
+            self.MCU.copy_cell_to_mcu(cell, filename=args.writefile)
+            return
+
+        if args.readfile:
+            log.debug(f"{args.readfile=},{args.new=}")
+            code = self.MCU.cell_from_mcu_file(args.readfile)
+            # if the first line contains a magic command, replace it with this magic command but with the options commented out
+            if code.startswith("# %%"):
+                code = "\n".join(code.split("\n")[1:])
+            code = f"# %%micropython # {line}\n{code}"  # todo - use the same notation for the first line as the original command
+            if args.new:
+                self.shell.set_next_input(code, replace=False)
+            else:
+                self.shell.set_next_input(code, replace=True)
+            return
+
+        if not cell:
+            raise UsageError("Please specify some MicroPython code to execute")
+        log.trace(f"{cell=}")
+        output = self.MCU.run_cell(
+            cell, timeout=args.timeout, follow=args.follow, mount=args.mount
+        )
+        # return PrettyOutput(output)
+
+    # -------------------------------------------------------------------------
+    # line magics
+    # -------------------------------------------------------------------------
+
+    @line_magic("micropython")
+    @line_magic("mpy")
+    @magic_arguments("mpy")
+    @argument_group("Code execution")
+    @argument("statement", nargs="*", help="Micropython code to run.", metavar="STATEMENT(S)")
+    @argument("--eval", "-e", nargs="*", help="Expression to evaluate", metavar="EXPRESSION")
+    @argument("--timeout", default=-1, help="maximum timeout for the cell to run")
+    @argument("--stream", action="store_true", help="stream each line of output as it is received")
+    @argument_group("Devices")
+    @argument("--list", "--devs", "-l", action="store_true", help="List available devices.")
+    @argument("--select", "-s", nargs="+", help="serial port to connect to", metavar="PORT")
+    @argument("--reset", "--soft-reset", action="store_true", help="reset device.")
+    @argument("--hard-reset", action="store_true", help="reset device.")
+    @argument("--info", action="store_true", help="get boardinfo from device")
+    @argument("--bootloader", action="store_true", help="make the device enter its bootloader")
+    @output_can_be_silenced
+    def mpy_line(self, line: str):
+        """
+        Run Micropython code on an attached device using mpremote.
+
+        - can be silenced with a trailing semicolon when used as a line magic
+        """
+        args = parse_argstring(self.mpy_line, line or "")
+        if args.timeout == -1:
+            args.timeout = self.timeout
+        if not isinstance(args.timeout, float):
+            args.timeout = float(args.timeout)  # type: ignore
+
+        # try to fixup the expression after shell and argparse mangled it
+        if args.statement and len(args.statement) >= 1:
+            args.statement = get_code(line, args.statement[0])
+        if args.eval and len(args.eval) >= 1:
+            args.eval = get_code(line, args.eval[0])
+        if isinstance(args.eval, list):
+            args.eval = " ".join(args.eval)
+        if args.hard_reset:  # avoid double resets
+            args.reset = False
+        #
+        if line:
+            log.debug(f"{args=}")
+        # pre processing - these can be combined with the main processing
+        if args.select:
+            if len(args.select) > 1:
+                log.warning(f"{args.select=} for multiple MCUs not yet implemented")
+                return
+            else:
+                self.select(args.select[0])
+        if args.hard_reset:
+            self.hard_reset()
+        elif args.reset:
+            self.soft_reset()
+        elif args.bootloader:
+            self.MCU.run_cmd(["bootloader"])
+
+        # processing
+        if args.list:
+            return self.list_devices()
+        elif args.info:
+            return self.get_fw_info(args.timeout)
+
+        elif args.eval:
+            return self.eval(args.eval)
+
+        elif args.statement:
+            # Assemble the command to run
+            statement = "\n".join(args.statement)
+            cmd = ["exec", statement]
+            log.debug(f"{cmd=}")
+
+            return self.MCU.run_cmd(
+                cmd,
+                stream_out=bool(args.stream),
+                timeout=float(args.timeout),
+            )
+
+    # -------------------------------------------------------------------------
+    # worker methods - these are called by the magics
+    # -------------------------------------------------------------------------
+
+    def list_devices(self) -> Optional[SList]:
+        """
+        Return a SList or list of the Micropython devices connected to the computer through serial ports or USB.
+        """
+        cmd = ["mpremote", "connect", "list"]
+        # output = self.shell.getoutput(cmd)
+        output = self.MCU.run_cmd(cmd, auto_connect=False, stream_out=False)
+        return output
+
+    def select(self, port: Optional[str]):
+        """
+        Select the device to connect to by specifying the serial port name.
+        """
+        device = port.strip() if port else "auto"
+        output = self.MCU.select_device(device)
+        return output
+
+    def eval(self, line: str):
+        """
+        Run a Micropython expression on an attached device using mpremote.
+        Note that the expression
+         * can only be a single expression
+         * it cannot contain  comments or newlines.
+
+        Runs the statement on the MCU and tries to convert the output to a python object.
+        If that fails it returns the raw output as a string.
+        """
+        # Assemble the command to run
+        statement = line.strip()
+        cmd_old = (
+            f'''exec "import json; print('{JSON_START}',json.dumps({statement}),'{JSON_END}')"'''
+        )
+        cmd = [
+            "exec",
+            # f'''"import json; print('{JSON_START}',json.dumps({statement}),'{JSON_END}')"''',
+            f"""import json; print('{JSON_START}',json.dumps({statement}),'{JSON_END}')""",
+        ]
+        log.trace(repr(cmd))
+        output = self.MCU.run_cmd(cmd, stream_out=False)
+        if isinstance(output, SList):
+            matchers = [r"^.*Error:", r"^.*Exception:"]
+            for ln in output.l:
+                # check for errors and raise them
+                if any(re.match(m, ln) for m in matchers):
+                    raise MCUException(ln) from eval(ln.split(":")[0])
+                # check for json output and try to convert it
+                if ln.startswith(JSON_START) and ln.endswith(JSON_END):
+                    result = self.MCU.load_json_from_MCU(ln)
+                    if result != DONT_KNOW:
+                        return result
+        return output
+
+    def soft_reset(self):
+        """
+        Perform a soft-reset on the current Micropython device.
+        """
+        # Append an eval statement to avoid ending up in the repl
+        output = self.MCU.run_cmd(["soft-reset", "eval", "True"])
+        self.output = output
+        return just_text(output)
+
+    def hard_reset(self):
+        """
+        Perform a hard-reset on the current Micropython device.
+        """
+        output = self.MCU.run_cmd(["reset"])
+        self.output = output
+        return output
+
+    def get_fw_info(self, timeout: float):
+        return self.MCU.get_fw_info(timeout)
