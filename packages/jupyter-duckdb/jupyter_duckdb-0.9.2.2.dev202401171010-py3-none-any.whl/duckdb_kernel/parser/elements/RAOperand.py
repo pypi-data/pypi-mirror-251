8410822350d28d1aecd383eb2b27d2dd85fa2035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 17:03:53 +0000

    Add package jupyter-duckdb/0.9.2.2.dev202401171010/jupyter_duckdb-0.9.2.2.dev202401171010-py3-none-any.whl

---
+++ packages/jupyter-duckdb/jupyter_duckdb-0.9.2.2.dev202401171010-py3-none-any.whl/duckdb_kernel/parser/elements/RAOperand.py
@@ -0,0 +1,31 @@
+from typing import Dict, Tuple, Iterator
+
+from duckdb_kernel.db import Table
+from .RAElement import RAElement
+from ..util.RenamableColumnList import RenamableColumnList
+
+
+class RAOperand(RAElement):
+    def __init__(self, relation: str):
+        self.relation: str = relation
+
+    def __str__(self, indent: int = 0) -> str:
+        return f'{super().__str__(indent)}: {self.relation}'
+
+    @property
+    def name(self) -> str:
+        return self.relation
+
+    @property
+    def children(self) -> Iterator[RAElement]:
+        return
+        yield
+
+    def to_sql(self, tables: Dict[str, Table]) -> Tuple[str, RenamableColumnList]:
+        if self.relation not in tables:
+            raise AssertionError(f'unknown relation {self.relation}')
+
+        cols = RenamableColumnList.from_iter(tables[self.name].columns)
+        column_names = ', '.join(c.rename() for c in cols)
+
+        return f'SELECT DISTINCT {column_names} FROM {self.relation}', cols
