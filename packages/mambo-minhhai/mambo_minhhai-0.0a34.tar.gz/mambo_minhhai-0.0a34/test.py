22d6ec5d7d4553a8e18549d50839147a8416bfb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:36 +0000

    Add package mambo-minhhai/0.0a34/mambo_minhhai-0.0a34.tar.gz

---
+++ packages/mambo-minhhai/mambo_minhhai-0.0a34.tar.gz/mambo_minhhai-0.0a34/test.py
@@ -0,0 +1,9 @@
+#  %% Import
+if __name__ == "__main__":
+    import src.mambo_minhhai as mambo
+    from src.mambo_minhhai import utils as utils
+    from src.mambo_minhhai.utils import display_utils
+    import src.mambo_minhhai.utils.display_utils as display_utils
+    print("Successfully import the mambo package !!")
+
+# %%
