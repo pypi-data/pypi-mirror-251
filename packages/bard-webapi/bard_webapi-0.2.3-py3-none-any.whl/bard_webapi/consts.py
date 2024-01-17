57db5274e9bc7c02db80f51c8aac39da8f4c9205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 14:04:45 +0000

    Add package bard-webapi/0.2.3/bard_webapi-0.2.3-py3-none-any.whl

---
+++ packages/bard-webapi/bard_webapi-0.2.3-py3-none-any.whl/bard_webapi/consts.py
@@ -0,0 +1,10 @@
+HEADERS = {
+    "Accept": "*/*",
+    "Host": "bard.google.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+    "Origin": "https://bard.google.com",
+    "Referer": "https://bard.google.com/",
+    "Connection": "keep-alive",
+    "X-Same-Domain": "1",
+}
