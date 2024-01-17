97119cc2abd2d0382cb3ca1b9442a4766a0ee132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:29 +0000

    Add package jgtutils/0.1.19/jgtutils-0.1.19.tar.gz

---
+++ packages/jgtutils/jgtutils-0.1.19.tar.gz/jgtutils-0.1.19/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jgtutils',
+    version='0.1.19',
+    packages=find_packages(),
+    description='A utility package common to other JGT projects.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Guillaume Isabelle',
+    author_email='jgi@jgwill.com',
+    url='https://github.com/jgwill/jgtutils',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],    
+    #entry_points={
+    #    'console_scripts': ['fxcli2console=jgtutils.fxcli2console:main'],
+    #},
+    keywords='utilities',
+    project_urls={
+        'Bug Reports': 'https://github.com/jgwill/jgtutils/issues',
+        'Source': 'https://github.com/jgwill/jgtutils',
+    },
+)
