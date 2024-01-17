4a289e8c91aa9b50c7258358e3edc17e9ca67577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:04:41 +0000

    Add package numerous.sdk/1.3.0/numerous.sdk-1.3.0-py3-none-any.whl

---
+++ packages/numerous.sdk/numerous.sdk-1.3.0-py3-none-any.whl/numerous/sdk/models/project.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from typing import Any
+
+from .parameter import Parameter, get_parameters_dict
+from numerous.grpc.spm_pb2_grpc import FileManagerStub
+
+
+@dataclass
+class Project:
+    id: str
+    name: str
+    parameters: dict[str, Parameter]
+
+    @staticmethod
+    def from_document(
+        data: dict[str, Any], file_manager_client: FileManagerStub
+    ) -> "Project":
+        return Project(
+            id=data["id"],
+            name=data["name"],
+            parameters=get_parameters_dict(
+                data.get("parameters", []), file_manager_client
+            ),
+        )
