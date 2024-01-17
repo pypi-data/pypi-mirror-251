93ba5c289a3f90a1c61f8c0f1b152e774929ddad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:04:16 +0000

    Add package delitoolkit/0.3.8/delitoolkit-0.3.8-py2.py3-none-any.whl

---
+++ packages/delitoolkit/delitoolkit-0.3.8-py2.py3-none-any.whl/delitoolkit/delidata.py
@@ -0,0 +1,22 @@
+import csv
+import os
+
+
+
+class DeliData:
+    def __init__(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        files = os.listdir(dir_path + '/delidata_corpus/')
+        print(dir_path)
+        print(files)
+        self.corpus = {}
+        for f in files:
+            if f == '__init__.py' or f == "__pycache__":
+                continue
+            with open(dir_path + '/delidata_corpus/' + f, 'r', encoding="utf8") as rf:
+                a = [{k: v for k, v in row.items()} for row in
+                     csv.DictReader(rf, delimiter='\t', skipinitialspace=True)]
+                self.corpus[f] = a
+
+
+
