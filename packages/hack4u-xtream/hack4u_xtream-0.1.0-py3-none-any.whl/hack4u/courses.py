63a5e576dd9abf664681fb807b2cdc3ec4185580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 14:04:36 +0000

    Add package hack4u-xtream/0.1.0/hack4u_xtream-0.1.0-py3-none-any.whl

---
+++ packages/hack4u-xtream/hack4u_xtream-0.1.0-py3-none-any.whl/hack4u/courses.py
@@ -0,0 +1,28 @@
+class Course:
+
+    def __init__(self, name, duration, link):
+        self.name = name
+        self.duration = duration
+        self.link = link
+
+    def __repr__(self):
+
+        return f"El curso: {self.name} tiene una duración de {self.duration} horas y su link es: {self.link}"
+
+courses = [
+        Course("Introducción a Linux", 15, "https://hack4u.io/cursos/introduccion-a-linux/"), 
+        Course("Personalización de Linux", 3, "https://hack4u.io/cursos/personalizacion-de-entorno-en-linux/"), 
+        Course("Introducción al Hacking", 53, "https://hack4u.io/cursos/introduccion-al-hacking/")
+        ]
+
+def list_courses():
+    for course in courses:
+        print(course)
+
+def search_course_by_name(name):
+    for course in courses:
+        if course.name == name:
+            return course
+
+    return None
+
