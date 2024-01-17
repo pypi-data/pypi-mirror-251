e71aa7a932f74fbfa66301f1ad1e16c7a7fa9be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:05:53 +0000

    Add package ibis-mssqlops/0.0.53/ibis_mssqlops-0.0.53.tar.gz

---
+++ packages/ibis-mssqlops/ibis_mssqlops-0.0.53.tar.gz/ibis_mssqlops-0.0.53/ibis_mssqlops/__init__.py
@@ -0,0 +1,12 @@
+import ibis_mssqlops.addibis
+import ibis_mssqlops.addsql
+from ibis_mssqlops.consts import (
+    DatePart as datepart,
+    MssqlTypeName as mssqltype,
+    Style as style
+)
+from ibis_mssqlops.connect import (
+    Connect as mssql_connect
+)
+
+__version__ = '0.0.53'
---
+++ packages/ibis-mssqlops/ibis_mssqlops-0.0.53.tar.gz/ibis_mssqlops-0.0.53/ibis_mssqlops/addibis.py
@@ -0,0 +1,14 @@
+from ibis_mssqlops.funcs import *
+from ibis.expr import types as ir
+
+ir.StringValue.replicate = replicate
+ir.StringValue.collatelatin = collatelatin
+ir.StringValue.trim = trim
+ir.Value.convert = convert
+ir.Value.isnumeric = isnumeric
+ir.DateValue.datefromparts = datefromparts
+ir.TimestampValue.datefromparts = datefromparts
+ir.DateValue.dateadd = dateadd
+ir.TimestampValue.dateadd = dateadd
+ir.DateValue.datediff = datediff
+ir.TimestampValue.datediff = datediff
