431a05e1a12456a137d42be57be307b73124bcf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 07:03:28 +0000

    Add package musify/2024.1.1/musify-2024.1.1-py3-none-any.whl

---
+++ packages/musify/musify-2024.1.1-py3-none-any.whl/musify/local/track/__init__.py
@@ -0,0 +1,7 @@
+from .base.track import LocalTrack
+from .base.writer import SyncResultTrack
+from .flac import FLAC
+from .m4a import M4A
+from .mp3 import MP3
+from .utils import TRACK_CLASSES, TRACK_FILETYPES, load_track
+from .wma import WMA
