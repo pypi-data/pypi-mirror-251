61127ed60a998f977ae498a115132391c861cf36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:04:30 +0000

    Add package amsdal/0.0.21/amsdal-0.0.21-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/amsdal/amsdal-0.0.21-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/amsdal/authentication/handlers/credentials.pyi
@@ -0,0 +1,16 @@
+from _typeshed import Incomplete
+from amsdal.__about__ import __version__ as __version__
+from amsdal.authentication.handlers.base import AuthHandlerBase as AuthHandlerBase, BASE_AUTH_URL as BASE_AUTH_URL, ENCRYPT_PUBLIC_KEY as ENCRYPT_PUBLIC_KEY, SYNC_KEY as SYNC_KEY
+from amsdal.authentication.handlers.token import TokenAuthHandler as TokenAuthHandler
+from amsdal.errors import AmsdalAuthenticationError as AmsdalAuthenticationError
+from amsdal.schemas.manager import SchemaManager as SchemaManager
+
+class CredentialsAuthHandler(AuthHandlerBase):
+    __access_key_id: Incomplete
+    __secret_access_key: Incomplete
+    __fernet: Incomplete
+    def __init__(self, access_key_id: str | None, secret_access_key: str | None) -> None: ...
+    def _create_session(self) -> str: ...
+    def _get_public_key(self) -> str: ...
+    def _decode_public_key(self, key: str) -> str: ...
+    def validate_credentials(self) -> None: ...
