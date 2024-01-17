cbd7d6d29ba42d649332c95b3d6faf89be10bb9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:05:52 +0000

    Add package edwh-bundler-plugin/1.0.2/edwh_bundler_plugin-1.0.2.tar.gz

---
+++ packages/edwh-bundler-plugin/edwh_bundler_plugin-1.0.2.tar.gz/edwh_bundler_plugin-1.0.2/src/edwh_bundler_plugin/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2023-present Remco Boerma <remco.b@educationwarehouse.nl>
+#
+# SPDX-License-Identifier: MIT
+
+from .bundler_plugin import build, build_css, build_js, bundle_css, bundle_js
+from .css import extract_contents_for_css
+from .js import extract_contents_for_js
+from .lazy import JIT
