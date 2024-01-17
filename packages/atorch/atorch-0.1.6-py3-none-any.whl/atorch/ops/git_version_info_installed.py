d4c286cbc73d20c757a48f6fd5c86df8bdefb2d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:03 +0000

    Add package atorch/0.1.6/atorch-0.1.6-py3-none-any.whl

---
+++ packages/atorch/atorch-0.1.6-py3-none-any.whl/atorch/ops/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version = '0.1.6+d6d5647a'
+git_hash = 'd6d5647a'
+git_branch = 'merge_202401a'
+installed_ops = {'quantization_optimizer': False, 'quantizer': False}
+compatible_ops = {'quantization_optimizer': True, 'quantizer': True, 'atorch_not_implemented': False}
+torch_info = {'version': '1.12', 'bf16_support': False, 'cuda_version': '11.3', 'nccl_version': '2.10'}
