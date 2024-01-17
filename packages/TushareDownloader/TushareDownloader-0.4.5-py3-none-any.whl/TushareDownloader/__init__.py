ec31ddc53a484a4b66fc0722bba79fa72da87c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:06:02 +0000

    Add package TushareDownloader/0.4.5/TushareDownloader-0.4.5-py3-none-any.whl

---
+++ packages/TushareDownloader/TushareDownloader-0.4.5-py3-none-any.whl/TushareDownloader/__init__.py
@@ -0,0 +1,9 @@
+"""
+TushareDownloader package
+Author: Yanzhong Huang
+Email: yanzhong.huang@outlook.com
+"""
+
+from .download import *
+from .query import *
+from .initialize_db import *
---
+++ packages/TushareDownloader/TushareDownloader-0.4.5-py3-none-any.whl/TushareDownloader/query/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module Name Here
+
+Author: Yanzhong Huang
+Email: yanzhong.huang@outlook.com
+"""
+
+if __name__ == '__main__':
+    pass
