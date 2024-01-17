e36a38ae81880b5dbc3d11832a2e13d151341394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:04:29 +0000

    Add package LMchain/0.1.72/LMchain-0.1.72.tar.gz

---
+++ packages/LMchain/LMchain-0.1.72.tar.gz/LMchain-0.1.72/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="lmchain",  # 模块名称
+    version="0.1.72",  # 当前版本
+    author="xiaohuaWang",  # 作者
+    author_email="5847713@qq.com",  # 作者邮箱
+    description="LMchain是一个专门适配大模型chain的工具包",  # 模块简介
+    long_description=long_description,  # 模块详细介绍
+    long_description_content_type="text/markdown",  # 模块详细介绍格式
+    # url="https://github.com/",  # 模块github地址
+    packages=setuptools.find_packages(),  # 自动找到项目中导入的模块
+    include_package_data=True,
+    # 模块相关的元数据
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    # 依赖模块
+    install_requires=[
+        'uvicorn>= 0.23.2',
+        'fastapi>=0.108.0',
+        "numexpr>=2.8.7",
+        "langchain>=0.1.0",
+        "zhipuai>=1.0.7",
+        "nltk>=3.8.1"
+    ],
+    python_requires='>=3',
+)
