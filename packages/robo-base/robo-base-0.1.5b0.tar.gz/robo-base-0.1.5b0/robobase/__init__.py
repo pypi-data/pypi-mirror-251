5e640404ad23aa0e796933d6524b0a2c010411ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 07:03:30 +0000

    Add package robo-base/0.1.5b0/robo-base-0.1.5b0.tar.gz

---
+++ packages/robo-base/robo-base-0.1.5b0.tar.gz/robo-base-0.1.5b0/robobase/__init__.py
@@ -0,0 +1,17 @@
+
+# robo-base - primary objects 
+
+# flit et al uses __version__ as one source version 
+
+__version__ = 'v0.01.05b'
+
+# import acts to export 
+
+from .object import Object
+from .system import System
+
+
+
+
+
+
