1b58ff635ea0eef5dc937044af8bb72fb08baaf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:04:18 +0000

    Add package flightplotting/0.2.1/flightplotting-0.2.1.tar.gz

---
+++ packages/flightplotting/flightplotting-0.2.1.tar.gz/flightplotting-0.2.1/flightplotting/__init__.py
@@ -0,0 +1,4 @@
+
+
+from flightplotting.plots import plotsec, plot_analysis, plotdtw, create_3d_plot
+from flightplotting.traces import axestrace, trace3d, dtwtrace, vector, vectors, boxtrace, axis_rate_traces
