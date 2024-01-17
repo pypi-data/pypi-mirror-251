464a43ca23e13a4dfe814a3dfccbdb40af1e3289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:05:34 +0000

    Add package athina/0.1.9/athina-0.1.9.tar.gz

---
+++ packages/athina/athina-0.1.9.tar.gz/athina-0.1.9/athina/metrics/metric_type.py
@@ -0,0 +1,36 @@
+from enum import Enum
+from .agreement_score import AgreementScore
+from .hallucination_score import HallucinationScore
+from .contradiction_score import ContradictionScore
+from .ragas_context_relevancy import RagasContextRelevancy
+from .ragas_answer_relevancy import RagasAnswerRelevancy
+from .passed import Passed
+from .metric import Metric
+
+class MetricType(Enum):
+    AGREEMENT_SCORE = "agreement_score"
+    HALLUCINATION_SCORE = "hallucination_score"
+    CONTRADICTION_SCORE = "contradiction_score"
+    RAGAS_CONTEXT_RELEVANCY = "ragas_context_relevancy"
+    RAGAS_ANSWER_RELEVANCY = "ragas_answer_relevancy"
+    PASSED = 'passed'
+
+    @staticmethod
+    def get_class(metric_type):
+        """
+        Returns the class of the metric type.
+        """
+        if metric_type == MetricType.AGREEMENT_SCORE.value:
+            return AgreementScore
+        elif metric_type == MetricType.HALLUCINATION_SCORE.value:
+            return HallucinationScore
+        elif metric_type == MetricType.CONTRADICTION_SCORE.value:
+            return ContradictionScore
+        elif metric_type == MetricType.RAGAS_CONTEXT_RELEVANCY.value:
+            return RagasContextRelevancy
+        elif metric_type == MetricType.RAGAS_ANSWER_RELEVANCY.value:
+            return RagasAnswerRelevancy
+        elif metric_type == MetricType.PASSED.value:
+            return Passed
+        else:
+            raise NotImplementedError(f"Metric type {metric_type} not implemented.")
