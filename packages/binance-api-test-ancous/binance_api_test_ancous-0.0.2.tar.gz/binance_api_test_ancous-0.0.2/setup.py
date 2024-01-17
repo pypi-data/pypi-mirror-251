5cb7c8e53e8754b673c8d2df29d7b43224828db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:04:55 +0000

    Add package binance-api-test-ancous/0.0.2/binance_api_test_ancous-0.0.2.tar.gz

---
+++ packages/binance-api-test-ancous/binance_api_test_ancous-0.0.2.tar.gz/binance_api_test_ancous-0.0.2/setup.py
@@ -0,0 +1,23 @@
+"""
+pass
+"""
+
+from setuptools import setup
+
+setup(
+    name="binance_api_test_ancous",
+    version="0.0.2",
+    author="Ancous",
+    author_email="alex_taras@bk.ru",
+    description="Interaction with the Binance exchange",
+    url="https://github.com/Ancous/binance-api.git",
+    packages=[
+        "binance_api_test_ancous"
+    ],
+    install_requires=['requests>=2.31.0'],
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
