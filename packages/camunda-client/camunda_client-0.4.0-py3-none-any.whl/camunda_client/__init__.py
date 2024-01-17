d4c45806d1346f51e60eeb8fcf247b685a36d070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:06:15 +0000

    Add package camunda-client/0.4.0/camunda_client-0.4.0-py3-none-any.whl

---
+++ packages/camunda-client/camunda_client-0.4.0-py3-none-any.whl/camunda_client/__init__.py
@@ -0,0 +1,27 @@
+from .clients import (
+    CamundaEngineClient,
+    CamundaUrls,
+    ExternalTaskClient,
+    ExternalTaskConfig,
+)
+from .worker import (
+    ExternalTaskContext,
+    ExternalTaskDTO,
+    ExternalTaskWorker,
+    TopicConsumer,
+)
+
+__all__ = [
+    "CamundaEngineClient",
+    "CamundaUrls",
+    "ExternalTaskClient",
+    "ExternalTaskConfig",
+    "ExternalTaskDTO",
+    "TopicConsumer",
+    "ExternalTaskContext",
+    "ExternalTaskWorker",
+    "ExternalTaskDTO",
+]
+
+
+__version__ = "0.4.0"
