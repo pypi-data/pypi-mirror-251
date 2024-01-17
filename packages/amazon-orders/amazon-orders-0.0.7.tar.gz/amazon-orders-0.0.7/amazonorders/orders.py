719d8a67b3047a82ba192ae16c1dd5526996bd8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 14:04:52 +0000

    Add package amazon-orders/0.0.7/amazon-orders-0.0.7.tar.gz

---
+++ packages/amazon-orders/amazon-orders-0.0.7.tar.gz/amazon-orders-0.0.7/amazonorders/orders.py
@@ -0,0 +1,84 @@
+import datetime
+import logging
+from typing import List, Optional
+
+from amazonorders.entity.order import Order
+from amazonorders.exception import AmazonOrdersError
+from amazonorders.session import BASE_URL, AmazonSession
+
+__author__ = "Alex Laird"
+__copyright__ = "Copyright 2024, Alex Laird"
+__version__ = "0.0.7"
+
+logger = logging.getLogger(__name__)
+
+
+class AmazonOrders:
+    def __init__(self,
+                 amazon_session: AmazonSession,
+                 debug: bool = False,
+                 print_output: bool = False) -> None:
+        self.amazon_session: AmazonSession = amazon_session
+
+        self.debug: bool = debug
+        if self.debug:
+            logger.setLevel(logging.DEBUG)
+        self.print_output: bool = print_output
+
+    def get_order_history(self,
+                          year: int = datetime.date.today().year,
+                          start_index: Optional[int] = None,
+                          full_details: bool = False) -> List[Order]:
+        if not self.amazon_session.is_authenticated:
+            raise AmazonOrdersError("Call AmazonSession.login() to authenticate first.")
+
+        orders = []
+        next_page = "{}/your-orders/orders?timeFilter=year-{}{}".format(BASE_URL,
+                                                                        year,
+                                                                        "&startIndex={}".format(
+                                                                            start_index) if start_index else "")
+        while next_page:
+            self.amazon_session.get(next_page)
+            response_parsed = self.amazon_session.last_response_parsed
+
+            for order_tag in response_parsed.find_all("div", {"class": "order-card"}):
+                order = Order(order_tag)
+
+                if full_details:
+                    self.amazon_session.get(order.order_details_link)
+                    order_details_tag = self.amazon_session.last_response_parsed.find("div", id="orderDetails")
+                    order = Order(order_details_tag, full_details=True, clone=order)
+
+                orders.append(order)
+
+            next_page = None
+            if start_index is None:
+                try:
+                    next_page = "{}{}".format(BASE_URL,
+                                              response_parsed.find("ul", {"class", "a-pagination"}).find(
+                                                  "li", {"class": "a-last"}).find("a").attrs["href"])
+                except AttributeError:
+                    logger.debug("No next page")
+            else:
+                logger.debug("start_index is given, not paging")
+
+        if self.print_output:
+            for order in orders:
+                print(order)
+
+        return orders
+
+    def get_order(self,
+                  order_id: str) -> Order:
+        if not self.amazon_session.is_authenticated:
+            raise AmazonOrdersError("Call AmazonSession.login() to authenticate first.")
+
+        self.amazon_session.get("{}/gp/your-account/order-details?orderID={}".format(BASE_URL, order_id))
+
+        order_details_tag = self.amazon_session.last_response_parsed.find("div", id="orderDetails")
+        order = Order(order_details_tag, full_details=True)
+
+        if self.print_output:
+            print(order)
+
+        return order
