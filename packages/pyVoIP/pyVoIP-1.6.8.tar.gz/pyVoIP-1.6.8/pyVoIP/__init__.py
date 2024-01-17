d64266a7451694ef52469285ed63ed8cb12f3612
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:14 +0000

    Add package pyVoIP/1.6.8/pyVoIP-1.6.8.tar.gz

---
+++ packages/pyVoIP/pyVoIP-1.6.8.tar.gz/pyVoIP-1.6.8/pyVoIP/__init__.py
@@ -0,0 +1,37 @@
+__all__ = ["SIP", "RTP", "VoIP"]
+
+version_info = (1, 6, "8")
+
+__version__ = ".".join([str(x) for x in version_info])
+
+DEBUG = False
+
+"""
+The higher this variable is, the more often RTP packets are sent.
+This should only ever need to be 0.0. However, when testing on Windows,
+there has sometimes been jittering, setting this to 0.75 fixed this in testing.
+"""
+TRANSMIT_DELAY_REDUCTION = 0.0
+
+"""
+If registration fails this many times, VoIPPhone's status will be set to FAILED
+and the phone will stop.
+"""
+REGISTER_FAILURE_THRESHOLD = 3
+
+
+def debug(s, e=None):
+    if DEBUG:
+        print(s)
+    elif e is not None:
+        print(e)
+
+
+# noqa because import will fail if debug is not defined
+from pyVoIP.RTP import PayloadType  # noqa: E402
+
+SIPCompatibleMethods = ["INVITE", "ACK", "BYE", "CANCEL"]
+SIPCompatibleVersions = ["SIP/2.0"]
+
+RTPCompatibleVersions = [2]
+RTPCompatibleCodecs = [PayloadType.PCMU, PayloadType.PCMA, PayloadType.EVENT]
