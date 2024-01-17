f0e73c0aa3ebe19eb03b49d114d991a1de855d2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:04:40 +0000

    Add package FlyBaseDownloads/2.0.2/FlyBaseDownloads-2.0.2-py3-none-any.whl

---
+++ packages/FlyBaseDownloads/FlyBaseDownloads-2.0.2-py3-none-any.whl/FlyBaseDownloads/classes/Gene_groups.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 15 17:27:27 2023
+
+@author: usuario
+"""
+
+from FlyBaseDownloads.downloads.Downloads import Downloads 
+
+class Gene_groups():
+    
+    def __init__(self, main_url, cred, continue_):
+        self.cred = cred
+        self.main_url = main_url
+        self.gen_url = 'genes/'
+        self.continue_ = continue_
+        
+    def get(self):
+        
+        url = self.main_url + self.gen_url + self.un_url
+        descargas = Downloads(url, self.cred, self.continue_)
+        
+        if self.continue_:
+            df = descargas.get(self.header)
+            
+            df.columns = df.iloc[0]
+            return df[1:].reset_index(drop=True)
+        
+    def Gene_group(self):
+        self.un_url = 'gene_group_data_fb_*.tsv.gz'
+        self.header = 6
+        return self.get()
+    
+    def Gene_groups_HGNC(self):
+        self.un_url = 'gene_groups_HGNC_fb_*.tsv.gz'
+        self.header = 6
+        return self.get()
+        
+    def Pathway_group(self):
+        self.un_url = 'pathway_group_data_fb_*.tsv.gz'
+        self.header = 6
+        return self.get()
