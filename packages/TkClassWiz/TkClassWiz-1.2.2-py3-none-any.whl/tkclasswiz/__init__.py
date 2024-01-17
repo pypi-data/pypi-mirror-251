fa251359f184504cd577f6f2edd698a16ae6a1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:04:37 +0000

    Add package TkClassWiz/1.2.2/TkClassWiz-1.2.2-py3-none-any.whl

---
+++ packages/TkClassWiz/TkClassWiz-1.2.2-py3-none-any.whl/tkclasswiz/__init__.py
@@ -0,0 +1,16 @@
+"""
+TkClassWizard - Library for graphically defining objects based on class annotations.
+Works with Tkinter / TTKBootstrap.
+"""
+
+__version__ = "1.2.2"
+
+from .object_frame import *
+from .annotations import *
+from .cache import *
+from .convert import *
+from .extensions import *
+from .messagebox import *
+from .storage import *
+from .utilities import *
+from .aliasing import *
