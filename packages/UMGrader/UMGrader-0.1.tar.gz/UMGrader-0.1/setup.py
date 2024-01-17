e3a5ad0da8faa740164dc45c62d0fe8599b7ba17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:03:29 +0000

    Add package UMGrader/0.1/UMGrader-0.1.tar.gz

---
+++ packages/UMGrader/UMGrader-0.1.tar.gz/UMGrader-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='UMGrader',
+    version='0.1',
+    description='Grading system for ipynb',
+    author='Sittha Darapisut',
+    packages=find_packages(),
+)
