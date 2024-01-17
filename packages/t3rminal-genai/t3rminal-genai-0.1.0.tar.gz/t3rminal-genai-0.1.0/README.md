4b3ed4f8eb3d4c49c2a35205ea77230136afebdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:18 +0000

    Add package t3rminal-genai/0.1.0/t3rminal-genai-0.1.0.tar.gz

---
+++ packages/t3rminal-genai/t3rminal-genai-0.1.0.tar.gz/t3rminal-genai-0.1.0/README.md
@@ -0,0 +1,9 @@
+# t3rminal-genai
+
+Welcome to Your Package Name! To get started, follow these steps:
+
+1. Install the package using `pip install t3rminal-genai`.
+2. Users set their API key using your_package_name.configure_api_key("your_api_key")
+
+Now you're ready to use the package!
+
---
+++ packages/t3rminal-genai/t3rminal-genai-0.1.0.tar.gz/t3rminal-genai-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="t3rminal-genai",
+    version="0.1.0",
+    description="simple-python-ai-package",
+    author="Anubhav Mazumder",
+    author_email="terminalishere127@gamil.com",
+    packages=find_packages(),
+    install_requires=["google-generativeai"],
+)
+
