15d603058c8a110519f86988794a8fdac44b438f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 15:04:59 +0000

    Add package bolsasfnde/3.0.2/bolsasfnde-3.0.2.tar.gz

---
+++ packages/bolsasfnde/bolsasfnde-3.0.2.tar.gz/bolsasfnde-3.0.2/bolsasfnde/__main__.py
@@ -0,0 +1,4 @@
+from .main import caiu
+
+if __name__ == "__main__":
+    caiu()
---
+++ packages/bolsasfnde/bolsasfnde-3.0.2.tar.gz/bolsasfnde-3.0.2/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='bolsasfnde',
+    version='3.0.2',
+    packages=find_packages(),
+    install_requires=['requests'],
+    entry_points={
+        'console_scripts': [
+            'bolsasfnde = bolsasfnde.__main__:main',
+        ],
+    },
+    python_requires='>=3.6',
+    description="A biblioteca automatiza consultas periódicas ao site do FNDE para verificar se a tão esperada bolsa foi concedida. Adeus às verificações manuais repetitivas!",
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown')
