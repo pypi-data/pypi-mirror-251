ee999f10b0bbefc9f7d26be1e1d5ada41c8169fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:27 +0000

    Add package opyration/0.0.4/opyration-0.0.4.tar.gz

---
+++ packages/opyration/opyration-0.0.4.tar.gz/opyration-0.0.4/opyration/symbols.py
@@ -0,0 +1,6 @@
+SYMBOLS = {
+    'gt': '>', 'lt': '<', 'ge': '>=', 'gte': '>=', 'le': '<=', 'lte': '<=',
+    'eq': '=', 'lk': '%', 'ne': '!=',
+    'in': ' IN ', 'ni': ' NOT IN ', 'bw': ' BETWEEN ', 'nb': ' NOT BETWEEN ',
+    'is': ' IS ', 'no': ' IS NOT ', 'nu': ' IS NULL ', 'nn': 'IS NOT NULL'
+}
