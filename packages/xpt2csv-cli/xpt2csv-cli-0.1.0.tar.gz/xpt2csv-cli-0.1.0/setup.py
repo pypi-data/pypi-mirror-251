9de6d8c7760d5f5630962e28224ed3816646c909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 14:04:36 +0000

    Add package xpt2csv-cli/0.1.0/xpt2csv-cli-0.1.0.tar.gz

---
+++ packages/xpt2csv-cli/xpt2csv-cli-0.1.0.tar.gz/xpt2csv-cli-0.1.0/setup.py
@@ -0,0 +1,18 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name='xpt2csv-cli',
+    packages=find_packages(include= ['xpt2csv']),
+    version='0.1.0',
+    install_requires=['pyreadstat',
+                      'click'],
+    description='CLI app to convert xpt file to csv',
+    entry_points='''
+    [console_scripts]
+    to_csv=xpt2csv.to_csv:xpt_to_csv
+    ''',
+    author='Yousuf Ali',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
