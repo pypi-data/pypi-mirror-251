3b4dd5e11fc8770608e5d43adf8e52d95ebee014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:09 +0000

    Add package SIMNet/0.1.19/simnet-0.1.19.tar.gz

---
+++ packages/SIMNet/simnet-0.1.19.tar.gz/simnet-0.1.19/simnet/models/starrail/character.py
@@ -0,0 +1,34 @@
+"""Starrail base character model."""
+from simnet.models.base import APIModel
+
+
+class StarRailBaseCharacter(APIModel):
+    """Base character model."""
+
+    id: int
+    element: str
+    rarity: int
+    icon: str
+
+
+class StarRailPartialCharacter(StarRailBaseCharacter):
+    """Character without any equipment."""
+
+    name: str
+    level: int
+    rank: int
+
+
+class RogueCharacter(StarRailBaseCharacter):
+    """Rogue character model."""
+
+    level: int
+    rank: int
+    is_trial: bool = False
+
+
+class ActivityCharacter(StarRailBaseCharacter):
+    """Combat character model."""
+
+    level: int
+    is_trial: bool = False
