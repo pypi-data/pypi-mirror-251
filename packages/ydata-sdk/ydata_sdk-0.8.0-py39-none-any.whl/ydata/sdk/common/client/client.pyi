ff7a3b42e460271eac031c89bb4d0006a3c5b22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:06:01 +0000

    Add package ydata-sdk/0.8.0/ydata_sdk-0.8.0-py39-none-any.whl

---
+++ packages/ydata-sdk/ydata_sdk-0.8.0-py39-none-any.whl/ydata/sdk/common/client/client.pyi
@@ -0,0 +1,15 @@
+from _typeshed import Incomplete
+from httpx import Response as Response, codes as http_codes
+from typing import Dict, Optional, Union
+from ydata.sdk.common.client.singleton import SingletonClient
+from ydata.sdk.common.types import Project
+
+codes = http_codes
+HELP_TEXT: Incomplete
+
+class Client(metaclass=SingletonClient):
+    codes = codes
+    def __init__(self, credentials: Optional[Union[str, Dict]] = ..., project: Optional[Project] = ..., set_as_global: bool = ...) -> None: ...
+    def post(self, endpoint: str, data: Optional[Dict] = ..., json: Optional[Dict] = ..., project: Optional[Project] = ..., files: Optional[Dict] = ..., raise_for_status: bool = ...) -> Response: ...
+    def get(self, endpoint: str, params: Optional[Dict] = ..., project: Optional[Project] = ..., cookies: Optional[Dict] = ..., raise_for_status: bool = ...) -> Response: ...
+    def get_static_file(self, endpoint: str, project: Optional[Project] = ..., raise_for_status: bool = ...) -> Response: ...
