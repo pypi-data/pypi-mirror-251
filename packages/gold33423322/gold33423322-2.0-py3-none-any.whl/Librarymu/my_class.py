62269afe9e9fc54a268297d5098f01cc0f9109c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:19 +0000

    Add package gold33423322/2.0/gold33423322-2.0-py3-none-any.whl

---
+++ packages/gold33423322/gold33423322-2.0-py3-none-any.whl/Librarymu/my_class.py
@@ -0,0 +1,6 @@
+class MyClass:
+    def fungsi_Pertama(self):
+        print("Ini adalah Fungsi Pertama dari MyClass.")
+
+    def fungsi_kedua(self):
+        print("Ini adalah Fungsi Kedua dari MyClass.")
