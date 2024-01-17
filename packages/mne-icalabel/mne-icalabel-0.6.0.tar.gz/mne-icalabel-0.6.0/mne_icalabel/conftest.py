6b597e645ac29a71e16c5ad80e6454fc5e73e61d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:04:40 +0000

    Add package mne-icalabel/0.6.0/mne-icalabel-0.6.0.tar.gz

---
+++ packages/mne-icalabel/mne-icalabel-0.6.0.tar.gz/mne-icalabel-0.6.0/mne_icalabel/conftest.py
@@ -0,0 +1,16 @@
+from mne import set_log_level
+
+
+def pytest_configure(config):
+    """Configure pytest options."""
+    warnings_lines = r"""
+    error::
+    # Until MNE 1.6 is the minimum supported version
+    # c.f. https://github.com/mne-tools/mne-python/pull/12143
+    ignore:Setting non-standard config type.*:RuntimeWarning
+    """
+    for warning_line in warnings_lines.split("\n"):
+        warning_line = warning_line.strip()
+        if warning_line and not warning_line.startswith("#"):
+            config.addinivalue_line("filterwarnings", warning_line)
+    set_log_level("WARNING")
