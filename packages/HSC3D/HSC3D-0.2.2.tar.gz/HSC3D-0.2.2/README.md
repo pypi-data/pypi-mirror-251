444fde8a6b62aff0158a5aafab02a0f3f7ccb283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:00 +0000

    Add package HSC3D/0.2.2/HSC3D-0.2.2.tar.gz

---
+++ packages/HSC3D/HSC3D-0.2.2.tar.gz/HSC3D-0.2.2/README.md
@@ -0,0 +1,22 @@
+# HSC3D
+
+HSC3D: a Python package to quantify three-dimensional habitat structural complexity
+
+## Setup environment
+
+Install Anaconda or miniconda to your device. See more information at [this link](https://docs.conda.io/projects/conda/en/latest/user-guide/install/download.html).
+
+```
+# choose your own virtual environment name
+conda create -n hsc3d_test python=3.10 -y
+
+conda activate hsc3d_test
+```
+
+## Installation
+
+``pip install HSC3D``
+
+## Usage
+
+See ``sample_usage.ipynb``
---
+++ packages/HSC3D/HSC3D-0.2.2.tar.gz/HSC3D-0.2.2/setup.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import pkg_resources as pkg
+from setuptools import find_packages, setup
+
+FILE = Path(__file__).resolve()
+PARENT = FILE.parent  # root directory
+README = (PARENT / "README.md").read_text(encoding="utf-8")
+REQUIREMENTS = [
+    f"{x.name}{x.specifier}"
+    for x in pkg.parse_requirements(
+        (PARENT / "C:/Users/guyf0/Documents/GitHub/HSC3D/requirements.txt").read_text()
+    )
+]
+
+VERSION = "0.2.2"
+
+setup(
+    name="HSC3D",  # name of pypi package
+    version=VERSION,  # version of pypi package
+    python_requires=">=3.10",
+    license="MIT",
+    description=(
+        "HSC3D: a Python package to quantify three-dimensional habitat structural complexity"
+    ),
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),  # required
+    include_package_data=True,
+    install_requires=REQUIREMENTS,
+    keywords="3D point clouds, structural complexity",
+)
