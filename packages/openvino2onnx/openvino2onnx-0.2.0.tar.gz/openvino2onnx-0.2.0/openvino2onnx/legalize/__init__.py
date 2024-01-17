e7c2dc2db121f1cd3eece05cd70abc8e0fa37f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 15:05:36 +0000

    Add package openvino2onnx/0.2.0/openvino2onnx-0.2.0.tar.gz

---
+++ packages/openvino2onnx/openvino2onnx-0.2.0.tar.gz/openvino2onnx-0.2.0/openvino2onnx/legalize/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright Wenyi Tang 2023
+
+:Author: Wenyi Tang
+:Email: wenyitang@outlook.com
+
+"""
+
+from .compose import legalize
+from .single_node import *  # noqa: F403
+
+__all__ = ["legalize"]
