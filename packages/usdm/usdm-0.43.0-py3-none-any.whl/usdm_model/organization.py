314bba2434baaf60fcf1204ca545df762363f997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:39 +0000

    Add package usdm/0.43.0/usdm-0.43.0-py3-none-any.whl

---
+++ packages/usdm/usdm-0.43.0-py3-none-any.whl/usdm_model/organization.py
@@ -0,0 +1,15 @@
+from typing import List, Literal, Union
+from .api_base_model import ApiBaseModelWithIdNameAndLabel
+from .code import Code
+from .address import Address
+
+class Organization(ApiBaseModelWithIdNameAndLabel):
+  type: Code
+  identifierScheme: str
+  identifier: str
+  legalAddress: Union[Address, None] = None
+  instanceType: Literal['Organization'] = 'Organization'
+
+class ResearchOrganization(Organization):
+  manageIds: List[str]
+  instanceType: Literal['ResearchOrganization'] = 'ResearchOrganization'
---
+++ packages/usdm/usdm-0.43.0-py3-none-any.whl/usdm_model/study_intervention.py
@@ -0,0 +1,15 @@
+from typing import List, Literal
+from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
+from .quantity import Quantity
+from .agent_administration import AgentAdministration
+from .code import Code
+
+class StudyIntervention(ApiBaseModelWithIdNameLabelAndDesc):
+  role:	Code
+  type:	Code
+  minimumResponseDuration: Quantity
+  codes: List[Code] = []
+  administrations: List[AgentAdministration]
+  productDesignation:	Code
+  pharmacologicClass:	Code
+  instanceType: Literal['StudyIntervention'] = 'StudyIntervention'
