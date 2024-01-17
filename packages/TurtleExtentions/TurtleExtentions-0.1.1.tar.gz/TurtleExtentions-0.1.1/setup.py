2ef9e201c995d26806cdb9f15bac84f2e8e45fd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:08 +0000

    Add package TurtleExtentions/0.1.1/TurtleExtentions-0.1.1.tar.gz

---
+++ packages/TurtleExtentions/TurtleExtentions-0.1.1.tar.gz/TurtleExtentions-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='TurtleExtentions',
+    version='0.1.1',
+    author='Pai',
+    author_email='irisdamodder@gmail.com',
+    description='Extention of the built in Turtle package',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
