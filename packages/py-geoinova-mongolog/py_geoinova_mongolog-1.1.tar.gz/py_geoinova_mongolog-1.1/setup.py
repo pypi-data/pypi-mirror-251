908a992d2861e7c6a9b44d612121acdcbfeebd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:04:16 +0000

    Add package py-geoinova-mongolog/1.1/py_geoinova_mongolog-1.1.tar.gz

---
+++ packages/py-geoinova-mongolog/py_geoinova_mongolog-1.1.tar.gz/py_geoinova_mongolog-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_geoinova_mongolog',
+    version='1.1',
+    packages=find_packages(),
+    description='Pacote responsável por salvar as informações de log no mongo',
+    author='Manoel Neco Jr',
+    author_email='manoel.junior@geoinova.com.br',
+)
