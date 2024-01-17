db4917bb035912b3c39a7752772cd47ab70debbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:05:19 +0000

    Add package kudamonomikiwame/0.1.0/kudamonomikiwame-0.1.0.tar.gz

---
+++ packages/kudamonomikiwame/kudamonomikiwame-0.1.0.tar.gz/kudamonomikiwame-0.1.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+setuptools.setup(
+  name="kudamonomikiwame",
+  version="0.1.0",
+  author="Hidekazu",
+  author_email="s2122030@stu.musashino-u.ac.jp",
+  description="A small example package",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/Im-not-yoneda/image_compression",
+  project_urls={
+    "Bug Tracker": "https://github.com/Hide4869/kudamonomikiwame"
+  },
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  package_dir={"": "src"},
+  py_modules=['imageComp'],
+  packages=setuptools.find_packages(where="src"),
+  python_requires=">=3.6",
+  entry_points = {
+      'console_scripts': [
+          'imageComp = imageComp:main'
+      ]
+  },
+) 
