63134567be9f04e817b417934db18b34a6cf8082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:04:24 +0000

    Add package biosimulators-simularium/0.5.4/biosimulators-simularium-0.5.4.tar.gz

---
+++ packages/biosimulators-simularium/biosimulators-simularium-0.5.4.tar.gz/biosimulators-simularium-0.5.4/biosimulators_simularium/__init__.py
@@ -0,0 +1,16 @@
+"""
+:Author: Alexander Patrie <apatrie@uchc.edu>
+:Date: 2023-09-16
+:Copyright: 2023, UConn Health
+:License: MIT
+"""
+
+
+import biosimulators_simularium
+from biosimulators_simularium.exec import generate_simularium_file
+
+
+__all__ = [
+    'biosimulators_simularium',
+    'generate_simularium_file'
+]
