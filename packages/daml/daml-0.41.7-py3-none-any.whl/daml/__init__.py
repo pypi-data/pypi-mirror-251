2f89939bece2264a10a1c939a9de70267a412444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 06:05:07 +0000

    Add package daml/0.41.7/daml-0.41.7-py3-none-any.whl

---
+++ packages/daml/daml-0.41.7-py3-none-any.whl/daml/__init__.py
@@ -0,0 +1,5 @@
+from . import datasets, interop, metrics
+
+__version__ = "0.41.7"
+
+__all__ = ["datasets", "interop", "metrics"]
---
+++ packages/daml/daml-0.41.7-py3-none-any.whl/daml/metrics/ber/aria/__init__.py
@@ -0,0 +1,4 @@
+from daml._internal.metrics.aria.ber import MultiClassBerFNN as BER_FNN
+from daml._internal.metrics.aria.ber import MultiClassBerMST as BER_MST
+
+__all__ = ["BER_FNN", "BER_MST"]
