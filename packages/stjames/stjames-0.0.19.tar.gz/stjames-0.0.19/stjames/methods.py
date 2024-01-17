c04241d8fb04d5f7556717ddf30f396657558543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 06:05:07 +0000

    Add package stjames/0.0.19/stjames-0.0.19.tar.gz

---
+++ packages/stjames/stjames-0.0.19.tar.gz/stjames-0.0.19/stjames/methods.py
@@ -0,0 +1,26 @@
+from .base import LowercaseStrEnum
+
+
+class Method(LowercaseStrEnum):
+    #### Hartree–Fock:
+    HARTREE_FOCK = "hf"
+    HF3C = "hf-3c"
+
+    #### DFT:
+    # LDA
+    LSDA = "lsda"
+
+    # local GGA
+    PBE = "pbe"
+    BLYP = "blyp"
+    BP86 = "bp86"
+    B97D3 = "b97-d3"
+    B973C = "b97-3c"
+
+    # hybrid GGA
+    PBE0 = "pbe0"
+    B3LYP = "b3lyp"
+    B3PW91 = "b3pw91"
+
+    # ML methods
+    AIMNET2_WB97MD3 = "aimnet2_wb97md3"
