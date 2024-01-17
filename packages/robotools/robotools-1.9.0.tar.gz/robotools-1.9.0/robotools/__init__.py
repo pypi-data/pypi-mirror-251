1b8ce8a87441df0cc1e90a3fbd798287b6507cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:05:07 +0000

    Add package robotools/1.9.0/robotools-1.9.0.tar.gz

---
+++ packages/robotools/robotools-1.9.0.tar.gz/robotools-1.9.0/robotools/__init__.py
@@ -0,0 +1,17 @@
+from . import evotools, liquidhandling
+from .evotools import EvoWorklist, InvalidOperationError, Labwares, Tip, Worklist
+from .evotools import commands as evo_cmd
+from .evotools import int_to_tip
+from .fluenttools import FluentWorklist
+from .liquidhandling import Labware, Trough, VolumeOverflowError, VolumeUnderflowError
+from .transform import (
+    WellRandomizer,
+    WellRotator,
+    WellShifter,
+    make_well_array,
+    make_well_index_dict,
+)
+from .utils import DilutionPlan, get_trough_wells
+from .worklists import BaseWorklist, CompatibilityError
+
+__version__ = "1.9.0"
