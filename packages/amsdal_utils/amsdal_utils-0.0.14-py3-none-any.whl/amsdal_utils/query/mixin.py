08e0b958cee4e3d0f1660a197400e913c9afa26d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:05:24 +0000

    Add package amsdal_utils/0.0.14/amsdal_utils-0.0.14-py3-none-any.whl

---
+++ packages/amsdal_utils/amsdal_utils-0.0.14-py3-none-any.whl/amsdal_utils/query/mixin.py
@@ -0,0 +1,11 @@
+from abc import ABC
+from abc import abstractmethod
+from typing import Any
+
+from amsdal_utils.query.utils import Q
+
+
+class QueryableMixin(ABC):
+    @abstractmethod
+    def to_query(self, *args: Any, **kwargs: Any) -> Q:
+        ...
