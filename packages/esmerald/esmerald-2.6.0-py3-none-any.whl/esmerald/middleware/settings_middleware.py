c6b85c2649aefcf8b694115eba3d3dd7bbd096ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:05:08 +0000

    Add package esmerald/2.6.0/esmerald-2.6.0-py3-none-any.whl

---
+++ packages/esmerald/esmerald-2.6.0-py3-none-any.whl/esmerald/middleware/settings_middleware.py
@@ -0,0 +1,21 @@
+from esmerald.conf import settings as esmerald_settings
+from esmerald.protocols.middleware import MiddlewareProtocol
+from esmerald.types import ASGIApp, Receive, Scope, Send
+
+
+class RequestSettingsMiddleware(MiddlewareProtocol):
+    def __init__(self, app: "ASGIApp"):
+        """Settings Middleware class.
+
+        Args:
+            app: The 'next' ASGI app to call.
+        """
+        super().__init__(app)
+        self.app = app
+
+    async def __call__(self, scope: "Scope", receive: "Receive", send: "Send") -> None:
+        app = scope["app"]
+        scope["global_settings"] = esmerald_settings
+        scope["app_settings"] = app.settings_config if app.settings_config else None
+
+        await self.app(scope, receive, send)
