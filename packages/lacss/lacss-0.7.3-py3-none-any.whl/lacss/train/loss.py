eee40a11b9dbd4f95e19ffc6592fe779bde9f148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:04:27 +0000

    Add package lacss/0.7.3/lacss-0.7.3-py3-none-any.whl

---
+++ packages/lacss/lacss-0.7.3-py3-none-any.whl/lacss/train/loss.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from typing import Sequence, Union
+
+import jax.numpy as jnp
+from flax import struct
+
+from ..typing import LossFunc
+from .utils import _get_name
+
+
+class LossLog(struct.PyTreeNode):
+    loss_fn: LossFunc = struct.field(pytree_node=False)
+    weight: jnp.ndarray = 1.0
+    cnt: jnp.ndarray = 0.0
+    sum: jnp.ndarray = 0.0
+
+    def update(self, **kwargs):
+        loss = self.loss_fn(**kwargs) * self.weight
+        new_log = self.replace(cnt=self.cnt + 1, sum=self.sum + loss)
+        return loss, new_log
+
+    def compute(self):
+        return self.sum / self.cnt
