dfba31ffd2e0bc9b63441ff46287a2e0fc21b531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:04:22 +0000

    Add package deebot-client/4.2.0/deebot_client-4.2.0-py3-none-any.whl

---
+++ packages/deebot-client/deebot_client-4.2.0-py3-none-any.whl/deebot_client/commands/json/clean_count.py
@@ -0,0 +1,39 @@
+"""Clean count command module."""
+
+from types import MappingProxyType
+from typing import Any
+
+from deebot_client.command import InitParam
+from deebot_client.event_bus import EventBus
+from deebot_client.events import CleanCountEvent
+from deebot_client.message import HandlingResult
+
+from .common import JsonGetCommand, JsonSetCommand
+
+
+class GetCleanCount(JsonGetCommand):
+    """Get clean count command."""
+
+    name = "getCleanCount"
+
+    @classmethod
+    def _handle_body_data_dict(
+        cls, event_bus: EventBus, data: dict[str, Any]
+    ) -> HandlingResult:
+        """Handle message->body->data and notify the correct event subscribers.
+
+        :return: A message response
+        """
+        event_bus.notify(CleanCountEvent(count=data["count"]))
+        return HandlingResult.success()
+
+
+class SetCleanCount(JsonSetCommand):
+    """Set clean count command."""
+
+    name = "setCleanCount"
+    get_command = GetCleanCount
+    _mqtt_params = MappingProxyType({"count": InitParam(int)})
+
+    def __init__(self, count: int) -> None:
+        super().__init__({"count": count})
