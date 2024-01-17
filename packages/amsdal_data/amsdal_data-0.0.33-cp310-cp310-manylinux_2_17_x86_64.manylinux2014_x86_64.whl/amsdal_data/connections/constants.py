5d404753d7e31aa38295fafca2e1362b59acedb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:06:20 +0000

    Add package amsdal_data/0.0.33/amsdal_data-0.0.33-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/amsdal_data/amsdal_data-0.0.33-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/amsdal_data/connections/constants.py
@@ -0,0 +1,11 @@
+ICEBERG_ALIAS = 'iceberg'
+SQLITE_ALIAS = 'sqlite'
+SQLITE_STATE_ALIAS = 'sqlite-state'
+SQLITE_HISTORICAL_ALIAS = 'sqlite-historical'
+
+CONNECTION_BACKEND_ALIASES: dict[str, str] = {
+    ICEBERG_ALIAS: 'amsdal_data.connections.implementations.iceberg_history.IcebergHistoricalConnection',
+    SQLITE_ALIAS: 'amsdal_data.connections.implementations.sqlite_state.SqliteStateConnection',
+    SQLITE_STATE_ALIAS: 'amsdal_data.connections.implementations.sqlite_state.SqliteStateConnection',
+    SQLITE_HISTORICAL_ALIAS: 'amsdal_data.connections.implementations.sqlite_history.SqliteHistoricalConnection',
+}
