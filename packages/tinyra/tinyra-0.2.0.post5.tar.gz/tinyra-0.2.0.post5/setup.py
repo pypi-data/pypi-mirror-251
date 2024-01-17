fd2497546eb3b67f6333afd1a367ad87c34c9526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 14:04:54 +0000

    Add package tinyra/0.2.0.post5/tinyra-0.2.0.post5.tar.gz

---
+++ packages/tinyra/tinyra-0.2.0.post5.tar.gz/tinyra-0.2.0.post5/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup
+
+setup(
+    name="tinyra",
+    version="0.2.0.post5",
+    description="A minimalistic research assistant built with AutoGen.",
+    packages=["tinyra"],
+    package_data={"tinyra": ["*"]},
+    install_requires=["textual", "tiktoken", "pyautogen"],
+    entry_points={
+        "console_scripts": [
+            "tinyra = tinyra:run_tinyra",
+        ],
+    },
+)
