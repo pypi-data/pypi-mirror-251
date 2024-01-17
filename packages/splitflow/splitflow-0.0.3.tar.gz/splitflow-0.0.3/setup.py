8ca81efa7200b140eaf32c5e974aa4dc1df3403c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:05:38 +0000

    Add package splitflow/0.0.3/splitflow-0.0.3.tar.gz

---
+++ packages/splitflow/splitflow-0.0.3.tar.gz/splitflow-0.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.3'
+DESCRIPTION = """
+Empower your data journey today.
+"""
+LONG_DESCRIPTION = """
+Introducing splitflow
+"""
+
+
+
+setup(
+    name="splitflow",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Daniel Lawrence",
+    author_email="daniellawrence4150@gmail.com",
+    
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['pandas','sklearn','scipy','matplotlib','seaborn','numpy','functools'],
+    keywords=['python','data science','data analytics','machine learning','data','analysis'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
