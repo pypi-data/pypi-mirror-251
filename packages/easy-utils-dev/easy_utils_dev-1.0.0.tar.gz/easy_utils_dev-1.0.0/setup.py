f4395f7670416223843f2691a23312f3707e3a88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 06:04:16 +0000

    Add package easy-utils-dev/1.0.0/easy_utils_dev-1.0.0.tar.gz

---
+++ packages/easy-utils-dev/easy_utils_dev-1.0.0.tar.gz/easy_utils_dev-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.0'
+
+# Setting up
+setup(
+    name="easy_utils_dev",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
