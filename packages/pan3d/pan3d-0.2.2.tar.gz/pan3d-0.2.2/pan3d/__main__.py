a7d432734dcc059fae11497b01076374fa4b5210
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 07:03:31 +0000

    Add package pan3d/0.2.2/pan3d-0.2.2.tar.gz

---
+++ packages/pan3d/pan3d-0.2.2.tar.gz/pan3d-0.2.2/pan3d/__main__.py
@@ -0,0 +1,26 @@
+from argparse import ArgumentParser, BooleanOptionalAction
+from pan3d.dataset_builder import DatasetBuilder
+
+
+def main():
+    parser = ArgumentParser(
+        prog="Pan3D",
+        description="Launch the Pan3D Dataset Builder",
+    )
+
+    parser.add_argument("-C", "--config_path")
+    parser.add_argument("-D", "--dataset")
+    parser.add_argument("-P", "--pangeo", action=BooleanOptionalAction)
+    parser.add_argument("-S", "--server", action=BooleanOptionalAction)
+    parser.add_argument("-d", "--debug", action=BooleanOptionalAction)
+
+    args = parser.parse_args()
+
+    builder = DatasetBuilder(dataset_path=args.dataset, pangeo=args.pangeo)
+    if args.config_path:
+        builder.import_config(args.config_path)
+    builder.viewer.server.start(debug=args.debug)
+
+
+if __name__ == "__main__":
+    main()
