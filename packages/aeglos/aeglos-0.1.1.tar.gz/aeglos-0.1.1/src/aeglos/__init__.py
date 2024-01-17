85d24bd4154dbeec288da212581bd10a2cf5f341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 14:05:18 +0000

    Add package aeglos/0.1.1/aeglos-0.1.1.tar.gz

---
+++ packages/aeglos/aeglos-0.1.1.tar.gz/aeglos-0.1.1/src/aeglos/__init__.py
@@ -0,0 +1,4 @@
+from .classes.GuardedChain import guard_chain
+from .classes.GuardedAgentExecutor import guard
+
+__all__ = ['guard', 'guard_chain']
---
+++ packages/aeglos/aeglos-0.1.1.tar.gz/aeglos-0.1.1/src/aeglos/interfaces/GlobalMessages.py
@@ -0,0 +1,8 @@
+malicious_input_found_message='''
+Critical System Alert: Immediate Action Required - Malicious Input Detected! 
+The system has identified a  security threat through input that is potentially harmful or malicious. 
+This poses a significant risk to system integrity and data security.
+Urgent Recommendations: Do Not Proceed - Cease all current activities and do not input any further data. 
+'''
+suspicious_message_on_invoke="That input was somewhat supicious!"
+check_unable="NOT ABLE TO ERROR CHECK"
