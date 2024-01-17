51ec7f96dfa103da717f72496b472001d272cffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:44 +0000

    Add package genaiterminal/0.1.0/genaiterminal-0.1.0-py3-none-any.whl

---
+++ packages/genaiterminal/genaiterminal-0.1.0-py3-none-any.whl/genaiterminal/__init__.py
@@ -0,0 +1,3 @@
+from .generate_code import generate_code
+from .config import configure_api_key
+
---
+++ packages/genaiterminal/genaiterminal-0.1.0-py3-none-any.whl/genaiterminal/generate_code.py
@@ -0,0 +1,29 @@
+import google.generativeai as genai
+
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
+
+safety_settings = [
+    {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+    {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
+]
+
+model = genai.GenerativeModel(
+    model_name="gemini-pro", generation_config=generation_config, safety_settings=safety_settings
+)
+
+def generate_code(prompt):
+    response = model.generate_content([prompt])
+
+    full_text = ""
+    for part in response.parts:
+        full_text += part.text
+
+    return full_text
+
