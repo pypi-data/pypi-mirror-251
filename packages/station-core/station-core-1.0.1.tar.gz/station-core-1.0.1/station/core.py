399482b5a612cc8f37428e86efd4172104b9015d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:18 +0000

    Add package station-core/1.0.1/station-core-1.0.1.tar.gz

---
+++ packages/station-core/station-core-1.0.1.tar.gz/station-core-1.0.1/station/core.py
@@ -0,0 +1,19 @@
+import importlib
+
+
+def load_plugins(plugins):
+    configured_plugins = {}
+    for plugin in plugins:
+        module = importlib.import_module(f"station.plugins.{plugin}.plugin")
+        configured_plugins[plugin] = module
+
+    return configured_plugins
+
+
+class Core:
+    def __init__(self, plugins):
+        self.configured_plugins = load_plugins(plugins)
+
+    def display(self):
+        for configured_plugin in self.configured_plugins.values():
+            configured_plugin.greeting()
