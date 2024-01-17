4ee43bcaaa033ecee8feb80564bffb1f883b653f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:31 +0000

    Add package test-poc-confused/99.99.99/test-poc-confused-99.99.99.tar.gz

---
+++ packages/test-poc-confused/test-poc-confused-99.99.99.tar.gz/test-poc-confused-99.99.99/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+version = '99.99.99'
+malice = True
+
+setup(
+    name='test-poc-confused',
+    version=version,
+    author='Confused',
+    author_email='confused@awooo.ru',
+    description=f'Malice: {malice}',
+    packages=[],
+    install_requires=[],
+)
