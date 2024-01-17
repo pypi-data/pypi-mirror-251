2e53a1d69a08b31d8f98e349f2c1805d50dbb647
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:04:34 +0000

    Add package bigau-probability/3.0/bigau_probability-3.0.tar.gz

---
+++ packages/bigau-probability/bigau_probability-3.0.tar.gz/bigau_probability-3.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+# Display README.md
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='bigau_probability',
+      version='3.0',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      description='Gaussian and Binomial distributions',
+      packages=['bigau_probability'],
+      author = 'Grace Omotoso',
+      author_email = 'gracomot@gmail.com',
+      zip_safe=False)
