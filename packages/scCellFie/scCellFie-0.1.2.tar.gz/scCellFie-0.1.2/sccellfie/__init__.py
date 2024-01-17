10f7347c316d9cc85be3caeb2ad051d9a0a23322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 03:06:13 +0000

    Add package scCellFie/0.1.2/scCellFie-0.1.2.tar.gz

---
+++ packages/scCellFie/scCellFie-0.1.2.tar.gz/scCellFie-0.1.2/sccellfie/__init__.py
@@ -0,0 +1,9 @@
+from . import external
+from . import gene_score
+from . import metabolic_task
+from . import reaction_activity
+from . import spatial
+from . import stats as tl
+from .expression import thresholds, aggregation
+
+__version__ = "0.1.2"
