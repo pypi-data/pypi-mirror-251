ec8d327afa4a2d501dab44678eeca7c797581748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:18 +0000

    Add package aio-scrapy/2.0.8/aio_scrapy-2.0.8-py3-none-any.whl

---
+++ packages/aio-scrapy/aio_scrapy-2.0.8-py3-none-any.whl/aioscrapy/dupefilters/__init__.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+
+from aioscrapy import Request, Spider
+
+
+class DupeFilterBase(metaclass=ABCMeta):
+    """Request Fingerprint duplicates filter"""
+
+    @classmethod
+    @abstractmethod
+    def from_crawler(cls, crawler: "aioscrapy.crawler.Crawler"):
+        """ Get Instance of RFPDupeFilter from crawler """
+
+    @abstractmethod
+    async def request_seen(self, request: Request) -> bool:
+        """ Check whether fingerprint of request exists """
+
+    @abstractmethod
+    async def close(self, reason: str = '') -> None:
+        """ Delete data on close """
+
+    @abstractmethod
+    def log(self, request: Request, spider: Spider) -> None:
+        """ Logs given request """
