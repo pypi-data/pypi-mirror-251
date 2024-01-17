ceab307bc2b0eac7d0a93439b1903af5515e204f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 14:05:20 +0000

    Add package easy-utils-dev/1.0.3/easy_utils_dev-1.0.3.tar.gz

---
+++ packages/easy-utils-dev/easy_utils_dev-1.0.3.tar.gz/easy_utils_dev-1.0.3/easy_utils_dev/custom_env.py
@@ -0,0 +1,21 @@
+import os
+
+'''
+in custom env. you can setup custom environment and store any object. not like normal env...
+which can store only strings.
+'''
+
+_custom_env = {}
+
+def enject_osenv() :
+    for key , value in os.environ.items():
+        _custom_env[key] = value
+
+def custom_env():
+    return _custom_env
+
+
+
+
+
+
