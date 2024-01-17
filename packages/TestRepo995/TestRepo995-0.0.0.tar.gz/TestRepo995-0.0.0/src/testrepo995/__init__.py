2a92fa568fdcc2c93e363e3b73de68c52dde08db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:00 +0000

    Add package TestRepo995/0.0.0/TestRepo995-0.0.0.tar.gz

---
+++ packages/TestRepo995/TestRepo995-0.0.0.tar.gz/TestRepo995-0.0.0/src/testrepo995/__init__.py
@@ -0,0 +1,38 @@
+"""TestRepo995
+
+Coolest Project Ever
+
+Replace this text with a short summary of TestRepo995, describing
+its purpose and main features. By default, this text is displayed on the repository's main README
+file, on the homepage of the project's website, on the project's PyPI and TestPyPI pages, and on the
+package's main docstring. Like all other entries in your repository's control center, this text can
+also contain dynamic references to other entries. By default, the first occurrence of the name of
+the project in this text is styled as bold/italic and linked to the project's website. The text can
+also contain (GitHub Flavored) HTML syntax, for example, to make text <b>bold</b>, <i>italic</i>, or
+to add <a href="https://pypackit.repodynamics.com">links</a>.
+
+Copyright 2024 Armin Ariamajd
+Copyright (C) 2024 Armin Ariamajd
+
+This program is free software: you can redistribute it and/or
+modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software
+Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is
+distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied
+warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public
+License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""  # noqa: D400
+
+
+__version_details__ = {"version": "0.0.0"}
+__version__ = __version_details__["version"]
