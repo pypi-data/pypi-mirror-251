6f7bd3fe39e8d761d2b00ef400dc4c111d01665f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 16:04:31 +0000

    Add package iamai/3.3.2/iamai-3.3.2-py3-none-any.whl

---
+++ packages/iamai/iamai-3.3.2-py3-none-any.whl/iamai/__init__.py
@@ -0,0 +1,32 @@
+"""iamai
+
+简单的 Python 异步多后端机器人框架
+
+本模块从子模块导入了以下内容：
+- `Bot` => [`iamai.bot.Bot`](./bot#Bot)
+- `Event` => [`iamai.event.Event`](./event#Event)
+- `MessageEvent` => [`iamai.event.MessageEvent`](./event#MessageEvent)
+- `Plugin` => [`iamai.plugin.Plugin`](./plugin#Plugin)
+- `Adapter` => [`iamai.adapter.Adapter`](./adapter/#Adapter)
+- `ConfigModel` => [`iamai.config.ConfigModel`](./config#ConfigModel)
+- `Depends` => [`iamai.dependencies.Depends`](./dependencies#Depends)
+- `Cli` => [`iamai.cli.Cli`](./cli#Cli)
+"""
+from iamai.adapter import Adapter
+from iamai.bot import Bot
+from iamai.cli import Cli
+from iamai.config import ConfigModel
+from iamai.dependencies import Depends
+from iamai.event import Event, MessageEvent
+from iamai.plugin import Plugin
+
+__all__ = [
+    "Adapter",
+    "Bot",
+    "ConfigModel",
+    "Depends",
+    "Event",
+    "MessageEvent",
+    "Plugin",
+    "Cli",
+]
