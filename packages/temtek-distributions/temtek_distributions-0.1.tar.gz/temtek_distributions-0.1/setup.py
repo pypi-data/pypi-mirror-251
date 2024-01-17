4aacdc655471bb0b09975a5604c4c8566a0a3e4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 05:04:25 +0000

    Add package temtek-distributions/0.1/temtek_distributions-0.1.tar.gz

---
+++ packages/temtek-distributions/temtek_distributions-0.1.tar.gz/temtek_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='temtek_distributions',
+      version='0.1',
+      description='Gaussian distributions and Binomial distributions',
+      packages=['temtek_distributions'],
+      author='Temesghen Tekeste',
+      author_email='temesghen.dev@gmail.com',
+      zip_safe=False)
