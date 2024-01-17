6e0bcb559156fc83c6ada276a6b812e267ac1329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 17:03:55 +0000

    Add package etcher/0.0.20/etcher-0.0.20.tar.gz

---
+++ packages/etcher/etcher-0.0.20.tar.gz/etcher-0.0.20/src/lib.rs
@@ -0,0 +1,100 @@
+#![warn(clippy::disallowed_types)]
+
+use std::ops::Deref;
+
+use colored::Colorize;
+use config::PY_CONTEXT;
+use pyo3::{exceptions::PyValueError, prelude::*};
+use pythonize::depythonize;
+
+mod args;
+mod config;
+mod init;
+mod read;
+mod render;
+mod run;
+mod utils;
+
+// If one of these is the first argument, won't auto assume etch render subcommand
+const ETCH_ROOT_ARGS: &[&str] = &["-h", "--help", "help", "-V", "--version", "version"];
+
+#[pyfunction]
+pub fn cli() {
+    match run::run() {
+        Ok(_) => std::process::exit(0),
+        Err(e) => {
+            #[allow(clippy::print_stderr)]
+            {
+                eprintln!("{}", "etch failed".red().bold());
+                eprintln!("{e}");
+            }
+            std::process::exit(1);
+        }
+    }
+}
+
+#[pyfunction]
+#[pyo3(name = "register_function")]
+pub fn py_register_function(py: Python, py_fn: &PyAny) -> PyResult<()> {
+    config::register_py_func(py, py_fn)?;
+    Ok(())
+}
+
+/// Get the current context as a Python dictionary to be used in custom user functions.
+#[pyfunction]
+#[pyo3(name = "context")]
+pub fn py_context(py: Python) -> PyResult<PyObject> {
+    let py_ctx = PY_CONTEXT.lock();
+    if let Some(py_ctx) = py_ctx.deref() {
+        Ok(py_ctx.clone_ref(py))
+    } else {
+        Err(PyValueError::new_err(
+            "Context not registered. This should only be called by custom user extensions.",
+        ))
+    }
+}
+
+#[pyfunction]
+#[pyo3(name = "_toml_update")]
+pub fn py_toml_update(
+    initial: &str,
+    update: Option<&PyAny>,
+    remove: Option<&PyAny>,
+) -> PyResult<String> {
+    let update: Option<serde_json::Value> = if let Some(update) = update {
+        depythonize(update)?
+    } else {
+        None
+    };
+    let remove: Option<Vec<Vec<String>>> = if let Some(remove) = remove {
+        depythonize(remove)?
+    } else {
+        None
+    };
+    Ok(utils::toml::update(initial, update, remove)?)
+}
+
+#[pyfunction]
+#[pyo3(name = "_hash_contents")]
+pub fn py_hash_contents(contents: &str) -> PyResult<String> {
+    Ok(bitbazaar::hash::fnv1a(contents.as_bytes()).to_string())
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+#[pyo3(name = "_rs")]
+fn root_module(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(cli, m)?)?;
+
+    m.add_function(wrap_pyfunction!(py_register_function, m)?)?;
+
+    m.add_function(wrap_pyfunction!(py_context, m)?)?;
+
+    m.add_function(wrap_pyfunction!(py_toml_update, m)?)?;
+
+    m.add_function(wrap_pyfunction!(py_hash_contents, m)?)?;
+
+    Ok(())
+}
---
+++ packages/etcher/etcher-0.0.20.tar.gz/etcher-0.0.20/src/run.rs
@@ -0,0 +1,69 @@
+use bitbazaar::{
+    errors::TracedErr,
+    logging::{setup_logger, LogTarget},
+    timing::GLOBAL_TIME_RECORDER,
+};
+use clap::{Parser, Subcommand};
+use log::debug;
+
+use crate::{
+    args::{self, get_py_args, get_version_info},
+    init, read, render, ETCH_ROOT_ARGS,
+};
+
+pub fn run() -> Result<(), TracedErr> {
+    let mut py_args = get_py_args()?;
+
+    // Clap doesn't support default subcommands but we want to run `render` by
+    // default for convenience, so we just preprocess the arguments accordingly before passing them to Clap.
+    let arg1 = py_args.get(1);
+    let add = {
+        if let Some(arg1) = arg1 {
+            // If the first argument isn't already a subcommand, and isn't a specific root arg/option, true:
+            !args::Command::has_subcommand(arg1) && !ETCH_ROOT_ARGS.contains(&arg1.as_str())
+        } else {
+            true
+        }
+    };
+    if add {
+        py_args.insert(1, "render".into());
+    }
+
+    let args = args::Args::parse_from(py_args);
+
+    let logger = setup_logger(vec![LogTarget {
+        msg_prefix: Some("etch".to_string()),
+        // If a read command and verbose hasn't been entered, only log errors to prevent polluting the chainable print output.
+        level_filter: if !args.log_level_args.verbose {
+            match &args.command {
+                args::Command::ReadConfig(_) => log::LevelFilter::Error,
+                args::Command::ReadVar(_) => log::LevelFilter::Error,
+                _ => args.log_level_args.level_filter(),
+            }
+        } else {
+            args.log_level_args.level_filter()
+        },
+        include_ts_till: Some(log::LevelFilter::Debug),
+        variant: bitbazaar::logging::LogTargetVariant::Stdout {},
+        ..Default::default()
+    }])?;
+    logger.apply()?;
+
+    let result = match &args.command {
+        args::Command::Render(render) => {
+            render::render(&args, render)?;
+            Ok(())
+        }
+        args::Command::ReadConfig(read_config) => Ok(read::read_config(&args, read_config)?),
+        args::Command::ReadVar(read_var) => Ok(read::read_var(&args, read_var)?),
+        args::Command::Init(init) => Ok(init::init(init)?),
+        args::Command::Version { output_format: _ } => {
+            println!("etch {}", get_version_info());
+            Ok(())
+        }
+    };
+
+    debug!("{}", GLOBAL_TIME_RECORDER.format_verbose()?);
+
+    result
+}
