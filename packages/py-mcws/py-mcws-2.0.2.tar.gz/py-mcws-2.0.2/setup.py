8f85a514c85f4d33c22365f707d500387be34301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 04:03:28 +0000

    Add package py-mcws/2.0.2/py-mcws-2.0.2.tar.gz

---
+++ packages/py-mcws/py-mcws-2.0.2.tar.gz/py-mcws-2.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# long_description(後述)に、GitHub用のREADME.mdを指定
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+
+setup(
+    name='py-mcws',
+    version="2.0.2",
+    description="Minecraft Bedrock WebSocket",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='HRTK92',
+    url="https://github.com/HRTK92/py-mcws",
+    license='MIT',
+    install_requires=_requires_from_file('requirements.txt'),
+    packages=['py_mcws']
+)
