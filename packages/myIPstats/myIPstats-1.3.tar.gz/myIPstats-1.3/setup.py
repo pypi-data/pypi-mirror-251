457f22346c851aefed62a5ccbf9db78761b6c63d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 14:05:06 +0000

    Add package myIPstats/1.3/myIPstats-1.3.tar.gz

---
+++ packages/myIPstats/myIPstats-1.3.tar.gz/myIPstats-1.3/setup.py
@@ -0,0 +1,29 @@
+# Setup file for myIPstats package on PyPI.
+
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="myIPstats",
+    version="1.3",
+    author="Eric Dennis",
+    author_email="ericdennis11@gmail.com",
+    license="MIT",
+    url="https://github.com/ericdennis7/myIPstats",
+    
+    description="A module to fetch your IP address information.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    package_dir={"myIPstats": "myIPstats"},
+
+    packages=find_packages(),
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
