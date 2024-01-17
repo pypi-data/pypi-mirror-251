c834a6f9d89c47f814040e76c52f005b853cfca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-17 15:05:55 +0000

    Add package normbipy/0.1/normbipy-0.1.tar.gz

---
+++ packages/normbipy/normbipy-0.1.tar.gz/normbipy-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='normbipy',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['normbipy'],
+      author=['Mohamed Nabil'],
+      zip_safe=False)
