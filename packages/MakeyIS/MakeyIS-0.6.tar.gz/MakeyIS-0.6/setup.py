ee4610dc3c03116a1f1d146e761167615144e3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:42 +0000

    Add package MakeyIS/0.6/MakeyIS-0.6.tar.gz

---
+++ packages/MakeyIS/MakeyIS-0.6.tar.gz/MakeyIS-0.6/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='MakeyIS',
+    version='0.6',
+    author='Makeenkov Igor',
+    author_email='pro100igo228@gmail.com',
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
+        'allure-pytest',
+        'allure-python-commons',
+    ],
+    description='Библиотека обеспечивает усовершенствованное управление параметрами PyTest и логирование, упрощая создание и отладку автоматизированных тестов за счет интуитивно понятных декораторов и продвинутых функций логирования. Она идеально подходит для повышения эффективности и наглядности процесса тестирования.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
