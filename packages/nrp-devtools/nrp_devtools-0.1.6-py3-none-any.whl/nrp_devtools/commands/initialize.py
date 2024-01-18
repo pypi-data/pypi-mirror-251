a3d19328d69c1fd2715755cdca07ba31aec5e4c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 05:04:20 +0000

    Add package nrp-devtools/0.1.6/nrp_devtools-0.1.6-py3-none-any.whl

---
+++ packages/nrp-devtools/nrp_devtools-0.1.6-py3-none-any.whl/nrp_devtools/commands/initialize.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+from nrp_devtools.commands.utils import run_cookiecutter
+from nrp_devtools.config import OARepoConfig
+from nrp_devtools.x509 import generate_selfsigned_cert
+
+
+def initialize_repository(config: OARepoConfig):
+    run_cookiecutter(
+        config.repository_dir.parent,
+        template=Path(__file__).parent.parent / "templates" / "repository",
+        extra_context={
+            "repository_name": config.repository_dir.name,
+            "shared_package": config.repository.shared_package,
+            "repository_package": config.repository.repository_package,
+            "repository_human_name": config.repository.repository_human_name,
+            "ui_package": config.repository.ui_package,
+            "model_package": config.repository.model_package,
+            "oarepo_version": config.repository.oarepo_version,
+        },
+    )
+
+    # generate the certificate
+    cert, key = generate_selfsigned_cert("localhost", ["127.0.0.1"])
+    (config.repository_dir / "docker" / "development.crt").write_bytes(cert)
+    (config.repository_dir / "docker" / "development.key").write_bytes(key)
+
+    # link the variables
+    (config.repository_dir / "docker" / ".env").symlink_to(
+        config.repository_dir / "variables"
+    )
+
+    # mark the nrp command executable
+    (config.repository_dir / "nrp").chmod(0o755)
---
+++ packages/nrp-devtools/nrp_devtools-0.1.6-py3-none-any.whl/nrp_devtools/templates/repository/{{cookiecutter.repository_name}}/i18n/webpack.py
@@ -0,0 +1,18 @@
+from invenio_assets.webpack import WebpackThemeBundle
+
+theme = WebpackThemeBundle(
+    __name__,
+    ".",
+    default="semantic-ui",
+    themes={
+        "semantic-ui": {
+            "entry": {},
+            "dependencies": {
+            },
+            "devDependencies": {},
+            "aliases": {
+                "@translations/i18next": "translations/i18next.js",
+            },
+        }
+    },
+)
