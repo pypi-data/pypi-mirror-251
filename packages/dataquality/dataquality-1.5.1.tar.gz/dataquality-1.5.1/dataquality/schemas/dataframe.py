7b1dab6aa56006d488ff336eb18317daee4c912e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 04:04:27 +0000

    Add package dataquality/1.5.1/dataquality-1.5.1.tar.gz

---
+++ packages/dataquality/dataquality-1.5.1.tar.gz/dataquality-1.5.1/dataquality/schemas/dataframe.py
@@ -0,0 +1,30 @@
+from dataclasses import dataclass
+from enum import Enum, unique
+
+from pydantic import BaseModel
+from vaex.dataframe import DataFrame
+
+
+class BaseLoggerDataFrames(BaseModel):
+    prob: DataFrame
+    emb: DataFrame
+    data: DataFrame
+
+    class Config:
+        arbitrary_types_allowed = True
+
+
+@unique
+class FileType(str, Enum):
+    """Valid file extensions for an exported dataframe"""
+
+    arrow = "arrow"
+    parquet = "parquet"
+    json = "json"
+    csv = "csv"
+
+
+@dataclass
+class DFVar:
+    skip_upload: str = "skip_upload"
+    progress_name: str = "progress_name"
