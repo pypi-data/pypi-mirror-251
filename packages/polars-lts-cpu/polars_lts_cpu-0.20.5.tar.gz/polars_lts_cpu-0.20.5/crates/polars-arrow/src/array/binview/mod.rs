78b6b6bc3dc005bee675de0afbdc17a1839459b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:04:57 +0000

    Add package polars-lts-cpu/0.20.5/polars_lts_cpu-0.20.5.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.20.5.tar.gz/polars_lts_cpu-0.20.5/crates/polars-arrow/src/array/binview/mod.rs
@@ -0,0 +1,417 @@
+//! See thread: https://lists.apache.org/thread/w88tpz76ox8h3rxkjl4so6rg3f1rv7wt
+mod ffi;
+pub(super) mod fmt;
+mod iterator;
+mod mutable;
+mod view;
+
+use std::any::Any;
+use std::fmt::Debug;
+use std::marker::PhantomData;
+use std::sync::Arc;
+
+use polars_error::*;
+
+use crate::array::Array;
+use crate::bitmap::Bitmap;
+use crate::buffer::Buffer;
+use crate::datatypes::ArrowDataType;
+
+mod private {
+    pub trait Sealed: Send + Sync {}
+
+    impl Sealed for str {}
+    impl Sealed for [u8] {}
+}
+pub use mutable::MutableBinaryViewArray;
+use private::Sealed;
+
+use crate::array::binview::iterator::BinaryViewValueIter;
+use crate::array::binview::view::{
+    validate_binary_view, validate_utf8_only_view, validate_utf8_view,
+};
+use crate::array::iterator::NonNullValuesIter;
+use crate::bitmap::utils::{BitmapIter, ZipValidity};
+
+pub type BinaryViewArray = BinaryViewArrayGeneric<[u8]>;
+pub type Utf8ViewArray = BinaryViewArrayGeneric<str>;
+
+pub trait ViewType: Sealed + 'static + PartialEq + AsRef<Self> {
+    const IS_UTF8: bool;
+    const DATA_TYPE: ArrowDataType;
+    type Owned: Debug + Clone + Sync + Send + AsRef<Self>;
+
+    /// # Safety
+    /// The caller must ensure `index < self.len()`.
+    unsafe fn from_bytes_unchecked(slice: &[u8]) -> &Self;
+
+    fn to_bytes(&self) -> &[u8];
+
+    #[allow(clippy::wrong_self_convention)]
+    fn into_owned(&self) -> Self::Owned;
+}
+
+impl ViewType for str {
+    const IS_UTF8: bool = true;
+    const DATA_TYPE: ArrowDataType = ArrowDataType::Utf8View;
+    type Owned = String;
+
+    #[inline(always)]
+    unsafe fn from_bytes_unchecked(slice: &[u8]) -> &Self {
+        std::str::from_utf8_unchecked(slice)
+    }
+
+    #[inline(always)]
+    fn to_bytes(&self) -> &[u8] {
+        self.as_bytes()
+    }
+
+    fn into_owned(&self) -> Self::Owned {
+        self.to_string()
+    }
+}
+
+impl ViewType for [u8] {
+    const IS_UTF8: bool = false;
+    const DATA_TYPE: ArrowDataType = ArrowDataType::BinaryView;
+    type Owned = Vec<u8>;
+
+    #[inline(always)]
+    unsafe fn from_bytes_unchecked(slice: &[u8]) -> &Self {
+        slice
+    }
+
+    #[inline(always)]
+    fn to_bytes(&self) -> &[u8] {
+        self
+    }
+
+    fn into_owned(&self) -> Self::Owned {
+        self.to_vec()
+    }
+}
+
+pub struct BinaryViewArrayGeneric<T: ViewType + ?Sized> {
+    data_type: ArrowDataType,
+    views: Buffer<u128>,
+    buffers: Arc<[Buffer<u8>]>,
+    // Raw buffer access. (pointer, len).
+    raw_buffers: Arc<[(*const u8, usize)]>,
+    validity: Option<Bitmap>,
+    phantom: PhantomData<T>,
+    /// Total bytes length if we would concatenate them all.
+    total_bytes_len: usize,
+    /// Total bytes in the buffer (excluding remaining capacity)
+    total_buffer_len: usize,
+}
+
+impl<T: ViewType + ?Sized> Clone for BinaryViewArrayGeneric<T> {
+    fn clone(&self) -> Self {
+        Self {
+            data_type: self.data_type.clone(),
+            views: self.views.clone(),
+            buffers: self.buffers.clone(),
+            raw_buffers: self.raw_buffers.clone(),
+            validity: self.validity.clone(),
+            phantom: Default::default(),
+            total_bytes_len: self.total_bytes_len,
+            total_buffer_len: self.total_buffer_len,
+        }
+    }
+}
+
+unsafe impl<T: ViewType + ?Sized> Send for BinaryViewArrayGeneric<T> {}
+unsafe impl<T: ViewType + ?Sized> Sync for BinaryViewArrayGeneric<T> {}
+
+fn buffers_into_raw<T>(buffers: &[Buffer<T>]) -> Arc<[(*const T, usize)]> {
+    buffers
+        .iter()
+        .map(|buf| (buf.storage_ptr(), buf.len()))
+        .collect()
+}
+
+impl<T: ViewType + ?Sized> BinaryViewArrayGeneric<T> {
+    /// # Safety
+    /// The caller must ensure
+    /// - the data is valid utf8 (if required)
+    /// - The offsets match the buffers.
+    pub unsafe fn new_unchecked(
+        data_type: ArrowDataType,
+        views: Buffer<u128>,
+        buffers: Arc<[Buffer<u8>]>,
+        validity: Option<Bitmap>,
+        total_bytes_len: usize,
+        total_buffer_len: usize,
+    ) -> Self {
+        let raw_buffers = buffers_into_raw(&buffers);
+        Self {
+            data_type,
+            views,
+            buffers,
+            raw_buffers,
+            validity,
+            phantom: Default::default(),
+            total_bytes_len,
+            total_buffer_len,
+        }
+    }
+
+    /// Create a new BinaryViewArray but initialize a statistics compute.
+    /// # Safety
+    /// The caller must ensure the invariants
+    pub unsafe fn new_unchecked_unknown_md(
+        data_type: ArrowDataType,
+        views: Buffer<u128>,
+        buffers: Arc<[Buffer<u8>]>,
+        validity: Option<Bitmap>,
+    ) -> Self {
+        let total_bytes_len = views.iter().map(|v| (*v as u32) as usize).sum();
+        let total_buffer_len = buffers.iter().map(|b| b.len()).sum();
+        Self::new_unchecked(
+            data_type,
+            views,
+            buffers,
+            validity,
+            total_bytes_len,
+            total_buffer_len,
+        )
+    }
+
+    pub fn data_buffers(&self) -> &Arc<[Buffer<u8>]> {
+        &self.buffers
+    }
+
+    pub fn variadic_buffer_lengths(&self) -> Vec<i64> {
+        self.buffers.iter().map(|buf| buf.len() as i64).collect()
+    }
+
+    pub fn views(&self) -> &Buffer<u128> {
+        &self.views
+    }
+
+    pub fn try_new(
+        data_type: ArrowDataType,
+        views: Buffer<u128>,
+        buffers: Arc<[Buffer<u8>]>,
+        validity: Option<Bitmap>,
+    ) -> PolarsResult<Self> {
+        if T::IS_UTF8 {
+            validate_utf8_view(views.as_ref(), buffers.as_ref())?;
+        } else {
+            validate_binary_view(views.as_ref(), buffers.as_ref())?;
+        }
+
+        if let Some(validity) = &validity {
+            polars_ensure!(validity.len()== views.len(), ComputeError: "validity mask length must match the number of values" )
+        }
+
+        unsafe {
+            Ok(Self::new_unchecked_unknown_md(
+                data_type, views, buffers, validity,
+            ))
+        }
+    }
+
+    /// Creates an empty [`BinaryViewArrayGeneric`], i.e. whose `.len` is zero.
+    #[inline]
+    pub fn new_empty(data_type: ArrowDataType) -> Self {
+        unsafe { Self::new_unchecked(data_type, Buffer::new(), Arc::from([]), None, 0, 0) }
+    }
+
+    /// Returns a new null [`BinaryViewArrayGeneric`] of `length`.
+    #[inline]
+    pub fn new_null(data_type: ArrowDataType, length: usize) -> Self {
+        let validity = Some(Bitmap::new_zeroed(length));
+        unsafe {
+            Self::new_unchecked(
+                data_type,
+                Buffer::zeroed(length),
+                Arc::from([]),
+                validity,
+                0,
+                0,
+            )
+        }
+    }
+
+    /// Returns the element at index `i`
+    /// # Panics
+    /// iff `i >= self.len()`
+    #[inline]
+    pub fn value(&self, i: usize) -> &T {
+        assert!(i < self.len());
+        unsafe { self.value_unchecked(i) }
+    }
+
+    /// Returns the element at index `i`
+    /// # Safety
+    /// Assumes that the `i < self.len`.
+    #[inline]
+    pub unsafe fn value_unchecked(&self, i: usize) -> &T {
+        let v = *self.views.get_unchecked(i);
+        let len = v as u32;
+
+        // view layout:
+        // length: 4 bytes
+        // prefix: 4 bytes
+        // buffer_index: 4 bytes
+        // offset: 4 bytes
+
+        // inlined layout:
+        // length: 4 bytes
+        // data: 12 bytes
+
+        let bytes = if len <= 12 {
+            let ptr = self.views.storage_ptr() as *const u8;
+            std::slice::from_raw_parts(ptr.add(i * 16 + 4), len as usize)
+        } else {
+            let buffer_idx = (v >> 64) as u32;
+            let offset = (v >> 96) as u32;
+            let (data_ptr, data_len) = *self.raw_buffers.get_unchecked(buffer_idx as usize);
+            let data = std::slice::from_raw_parts(data_ptr, data_len);
+            let offset = offset as usize;
+            data.get_unchecked(offset..offset + len as usize)
+        };
+        T::from_bytes_unchecked(bytes)
+    }
+
+    /// Returns an iterator of `Option<&T>` over every element of this array.
+    pub fn iter(&self) -> ZipValidity<&T, BinaryViewValueIter<T>, BitmapIter> {
+        ZipValidity::new_with_validity(self.values_iter(), self.validity.as_ref())
+    }
+
+    /// Returns an iterator of `&[u8]` over every element of this array, ignoring the validity
+    pub fn values_iter(&self) -> BinaryViewValueIter<T> {
+        BinaryViewValueIter::new(self)
+    }
+
+    /// Returns an iterator of the non-null values.
+    pub fn non_null_values_iter(&self) -> NonNullValuesIter<'_, BinaryViewArrayGeneric<T>> {
+        NonNullValuesIter::new(self, self.validity())
+    }
+
+    /// Returns an iterator of the non-null values.
+    pub fn non_null_views_iter(&self) -> NonNullValuesIter<'_, Buffer<u128>> {
+        NonNullValuesIter::new(self.views(), self.validity())
+    }
+
+    impl_sliced!();
+    impl_mut_validity!();
+    impl_into_array!();
+
+    pub fn from<S: AsRef<T>, P: AsRef<[Option<S>]>>(slice: P) -> Self {
+        let mutable = MutableBinaryViewArray::from_iterator(
+            slice.as_ref().iter().map(|opt_v| opt_v.as_ref()),
+        );
+        mutable.into()
+    }
+
+    /// Get the total length of bytes that it would take to concatenate all binary/str values in this array.
+    pub fn total_bytes_len(&self) -> usize {
+        self.total_bytes_len
+    }
+
+    /// Get the length of bytes that are stored in the variadic buffers.
+    pub fn total_buffer_len(&self) -> usize {
+        self.total_buffer_len
+    }
+
+    #[inline(always)]
+    pub fn len(&self) -> usize {
+        self.views.len()
+    }
+}
+
+impl BinaryViewArray {
+    /// Validate the underlying bytes on UTF-8.
+    pub fn validate_utf8(&self) -> PolarsResult<()> {
+        validate_utf8_only_view(&self.views, &self.buffers)
+    }
+
+    /// Convert [`BinaryViewArray`] to [`Utf8ViewArray`].
+    pub fn to_utf8view(&self) -> PolarsResult<Utf8ViewArray> {
+        self.validate_utf8()?;
+        unsafe { Ok(self.to_utf8view_unchecked()) }
+    }
+
+    /// Convert [`BinaryViewArray`] to [`Utf8ViewArray`] without checking UTF-8.
+    ///
+    /// # Safety
+    /// The caller must ensure the underlying data is valid UTF-8.
+    pub unsafe fn to_utf8view_unchecked(&self) -> Utf8ViewArray {
+        Utf8ViewArray::new_unchecked(
+            ArrowDataType::Utf8View,
+            self.views.clone(),
+            self.buffers.clone(),
+            self.validity.clone(),
+            self.total_bytes_len,
+            self.total_buffer_len,
+        )
+    }
+}
+
+impl Utf8ViewArray {
+    pub fn to_binview(&self) -> BinaryViewArray {
+        // SAFETY: same invariants.
+        unsafe {
+            BinaryViewArray::new_unchecked(
+                ArrowDataType::BinaryView,
+                self.views.clone(),
+                self.buffers.clone(),
+                self.validity.clone(),
+                self.total_bytes_len,
+                self.total_buffer_len,
+            )
+        }
+    }
+}
+
+impl<T: ViewType + ?Sized> Array for BinaryViewArrayGeneric<T> {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn Any {
+        self
+    }
+
+    #[inline(always)]
+    fn len(&self) -> usize {
+        BinaryViewArrayGeneric::len(self)
+    }
+
+    fn data_type(&self) -> &ArrowDataType {
+        &self.data_type
+    }
+
+    fn validity(&self) -> Option<&Bitmap> {
+        self.validity.as_ref()
+    }
+
+    fn slice(&mut self, offset: usize, length: usize) {
+        assert!(
+            offset + length <= self.len(),
+            "the offset of the new Buffer cannot exceed the existing length"
+        );
+        unsafe { self.slice_unchecked(offset, length) }
+    }
+
+    unsafe fn slice_unchecked(&mut self, offset: usize, length: usize) {
+        self.validity = self
+            .validity
+            .take()
+            .map(|bitmap| bitmap.sliced_unchecked(offset, length))
+            .filter(|bitmap| bitmap.unset_bits() > 0);
+        self.views.slice_unchecked(offset, length);
+    }
+
+    fn with_validity(&self, validity: Option<Bitmap>) -> Box<dyn Array> {
+        let mut new = self.clone();
+        new.validity = validity;
+        Box::new(new)
+    }
+
+    fn to_boxed(&self) -> Box<dyn Array> {
+        Box::new(self.clone())
+    }
+}
---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.20.5.tar.gz/polars_lts_cpu-0.20.5/crates/polars-plan/src/dsl/function_expr/range/int_range.rs
@@ -0,0 +1,115 @@
+use polars_core::prelude::*;
+use polars_core::series::{IsSorted, Series};
+use polars_core::with_match_physical_integer_polars_type;
+
+use super::utils::{ensure_range_bounds_contain_exactly_one_value, numeric_ranges_impl_broadcast};
+
+const CAPACITY_FACTOR: usize = 5;
+
+pub(super) fn int_range(s: &[Series], step: i64, dtype: DataType) -> PolarsResult<Series> {
+    let mut start = &s[0];
+    let mut end = &s[1];
+    let name = start.name();
+
+    ensure_range_bounds_contain_exactly_one_value(start, end)?;
+    polars_ensure!(dtype.is_integer(), ComputeError: "non-integer `dtype` passed to `int_range`: {:?}", dtype);
+
+    let (start_storage, end_storage);
+    if *start.dtype() != dtype {
+        start_storage = start.strict_cast(&dtype)?;
+        start = &start_storage;
+    }
+    if *end.dtype() != dtype {
+        end_storage = end.strict_cast(&dtype)?;
+        end = &end_storage;
+    }
+
+    with_match_physical_integer_polars_type!(dtype, |$T| {
+        let start_v = get_first_series_value::<$T>(start)?;
+        let end_v = get_first_series_value::<$T>(end)?;
+        int_range_impl::<$T>(start_v, end_v, step, name)
+    })
+}
+
+fn get_first_series_value<T>(s: &Series) -> PolarsResult<T::Native>
+where
+    T: PolarsIntegerType,
+{
+    let ca: &ChunkedArray<T> = s.as_any().downcast_ref().unwrap();
+    let value_opt = ca.get(0);
+    let value =
+        value_opt.ok_or_else(|| polars_err!(ComputeError: "invalid null input for `int_range`"))?;
+    Ok(value)
+}
+
+fn int_range_impl<T>(
+    start: T::Native,
+    end: T::Native,
+    step: i64,
+    name: &str,
+) -> PolarsResult<Series>
+where
+    T: PolarsIntegerType,
+    ChunkedArray<T>: IntoSeries,
+    std::ops::Range<T::Native>: DoubleEndedIterator<Item = T::Native>,
+{
+    let mut ca = match step {
+        0 => polars_bail!(InvalidOperation: "step must not be zero"),
+        1 => ChunkedArray::<T>::from_iter_values(name, start..end),
+        2.. => ChunkedArray::<T>::from_iter_values(name, (start..end).step_by(step as usize)),
+        _ => ChunkedArray::<T>::from_iter_values(
+            name,
+            (end..start)
+                .step_by(step.unsigned_abs() as usize)
+                .map(|x| start - (x - end)),
+        ),
+    };
+
+    let is_sorted = if end < start {
+        IsSorted::Descending
+    } else {
+        IsSorted::Ascending
+    };
+    ca.set_sorted_flag(is_sorted);
+
+    Ok(ca.into_series())
+}
+
+pub(super) fn int_ranges(s: &[Series]) -> PolarsResult<Series> {
+    let start = &s[0];
+    let end = &s[1];
+    let step = &s[2];
+
+    let start = start.cast(&DataType::Int64)?;
+    let end = end.cast(&DataType::Int64)?;
+    let step = step.cast(&DataType::Int64)?;
+
+    let start = start.i64()?;
+    let end = end.i64()?;
+    let step = step.i64()?;
+
+    let len = std::cmp::max(start.len(), end.len());
+    let mut builder = ListPrimitiveChunkedBuilder::<Int64Type>::new(
+        // The name should follow our left hand rule.
+        start.name(),
+        len,
+        len * CAPACITY_FACTOR,
+        DataType::Int64,
+    );
+
+    let range_impl =
+        |start, end, step: i64, builder: &mut ListPrimitiveChunkedBuilder<Int64Type>| {
+            match step {
+                1 => builder.append_iter_values(start..end),
+                2.. => builder.append_iter_values((start..end).step_by(step as usize)),
+                _ => builder.append_iter_values(
+                    (end..start)
+                        .step_by(step.unsigned_abs() as usize)
+                        .map(|x| start - (x - end)),
+                ),
+            };
+            Ok(())
+        };
+
+    numeric_ranges_impl_broadcast(start, end, step, range_impl, &mut builder)
+}
