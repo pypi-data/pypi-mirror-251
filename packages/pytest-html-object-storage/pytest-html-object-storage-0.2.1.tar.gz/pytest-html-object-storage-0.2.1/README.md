1649a9aefba48677e481e0d3c1fe89f464b7a7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 03:03:56 +0000

    Add package pytest-html-object-storage/0.2.1/pytest-html-object-storage-0.2.1.tar.gz

---
+++ packages/pytest-html-object-storage/pytest-html-object-storage-0.2.1.tar.gz/pytest-html-object-storage-0.2.1/README.md
@@ -0,0 +1,72 @@
+# pytest-html-object-storage
+
+Pytest report plugin for send HTML report on object-storage
+
+Allow to send HTML report on object-storage.
+
+## Implementations
+
+* [MinIO](https://min.io/) (tested with Scaleway)
+* [Swift](https://docs.openstack.org/python-swiftclient/newton/swiftclient.html) (tested with OVH)
+
+## installation
+
+    pip install pytest-html-object-storage
+
+## Configure via env var
+
+### Common
+
+    OBJECT_STORAGE_ENDPOINT="localhost:9000"
+    OBJECT_STORAGE_BUCKET="bucket"
+    OBJECT_STORAGE_USERNAME="admin"
+    OBJECT_STORAGE_PASSWORD="password"
+    OBJECT_STORAGE_REGION_NAME=""
+
+### Common Optional
+
+    OBJECT_STORAGE_POLICY="private" // public-read
+    OBJECT_STORAGE_RETENTION="30" // day unit
+
+### Specific MinIO
+
+#### Optional
+
+    OBJECT_STORAGE_SECURE="false"
+    OBJECT_STORAGE_PROVIDER="default" // scaleway
+
+### Specific Swift
+
+    OBJECT_STORAGE_TENANT_ID=""
+    OBJECT_STORAGE_TENANT_NAME=""
+
+## Add option to send HTML report
+
+### MinIO
+
+    pytest --store-minio
+
+### Swift
+
+    pytest --store-swift
+
+## Dev
+
+### Change version
+
+edit
+
+    pytest_html_object_storage/__init__.py
+
+commit
+
+    git commit -m "v0.1.0"
+
+tag
+
+    git tag v0.1.0
+
+### Build package
+
+    python -m build
+    twine upload dist/*
---
+++ packages/pytest-html-object-storage/pytest-html-object-storage-0.2.1.tar.gz/pytest-html-object-storage-0.2.1/pytest_html_object_storage/minio.py
@@ -0,0 +1,163 @@
+import io
+import json
+import logging
+import os
+import uuid
+from typing import Union
+
+import pytest
+from _pytest.config import Config, ExitCode
+from _pytest.main import Session
+from _pytest.terminal import TerminalReporter
+from minio import Minio
+from minio.commonconfig import ENABLED, Filter
+from minio.lifecycleconfig import Rule, Expiration, LifecycleConfig
+
+
+log = logging.getLogger(__name__)
+
+
+class HTMLMinio:
+    def __init__(self, config: Config):
+        self.config = config
+        self.os_endpoint = os.environ.get("OBJECT_STORAGE_ENDPOINT")
+        self.os_bucket = os.environ.get("OBJECT_STORAGE_BUCKET")
+        self.os_username = os.environ.get("OBJECT_STORAGE_USERNAME")
+        self.os_password = os.environ.get("OBJECT_STORAGE_PASSWORD")
+        self.os_region_name = os.environ.get("OBJECT_STORAGE_REGION_NAME")
+        self.os_scheme, self.os_secure = self._get_secure()
+        self.os_retention = self._get_retention()
+        self.os_policy = self._get_policy()
+        self.os_provider = os.environ.get("OBJECT_STORAGE_PROVIDER")
+        self.os_http_report_url = os.environ.get("HTTP_REPORT_URL")
+        self.access_url = None
+
+
+    def get_access_url(self, name: str) -> str:
+        if self.os_http_report_url:
+            self.access_url = (
+                f"{self.os_scheme}://{self.os_http_report_url}/{self.os_bucket}/{name}"
+            )
+        else:
+            self.access_url = (
+                f"{self.os_scheme}://{self.os_endpoint}/{self.os_bucket}/{name}"
+            )
+
+        return self.access_url
+
+
+    @staticmethod
+    def _get_secure() -> [str, bool]:
+        if os.environ.get("OBJECT_STORAGE_SECURE") == "false":
+            return "http", False
+        else:
+            return "https", True
+
+
+    @staticmethod
+    def _get_retention() -> int:
+        retention = os.environ.get("OBJECT_STORAGE_RETENTION")
+        if retention and int(retention) > 0:
+            return int(retention)
+        else:
+            return 0
+
+
+    def _get_policy(self) -> str:
+        policy = os.environ.get("OBJECT_STORAGE_POLICY")
+        if not policy or policy == "public-read":
+            return "public-read"
+        else:
+            return ""
+
+
+    def send_html(self, name, contentfile: str):
+        client = Minio(
+            self.os_endpoint,
+            self.os_username,
+            self.os_password,
+            region=self.os_region_name,
+            secure=self.os_secure,
+        )
+        found = client.bucket_exists(self.os_bucket)
+        if not found:
+            client.make_bucket(self.os_bucket)
+            if self.os_policy == "public-read":
+                if not self.os_provider or self.os_provider == "scaleway":
+                    policy = {
+                        "Version": "2012-10-17",
+                        "Id": f"{self.os_bucket}Policy",
+                        "Statement": [
+                            {
+                                "Sid": "Grant List and GET to everyone",
+                                "Effect": "Allow",
+                                "Principal": "*",
+                                "Action": ["s3:ListBucket", "s3:GetObject"],
+                                "Resource": [self.os_bucket, f"{self.os_bucket}/*"],
+                            }
+                        ],
+                    }
+                else:
+                    policy = {
+                        "Version": "2012-10-17",
+                        "Statement": [
+                            {
+                                "Effect": "Allow",
+                                "Principal": {"AWS": "*"},
+                                "Action": ["s3:GetBucketLocation", "s3:ListBucket"],
+                                "Resource": f"arn:aws:s3:::{self.os_bucket}",
+                            },
+                            {
+                                "Effect": "Allow",
+                                "Principal": {"AWS": "*"},
+                                "Action": "s3:GetObject",
+                                "Resource": f"arn:aws:s3:::{self.os_bucket}/*",
+                            },
+                        ],
+                    }
+                client.set_bucket_policy(self.os_bucket, json.dumps(policy))
+            if self.os_retention:
+                config = LifecycleConfig(
+                    [
+                        Rule(
+                            ENABLED,
+                            rule_filter=Filter(prefix="*/"),
+                            rule_id="rule_retention",
+                            expiration=Expiration(days=self.os_retention),
+                        ),
+                    ],
+                )
+                client.set_bucket_lifecycle(self.os_bucket, config)
+        client.fput_object(
+            self.os_bucket,
+            name,
+            contentfile,
+            content_type="text/html",
+        )
+
+
+    @pytest.hookimpl(trylast=True, hookwrapper=True)
+    def pytest_sessionfinish(self, session, exitstatus):
+        outcome = yield
+        htmlfile = getattr(session.config.option, "htmlpath", None)
+        if htmlfile:
+            name = f"{str(uuid.uuid4())}/report.html"
+            try:
+                self.send_html(
+                    name,
+                    htmlfile,
+                )
+                session.config._report_url = self.get_access_url(name)
+            except Exception as e:
+                log.error(f"Minio send_html error: {self.os_endpoint} - {e}")
+
+
+    def pytest_terminal_summary(
+        self,
+        terminalreporter: TerminalReporter,
+        exitstatus: Union[int, ExitCode],
+        config: Config,
+    ):
+        terminalreporter.write_sep(
+            "-", f"HTML report sent on MinIO object storage at {self.access_url}"
+        )
