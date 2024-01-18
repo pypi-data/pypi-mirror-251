60896e4888acb51e8946405ef21d1e08fae6a2c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 05:03:49 +0000

    Add package manga-scrape/4.7.0/manga_scrape-4.7.0-py3-none-any.whl

---
+++ packages/manga-scrape/manga_scrape-4.7.0-py3-none-any.whl/manga/sites/test.py
@@ -0,0 +1,31 @@
+from typing import Callable
+
+import tldextract
+import requests
+
+from .unknown_domain import UnknownDomain
+from .domains import domains
+
+
+def get_domain(url: str) -> str:
+    """
+    Extract the domain portion of a URL
+    """
+    info: tldextract.tldextract.ExtractResult = tldextract.extract(url)
+    return f"{info.domain}.{info.suffix}"
+
+
+def test(url: str, timeout: int = 15) -> bool:
+    """
+    Return true if the given chapter is found
+    """
+    domain: str = get_domain(url)
+    try:
+        fn: Callable[[str], bool] = domains[domain]
+    except KeyError:
+        raise UnknownDomain(url)  # pylint: disable=raise-missing-from
+    session = requests.Session()
+    session.headers.update(
+        {"User-Agent": "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4"}
+    )
+    return fn(session.get(url, timeout=timeout).text)
