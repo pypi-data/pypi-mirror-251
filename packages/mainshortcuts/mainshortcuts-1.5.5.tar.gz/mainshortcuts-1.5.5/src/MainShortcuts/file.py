c0b590fde3ac34be19fd0de95198fcf4155f11da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:37 +0000

    Add package mainshortcuts/1.5.5/mainshortcuts-1.5.5.tar.gz

---
+++ packages/mainshortcuts/mainshortcuts-1.5.5.tar.gz/mainshortcuts-1.5.5/src/MainShortcuts/file.py
@@ -0,0 +1,59 @@
+import MainShortcuts.path as m_path
+import os as _os
+import shutil as _shutil
+def read(path,encoding="utf-8"): # Прочитать текстовый файл
+  if m_path.info(path)["type"]=="file":
+    with open(path,"r",encoding=encoding) as f:
+      text=f.read()
+  else:
+    text=""
+  return text
+def write(path,text="",encoding="utf-8",force=False): # Записать текстовый файл
+  if m_path.info(path)["type"]=="dir" and force:
+    _os.remove(path)
+  with open(path,"w",encoding=encoding) as f:
+    f.write(f"{text}")
+  return True
+def open(path): # Открыть содержимое файла
+  if _os.path.exists(path):
+    with open(path,"rb") as f:
+      content=f.read()
+  else:
+    content=None
+  return content
+def save(path,content,force=False): # Сохранить содержимое файла
+  if m_path.info(path)["type"]=="dir" and force:
+    _os.remove(path)
+  with open(path,"wb") as f:
+    f.write(content)
+  return True
+def delete(path):
+  typt=m_path.info(path)["type"]
+  if typt=="file":
+    _os.remove(path)
+  else:
+    raise Exception("Unknown type: "+typt)
+def copy(fr,to,force=False):
+  type=m_path.info(fr)["type"]
+  if type=="file":
+    if m_path.exists(to) and force:
+      m_path.delete(to)
+    _shutil.copy(fr,to)
+  else:
+    raise Exception("Unknown type: "+type)
+def move(fr,to,force=False):
+  type=m_path.info(fr)["type"]
+  if type=="file":
+    if m_path.exists(to) and force:
+      m_path.delete(to)
+    _shutil.move(fr,to)
+  else:
+    raise Exception("Unknown type: "+type)
+def rename(fr,to,force=False):
+  type=m_path.info(fr)["type"]
+  if type=="file":
+    if m_path.exists(to) and force:
+      m_path.delete(to)
+    _os.rename(fr,to)
+  else:
+    raise Exception("Unknown type: "+type)
