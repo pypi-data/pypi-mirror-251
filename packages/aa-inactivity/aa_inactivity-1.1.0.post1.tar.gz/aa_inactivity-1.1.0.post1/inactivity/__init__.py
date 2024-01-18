f3bbeeb1164b57da58a998e7c550ba5b5ac5b1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:05:01 +0000

    Add package aa-inactivity/1.1.0.post1/aa_inactivity-1.1.0.post1.tar.gz

---
+++ packages/aa-inactivity/aa_inactivity-1.1.0.post1.tar.gz/aa_inactivity-1.1.0.post1/inactivity/__init__.py
@@ -0,0 +1,8 @@
+""""Activity monitoring app for Alliance Auth."""
+
+# pylint: disable = invalid-name
+default_app_config = "inactivity.apps.InactivityConfig"
+
+__version__ = "1.1.0.post1"
+
+__title__ = "Inactivity"
