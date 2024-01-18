dd5126abcad998c3cdb2ec29b87a978255be7c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 06:04:11 +0000

    Add package npc_lims/0.1.105/npc_lims-0.1.105.tar.gz

---
+++ packages/npc_lims/npc_lims-0.1.105.tar.gz/npc_lims-0.1.105/src/npc_lims/scripts/run_video_processing.py
@@ -0,0 +1,17 @@
+import npc_lims.metadata.codeocean as codeocean
+import npc_lims.status as status
+
+
+def main() -> None:
+    for session_info in status.get_session_info():
+        if not session_info.is_uploaded:
+            continue
+
+        codeocean.run_capsule(session_info.id, "dlc_eye")
+        codeocean.run_capsule(session_info.id, "dlc_front")
+        codeocean.run_capsule(session_info.id, "dlc_side")
+        codeocean.run_capsule(session_info.id, "facemap")
+
+
+if __name__ == "__main__":
+    main()
