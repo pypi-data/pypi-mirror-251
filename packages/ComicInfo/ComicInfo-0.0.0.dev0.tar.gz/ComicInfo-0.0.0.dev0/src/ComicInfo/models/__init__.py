2c6a7acc655756a466a7c9f1604b9b7d10f574af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 06:04:14 +0000

    Add package ComicInfo/0.0.0.dev0/ComicInfo-0.0.0.dev0.tar.gz

---
+++ packages/ComicInfo/ComicInfo-0.0.0.dev0.tar.gz/ComicInfo-0.0.0.dev0/src/ComicInfo/models/__init__.py
@@ -0,0 +1,14 @@
+from .AgeRating import AgeRating
+from .ComicPageType import ComicPageType
+from .Manga import Manga
+from .YesNo import YesNo
+
+Formats = (
+    "", "Special", "Reference", "Director's Cut", "Box Set", "Annual", "Anthology", "Epilogue", "One-Shot", "Prologue",
+    "TPB", "Trade Paper Back", "Omnibus", "Compendium", "Absolute", "Graphic Novel", "GN", "FCB"
+)
+
+PeopleTags = (
+    "Writer", "Penciller", "Inker", "Letterer", "CoverArtist", "Editor", "Translator",
+    "Publisher", "Imprint", "Other",
+)
