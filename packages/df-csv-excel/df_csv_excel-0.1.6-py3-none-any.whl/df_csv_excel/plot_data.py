6d7eb3d49f02278bd99f76ddb956688631cf53cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 03:04:34 +0000

    Add package df-csv-excel/0.1.6/df_csv_excel-0.1.6-py3-none-any.whl

---
+++ packages/df-csv-excel/df_csv_excel-0.1.6-py3-none-any.whl/df_csv_excel/plot_data.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import pandas as pd
+import numpy as np
+import os
+import plotly.express as px
+
+#####################################################
+#           Plot             #
+#####################################################
+def plot_histgram(df, column_name, parameter_root = 0, parameter_log = False):
+    if parameter_log:
+        df[f'log_{column_name}'] = df[column_name].apply(lambda x: np.log(x+0.00001))
+        fig = px.histogram(df, x=f'log_{column_name}')
+        fig.show()
+    if parameter_root > 0:
+        df[f'root_{column_name}'] = df[column_name].apply(lambda x: x**parameter_root)
+        fig = px.histogram(df, x=f'root_{column_name}')
+        fig.show()
+    if parameter_root == 0 and parameter_log == False:
+        fig = px.histogram(df, x=column_name)
+        fig.show()
+    
