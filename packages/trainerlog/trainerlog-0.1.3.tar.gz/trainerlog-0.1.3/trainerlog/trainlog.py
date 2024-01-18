c7627c7f133e0290575b2a58c9ecf311c47f1c0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 04:04:46 +0000

    Add package trainerlog/0.1.3/trainerlog-0.1.3.tar.gz

---
+++ packages/trainerlog/trainerlog-0.1.3.tar.gz/trainerlog-0.1.3/trainerlog/trainlog.py
@@ -0,0 +1,20 @@
+import sys
+import logging, colorlog
+
+LOG_COLORS = {'DEBUG':'cyan', 'INFO':'green', 'TRAIN':'blue', 'WARNING':'yellow', 'ERROR': 'red', 'CRITICAL':'red,bg_white'}
+LOG_LEVELS = {"CRITICAL": 50, "ERROR": 40, "WARNING": 30, "TRAIN": 25, "INFO": 20, "DEBUG": 10, "NOTSET": 0}
+logging.addLevelName(LOG_LEVELS["TRAIN"], 'TRAIN')
+
+def get_logger(level="TRAIN", name="training"):    
+    handler = colorlog.StreamHandler(stream=sys.stdout)
+    handler.setFormatter(colorlog.ColoredFormatter('%(log_color)s%(asctime)s [%(levelname)s] %(white)s(%(name)s)%(reset)s: %(message)s',
+                                                   log_colors=LOG_COLORS,
+                                                   datefmt="%H:%M:%S",
+                                                   stream=sys.stdout))
+
+    logger = colorlog.getLogger(name)
+    logger.addHandler(handler)
+    logger.train = lambda message: logger.log(TRAIN, message)
+    logger.setLevel(LOG_LEVELS[level])
+
+    return logger
