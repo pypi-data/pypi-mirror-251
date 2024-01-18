e242d0bece76348672201110ad3361e125fee82c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:42 +0000

    Add package BatToPy/0.0.1/BatToPy-0.0.1.tar.gz

---
+++ packages/BatToPy/BatToPy-0.0.1.tar.gz/BatToPy-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BatToPy",
+    version="0.0.1",
+    install_requires=[
+        "subprocess",
+    ],
+    keywords=[
+        "Converter",
+        "Ease of use",
+        "beginner friendly",
+        "helpfull",
+        "easy to use",
+        "python",
+    ],
+    packages=find_packages(),
+)
