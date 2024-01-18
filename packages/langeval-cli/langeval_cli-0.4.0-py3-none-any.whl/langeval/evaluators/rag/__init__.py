68b15217f7d92e1a42205c09b05f5b64d4584efa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:42 +0000

    Add package langeval-cli/0.4.0/langeval_cli-0.4.0-py3-none-any.whl

---
+++ packages/langeval-cli/langeval_cli-0.4.0-py3-none-any.whl/langeval/evaluators/rag/__init__.py
@@ -0,0 +1,137 @@
+import enum
+import logging
+from typing import Any, Optional
+
+import jinja2
+
+try:
+    import pydantic.v1 as pc
+except ImportError:
+    import pydantic as pc
+
+from sklearn.metrics import ndcg_score
+
+from langeval.evaluators.exception import EvalRunError
+from langeval.evaluators.rag.utils import overlap_coefficient_contain
+from langeval.models import LLM
+from langeval.providers.output_parser import SimpleJsonOutputParser
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_ANSWER_SIMILARITY_THRESHOLD = 0.8
+DEFAULT_ANSWER_CORRECTNESS_PROMPT = """
+你是一名教师，接下来你需要根据【问题】和【参考答案】，来评价【学生答案】。
+
+# 评分过程
+
+1. 首先思考【问题】下，【参考答案】和【学生答案】的一致性。
+2. 根据一致性，给出对【学生答案】的评价理由。
+3. 根据评价理由，给出对【学生答案】的评分。（0代表错误，0.5代表部分正确，1代表完全正确）。
+
+# 输入
+
+问题："{{ question }}"
+参考答案："{{ reference_answer }}"
+学生答案："{{ answer }}"
+
+# 输出
+
+输出为 Markdown JSON 格式的字符串，示例：
+
+```json
+{
+    "answer_correctness_reasoning": "<评价理由>",
+    "answer_correctness": 0
+}
+```
+
+注意分数只能为 0、0.5、1，0 代表错误，0.5 代表部分正确，2 代表完全正确。
+
+输出（Markdown JSON 格式）：
+"""
+
+class RagMetric(str, enum.Enum):
+    # Compare contexts and reference_context
+    RetrievalRecall = "retrieval_recall"
+    # Compare answer and reference_answer with LLM-JUDGE correctness
+    AnswerCorrectness = "answer_correctness"
+
+
+class Rag(pc.BaseModel):
+    rag_metrics: list[RagMetric]
+    rag_llm: Optional[LLM] = None
+    answer_correctness_prompt: Optional[str] = None
+
+
+def retrieval_recall(rag: Rag, kwargs: dict[str, Any], timeout, default_llm):   # noqa: ARG001
+    contexts = kwargs["contexts"]
+    reference_context = kwargs["reference_context"]
+    if len(contexts) == 0:
+        return {
+            #"retrieval_recall_ndgc_10": 0.0,
+            "retrieval_recall_hit_rate": 0.0,
+            "retrieval_recall_mrr": 0.0
+        }
+    # 通过判断 reference_context 是否在 contexts 中，以及所在的位置来计算。
+    # 这里使用 ndcg 算法：https://scikit-learn.org/stable/modules/generated/sklearn.metrics.ndcg_score.html
+    # 假设 reference_context 和 contexts 是同一批语料的不同切分形式，所以不需要语义相似度，而是比对文本相似度。
+    # 使用修改后的 Overlap Coefficient 算法计算相似度。取值范围是 0-1
+    # 这种算法，当 contexts 包含 reference_context 时，得分是 1。
+    #true_relevance: list[float] = []
+    # 如下是假设 reference_context 和 contexts 是相同语料。
+    hit_rate = 0.0 # 代表 reference_context 是否在 contexts 中精确匹配。
+    mrr = 0.0 # 计算 reference_context 的精确匹配位置，首位为 1.0，次位为 0.5，以此类推。
+    for i, context in enumerate(contexts):
+        if reference_context == context:
+            hit_rate = 1.0
+            if mrr == 0:
+                # if 是避免 contexts 中有重复内容。
+                mrr = 1.0 / (i + 1)
+        #true_relevance.append(overlap_coefficient_contain(
+        #    context, reference_context
+        #))
+    # scores 的绝对值没有意义，只要是倒序排列就行。
+    # scores = list(range(len(contexts), 0, -1))
+    # k = 10 代表只统计前 10 个结果
+    # ndgc_10 = ndcg_score([true_relevance], [scores], k=10) # type: ignore
+
+    return {
+        "retrieval_recall_hit_rate": hit_rate,
+        "retrieval_recall_mrr": mrr,
+        #"retrieval_recall_ndgc_10": ndgc_10,
+    }
+
+def answer_correctness(rag: Rag, kwargs: dict[str, Any], timeout, default_llm):
+    if rag.rag_llm:
+        llm = rag.rag_llm
+    else:
+        llm = default_llm
+    if llm is None:
+        raise EvalRunError("llm is None, can not eval answer_correctness")
+
+    prompt_tpl = DEFAULT_ANSWER_CORRECTNESS_PROMPT \
+        if rag.answer_correctness_prompt is None else rag.answer_correctness_prompt
+    prompt = jinja2.Template(prompt_tpl).render(**kwargs)
+    text = llm.completion(prompt, timeout=timeout)
+    eval_result = {}
+    eval_result = SimpleJsonOutputParser().parse(text)
+    if "answer_correctness" not in eval_result:
+        raise EvalRunError("eval completion result missing key: answer_correctness")
+    return eval_result
+
+
+metrics_eval_funcs = {
+    RagMetric.RetrievalRecall: retrieval_recall,
+    RagMetric.AnswerCorrectness: answer_correctness,
+}
+
+def eval_rag(rag: Rag, kwargs: dict[str, Any], timeout, default_llm) -> dict[str, Any]:
+    """Rag eval"""
+    eval_result = {}
+    for metric in rag.rag_metrics:
+        if metric not in metrics_eval_funcs:
+            raise EvalRunError(f"eval rag metric not supported: {metric}")
+        eval_func = metrics_eval_funcs[metric]
+        r = eval_func(rag, kwargs, timeout, default_llm)
+        eval_result.update(r)
+    return eval_result
---
+++ packages/langeval-cli/langeval_cli-0.4.0-py3-none-any.whl/langeval/tasks/task.py
@@ -0,0 +1,236 @@
+import csv
+import io
+import json
+import logging
+import os
+import threading
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from time import perf_counter
+from typing import Any, List, Optional
+
+import pandas as pd
+import yaml
+
+try:
+    import pydantic.v1 as pc
+except ImportError:
+    import pydantic as pc
+
+from langeval.evaluators import Evaluator
+from langeval.models import LLM
+from langeval.providers import Provider
+from langeval.tasks.ratelimiter import ThreadingRateLimiter
+
+logger = logging.getLogger(__name__)
+
+class RunResult(pc.BaseModel):
+    """Result for one data run"""
+    error: str = ""
+    outputs: dict[str, Any] = {}
+    elapsed_secs: float = 0
+
+class EvalResult(pc.BaseModel):
+    """Result for one data eval"""
+    error: str = ""
+    outputs: dict[str, Any] = {}
+    elapsed_secs: float = 0
+
+
+class Result(pc.BaseModel):
+    # uuid: str
+    inputs: dict[str, Any]
+    run: RunResult = RunResult()
+    evals: dict[str, EvalResult] = {}
+
+    def to_jsonl(self) -> str:
+        return json.dumps(self.dict(), ensure_ascii=False) + "\n"
+
+    @classmethod
+    def from_json(cls, json_str: str) -> "Result":
+        return cls(**json.loads(json_str))
+
+
+class TaskRunConfig(pc.BaseModel):
+    parallelism: int = pc.Field(default=1, ge=1, le=30)
+    timeout: int = pc.Field(default=30, ge=1, le=600)
+    rounds: int = pc.Field(default=1, ge=1, le=10)
+    batch_size: int = pc.Field(default=1, ge=1, le=10000)
+    query_per_second: float = pc.Field(default=0, ge=0.1, le=100)
+
+    def to_yaml(self) -> str:
+        return yaml.safe_dump(self.dict(exclude_unset=True), encoding="utf-8", allow_unicode=True).decode("utf-8")
+
+    @staticmethod
+    def from_yaml(yaml_str: str) -> "TaskRunConfig":
+        try:
+            return TaskRunConfig(**yaml.safe_load(yaml_str))
+        except Exception as e:
+            raise ValueError(f"Invalid yaml: {e}") from e
+
+
+class EvalTask(pc.BaseModel):
+    # config
+    provider: Optional[Provider] = None
+    input_dataset_binary: Optional[bytes] = None
+    # Only jsonl, csv supported
+    input_dataset_name: str = pc.Field(..., min_length=1, max_length=255)
+    # evaluator
+    evaluators: List[Evaluator]
+    # Run config
+    run_config: TaskRunConfig
+
+    class Config:
+        validate_assignment = True
+
+    @pc.validator("input_dataset_name")
+    def input_dataset_name_must_be_valid(cls, v):  # noqa: N805
+        if not v.endswith(".csv") and not v.endswith(".jsonl"):
+            raise ValueError(f"Invalid input_dataset_name: {v}")
+        return v
+
+    @classmethod
+    def from_yaml(cls, yaml_str: str, dataset_dir: Optional[str] = None) -> "EvalTask":
+        obj = yaml.safe_load(yaml_str)
+        input_dataset_name = obj.get("input_dataset_name")
+        if input_dataset_name:
+            if dataset_dir:
+                input_dataset_base_name = os.path.basename(input_dataset_name)
+                path = os.path.join(dataset_dir, input_dataset_base_name)
+            else:
+                path = input_dataset_name
+            with open(path, "rb") as f:
+                obj["input_dataset_binary"] = f.read()
+        logger.debug(f"EvalTask.from_yaml obj: {obj}")
+        task = cls(**obj)
+        logger.debug(f"EvalTask.from_yaml task: {task}")
+        return task
+
+    def run_provider(self, data_list: list[Result], stop_event: threading.Event):
+        """Run data list with batch"""
+        batch_size = self.run_config.batch_size
+        batch_data_list = [data_list[i:i + batch_size] for i in range(0, len(data_list), batch_size)]
+        limiter = ThreadingRateLimiter(self.run_config.query_per_second)
+
+        with ThreadPoolExecutor(max_workers=self.run_config.parallelism) as executor:
+            # Submit tasks for execution
+            futures = [
+                executor.submit(self.batch_run, batch_data=batch_data, limiter=limiter) for batch_data in batch_data_list
+            ]
+
+            # Collect results from completed tasks
+            for future in as_completed(futures):
+                if stop_event.is_set():
+                    return
+                batch_result = future.result()
+                yield from batch_result
+
+
+    def run_eval(self, evaluator: Evaluator, data_list: list[Result], stop_event: threading.Event, default_eval_llm: Optional[LLM] = None):
+        """Eval data list with batch"""
+        # TODO seperate eval run config
+        batch_size = self.run_config.batch_size
+        batch_data_list = [data_list[i:i + batch_size] for i in range(0, len(data_list), batch_size)]
+        limiter = ThreadingRateLimiter(evaluator.query_per_second)
+        with ThreadPoolExecutor(max_workers=self.run_config.parallelism) as executor:
+            # Submit tasks for execution
+            futures = [
+                executor.submit(
+                    self.batch_eval,
+                    evaluator=evaluator,
+                    batch_data=batch_data,
+                    limiter=limiter,
+                    default_eval_llm=default_eval_llm,
+                ) for batch_data in batch_data_list
+            ]
+
+            # Collect results from completed tasks
+            for future in as_completed(futures):
+                if stop_event.is_set():
+                    return
+                batch_result = future.result()
+                yield from batch_result
+
+    def batch_run(self, batch_data: list[Result], limiter: ThreadingRateLimiter) -> list[Result]:
+        """Batch run data"""
+        start = perf_counter()
+        run_error = ""
+        if self.provider is not None:
+            with limiter:
+                inputs = [data.inputs for data in batch_data]
+                try:
+                    # 1. 首先调用 LLM
+                    run_outputs = self.provider.batch_call(
+                        inputs, timeout=self.run_config.timeout)
+                    logger.debug(f"provider call: {inputs} -> {run_outputs}")
+                    for i, data in enumerate(batch_data):
+                        data.run.outputs = run_outputs[i]
+                except Exception as e:
+                    logger.error(f"provider call failed: {e}", exc_info=True)
+                    run_error = str(e)
+
+                for data in batch_data:
+                    data.run.error = run_error
+                    data.run.elapsed_secs = perf_counter() - start
+        return batch_data
+
+    def batch_eval(self,
+                   evaluator: Evaluator,
+                   batch_data: list[Result],
+                   limiter: ThreadingRateLimiter,
+                   default_eval_llm: Optional[LLM] = None) -> list[Result]:
+        start = perf_counter()
+        run_error = ""
+        with limiter:
+            for data in batch_data:
+                data.evals[evaluator.name] = EvalResult()
+            try:
+                eval_outputs = evaluator.batch_call(
+                    batch_inputs=[data.inputs for data in batch_data],
+                    batch_outputs=[data.run.outputs for data in batch_data],
+                    timeout=self.run_config.timeout,
+                    default_llm=default_eval_llm,
+                )
+                for i, data in enumerate(batch_data):
+                    data.evals[evaluator.name].outputs = eval_outputs[i]
+            except Exception as e:
+                logger.warning(f"evaluator call failed: {e}", exc_info=True)
+                run_error = str(e)
+
+            for data in batch_data:
+                data.evals[evaluator.name].error = run_error
+                data.evals[evaluator.name].elapsed_secs = perf_counter() - start
+        return batch_data
+
+    def split_dataset(self) -> list[dict[str, Any]]:
+        if self.input_dataset_name.endswith(".csv"):
+            return self.split_csv_dataset()
+        elif self.input_dataset_name.endswith(".jsonl"):
+            return self.split_jsonl_dataset()
+        else:
+            raise ValueError(
+                f"Invalid input_dataset_name: {self.input_dataset_name}")
+
+    def split_csv_dataset(self) -> list[dict[str, Any]]:
+        if not self.input_dataset_binary:
+            return []
+        data_list = []
+        with io.StringIO(self.input_dataset_binary.decode("utf-8")) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                data_list.append(row)
+        return data_list
+
+    def split_jsonl_dataset(self) -> list[dict[str, Any]]:
+        if not self.input_dataset_binary:
+            return []
+        data_list = []
+        for line in self.input_dataset_binary.decode("utf-8").split("\n"):
+            if line.strip():
+                data_list.append(json.loads(line.strip()))
+        return data_list
+
+    def input_dataset_pd(self, limit: int = 5) -> pd.DataFrame:
+        data = self.split_dataset()
+        if len(data) > limit:
+            data = data[:limit]
+        return pd.DataFrame(data)
