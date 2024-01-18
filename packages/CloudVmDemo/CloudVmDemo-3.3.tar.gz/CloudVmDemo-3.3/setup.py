1389c04e7ab6a0943c5118b00d680596025986cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:14 +0000

    Add package CloudVmDemo/3.3/CloudVmDemo-3.3.tar.gz

---
+++ packages/CloudVmDemo/CloudVmDemo-3.3.tar.gz/CloudVmDemo-3.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CloudVmDemo',
+    version='3.3',
+    description='eNlight Cloud VM create, update, retrive, delete and VM actions',
+    author='Akshay Ghatol',
+    author_email='akshay.ghatol@esds.co.in',
+    packages=find_packages(),
+    install_requires=[
+        # Add your dependencies here
+    ],
+)
+
