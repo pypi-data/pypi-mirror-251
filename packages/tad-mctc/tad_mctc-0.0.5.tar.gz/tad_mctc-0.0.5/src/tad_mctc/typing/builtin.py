d04947fa8c89f631481054a6ee9bfe9d7c7cdf24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 03:03:59 +0000

    Add package tad-mctc/0.0.5/tad_mctc-0.0.5.tar.gz

---
+++ packages/tad-mctc/tad_mctc-0.0.5.tar.gz/tad_mctc-0.0.5/src/tad_mctc/typing/builtin.py
@@ -0,0 +1,46 @@
+# This file is part of tad-mctc.
+#
+# SPDX-Identifier: LGPL-3.0
+# Copyright (C) 2023 Marvin Friede
+#
+# tad-mctc is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# tad_mctc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with tad-mctc. If not, see <https://www.gnu.org/licenses/>.
+"""
+Typing: Builtins
+================
+
+This module contains all used built-in type annotations.
+"""
+from typing import (
+    IO,
+    Any,
+    Iterable,
+    Literal,
+    NoReturn,
+    Protocol,
+    TypedDict,
+    overload,
+    runtime_checkable,
+)
+
+__all__ = [
+    "IO",
+    "Any",
+    "Literal",
+    "Iterable",
+    "NoReturn",
+    "Protocol",
+    "runtime_checkable",
+    "TypedDict",
+    "overload",
+]
