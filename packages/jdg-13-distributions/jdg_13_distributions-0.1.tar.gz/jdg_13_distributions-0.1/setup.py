daba892c5a6b730798c0b41e12f3e7466e6ec362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 04:04:49 +0000

    Add package jdg-13-distributions/0.1/jdg_13_distributions-0.1.tar.gz

---
+++ packages/jdg-13-distributions/jdg_13_distributions-0.1.tar.gz/jdg_13_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jdg_13_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['jdg_13_distributions'],
+      author='Joshua Glenn',
+      author_email='jdglenn_13@yahoo.com',
+      zip_safe=False)
