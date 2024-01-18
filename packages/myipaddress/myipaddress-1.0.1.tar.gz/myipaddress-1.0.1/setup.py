c9e04c17c790ef24c5372b6102f1095bfc471596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:04:22 +0000

    Add package myipaddress/1.0.1/myipaddress-1.0.1.tar.gz

---
+++ packages/myipaddress/myipaddress-1.0.1.tar.gz/myipaddress-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  "Operating System :: OS Independent",
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='myipaddress',
+  version='1.0.1',
+  description="A package to get your/others IP information.",
+  url='https://github.com/ericdennis7/myIPaddress',  
+  author='Eric Dennis',
+  author_email='ericdennis11@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords=['IP', 'myip', 'IP address', 'geolocation', 'public IP', 'private IP', 'socket', 'ports', 'open ports'], 
+  packages=find_packages(),
+  install_requires=[''] 
+)
