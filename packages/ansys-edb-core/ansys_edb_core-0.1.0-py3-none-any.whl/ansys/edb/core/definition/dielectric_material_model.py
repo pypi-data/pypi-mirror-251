36274b17a549929891622dadc4c491c39eb9e7c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 05:03:47 +0000

    Add package ansys-edb-core/0.1.0/ansys_edb_core-0.1.0-py3-none-any.whl

---
+++ packages/ansys-edb-core/ansys_edb_core-0.1.0-py3-none-any.whl/ansys/edb/core/definition/dielectric_material_model.py
@@ -0,0 +1,33 @@
+"""Dielectric material definition."""
+from enum import Enum
+
+from ansys.api.edb.v1 import dielectric_material_model_pb2_grpc
+
+from ansys.edb.core import session
+from ansys.edb.core.inner import ObjBase
+
+
+class DielectricMaterialModelType(Enum):
+    """Provides an eum representing dielectric material model types.
+
+    - DEBYE
+    - MULTIPOLE_DEBYE
+    - DJORDJECVIC_SARKAR
+    """
+
+    DEBYE = 0
+    MULTIPOLE_DEBYE = 1
+    DJORDJECVIC_SARKAR = 2
+
+
+class DielectricMaterialModel(ObjBase):
+    """Represents a dielectric material model."""
+
+    __stub: dielectric_material_model_pb2_grpc.DielectricMaterialModelServiceStub = (
+        session.StubAccessor(session.StubType.dielectric_material_model)
+    )
+
+    @property
+    def type(self):
+        """:class:`DielectricMaterialModelType`: Type of the dielectric material model."""
+        return DielectricMaterialModelType(self.__stub.GetType(self.msg).value)
