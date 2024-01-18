0b6b3e01547a47a6115f0e136a33b5cbcd0f8482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 03:04:34 +0000

    Add package df-csv-excel/0.1.6/df-csv-excel-0.1.6.tar.gz

---
+++ packages/df-csv-excel/df-csv-excel-0.1.6.tar.gz/df-csv-excel-0.1.6/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='df-csv-excel',
+    version='0.1.6',
+    author="ZhangLe",
+    author_email="zhangle@gmail.com",
+    description="Commonly used functions for Data Scientists",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cheerzhang/read_csv_excel",
+    project_urls={
+        "Bug Tracker": "https://github.com/cheerzhang/read_csv_excel/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages("."),
+    install_requires=[
+        'pandas>=0.25.1',
+        'numpy>=1.21.5',
+        'plotly >= 5.18.0'
+    ],
+    python_requires=">=3.9",
+)
