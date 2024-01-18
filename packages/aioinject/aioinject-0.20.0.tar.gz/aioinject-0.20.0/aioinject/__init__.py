407f8800d1ce339bdd980a4864b04d06dd2521c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:51 +0000

    Add package aioinject/0.20.0/aioinject-0.20.0.tar.gz

---
+++ packages/aioinject/aioinject-0.20.0.tar.gz/aioinject-0.20.0/aioinject/__init__.py
@@ -0,0 +1,29 @@
+from aioinject.containers import Container
+from aioinject.context import InjectionContext, SyncInjectionContext
+from aioinject.decorators import inject
+from aioinject.markers import Inject
+from aioinject.providers import (
+    Callable,
+    Factory,
+    Object,
+    Provider,
+    Scoped,
+    Singleton,
+)
+
+
+__all__ = [
+    "Scoped",
+    "Factory",
+    "Callable",
+    "Container",
+    "Inject",
+    "InjectionContext",
+    "Object",
+    "Provider",
+    "Singleton",
+    "SyncInjectionContext",
+    "inject",
+]
+
+__version__ = "0.20.0"
