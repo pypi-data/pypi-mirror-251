f9a33f7fb3d371ae82ae60754b3803f86f4bc5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:04:25 +0000

    Add package CountToCython/0.0.3/CountToCython-0.0.3-py3-none-any.whl

---
+++ packages/CountToCython/CountToCython-0.0.3-py3-none-any.whl/CountTo/Wrapper.py
@@ -0,0 +1,12 @@
+import importlib
+
+Decorators = importlib.import_module(name="Decorators", package=".Decorators.pyd")
+
+CountMainFuncs = importlib.import_module(name="CountMainFuncs", package=".CountMainFuncs.pyd")
+
+def CountToNumber(numToStopAt, Interval = 1, numToStartAt = 1, PrintOutput = True):
+    CountMainFuncs.CountToNumber(numToStopAt=numToStopAt, Interval=Interval, numToStartAt=numToStartAt, PrintOutput=PrintOutput)
+def CountToNumberTimed(numToStopAt, Interval = 1, numToStartAt = 1, PrintOutput = True):
+    @TimeFunc
+    def ThrowawayFunc(): CountMainFuncs.CountToNumber(numToStopAt=numToStopAt, Interval=Interval, numToStartAt=numToStartAt, PrintOutput=PrintOutput)
+    ThrowawayFunc()
