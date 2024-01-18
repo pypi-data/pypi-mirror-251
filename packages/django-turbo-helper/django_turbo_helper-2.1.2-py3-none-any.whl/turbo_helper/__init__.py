b640cc03f3eafd0346599f143e8672f3f2cc62ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:21 +0000

    Add package django-turbo-helper/2.1.2/django_turbo_helper-2.1.2-py3-none-any.whl

---
+++ packages/django-turbo-helper/django_turbo_helper-2.1.2-py3-none-any.whl/turbo_helper/__init__.py
@@ -0,0 +1,17 @@
+from .response import HttpResponseSeeOther, TurboStreamResponse
+from .shortcuts import redirect_303, respond_to
+from .stream import register_turbo_stream_action, turbo_stream
+from .templatetags.turbo_helper import dom_id
+
+# extend turbo_stream actions, inspired by https://github.com/marcoroth/turbo_power
+from .turbo_power import *  # noqa
+
+__all__ = [
+    "turbo_stream",
+    "register_turbo_stream_action",
+    "TurboStreamResponse",
+    "HttpResponseSeeOther",
+    "redirect_303",
+    "dom_id",
+    "respond_to",
+]
