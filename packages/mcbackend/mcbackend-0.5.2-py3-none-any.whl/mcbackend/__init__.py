a75592e3c16d8fcd74cf027d85b1135e2390c5e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:04:43 +0000

    Add package mcbackend/0.5.2/mcbackend-0.5.2-py3-none-any.whl

---
+++ packages/mcbackend/mcbackend-0.5.2-py3-none-any.whl/mcbackend/__init__.py
@@ -0,0 +1,29 @@
+"""
+A framework agnostic implementation for storage of MCMC draws.
+"""
+from .backends.numpy import NumPyBackend
+from .core import Backend, Chain, Run
+from .meta import ChainMeta, Coordinate, DataVariable, ExtendedValue, RunMeta, Variable
+
+# Backends
+try:
+    from .backends import clickhouse
+    from .backends.clickhouse import ClickHouseBackend
+except ModuleNotFoundError:
+    pass
+
+__version__ = "0.5.2"
+__all__ = [
+    "NumPyBackend",
+    "Backend",
+    "Chain",
+    "Run",
+    "ChainMeta",
+    "Coordinate",
+    "DataVariable",
+    "ExtendedValue",
+    "RunMeta",
+    "Variable",
+    "clickhouse",
+    "ClickHouseBackend",
+]
