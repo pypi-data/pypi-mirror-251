ec7683b015969e12e6bee5e190efba76a8083c61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 04:04:49 +0000

    Add package haskellian/0.1.9/haskellian-0.1.9-py3-none-any.whl

---
+++ packages/haskellian/haskellian-0.1.9-py3-none-any.whl/haskellian/asynch/__init__.py
@@ -0,0 +1,3 @@
+from .asynch import either, uneither, safe, wrap, then, bind
+from .iterables import map, enumerate, flatten, asyncify, syncify
+from .memo import AsyncMemo
---
+++ packages/haskellian/haskellian-0.1.9-py3-none-any.whl/haskellian/asynch/asynch.py
@@ -0,0 +1,35 @@
+import asyncio
+from typing import Callable, Awaitable, TypeVar
+import ramda as R
+
+A = TypeVar("A")
+B = TypeVar("B")
+
+async def either(x: Awaitable[A]) -> A | Exception:
+    try:
+        return await x
+    except Exception as e:
+        return e
+    
+async def uneither(x: Awaitable[A] | Exception) -> Awaitable[A]:
+    match (await x):
+        case Exception() as e:
+            raise e
+        case v:
+            return v
+    
+async def safe(x: Awaitable[A]) -> A | None:
+    try:
+        return await x
+    except:
+        return None
+    
+    
+async def wrap(x: A) -> Awaitable[A]:
+    return x
+    
+async def then(f: Callable[[A], B], x: Awaitable[A]) -> Awaitable[B]:
+    return f(await x)
+
+async def bind(f: Callable[[A], Awaitable[B]], x: Awaitable[A]) -> Awaitable[B]:
+    return await f(await x)
