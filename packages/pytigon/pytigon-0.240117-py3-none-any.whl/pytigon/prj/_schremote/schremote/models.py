0f990f969742b97f2949746ee91af5d15d145dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 06:04:07 +0000

    Add package pytigon/0.240117/pytigon-0.240117-py3-none-any.whl

---
+++ packages/pytigon/pytigon-0.240117-py3-none-any.whl/pytigon/prj/_schremote/schremote/models.py
@@ -0,0 +1,26 @@
+import django
+from django.db import models
+from django.utils import timezone
+
+from pytigon_lib.schdjangoext.fields import *
+from pytigon_lib.schdjangoext.models import *
+import pytigon_lib.schdjangoext.fields as ext_models
+from pytigon_lib.schtools import schjson
+
+from django.utils.translation import gettext_lazy as _
+from django.contrib import admin
+
+import os, os.path
+import sys
+from pytigon_lib.schhtml.htmltools import superstrip
+
+
+from schsimplescripts.models import *
+
+from schtools.models import *
+
+from schwiki.models import *
+
+from schcommander.models import *
+
+from schtasks.models import *
