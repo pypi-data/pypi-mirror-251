d0f0bec7b5db7acf09e77a55498dedb7e4a9d506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 04:04:58 +0000

    Add package django-oscar-newsletter/0.10.0/django_oscar_newsletter-0.10.0-py3-none-any.whl

---
+++ packages/django-oscar-newsletter/django_oscar_newsletter-0.10.0-py3-none-any.whl/newsletter/__init__.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+
+def get_version():
+    path = Path(str(Path(__file__).parent.parent) + os.sep + 'VERSION')
+    if path.is_file():
+        with open(path) as f:
+            return f.read().strip()
+    return '0.0.0'
+
+
+__version__ = get_version()
