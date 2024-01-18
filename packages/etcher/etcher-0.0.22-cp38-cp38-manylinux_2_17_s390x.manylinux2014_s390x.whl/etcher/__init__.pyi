1519b43f0b2164be154505b08e6e636244201acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 04:04:39 +0000

    Add package etcher/0.0.22/etcher-0.0.22-cp38-cp38-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/etcher/etcher-0.0.22-cp38-cp38-manylinux_2_17_s390x.manylinux2014_s390x.whl/etcher/__init__.pyi
@@ -0,0 +1,42 @@
+import typing as tp
+
+def register_function(func: tp.Callable) -> None:
+    """Register a custom function to be available in the template context.
+
+    Example:
+        >>> @etch.register_function
+        ... def adder(a: int, b: int = 3) -> int:
+        ...     return a + b
+        ...
+        >>> "{{ foo(3, b=5) }}"
+        8
+
+    Args:
+        func (tp.Callable): The function to register.
+    """
+    ...
+
+def context() -> dict[str, tp.Any]:
+    """Return the configured context globals for this run of etch, can be run during custom extensions.
+
+    Example:
+        >>> @etch.register_function
+        ... def foo() -> str:
+        ...     return etch.context()["foo"]
+        ...
+        >>> "{{ foo() }}" # Assuming the config has foo = {"value": "bar"}
+        "bar"
+
+    Returns:
+        dict[str, tp.Any]: The configured context globals.
+    """
+    ...
+
+def _toml_update(
+    initial: str, update: tp.Any | None = None, remove: list[list[str]] | None = None
+) -> str: ...
+def _hash_contents(contents: str) -> str: ...
+
+__version__: str
+
+__all__ = ["__version__", "_hash_contents", "_toml_update"]
