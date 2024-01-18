674f70daf061820d4477965d46530123e9330f01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 04:05:05 +0000

    Add package kaiming-ascii/0.1.1/kaiming_ascii-0.1.1-py3-none-any.whl

---
+++ packages/kaiming-ascii/kaiming_ascii-0.1.1-py3-none-any.whl/kaiming_ascii/__init__.py
@@ -0,0 +1,62 @@
+kaiming_ascii = r"""
+        JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJFJJJJJJJJJJJJJJJJJJJJJJJJJJFJ
+        JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJrJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ
+        JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ
+        JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ
+        JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ
+        JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ JJJJJJJJJJJJJJJJJJJJJJJ<JJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJ
+        rrrrrrrrrrrrrrrrrrrrrrJrrr<<rrJrrrrrrrr<<<<<<<<<<r<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<   < < < << <                 <
+        rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr<rrrrrrrrrrr<<<<<<<<<JJJJJJJJFJJJJJ<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< <<<<<< <<<<< <<  <<<<<
+        rrrrrrrrrrrrrrrrrrrrrrrrrrrr<rrrrrr<rrrrrrrrrr<rJJJJr<<JJJJJJJ<   <<r<<<<<<<<<<<< << <<<<<<<<<<< <<<<< << <<<<   <<<<<
+        r<rrrrrrrrrrrrrrrrrrrrrrrrrrrrr<rrrr<<<rr<<<JJJJJ<JJJ<JJF99FFJJr<<<<<Jr<J<<<<<<<<<<<<<<<<<<<<<<<<<< <<<<<<  <<<< <<<<<<<
+        rrrrrrrrrrrrrrrrrrrrrrrrJrrrrrrrrrrrrrrrrrJ<r<<   Jr<< JF9JrJJFJJr<   <JJ JJ< <<<<<<<<<< <<< <<<<<  <<<<<<<<<<<<    <
+        rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr<J r<      << JJJrJFF99JFFFJJr< JJJJJFJJr<<<<<<<<<<<<<<<<<<< <<<<<<<  < <<<<<<< <<
+        rrr<rrrrrrrrrrrrr<rrrrrrrrrJr<<rr<rrrJ<r        <      JJ<JFJJJJJrJJF rr<rJ<rJJJ<<<<<<< <<<<<<<<<< <<<<<< <<<  <<<  < <
+        rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrJrrJ               <   < <    <JJJJJJJ   rrJ< JJr <<<<<<<<<<<<<<<  < <<        < < < <<
+        rrrrrrrrrrrrrrrrrrrrrrrrrrrrJJJrrrrr <   <                r<rJJJFMMMMMMJr < rJ <<<J<<<<<<<<<<<< <<<<<        <     <<
+        <<<rrrrrr<rrrrrrrrrrrrJJrrrrrrrrrrr <   <          JrrrrJJJJJJFF9MMMMMMMFJ    < <r<<<<  <<<<<<<<  <<          <     <
+         <   < <<<<<<<<<<<<<<rr<rr<<rJrr<<<<  <      r  J<JJJJJJJJFFFFFFFMMMMMMMMFJ       << <<<<<<<<<<  <<<<<        <
+                      <      <      <<<<  <        <rJJJJJJFFFFFFFFFFFFFFMMMMMMMMMF<    < < <<<<< <<<<<<<   <<     < <    <
+                                                    JFFFFFFFFFFFFFFFFFFFFMMMMMMM99FJ        <<<<<<<<<<<<<<<<<
+             <                      <  <          <JFFFFFFFFFFFFFFFFFFFFMMMMMMMM99FJ        <      < < <<  < < < <<<<<<<<<<< <<<
+                                           <       JFFFFFFFFFFFFFFFFFFFFFMMMMMMM99FF<              <            < <<<<  <<<< <<<
+                                           r      JFFFFFFJFJJJJFFFFFFFFFFFFFF9MMM9FFJ                 <     <    <    <   < <  <
+                                           <      FFFFFFJJJJJJJFFFFFFFFFJJJJJJJJJFFFJ       <                       <     <<
+                <                             <  <rFJ   rFFFJ <JFFFFFFFFFJFFFMMMMFFMF                   <  <  <<        <<   < <
+                                             JJ  JFFFFFFFJ    JJFFFFFFJJJJJ<< JJ9MFMJJ
+                                             FJr rFFFFFFFFFFFFFFFFFFM9JJJJJFFFJFFMFFFF                                   <<
+                                             JFJ <FFFFFFFFFFFFFFFJFFFFFFFFFFFFFM9F9F99  <                                   <
+                             <                FF<rFFFFF9FFFFFFFFFFFFFMMFM9FMMMMMMM9MFF r<                              <   <   <
+                                               FFJFFFFFFFFFFFFFFFFFFFFFMMM9FFFMMMMMMFF F                                      <<
+                                                FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFJF
+                <                               FFFFFFFFFFFFFFFFFJJ<FJJrJJFFFFFFFFFFFFFF                     <
+        <     <      <            <   <           FFFFFFFFFFFFFFFFFFFFFFFFFFJFFFFFFFJJ                                    < <
+         <<   <<   <     <<<<    <<    <   <  <    FFFFFFFFFFFFFFFFFFFFFFFFFJJFFFFFJJJ
+        < < < <   <  < < <  <         < <  <       rFFFFFFFFF< FMM9M9MMFJJ  JFFFFFJJJ
+          < <  < <      <<  << << <<       < <    <<JFFFFFFFFFFFFJ<JJFJJFFFFFFFFFJJJ<
+        <<  <<< < <<<    <<<< < <     <  < <<    <<   FFFFFFFFFFFFFFFFFFFFFFFFFJJJr <J             <      <               <
+        <<<  <  <   <  << << <<<<<   < <  <  << <<   J FFFFFFFFFFFFFFFFFFFFFFFJJJ<J<<<r     <     <      <                 < <<
+        < <<< < < <<<<<<< <<<<< <<< <   <<  <<<  <r<<<< FFFFFFFFFFFFFFFFFFFFFJJrrJ<  <J<   <                       < < <     < <
+        <<<<<  <<<<< <<<<< < << <<<<<<< <<<<  Jr<<<<<<< FFFFFFFFFFFFFFFFFFFFJJrr<J<r<<<r<    < < <<<   << <  <    <  <<<     <<<
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<< <<<<  J<<<<<<<<<<<< FFFFFFFFFFFFFFFFJJJJJJr<<<<<<<<<rJr<rrJr  < <  <<<    <<<<<<< <<<<<<<<
+        <<<<< <<<<<<<<<<<<<<<<<<<<<<<<< F<<<<<<<<<<<<<<< FFFFFFFFFFFFFFJJJJJJJJJ< <<<<<<<JJJr<rrJJJJ <    < <<< << <<<<<<<  << <
+        <<<<<<<<<<<<<<<<<<<<<<<<<<< rFJ<<<<<<<<<<<<<<<<< FFFFFFFFFFFFFFJJJJJJJJ<<<<<<<<<rrrrrrr<JJJJJrF<     <<<<<<<<<<<<  <<<<
+        <<<<<<<<<<<<<<<<<<<<< <rrJ<<<<<<<<<<<<<<<<<<<< rJFFFFFFFFFFFFFJJJJJJJJr<<<<<<r<<rr<<rrrJJrJrrJFJFrJrJJJJJ <<<<<<<<<<<<<<
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<JJFFFFFFFFFFFFFFFFJJJJJJ<<<<<<<<rrrJrrrrJJrFJrrrrrJJFF<JrJ<Fr <<<<<<<<<<<<
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<JrJJFFFFFFFFFFFFFFFFFFJJJJr<<<<r<<rrrrFrrJrJr<r<rrrF<rJFrJJJrJJJJ<<<< <<<<<<<
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<rJJFFFFFFFFFFFFFFFFFFFJJJr<<<<<<<rrrrr<rFrrJrJr<<<<r<<rFJrrJJJJJrJJ<<<<<<<<<<
+        <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<JJFFFFFFFFFFFFFFFFFFFJJJJ<<<<<<<r<r<<rrrrJrr<<r<r<r<r<Jr<Jr<<r<rJrJJJ<<<<<<<<
+        <<<<<<<<<<<<<<<<<<<<<<JJF<<<<<<<<<<<<<<<<<<JJFFFFFFFFFFFFFFFFFFJJJ<<<<<<<<<r<<<rrJrrrr<<<<<<<r<<rr<<r<r<rr<<<<r<<r <<<<<
+        <<<<<<<<<<<<<<<<<<<<FFFFFJ<<<<<<<<<<<<<<<<rJJJJJFFFFFFJJJFFJJJJJ<<<<<<<<<<r<<rJrrFr<r<<<<<<<<r<<<r<<rrrrrrr<<<<<<<<<<<<<
+        <<<<<<<<<<<<<<<<<<FFJJJJJJr<<<<<<<<<<<<<<rJJJJJJJJJJJJJJJJJJJJJ<<<<<<<<<<<<<rrJrr<<<<<<<<<<<<<rrF<r<<<JrJJ<<<rr<rrr< <<<
+        <<<<<<<<<<<<<<<JFFFJFJrrrJrJJ<<<<<<<<<<<<rJJJJJJJJJJJJJJJJJJJ <<<<<<<<<<r<rrrr<<<<<<<<<<<<<Jrrr<<<<rr<rr<<<r<<<<<<r<r <<
+        <<<<<<<<<<<<<FJJFJFrJrrr<rr<<Jr<<<<<<<<<<JJJJJJJJJJJJJJJJJJJ<<<<<r<<<<<<<<<<<<<<<<<<<<<<<<r<r<r<<r<<<r<<<<<<<<<r<<<<<<<<
+        <<<<<<<<<<<<FFJFJJJJrr<<<<rrr<rr<<<<<<<<<rJJJJJJJJJJJJJJJFF <<<<<<<<<<<<r<<r<<<<<<<<<<<<<<J<<J<<J<<<<<<<<<<<<<<<<<<<<<<<
+        <<<<<<<<<<<<JJJJFJFrrJ<<<<<Jr<<r<<<<<<<<<rrJJJJJJJJJJJFFFFJ<r<<<<<<<<<<<r<<<<<<<<<<<<r<<rrrrr<<<<<<<<<<<<<<<<r<r<<<<<<<<
+        <<<<<<<<<<<<JFJJJJrrJr<<<<<J<<<<<<<<<<<<<JJJJJJJJJJJFFFFFJ<<<<<r<<<<<<r<<<<<<<<<<<<<<<<<rrrrr<<<<<<<<<<<<<r<<<<<<<<<<<<<
+        <<<<<<<<<<<<<J<rr<r<rr<<<<<r<r<<<<<<<<<<rJJJJJJJJJFFFFFFJ< r<<r<<<<rJr<<<<<<<<J<<<<<<r<<r<<<<<<<<<<r<<<<<rr<<rrr<<<<<<<r
+        <<<<<<<<<<<Jr<r<r<r<rrr<<<<J<<<<<<<<<<<<rJJJJJJJFFFFFFFJJ<<<<r<<<<<r<<<<<<<<<rJr<<<rr<r<<<<<<<<<J<<<<<<<rr<r<JrJ<rrJJrr<
+        <<<<<<<<<<<JJ<<<<rrr<<<<<<<J<<<<<<<<<<<<JJJJJJFFFFJJJFJJ<<<r<<<<<<<<<<<<<<<<<rF<<<<<J<<r<<<<<<<<Jr<<<<r<<<<<<<<r<<<<<<<<
+        <<<<<<<<<<rr<<<<<<<<<r<<<<<rJr<<<<<<<<<<JJJJJFFJJJJJJJJJ<<r<<<<<<r<<<r<<<<<<<<<<<<r<<r<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<r<<
+        <<<<<<<<<<JJ<r<<<<<<r<<<<<<r<r<<<<<r<<<rJJJJJJJJJJJJJJrJ<<<<<<<<<<<<<<<<<<<<<<<r<r<<<<<<<<<<<<<r<<<<<<<<<<<<<<<<<<<<<<<<
+        """
