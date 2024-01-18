fe599d41a5210d4580008ec65987636822175845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 05:03:53 +0000

    Add package cappa/0.15.4/cappa-0.15.4-py3-none-any.whl

---
+++ packages/cappa/cappa-0.15.4-py3-none-any.whl/cappa/__init__.py
@@ -0,0 +1,35 @@
+from cappa.base import collect, command, invoke, invoke_async, parse
+from cappa.command import Command
+from cappa.completion.types import Completion
+from cappa.env import Env
+from cappa.invoke import Dep
+from cappa.output import Exit, HelpExit, Output
+from cappa.subcommand import Subcommand, Subcommands
+
+# isort: split
+from cappa.arg import Arg, ArgAction
+
+# isort: split
+from cappa import argparse
+from cappa.parser import backend
+
+__all__ = [
+    "Arg",
+    "ArgAction",
+    "Command",
+    "Completion",
+    "Dep",
+    "Env",
+    "Exit",
+    "HelpExit",
+    "Output",
+    "Subcommand",
+    "Subcommands",
+    "argparse",
+    "backend",
+    "collect",
+    "command",
+    "invoke",
+    "invoke_async",
+    "parse",
+]
