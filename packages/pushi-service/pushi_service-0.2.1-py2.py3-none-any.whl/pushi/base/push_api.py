598df46356546eb76be6e7c9106e554d401c7851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 03:03:55 +0000

    Add package pushi-service/0.2.1/pushi_service-0.2.1-py2.py3-none-any.whl

---
+++ packages/pushi-service/pushi_service-0.2.1-py2.py3-none-any.whl/pushi/base/push_api.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Hive Pushi System
+# Copyright (c) 2008-2024 Hive Solutions Lda.
+#
+# This file is part of Hive Pushi System.
+#
+# Hive Pushi System is free software: you can redistribute it and/or modify
+# it under the terms of the Apache License as published by the Apache
+# Foundation, either version 2.0 of the License, or (at your option) any
+# later version.
+#
+# Hive Pushi System is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# Apache License for more details.
+#
+# You should have received a copy of the Apache License along with
+# Hive Pushi System. If not, see <http://www.apache.org/licenses/>.
+
+__author__ = "João Magalhães <joamag@hive.pt>"
+""" The author(s) of the module """
+
+__copyright__ = "Copyright (c) 2008-2024 Hive Solutions Lda."
+""" The copyright for the module """
+
+__license__ = "Apache License, Version 2.0"
+""" The license for the module """
+
+from . import handler
+
+
+class PushAPIHandler(handler.Handler):
+    """
+    Pushi handler (adapter) for the Push API infra-structure
+    (APN) infra-structure, so that it's possible to send
+    push notification to modern Web browsers.
+
+    :see: https://w3c.github.io/push-api
+    """
+
+    def __init__(self, owner):
+        handler.Handler.__init__(self, owner, name="push_api")
+        self.subs = {}
