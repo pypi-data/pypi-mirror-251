485b10bc3ce4b7ff8083f289d40d89f07fe52e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 04:04:30 +0000

    Add package snk/0.19.1/snk-0.19.1.tar.gz

---
+++ packages/snk/snk-0.19.1.tar.gz/snk-0.19.1/snk/cli/options/option.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class Option:
+    name: str
+    original_key: str
+    default: Any
+    updated: bool
+    help: str
+    type: Any
+    required: bool
+    short: str
