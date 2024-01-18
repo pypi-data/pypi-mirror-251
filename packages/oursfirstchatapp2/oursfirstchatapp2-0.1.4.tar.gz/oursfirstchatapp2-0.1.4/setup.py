d73903f6c3776a1fdb2a0478fb7a8114e689baf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:51 +0000

    Add package oursfirstchatapp2/0.1.4/oursfirstchatapp2-0.1.4.tar.gz

---
+++ packages/oursfirstchatapp2/oursfirstchatapp2-0.1.4.tar.gz/oursfirstchatapp2-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='oursfirstchatapp2',
+  version='0.1.4',
+  description='Our First Intelligent Chatting UI',
+  author='Jean Guillaume',
+  author_email='jgi@jgwill.com',
+  url='https://github.com/jgwill/our-prototypes-240117/tree/openai_samples_240117/openai/qt-our-first-intelligent-chatting-ui',
+  packages=find_packages('src'),
+  package_dir={'': 'oursfirstchatapp2'},
+  install_requires=[
+    'python-dotenv',
+    'requests',
+    'pyperclip',
+    'pyside2',
+    'PyQt5'
+  ],
+  entry_points={
+    'console_scripts': [
+      'oursfirstchatapp2=oursfirstchatapp2.app:main',
+    ],
+  },
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
