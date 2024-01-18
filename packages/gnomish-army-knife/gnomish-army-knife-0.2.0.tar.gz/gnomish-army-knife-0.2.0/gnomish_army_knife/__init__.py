6c5fc3c00884347e54108057b861dd22b1495b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:05:01 +0000

    Add package gnomish-army-knife/0.2.0/gnomish-army-knife-0.2.0.tar.gz

---
+++ packages/gnomish-army-knife/gnomish-army-knife-0.2.0.tar.gz/gnomish-army-knife-0.2.0/gnomish_army_knife/__init__.py
@@ -0,0 +1,18 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=9cd87eb5576cb5c5c108051364d37782
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "Software tools for WoW arena analysis."
+PKG_NAME = "gnomish-army-knife"
+VERSION = "0.2.0"
+
+# gnomish-army-knife-specific content.
+PKG_ABBREV = "gak"
+DEFAULT_CONFIG = f"{PKG_ABBREV}.yaml"
+WOW_DIR = "World of Warcraft"
