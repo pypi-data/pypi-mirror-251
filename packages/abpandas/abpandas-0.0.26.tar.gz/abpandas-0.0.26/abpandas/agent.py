e02f0de3f9fd7c84397c5263d226e1375c380cc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 03:03:49 +0000

    Add package abpandas/0.0.26/abpandas-0.0.26.tar.gz

---
+++ packages/abpandas/abpandas-0.0.26.tar.gz/abpandas-0.0.26/abpandas/agent.py
@@ -0,0 +1,19 @@
+class Agent:
+    def __init__(self, properties={}):
+        """
+        a class representing a computational unit with properties that can interact with other agents and with a space
+
+        Attributes
+        ----------
+        properties: dictionary, default= {}
+            the properties of the agent object
+        __id: int
+            a unique id given to each agent (accessed by the Model class when creating the Agent)
+        location_index: int, default= None
+            the index to which the agent is assigned (accessed by the Model class when creating or moving the Agent)
+        """
+        self.my_class = None
+        self.props = properties
+        self.__id = 0
+        self.location_index = None
+        self.location_xy = None
