a393c50787b66aea465b457108965a44165fc136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:21 +0000

    Add package he-html-document/0.1/he_html_document-0.1.tar.gz

---
+++ packages/he-html-document/he_html_document-0.1.tar.gz/he_html_document-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='he_html_document',
+    version='0.1',
+    author='He Bill',
+    author_email='hebill@me.com',
+    packages=find_packages(),
+    url='https://py.hebill.net/he_html_document',  # 项目的URL
+    install_requires=[
+        # 你的软件包依赖的其他库
+    ],
+)
