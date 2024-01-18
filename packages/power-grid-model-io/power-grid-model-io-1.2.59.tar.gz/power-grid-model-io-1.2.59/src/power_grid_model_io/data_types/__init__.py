dc6397f988f526272e3e82df8a5cd4bb901aaaa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:47 +0000

    Add package power-grid-model-io/1.2.59/power-grid-model-io-1.2.59.tar.gz

---
+++ packages/power-grid-model-io/power-grid-model-io-1.2.59.tar.gz/power-grid-model-io-1.2.59/src/power_grid_model_io/data_types/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2022 Contributors to the Power Grid Model project <dynamic.grid.calculation@alliander.com>
+#
+# SPDX-License-Identifier: MPL-2.0
+"""
+Common data types used in the Power Grid Model project
+"""
+
+from power_grid_model_io.data_types._data_types import ExtraInfo, ExtraInfoLookup, StructuredData
+from power_grid_model_io.data_types.tabular_data import LazyDataFrame, TabularData
