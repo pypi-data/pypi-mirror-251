fa7e0f6f2474a55cfc9b7d9f116e4f787e595163
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 05:04:07 +0000

    Add package sdwi2iextender/0.0.1/sdwi2iextender-0.0.1-py3-none-any.whl

---
+++ packages/sdwi2iextender/sdwi2iextender-0.0.1-py3-none-any.whl/sdwi2iextender/lib/custom_tab.py
@@ -0,0 +1,14 @@
+
+
+class CustomTab:
+    show_inpaint_params = True
+    requested_elem_ids = []
+
+    def tab(self):
+        pass
+
+    def section(self, components):
+        pass
+
+    def gradio_events(self, img2img_tabs):
+        pass
