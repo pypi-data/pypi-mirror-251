8a9019faed498a43852acc1b7fe678e79b591482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:05:01 +0000

    Add package ulflib/0.0.18/ulflib-0.0.18.tar.gz

---
+++ packages/ulflib/ulflib-0.0.18.tar.gz/ulflib-0.0.18/src/ulflib/tense_tree/tense_tree.py
@@ -0,0 +1,202 @@
+"""Tense tree implementation"""
+
+from ulflib.util import atom, listp
+from ulflib.sexpr import parse_s_expr
+from ulflib.ulflib import lex_tense_p, lex_aspect_p, tensed_sent_reifier_p, adv_e_p, adv_f_p, lex_rel_p, ps_p
+
+from ulflib.tense_tree.constants import *
+from ulflib.tense_tree.preprocess import preprocess
+from ulflib.tense_tree.tense_node import TenseNode
+
+def get_mood(ulf):
+  """Determine the mood of the ULF."""
+  if atom(ulf):
+    return MOOD_DECL
+  elif ulf[-1] in ['?', '.?']:
+    return MOOD_QUES
+  elif ulf[-1] == '!':
+    return MOOD_IMPER
+  else:
+    return MOOD_DECL
+  
+
+class TenseTree:
+  """A tense tree.
+  
+  Attributes
+  ----------
+  root : TenseNode
+    The root node of this tense tree.
+  now_idx : int
+    A counter for the current 'Now' episode, to be updated
+    with each logical form that gets deindexed.
+  ep_idx : int
+    A counter for each episode variable created.
+  utt_idx : int
+    A counter for each utterance variable created.
+  disable_speech_acts : bool
+    Whether to globally set speech_act = False when deindexing.
+  """
+
+  def __init__(self, disable_speech_acts=False):
+    self.reset()
+    self.disable_speech_acts=disable_speech_acts
+
+
+  def reset(self):
+    self.now_idx = 0
+    self.ep_idx = 0
+    self.utt_idx = 0
+    self.root = TenseNode()
+
+
+  def now(self):
+    """Return the current 'Now' symbol."""
+    return NOW + str(self.now_idx)
+
+
+  def new_now(self):
+    """Create a new 'Now' symbol."""
+    self.now_idx += 1
+    return self.now()
+
+
+  def new_ep_var(self):
+    """Create a new episode variable."""
+    self.ep_idx += 1
+    return EP_VAR + str(self.ep_idx)
+  
+
+  def new_utt_var(self):
+    """Create a new utterance variable."""
+    self.utt_idx += 1
+    return UTT_VAR + str(self.utt_idx)
+  
+
+  def get_emb_episode(self, emb_node):
+    """Get the episode corresponding to an embedding node.
+    
+    This is either the last episode of the node, or the current 'Now' if the node
+    is the root of the tense tree.
+    """
+    if emb_node is self.root:
+      return self.now()
+    else:
+      return emb_node.last_episode()
+    
+
+  def deindex(self, ulf, speech_act=True, is_scoped=False):
+    """The top-level function to de-index a (possibly unscoped) logical form.
+
+    Parameters
+    ----------
+    ulf : s-expr
+      An "S-expression" (possibly nested lists of strings) representing an
+      unscoped logical form (ULF) formula.
+    speech_act : bool, default=True
+      Whether to treat the input formula as a speech act, i.e., adding top-level
+      speech act episodes to the resulting formula.
+    is_scoped : bool, default=False
+      Whether the input formula has already been scoped.
+    
+    Notes
+    -----
+    TODO: I'm not currently sure what should be done in the case of combined tense/aspect operators,
+    such as (pres perf), or even whether this is currently supported by the scoping method used in
+    preprocessing ULFs.
+    """
+    def deindex_recur(ulf, emb_node, focus):
+      assert isinstance(emb_node, TenseNode) and isinstance(focus, TenseNode)
+      
+      if atom(ulf):
+        return ulf
+      op = ulf[0]
+
+      # 1. Tense or aspect operator
+      if lex_tense_p(op) or lex_aspect_p(op):
+        e = self.new_ep_var()
+        e_emb = self.get_emb_episode(emb_node)
+
+        if op == PAST:
+          restrictor = [e, focus.bef(), e_emb]
+          new_focus = focus.move_left()
+        elif op == PRES:
+          restrictor = [e, AT_ABOUT, e_emb]
+          new_focus = focus
+        elif op == FUTR:
+          restrictor = [e, AFTER, focus.last_episode()]
+          new_focus = focus.move_right()
+        elif op == PERF:
+          restrictor = [e, IMPINGES_ON, focus.last_episode()]
+          new_focus = focus.move_down()
+        # other tense/aspect operators such as 'prog' or 'cf' are not yet supported
+        else:
+          return [op, deindex_recur(ulf[1], emb_node, focus)]
+        
+        if new_focus.last_episode():
+          restrictor = [restrictor, AND, [new_focus.last_episode(), ORIENTS, e]]
+
+        nucleus = [deindex_recur(ulf[1], emb_node, new_focus.add_episode(e)), CH, e]
+        return [EXISTS, e, restrictor, nucleus]
+      
+      # 2. Embedding operator
+      elif tensed_sent_reifier_p(op):
+        return [op, deindex_recur(ulf[1], focus, focus.move_embedded_new())]
+
+      # 3. Temporal or frequency adverb
+      elif (adv_e_p(op) or adv_f_p(op)) and listp(op):
+        e = focus.last_episode()
+        adv_pred = deindex_recur(op[1], emb_node, focus)
+        nucleus = deindex_recur(ulf[1], emb_node, focus)
+        if adv_e_p(op):
+          return [[e, adv_pred], AND, nucleus]
+        else:
+          return [[e, adv_pred], AND, [MULT, nucleus]]
+
+      # 4. Relativizers (e.g., that.rel)
+      elif lex_rel_p(op):
+        # TODO
+        pass
+
+      # 5. Sentential prepositions (e.g., before.ps)
+      elif ps_p(op) and listp(op):
+        e = focus.last_episode()
+        ps_res = deindex_recur(op[1], emb_node, focus)
+        prep = op[0][:-1]
+        if ps_res[0] == EXISTS:
+          _, e2, restrictor, nucleus = ps_res
+          restrictor_new = restrictor + [AND, [e, prep, e2]]
+          return [EXISTS, e2, restrictor_new, nucleus]
+
+      return [deindex_recur(ulf_part, emb_node, focus) for ulf_part in ulf]
+
+    # ===== MAIN BODY OF FUNCTION =====
+    
+    if isinstance(ulf, str):
+      ulf = parse_s_expr(ulf)
+
+    mood = get_mood(ulf)
+    ulf = preprocess(ulf, is_scoped=is_scoped)
+    u = self.new_utt_var()
+    now = self.new_now()
+    restrictor = [u, SAME_TIME, now]
+    focus = self.root
+
+    # The utterance episode is assumed to be just after the preceeding episode in focus
+    if focus.last_episode():
+      restrictor = [restrictor, AND, [u, JUST_AFTER, focus.last_episode()]]
+
+    embedded = deindex_recur(ulf, focus, focus.add_episode(u).move_embedded_cur())
+
+    if self.disable_speech_acts or not speech_act:
+      return embedded
+
+    # Form speech act logical form depending on mood of ULF
+    if mood == MOOD_DECL:
+      lf = [EXISTS, u, restrictor, [[SPEAKER, TELL, HEARER, [THAT, embedded]], CH, u]]
+    elif mood == MOOD_QUES:
+      lf = [EXISTS, u, restrictor, [[SPEAKER, ASK, HEARER, [ANS_TO, embedded]], CH, u]]
+    elif mood == MOOD_IMPER:
+      lf = [EXISTS, u, restrictor, [[SPEAKER, INSTRUCT, HEARER, [KA, embedded]], CH, u]]
+
+    return lf
---
+++ packages/ulflib/ulflib-0.0.18.tar.gz/ulflib-0.0.18/test/tense_tree_test.py
@@ -0,0 +1,210 @@
+import sys
+sys.path.append("src/")
+
+import pytest
+
+from ulflib.util import variablep
+from ulflib.tense_tree.preprocess import *
+from ulflib.tense_tree import TenseTree
+
+def equal_with_variables(ulf1, ulf2):
+  var_map = {}
+  def rec(x1, x2):
+    if atom(x1) and atom(x2):
+      if variablep(x1) and variablep(x2):
+        if x1 not in var_map:
+          var_map[x1] = x2
+          return True
+        else:
+          return var_map[x1] == x2
+      else:
+        return x1 == x2
+    elif atom(x1) or atom(x2):
+      return False
+    elif len(x1) != len(x2):
+      return False
+    else:
+      return all([rec(y1, y2) for y1, y2 in zip(x1, x2)])
+  return rec(ulf1, ulf2)
+
+
+
+class TestPreprocessingUninvertAux:
+  def test_1(self):
+    ulf = [[['pres', 'do.aux-s'], 'john', ['go.v', ['to', ['the.d', 'store.n']]]], '?']
+    ulf1 = [['john', [['pres', 'do.aux-s'], ['go.v', ['to', ['the.d', 'store.n']]]]], '?']
+    assert equal_with_variables(uninvert_aux(ulf), ulf1)
+
+
+
+class TestPreprocessingLiftSentMods:
+  def test_1(self):
+    ulf = ['past', ['mary', ['leave.v', 'yesterday.adv-e']]]
+    ulf1 = ['past', ['yesterday.adv-e', ['mary', 'leave.v']]]
+    assert equal_with_variables(lift_sent_mods(ulf), ulf1)
+
+
+  def test_2(self):
+    ulf = ['past', ['mary', ['leave.v', 'yesterday.adv-e', ['adv-e', ['before.p', ['k', 'noon.n']]]]]]
+    ulf1 = ['past', ['yesterday.adv-e', [['adv-e', ['before.p', ['k', 'noon.n']]], ['mary', 'leave.v']]]]
+    assert equal_with_variables(lift_sent_mods(ulf), ulf1)
+
+
+  def test_3(self):
+    ulf = ['past', ['john', ['go.v', ['to.p', ['the.d', 'bathroom.n']], 'frequently.adv-f', 'yesterday.adv-e']]]
+    ulf1 = ['past', ['frequently.adv-f', ['yesterday.adv-e', ['john', ['go.v', ['to.p', ['the.d', 'bathroom.n']]]]]]]
+    assert equal_with_variables(lift_sent_mods(ulf), ulf1)
+
+
+  def test_4(self):
+    ulf = ['past', [['the.d', ['red.a', 'block.n']], [['pasv', 'move.v'], ['adv-e', ['most.mod-a', ['adv-e', 'recent.a']]]]]]
+    ulf1 = ['past', [['adv-e', ['most.mod-a', ['adv-e', 'recent.a']]], [['the.d', ['red.a', 'block.n']], ['pasv', 'move.v']]]]
+    assert equal_with_variables(lift_sent_mods(ulf), ulf1)
+
+
+  def test_5(self):
+    ulf = ['past', ['john', 'just.adv-e', ['move.v', ['the.d', ['green.a', 'block.n']],
+                                           ['before.ps', ['past', [['the.d', ['red.a', 'block.n']], [['pasv', 'move.v'],
+                                                                      ['adv-e', ['most.mod-a', ['adv-e', 'recent.a']]]]]]]]]]
+    ulf1 = ['past', ['just.adv-e', [['before.ps', ['past', [['adv-e', ['most.mod-a', ['adv-e', 'recent.a']]],
+                                                            [['the.d', ['red.a', 'block.n']], ['pasv', 'move.v']]]]],
+                                    ['john', ['move.v', ['the.d', ['green.a', 'block.n']]]]]]]
+    assert equal_with_variables(lift_sent_mods(ulf), ulf1)
+
+
+
+class TestPreprocessingExpandTemporalMods:
+  def test_ever(self):
+    ulf = ['past', ['ever.adv-e', ['john', ['go.v', ['to.p', ['the.d', 'store.n']]]]]]
+    ulf1 = ['past', [['adv-e', 'episode.n'], ['john', ['go.v', ['to.p', ['the.d', 'store.n']]]]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+  def test_indexical(self):
+    ulf = ['past', ['yesterday.adv-e', ['mary', 'leave.v']]]
+    ulf1 = ['past', [['adv-e', ['during.p', '^yesterday']], ['mary', 'leave.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', ['now.adv-e', ['mary', 'leave.v']]]
+    ulf1 = ['past', [['adv-e', ['during.p', '^now']], ['mary', 'leave.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', ['currently.adv-e', ['mary', 'leave.v']]]
+    ulf1 = ['past', [['adv-e', ['during.p', '^now']], ['mary', 'leave.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+  def test_temporal_location(self):
+    ulf = ['past', [['adv-e', ['on.p', ['k', ['last.a', 'saturday.n']]]], ['mary', 'leave.v']]]
+    ulf1 = ['past', [['adv-e', ['during.p', ['the', '?t', ['?t', ['last.a', 'saturday.n']]]]], ['mary', 'leave.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', ['just.adv-e', ['mary', 'leave.v']]]
+    ulf1 = ['past', [['adv-e', ['during.p', ['the', '?t', ['?t', ['previous.a', 'episode.n']]]]], ['mary', 'leave.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', ['initially.adv-e', ['mary', 'leave.v']]]
+    ulf1 = ['past', [['adv-e', ['during.p', ['the', '?t', ['?t', ['initial.a', 'episode.n']]]]], ['mary', 'leave.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', [['adv-e', ['before.p', ['this.d', 'wednesday.n']]],
+                    [['adv-e', ['after.p', ['the.d', ['last.a', 'weekend.n']]]], ['mary', 'leave.v']]]]
+    ulf1 = ['past', [['adv-e', ['before.p', ['this', '?t1', ['?t1', 'wednesday.n']]]],
+                     [['adv-e', ['after.p', ['the', '?t2', ['?t2', ['last.a', 'weekend.n']]]]], ['mary', 'leave.v']]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+  
+  def test_temporal_range(self):
+    ulf = ['past', ['recently.adv-e', ['mary', 'leave.v']]]
+    ulf1 = ['past', [['adv-e', ['during.p', ['some', '?t', ['?t', ['recent.a', 'episode.n']]]]], ['mary', 'leave.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', ['before.adv-e', ['mary', 'leave.v']]]
+    ulf1 = ['past', [['adv-e', ['during.p', ['some', '?t', ['?t', ['previous.a', 'episode.n']]]]], ['mary', 'leave.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+  def test_duration(self):
+    ulf = ['past', [['adv-e', ['for.p', ['two.d', ['plur', 'hour.n']]]], ['john', 'sleep.v']]]
+    ulf1 = ['past', [['adv-e', ['has-duration.p', ['k', ['2.mod-n', ['plur', 'hour.n']]]]], ['john', 'sleep.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', [['adv-e', ['in.p', ['two.d', ['plur', 'hour.n']]]], ['john', ['run.v', ['the.d', 'race.n']]]]]
+    ulf1 = ['past', [['adv-e', ['in-span-of.p', ['k', ['2.mod-n', ['plur', 'hour.n']]]]], ['john', ['run.v', ['the.d', 'race.n']]]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+  def test_count(self):
+    ulf = ['past', ['twice.adv-f', ['john', ['see.v', ['the.d', 'movie.n']]]]]
+    ulf1 = ['past', [['adv-f', ['2.mod-n', ['plur', 'episode.n']]], ['john', ['see.v', ['the.d', 'movie.n']]]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', [['adv-f', ['for.p', ['three.d', ['plur', 'time.n']]]], ['mary', ['visit.v', 'paris']]]]
+    ulf1 = ['past', [['adv-f', ['3.mod-n', ['plur', 'time.n']]], ['mary', ['visit.v', 'paris']]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', [['adv-f', 'twice.a'], ['mary', ['visit.v', 'paris']]]]
+    ulf1 = ['past', [['adv-f', ['2.mod-n', ['plur', 'episode.n']]], ['mary', ['visit.v', 'paris']]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+  def test_frequency(self):
+    ulf = ['past', ['frequently.adv-f', ['john', ['call.v', 'mary']]]]
+    ulf1 = ['past', [['adv-f', [['attr', 'frequent.a'], ['plur', 'episode.n']]], ['john', ['call.v', 'mary']]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', ['rarely.adv-f', ['john', ['call.v', 'mary']]]]
+    ulf1 = ['past', [['adv-f', [['attr', 'infrequent.a'], ['plur', 'episode.n']]], ['john', ['call.v', 'mary']]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+  def test_periodicity(self):
+    ulf = ['past', [['adv-f', ['for.p', ['every.d', ['four.a', ['plur', 'hour.n']]]]], ['john', ['take.v', ['k', 'medicine.n']]]]]
+    ulf1 = ['past', [['adv-f', [':l', '?s', [['?s', [['attr', 'periodic.a'], ['plur', 'episode.n']]], 'and.cc',
+                                             [['period-of.f', '?s'], '=', ['k', ['4.mod-n', ['plur', 'hour.n']]]]]]],
+                                             ['john', ['take.v', ['k', 'medicine.n']]]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+  def test_recurrence(self):
+    ulf = ['pres', [['adv-f', ['for.p', ['every.d', 'saturday.n']]], ['mary', 'swim.v']]]
+    ulf1 = ['pres', [['adv-f', [':l', '?s', ['all', '?t', ['?t', 'saturday.n'],
+                                             ['exists', '?e', [['?e', 'member-of.p', '?s'], 'and.cc',
+                                                               ['?e', 'during.p', '?t']]]]]], ['mary', 'swim.v']]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+  # def test_temporal_relation(self):
+  #   # TODO: unsure if this needs to be preprocessed
+  #   ulf = ['past', [['since.ps', ['past', ['john', 'go_off.v', ['to.p', ['k', 'college.n']]]]], ['mary', 'take_care_of.v', ['the.d', 'dog.n']]]]
+  #   ulf1 = ['past', [['adv-e', [':l', '?e', ['the-earliest', '?t',
+  #                                            ['past', [['adv-e', ['at-time.p', '?t']], ['john', 'go_off.v', ['to.p', ['k', 'college.n']]]]],
+  #                                            ['?e', 'after.p', '?t']]]],
+  #                       ['mary', 'take_care_of.v', ['the.d', 'dog.n']]]]
+  #   assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+  def test_combinations(self):
+    ulf = ['past', [['adv-e', ['in.p', ['two.d', ['plur', 'month.n']]]],
+                    [['adv-f', ['for.p', ['three.d', ['plur', 'time.n']]]], ['mary', ['visit.v', 'paris']]]]]
+    ulf1 = ['past', [['adv-e', ['in-span-of.p', ['k', ['2.mod-n', ['plur', 'month.n']]]]],
+                    [['adv-f', ['3.mod-n', ['plur', 'time.n']]], ['mary', ['visit.v', 'paris']]]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+    ulf = ['past', [['adv-e', ['for.p', ['ten.d', ['plur', 'day.n']]]],
+                    [['adv-f', ['for.p', ['every.d', ['four.a', ['plur', 'hour.n']]]]], ['john', ['take.v', ['k', 'medicine.n']]]]]]
+    ulf1 = ['past', [['adv-e', ['has-duration.p', ['k', ['10.mod-n', ['plur', 'day.n']]]]],
+                    [['adv-f', [':l', '?s', [['?s', [['attr', 'periodic.a'], ['plur', 'episode.n']]], 'and.cc',
+                                             [['period-of.f', '?s'], '=', ['k', ['4.mod-n', ['plur', 'hour.n']]]]]]],
+                        ['john', ['take.v', ['k', 'medicine.n']]]]]]
+    assert equal_with_variables(expand_temporal_mods(ulf), ulf1)
+
+
+
+tree = TenseTree(disable_speech_acts=True)
+# ulf = ['mary', [['past', 'leave.v'], ['adv-e', ['before.p', ['this.d', 'wednesday.n']]]]]
+ulf = ['past', ['frequently.adv-f', ['john', ['call.v', 'mary']]]]
+print(tree.deindex(ulf))
+# print(preprocess(ulf))
+# print(scoping.scope(ulf))
+
