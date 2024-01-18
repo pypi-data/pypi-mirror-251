0349fb2f8111ae6e9e60516fe2714bb5f2e9f453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:05:04 +0000

    Add package webint-code/0.0.70/webint_code-0.0.70.tar.gz

---
+++ packages/webint-code/webint_code-0.0.70.tar.gz/webint_code-0.0.70/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['webint_code', 'webint_code.templates']
+
+package_data = \
+{'': ['*'],
+ 'webint_code.templates': ['project/*',
+                           'project/issues/*',
+                           'pypi/*',
+                           'search/*',
+                           'snippets/*']}
+
+install_requires = \
+['gmpg>=0.0', 'webagt>=0.0', 'webint>=0.0']
+
+entry_points = \
+{'webapps': ['code = webint_code:app']}
+
+setup_kwargs = {
+    'name': 'webint-code',
+    'version': '0.0.70',
+    'description': 'manage code on your website',
+    'long_description': 'None',
+    'author': 'Angelo Gladding',
+    'author_email': 'angelo@ragt.ag',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://ragt.ag/code/projects/webint-code',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<3.11',
+}
+
+
+setup(**setup_kwargs)
