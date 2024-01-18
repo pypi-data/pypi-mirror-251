c1e50e6ab5e1a077621aa822368b57bed1a40ccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:06 +0000

    Add package gpycraft/1.0.1/gpycraft-1.0.1.tar.gz

---
+++ packages/gpycraft/gpycraft-1.0.1.tar.gz/gpycraft-1.0.1/gpycraft/__init__.py
@@ -0,0 +1,4 @@
+from gpycraft.app_config import Admin
+
+
+
---
+++ packages/gpycraft/gpycraft-1.0.1.tar.gz/gpycraft-1.0.1/setup.py
@@ -0,0 +1,28 @@
+# setup.py
+
+from setuptools import setup,find_packages
+
+with open('README.md', 'r') as f:
+    description = f.read()
+
+setup(
+    name='gpycraft',
+    version='1.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'firebase-admin',
+        'gspread',
+        'oauth2client',
+        'pandas',
+        'PyYAML',
+        'tqdm',
+    ],
+    description='A Python package for manipulating google sheets directly on any coding platform and perform CRUD process easily. Sheets can be stored and retrieved in firebase storage',
+    entry_points={
+        'console_scripts': [
+            'begin = gpycraft.app_config:Admin.begin'
+        ],
+    },
+    long_description=description,
+    long_description_content_type='text/markdown',
+)
