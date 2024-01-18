fbc1254753585ce75b5a438a64e12dc5e63d3b35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:04:48 +0000

    Add package pyiron-ontology/0.1.4/pyiron_ontology-0.1.4.tar.gz

---
+++ packages/pyiron-ontology/pyiron_ontology-0.1.4.tar.gz/pyiron_ontology-0.1.4/setup.py
@@ -0,0 +1,39 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='pyiron_ontology',
+    version=versioneer.get_version(),
+    description='pyiron_ontology - module extension to pyiron.',
+    long_description='http://pyiron.org',
+
+    url='https://github.com/pyiron/pyiron_ontology',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='liamhuber@greyhavensolutions.com',
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
+    install_requires=[
+        'numpy>=1.26.3',
+        'owlready2>=0.45',
+        'pandas>=2.1.4',
+        'pint>=0.23',
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+
+    )
