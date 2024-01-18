85a133f2c11e10a962e40be8363bd79714e70da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 03:04:13 +0000

    Add package pyrostep/2.10.19/pyrostep-2.10.19.tar.gz

---
+++ packages/pyrostep/pyrostep-2.10.19.tar.gz/pyrostep-2.10.19/pyrostep/__init__.py
@@ -0,0 +1,33 @@
+"""
+A Python library to handle steps in pyrogram framework.
+"""
+__author__ = "aWolver"
+__version__ = "2.10.19"
+
+__all__ = [
+    "change_root_store",
+    "listen",
+    "register_next_step",
+    "unregister_steps",
+    "wait_for",
+    "clear",
+    "shortcuts",
+]
+
+from .steps import (
+    MetaStore as MetaStore,
+    change_root_store as change_root_store,
+    listen as listen,
+    register_next_step as register_next_step,
+    unregister_steps as unregister_steps,
+    wait_for as wait_for,
+    clear as clear,
+)
+
+from . import (
+    shortcuts as shortcuts,
+    connection as connection,
+)
+
+from ._idle import safe_idle as safe_idle
+from ._install import install as install
---
+++ packages/pyrostep/pyrostep-2.10.19.tar.gz/pyrostep-2.10.19/pyrostep/_idle.py
@@ -0,0 +1,12 @@
+from pyrogram import idle
+
+
+async def safe_idle() -> None:
+    import warnings
+
+    warnings.warn(
+        "This function is deprecated, use pyrogram.idle instead of this.",
+        category=DeprecationWarning,
+    )
+
+    await idle()
