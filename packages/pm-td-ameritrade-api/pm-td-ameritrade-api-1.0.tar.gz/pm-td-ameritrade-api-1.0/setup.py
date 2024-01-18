4fe96e19b13413e071e03cabd806ddc06ce60bd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:05:10 +0000

    Add package pm-td-ameritrade-api/1.0/pm-td-ameritrade-api-1.0.tar.gz

---
+++ packages/pm-td-ameritrade-api/pm-td-ameritrade-api-1.0.tar.gz/pm-td-ameritrade-api-1.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_namespace_packages, setup
+
+# load the README file.
+with open(file="README.md", mode="r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pm-td-ameritrade-api",
+    author="primalmachinai",
+    author_email="primalmachinai@gmail.com",
+    version="1.0",
+    description="A python client library for the TD Ameritrade API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/primalmachinai/pm-td-ameritrade-api",
+    install_requires=[
+        "aiofiles>=23.2.1",
+        "pydantic>=2.4.2",
+        "rich>=13.6.0",
+        "websockets>=11.0.3",
+        "colorlog>=6.7.0",
+        "selenium>=4.14.0",
+        "requests>=2.31.0",
+        "webdriver_manager>=4.0.1",
+    ],
+    keywords="finance, td ameritrade, api",
+    packages=find_namespace_packages(
+        include=["pm_td_ameritrade_api.*"], exclude=["config*"]
+    ),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Financial and Insurance Industry",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.10",
+)
