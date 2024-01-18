071491a1bb3441c1ceab8e509320ac81e21ed134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 06:04:08 +0000

    Add package jupyterlab-pachyderm/2.9.0a2/jupyterlab_pachyderm-2.9.0a2-py3-none-any.whl

---
+++ packages/jupyterlab-pachyderm/jupyterlab_pachyderm-2.9.0a2-py3-none-any.whl/jupyterlab_pachyderm/env.py
@@ -0,0 +1,14 @@
+import os
+from pathlib import Path
+from distutils.util import strtobool
+
+PACH_CONFIG = os.environ.get("PACH_CONFIG", Path.home() / ".pachyderm/config.json")
+PFS_MOUNT_DIR = os.environ.get("PFS_MOUNT_DIR", "/pfs")
+
+PACHYDERM_EXT_DEBUG = strtobool(os.environ.get("PACHYDERM_EXT_DEBUG", "False").lower())
+if PACHYDERM_EXT_DEBUG:
+    from jupyterlab_pachyderm.log import get_logger
+
+    logger = get_logger()
+    logger.setLevel("DEBUG")
+    logger.debug("DEBUG mode activated for pachyderm extension")
