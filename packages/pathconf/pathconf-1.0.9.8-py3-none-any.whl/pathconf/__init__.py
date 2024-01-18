f7c36c06525aa349accd3254758ede5445f336c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 17:04:39 +0000

    Add package pathconf/1.0.9.8/pathconf-1.0.9.8-py3-none-any.whl

---
+++ packages/pathconf/pathconf-1.0.9.8-py3-none-any.whl/pathconf/__init__.py
@@ -0,0 +1,5 @@
+from .pathconf import find_path, load_json_config
+from .pathconf import reset, remove, list_paths, get_paths, index
+
+__all__ = ['find_path', 'load_json_config', 'reset',
+           'remove', 'list_paths', 'get_paths', 'index']
