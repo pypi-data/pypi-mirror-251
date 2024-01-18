d9ee5f8f6e442fc846cd7c3907bc9b4d7597034a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:25 +0000

    Add package banglanlptoolkit/1.1.4.dev2/banglanlptoolkit-1.1.4.dev2.tar.gz

---
+++ packages/banglanlptoolkit/banglanlptoolkit-1.1.4.dev2.tar.gz/banglanlptoolkit-1.1.4.dev2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.1.4.dev2' 
+DESCRIPTION = 'Toolkits for text processing and augmentation for Bangla NLP'
+
+setup(
+        name="banglanlptoolkit", 
+        version=VERSION,
+        author="A F M Mahfuzul Kabir",
+        author_email="<afmmahfuzulkabir@gmail.com>",
+        description=DESCRIPTION,
+        long_description=open('README.md').read(),
+        long_description_content_type='text/markdown',
+        packages=find_packages(),
+        install_requires=['requirements.txt'
+            # 'bnunicodenormalizer',
+            # 'numpy',
+            # 'pandas',
+            # 'sentencepiece',
+            # 'torch',
+            # 'tqdm',
+            # 'transformers',
+            # 'langdetect',
+            # 'git+github.com/csebuetnlp/normalizer.git',
+        ],
+        classifiers = [
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+        keywords=['Bangla NLP','Bangla','NLP','NLP Toolkit','Bengali NLP','Bengali Augmentation','Bengali Text Normalization','Bangla NLP Toolkit','Bangla Punctuation'],
+        # dependency_links=['git+github.com/csebuetnlp/normalizer.git'],
+        url='https://github.com/Kabir5296/banglanlptoolkit',
+        project_urls={'Repository': 'https://github.com/Kabir5296/banglanlptoolkit'},
+)
