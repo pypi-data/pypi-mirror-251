8729fde68a8a2fceca1acb285422f61343f7ed58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:04:35 +0000

    Add package floggit/0.0.3/floggit-0.0.3.tar.gz

---
+++ packages/floggit/floggit-0.0.3.tar.gz/floggit-0.0.3/setup.py
@@ -0,0 +1,61 @@
+'''
+# python function in/out logger
+
+## Install
+
+Requires Python 3
+
+### Using pip
+
+```
+pip3 install loggit -U
+```
+
+### Using pipenv
+
+```
+pipenv install loggit
+```
+
+## Usage
+
+### Simple monitoring
+
+```python
+from loggit import log
+
+@flog
+def myfunc(*args, **kwargs):
+    ...
+    return result
+```
+'''
+
+from setuptools import setup, find_packages
+
+import floggit
+
+setup(
+    name="floggit",
+    version=floggit.__version__,
+    author="dcyd, inc.",
+    author_email="info@dcyd.io",
+    description="log function inputs and outputs at runtime",
+    long_description=__doc__,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dcyd-inc/floggit",
+    packages=find_packages(
+        exclude=["*.tests", "*.tests.*", "tests.*", "tests"]
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'google-cloud-logging==2.2.0',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+    python_requires='>=3.9'
+)
