8a5373f053e7ac687e7cb05410adf7c942791103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:04:46 +0000

    Add package pdoc/14.4.0/pdoc-14.4.0.tar.gz

---
+++ packages/pdoc/pdoc-14.4.0.tar.gz/pdoc-14.4.0/test/test_docstrings.py
@@ -0,0 +1,31 @@
+from hypothesis import given
+from hypothesis.strategies import text
+import pytest
+
+from pdoc import docstrings
+
+# The important tests are in test_snapshot.py (and, by extension, testdata/)
+# only some fuzzing here.
+
+
+@given(text())
+def test_google(s):
+    ret = docstrings.google(s)
+    assert not s or ret
+
+
+@given(text())
+def test_numpy(s):
+    ret = docstrings.numpy(s)
+    assert not s or ret
+
+
+@given(text())
+def test_rst(s):
+    ret = docstrings.rst(s, None)
+    assert not s or ret
+
+
+def test_rst_include_nonexistent():
+    with pytest.warns(UserWarning, match="Cannot include 'nonexistent.txt'"):
+        docstrings.rst(".. include:: nonexistent.txt", None)
