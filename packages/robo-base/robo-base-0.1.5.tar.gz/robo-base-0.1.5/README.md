f4e6cfbed05494c7397435bc2655af2c855429de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 06:03:54 +0000

    Add package robo-base/0.1.5/robo-base-0.1.5.tar.gz

---
+++ packages/robo-base/robo-base-0.1.5.tar.gz/robo-base-0.1.5/README.md
@@ -0,0 +1,11 @@
+**robo-base**
+
+
+Base objects required for mobile robotics modules
+
+
+Documentation at https://robo-base.readthedocs.io/en/latest/
+
+
+
+
---
+++ packages/robo-base/robo-base-0.1.5.tar.gz/robo-base-0.1.5/setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from setuptools import Command, Extension, setup
+from setuptools.command.build_ext import build_ext
+
+
+DISTNAME = "robo-base"
+#DESCRIPTION = "primary base object"
+
+if __name__ == "__main__":
+    setup()
+ 
+#mk  works
+
+import robobase
+
+#import robo_base as robobase
+
+#
+
+
