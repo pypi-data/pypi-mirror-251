bddfa6bcb76daa5565e454fe4caf7a7b31e5580f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 14:04:42 +0000

    Add package squarecloud-api/3.3.0/squarecloud_api-3.3.0.tar.gz

---
+++ packages/squarecloud-api/squarecloud_api-3.3.0.tar.gz/squarecloud_api-3.3.0/squarecloud/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from .client import Client, create_config_file
+from .data import (
+    AppData,
+    BackupData,
+    FileInfo,
+    LogsData,
+    PlanData,
+    StatisticsData,
+    StatusData,
+    UploadData,
+    UserData,
+)
+from .file import File
+from .http.endpoints import Endpoint
+
+__version__ = '3.3.0'
