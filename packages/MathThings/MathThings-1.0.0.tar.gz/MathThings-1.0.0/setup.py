a1f23e345f693cbc20ccbd7a61f0207082d40cc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 03:04:20 +0000

    Add package MathThings/1.0.0/MathThings-1.0.0.tar.gz

---
+++ packages/MathThings/MathThings-1.0.0.tar.gz/MathThings-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MathThings',
+    version='1.0.0',
+    author='Segev The kings',
+    author_email='lalalala@gmail.com',
+    description='Testttttt',
+    packages=find_packages(),
+    python_requires='>=3.6',
+)
+
