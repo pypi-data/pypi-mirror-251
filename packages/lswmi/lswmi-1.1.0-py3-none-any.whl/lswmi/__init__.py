ba07f1dd722f06cdd59441c54b32b85de58a4632
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 03:04:09 +0000

    Add package lswmi/1.1.0/lswmi-1.1.0-py3-none-any.whl

---
+++ packages/lswmi/lswmi-1.1.0-py3-none-any.whl/lswmi/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Utility to retrieve information about WMI devices on Linux """
+
+__version__ = "1.1.0"
+__author__ = "Armin Wolf"
+__email__ = "W_Armin@gmx.de"
+__all__ = (
+    "device",
+    "provider",
+    "main"
+)
