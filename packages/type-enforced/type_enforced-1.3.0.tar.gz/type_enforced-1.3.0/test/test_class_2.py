6030acdfbdfa96606f54e23cbee17cd2004e47b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 05:03:43 +0000

    Add package type-enforced/1.3.0/type_enforced-1.3.0.tar.gz

---
+++ packages/type-enforced/type_enforced-1.3.0.tar.gz/type_enforced-1.3.0/test/test_class_2.py
@@ -0,0 +1,25 @@
+import type_enforced
+
+
+@type_enforced.Enforcer
+class my_class:
+    def __init__(self):
+        self.a = 10
+
+    def my_fn(self, b: int):
+        print(self.a, b)
+
+
+mc = my_class()
+
+success = False
+try:
+    mc.my_fn("a")
+except Exception as e:
+    if "Type mismatch" in str(e):
+        success = True
+
+if success:
+    print("test_class_2.py passed")
+else:
+    print("test_class_2.py failed")
