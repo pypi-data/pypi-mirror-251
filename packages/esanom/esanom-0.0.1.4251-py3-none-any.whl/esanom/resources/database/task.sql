b21f8ba24da7f49720f08a5119a84144de868376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:49 +0000

    Add package esanom/0.0.1.4251/esanom-0.0.1.4251-py3-none-any.whl

---
+++ packages/esanom/esanom-0.0.1.4251-py3-none-any.whl/esanom/resources/database/task.sql
@@ -0,0 +1,59 @@
+
+/*
+#################################################################################
+#
+# {___     {__          {__       {__
+# {_ {__   {__          {_ {__   {___
+# {__ {__  {__   {__    {__ {__ { {__
+# {__  {__ {__ {__  {__ {__  {__  {__
+# {__   {_ {__{__    {__{__   {_  {__
+# {__    {_ __ {__  {__ {__       {__
+# {__      {__   {__    {__       {__
+#
+# (C) Copyright European Space Agency, 2024
+# 
+# This file is subject to the terms and conditions defined in file 'LICENCE.txt', 
+# which is part of this source code package. No part of the package, including 
+# this file, may be copied, modified, propagated, or distributed except 
+# according to the terms contained in the file ‘LICENCE.txt’.“ 
+#
+#################################################################################
+*/
+
+CREATE TABLE `task` (
+    
+    `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+    
+    `api_id` INT UNSIGNED DEFAULT NULL,
+    `rolemodel_id` INT UNSIGNED DEFAULT NULL,
+
+    `status` varchar(255) DEFAULT "CREATED" ,
+
+    `project` varchar(255) DEFAULT "",
+
+    `inputs_fp` varchar(255) DEFAULT "",
+    `outputs_fp` varchar(255) DEFAULT "",
+
+    `inputs_size` INT UNSIGNED DEFAULT 0,
+    `outputs_size` INT UNSIGNED DEFAULT 0,
+
+    `archived` BOOLEAN DEFAULT 0,
+    `heartbeats` INT UNSIGNED DEFAULT 0,
+
+    `scheduled_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `running_at` DATETIME DEFAULT NULL ,
+    `ended_at` DATETIME DEFAULT NULL ,
+    `heartbeat_at` DATETIME DEFAULT NULL ,
+    `retrieved_at` DATETIME DEFAULT NULL ,
+
+    `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` DATETIME DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+
+    PRIMARY KEY (`id`),
+    KEY `api_id` (`api_id`),
+    KEY `rolemodel_id` (`rolemodel_id`),
+    KEY `status` (`status`) 
+
+) ENGINE=InnoDB,AUTO_INCREMENT=101;
+
+
