1a9fedfbaab4d51ae84c67101d8c89a7a92c739e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-18 16:06:30 +0000

    Add package tfrq/2.0.99/tfrq-2.0.99.tar.gz

---
+++ packages/tfrq/tfrq-2.0.99.tar.gz/tfrq-2.0.99/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst', "r", encoding="utf8") as f:
+        return f.read()
+
+
+setup(
+    name='tfrq',
+    version='2.0.99',
+    description='A library to parallelize the execution of a function in Python',
+    long_description=readme(),
+    long_description_content_type='text/x-rst',
+    author='Foad Abo Dahood',
+    author_email='Foad.ad5491@gmail.com',
+    license='MIT',
+    py_modules=['tfrq'],
+    install_requires=[
+        'tqdm'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
