626fdd36b139219773d29c00ecea5bc993a85f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:13 +0000

    Add package rsome/1.2.6/rsome-1.2.6-py3-none-any.whl

---
+++ packages/rsome/rsome-1.2.6-py3-none-any.whl/rsome/__init__.py
@@ -0,0 +1,6 @@
+from .math import norm, square, sumsqr, quad, rsocone, fnorm
+from .math import expcone, exp, log, pexp, plog, entropy, softplus, kldiv
+from .math import maxof, minof
+from .math import trace, diag, tril, triu
+from .subroutines import E
+from .lp import concat, rstack, cstack, vec
