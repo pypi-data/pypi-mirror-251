c27d18bbcca64f14face77fff2b7fd25ef533303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:27 +0000

    Add package jaxsim/0.1.dev354/jaxsim-0.1.dev354-py3-none-any.whl

---
+++ packages/jaxsim/jaxsim-0.1.dev354-py3-none-any.whl/jaxsim/math/inertia.py
@@ -0,0 +1,35 @@
+from typing import Tuple
+
+import jax.numpy as jnp
+
+import jaxsim.typing as jtp
+
+from .skew import Skew
+
+
+class Inertia:
+    @staticmethod
+    def to_sixd(mass: jtp.Float, com: jtp.Vector, I: jtp.Matrix) -> jtp.Matrix:
+        if I.shape != (3, 3):
+            raise ValueError(I, I.shape)
+
+        c = Skew.wedge(vector=com)
+
+        M = jnp.vstack(
+            [
+                jnp.block([mass * jnp.eye(3), mass * c.T]),
+                jnp.block([mass * c, I + mass * c @ c.T]),
+            ]
+        )
+
+        return M
+
+    @staticmethod
+    def to_params(M: jtp.Matrix) -> Tuple[jtp.Float, jtp.Vector, jtp.Matrix]:
+        m = jnp.diag(M[0:3, 0:3]).sum() / 3
+
+        mC = M[3:6, 0:3]
+        c = Skew.vee(mC) / m
+        I = M[3:6, 3:6] - (mC @ mC.T / m)
+
+        return m, c, I
