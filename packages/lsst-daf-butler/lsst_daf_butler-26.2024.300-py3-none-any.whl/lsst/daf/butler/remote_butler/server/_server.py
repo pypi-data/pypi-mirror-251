62bd2f59335e5fecb84629a634c2b40bfc5eb547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:05:02 +0000

    Add package lsst-daf-butler/26.2024.300/lsst_daf_butler-26.2024.300-py3-none-any.whl

---
+++ packages/lsst-daf-butler/lsst_daf_butler-26.2024.300-py3-none-any.whl/lsst/daf/butler/remote_butler/server/_server.py
@@ -0,0 +1,84 @@
+# This file is part of daf_butler.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (http://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This software is dual licensed under the GNU General Public License and also
+# under a 3-clause BSD license. Recipients may choose which of these licenses
+# to use; please see the files gpl-3.0.txt and/or bsd_license.txt,
+# respectively.  If you choose the GPL option then the following text applies
+# (but note that there is still no warranty even if you opt for BSD instead):
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+__all__ = ("app",)
+
+import logging
+
+from fastapi import FastAPI, Request
+from fastapi.middleware.gzip import GZipMiddleware
+from fastapi.responses import JSONResponse
+from lsst.daf.butler import MissingDatasetTypeError
+from safir.metadata import Metadata, get_metadata
+
+from .handlers._external import external_router
+
+_DEFAULT_API_PATH = "/api/butler"
+
+log = logging.getLogger(__name__)
+
+app = FastAPI()
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+
+# A single instance of the server can serve data from multiple Butler
+# repositories.  This 'repository' path placeholder is consumed by
+# factory_dependency().
+repository_placeholder = "{repository}"
+app.include_router(external_router, prefix=f"{_DEFAULT_API_PATH}/repo/{repository_placeholder}")
+
+
+@app.exception_handler(MissingDatasetTypeError)
+def missing_dataset_type_exception_handler(request: Request, exc: MissingDatasetTypeError) -> JSONResponse:
+    # Remove the double quotes around the string form. These confuse
+    # the JSON serialization when single quotes are in the message.
+    message = str(exc).strip('"')
+    return JSONResponse(
+        status_code=404,
+        content={"detail": message, "exception": "MissingDatasetTypeError"},
+    )
+
+
+@app.get(
+    "/",
+    description=(
+        "Return metadata about the running application. Can also be used as"
+        " a health check. This route is not exposed outside the cluster and"
+        " therefore cannot be used by external clients."
+    ),
+    include_in_schema=False,
+    response_model=Metadata,
+    response_model_exclude_none=True,
+    summary="Application metadata",
+)
+async def get_index() -> Metadata:
+    """GET ``/`` (the app's internal root).
+
+    By convention, this endpoint returns only the application's metadata.
+    """
+    return get_metadata(package_name="lsst.daf.butler", application_name="butler")
