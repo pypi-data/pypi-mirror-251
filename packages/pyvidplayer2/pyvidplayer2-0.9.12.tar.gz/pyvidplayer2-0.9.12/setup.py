4e99fb5d1a9ca5177b0a322f9e4da2c904f5b987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:05:40 +0000

    Add package pyvidplayer2/0.9.12/pyvidplayer2-0.9.12.tar.gz

---
+++ packages/pyvidplayer2/pyvidplayer2-0.9.12.tar.gz/pyvidplayer2-0.9.12/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from pyvidplayer2 import _VERSION
+
+
+with open("README.md", 'r') as f:
+    long_desc = f.read()
+
+
+setup(
+    name="pyvidplayer2",
+    version=_VERSION,
+    description="Video playback in Python",
+    long_description=long_desc,
+    long_description_content_type = "text/markdown",
+    author="Anray Liu",
+    author_email="anrayliu@gmail.com",
+    license="MIT",
+    packages=["pyvidplayer2"],
+    install_requires=["numpy<1.25,>=1.21",
+                    "opencv_python",
+                    "pygame",
+                    "pysubs2",
+                    "PyAudio"],
+    url="https://github.com/ree1261/pyvidplayer2",
+    platforms=["windows"],
+    keywords=["pygame", "video", "playback"],
+    python_requires=">=3.10"
+)
