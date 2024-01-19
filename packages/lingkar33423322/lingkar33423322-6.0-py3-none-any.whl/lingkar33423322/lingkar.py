34cc064b1d2aed0a658fbe9ae6a644699c4325a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:06:27 +0000

    Add package lingkar33423322/6.0/lingkar33423322-6.0-py3-none-any.whl

---
+++ packages/lingkar33423322/lingkar33423322-6.0-py3-none-any.whl/lingkar33423322/lingkar.py
@@ -0,0 +1,10 @@
+import math
+
+class Bunder:
+    def __init__(self, jari_jari):
+        self.jari_jari = jari_jari
+
+    def hitung_luas(self):
+        """Fungsi untuk menghitung luas lingkaran."""
+        luas = math.pi * self.jari_jari**2
+        return luas
