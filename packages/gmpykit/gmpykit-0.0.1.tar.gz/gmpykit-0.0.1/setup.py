e8fd21e552403302a74966441d64877ea7e8d47e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:29 +0000

    Add package gmpykit/0.0.1/gmpykit-0.0.1.tar.gz

---
+++ packages/gmpykit/gmpykit-0.0.1.tar.gz/gmpykit-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gmpykit",
+    version="0.0.1",
+    author='Gaétan Muck',
+    author_email='gaetan.muck@gmail.com',
+    description='Package with various python tools',
+    long_description='Package with various python tools',
+    packages=find_packages(),
+    install_require=[],
+    keywords=['python', 'toolkit', 'utilities', 'utils', 'tools']
+)
