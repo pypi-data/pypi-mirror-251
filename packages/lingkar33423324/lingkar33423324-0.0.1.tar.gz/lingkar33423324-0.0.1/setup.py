a6c30941337659f809644b54dd52cbc8c618103c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:06:30 +0000

    Add package lingkar33423324/0.0.1/lingkar33423324-0.0.1.tar.gz

---
+++ packages/lingkar33423324/lingkar33423324-0.0.1.tar.gz/lingkar33423324-0.0.1/setup.py
@@ -0,0 +1,11 @@
+# Proyek/setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='lingkar33423324',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'math',
+    ],
+)
