07684dff1a3045e7e24cd31ceedef96cc6c5e1bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:21 +0000

    Add package streamdal/0.0.44/streamdal-0.0.44.tar.gz

---
+++ packages/streamdal/streamdal-0.0.44.tar.gz/streamdal-0.0.44/setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name="streamdal",
+    version='0.0.44',
+    description="Python client SDK for Streamdal's open source observability server",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/streamdal/python-sdk",
+    author="Streamdal.com",
+    author_email="engineering@streamdal.com",
+    license="MIT",
+    packages=[
+        "streamdal",
+        "streamdal.common",
+        "streamdal.metrics",
+        "streamdal.validation",
+        "streamdal.tail",
+        "streamdal.hostfunc",
+    ],
+    install_requires=[""],
+    python_requires=">=3.8",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
