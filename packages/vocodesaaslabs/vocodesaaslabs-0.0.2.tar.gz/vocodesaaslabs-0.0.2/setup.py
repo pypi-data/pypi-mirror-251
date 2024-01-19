3af5a61def37ae8738da3ab0e34bb015f7f65a1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:37 +0000

    Add package vocodesaaslabs/0.0.2/vocodesaaslabs-0.0.2.tar.gz

---
+++ packages/vocodesaaslabs/vocodesaaslabs-0.0.2.tar.gz/vocodesaaslabs-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: MacOS',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='vocodesaaslabs',
+  version='0.0.2',
+  description='SaaS Labs version of Vocode',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='Ajeya Krishna',
+  author_email='ajeya@saaslabs.co',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='voice', 
+  packages=find_packages(),
+  install_requires=[''] 
+)
