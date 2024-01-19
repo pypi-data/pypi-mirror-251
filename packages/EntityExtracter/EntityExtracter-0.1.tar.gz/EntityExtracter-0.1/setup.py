96ce6d89d9ab91f53c344eb28df9f70a19b3a34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:36 +0000

    Add package EntityExtracter/0.1/EntityExtracter-0.1.tar.gz

---
+++ packages/EntityExtracter/EntityExtracter-0.1.tar.gz/EntityExtracter-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='EntityExtracter',
+    author='Ankit Mor',
+    version='0.1',
+    packages=find_packages(),
+    description='Extract specific entites from a text. Give text and get a json formatted output data.',
+    long_description=open('Readme.md').read(), 
+    long_description_content_type='text/markdown',
+    install_requires=['pandas>=2.0.3', 'numpy>=1.24.3', 'nltk>=3.8.1', 'requests', 'BeautifulSoup4','spacy>=3.7.2'],
+    
+)
