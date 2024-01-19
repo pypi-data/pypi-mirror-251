b1e757efd61392f25a6aca7d1a2875fea97d012f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 05:05:03 +0000

    Add package fprlib/1.2.6/fprlib-1.2.6.tar.gz

---
+++ packages/fprlib/fprlib-1.2.6.tar.gz/fprlib-1.2.6/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+from pathlib import Path
+import codecs
+types_of_encoding = ["utf8", "cp1252"]
+
+setup(
+    name="fprlib",
+    version="1.2.6",
+    description="Knjižnica za FPR",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description="https://github.com/DrKvass/fprlib",
+    long_description_content_type="text/markdown",
+    url="https://github.com/DrKvass/fprlib",
+    author="Dr.Kvass",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+    ],
+    install_requires=["numpy","scipy","matplotlib","statistics","uncertainties"],
+    python_required=">=3.11",
+)
