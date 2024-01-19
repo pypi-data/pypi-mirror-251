65571c80ed341e8ca2ca835c76aad2498b9ae2e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:45 +0000

    Add package sphinx-celery/2.1.2/sphinx_celery-2.1.2.tar.gz

---
+++ packages/sphinx-celery/sphinx_celery-2.1.2.tar.gz/sphinx_celery-2.1.2/sphinx_celery/setting_crossref.py
@@ -0,0 +1,13 @@
+from .utils import bytes_if_py2
+
+
+def setup(app):
+    app.add_crossref_type(
+        directivename=bytes_if_py2('setting'),
+        rolename=bytes_if_py2('setting'),
+        indextemplate=bytes_if_py2('pair: %s; setting'),
+    )
+
+    return {
+        'parallel_read_safe': True
+    }
