e2f86447ea4062a327c0a536202181534206c85e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:07:03 +0000

    Add package pygpt-net/2.0.110/pygpt_net-2.0.110-py3-none-any.whl

---
+++ packages/pygpt-net/pygpt_net-2.0.110-py3-none-any.whl/pygpt_net/core/camera.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.01.12 10:00:00                  #
+# ================================================== #
+
+import os
+import cv2
+import time
+
+from PySide6.QtCore import QObject, Signal, QRunnable, Slot
+
+
+class Camera:
+    def __init__(self, window=None):
+        """
+        Camera core
+
+        :param window: Window instance
+        """
+        self.window = window
+        self.capture = None
+        self.current = None
+
+    def install(self):
+        """Install provider data"""
+        img_dir = self.window.core.config.get_user_dir('capture')
+        if not os.path.exists(img_dir):
+            os.mkdir(img_dir)
+
+
+class CaptureSignals(QObject):
+    finished = Signal()
+    destroyed = Signal()
+    started = Signal()
+    stopped = Signal()
+    capture = Signal(object)
+    error = Signal(object)
+
+
+class CaptureWorker(QRunnable):
+    def __init__(self, *args, **kwargs):
+        super(CaptureWorker, self).__init__()
+        self.signals = CaptureSignals()
+        self.args = args
+        self.kwargs = kwargs
+        self.window = None
+        self.initialized = False
+        self.capture = None
+        self.frame = None
+
+    def setup_camera(self):
+        """Initialize camera"""
+        try:
+            # get params from global config
+            self.capture = cv2.VideoCapture(self.window.core.config.get('vision.capture.idx'))
+            self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.window.core.config.get('vision.capture.width'))
+            self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.window.core.config.get('vision.capture.height'))
+        except Exception as e:
+            # TODO: handle snap info
+            self.signals.error.emit(e)
+            print("Camera thread setup exception", e)
+            self.signals.finished.emit(e)
+
+    @Slot()
+    def run(self):
+        """Frame capture loop"""
+        target_fps = 30
+        fps_interval = 1.0 / target_fps
+        try:
+            if not self.initialized:
+                self.setup_camera()
+                self.signals.started.emit()
+                self.initialized = True
+            last_frame_time = time.time()
+            while True:
+                if self.window.is_closing \
+                        or self.capture is None \
+                        or not self.capture.isOpened() \
+                        or self.window.controller.camera.stop:
+                    self.release()  # release camera
+                    self.signals.stopped.emit()
+                    break
+                _, frame = self.capture.read()
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                now = time.time()
+                if now - last_frame_time >= fps_interval:
+                    self.signals.capture.emit(frame)
+                    last_frame_time = now
+        except Exception as e:
+            if self.signals is not None:
+                self.signals.error.emit(e)
+            print("Camera thread capture exception", e)
+
+        # release camera
+        self.release()
+        if self.signals is not None:
+            self.signals.finished.emit()
+
+    def release(self):
+        """Release camera"""
+        if self.capture is not None and self.capture.isOpened():
+            self.capture.release()
+            self.capture = None
+            self.frame = None
+            self.initialized = False
---
+++ packages/pygpt-net/pygpt_net-2.0.110-py3-none-any.whl/pygpt_net/ui/tray.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ================================================== #
+# This file is a part of PYGPT package               #
+# Website: https://pygpt.net                         #
+# GitHub:  https://github.com/szczyglis-dev/py-gpt   #
+# MIT License                                        #
+# Created By  : Marcin Szczygliński                  #
+# Updated Date: 2024.01.18 10:00:00                  #
+# ================================================== #
+
+from PySide6.QtWidgets import QSystemTrayIcon, QMenu
+
+from pygpt_net.utils import trans
+
+
+class Tray:
+    def __init__(self, window=None):
+        """
+        Tray icon setup
+
+        :param window: Window instance
+        """
+        self.window = window
+
+    def setup(self, app=None):
+        """
+        Setup tray menu
+
+        :param app: QApplication instance
+        """
+        tray = QSystemTrayIcon(self.window.ui.get_app_icon(), app)
+        tray.setToolTip("PyGPT v{}".format(self.window.meta['version']))
+        menu = QMenu()
+
+        tray_menu = {}
+        tray_menu['new'] = menu.addAction(trans("menu.file.new"))
+        tray_menu['new'].triggered.connect(self.new_ctx)
+        tray_menu['update'] = menu.addAction(trans("menu.info.updates"))
+        tray_menu['update'].triggered.connect(self.window.controller.launcher.check_updates)
+
+        # open notepad
+        if self.window.controller.notepad.get_num_notepads() > 0:
+            tray_menu['open_notepad'] = menu.addAction(trans("menu.tray.notepad"))
+            tray_menu['open_notepad'].triggered.connect(self.open_notepad)
+
+        # ask with screenshot
+        tray_menu['screenshot'] = menu.addAction(trans("menu.tray.screenshot"))
+        tray_menu['screenshot'].triggered.connect(self.make_screenshot)
+
+        tray_menu['exit'] = menu.addAction(trans("menu.file.exit"))
+        tray_menu['exit'].triggered.connect(app.quit)
+        tray.setContextMenu(menu)
+        tray.show()
+
+    def new_ctx(self):
+        """Create new context"""
+        self.window.controller.ctx.new()
+        self.window.activateWindow()
+
+    def open_notepad(self):
+        """Open notepad"""
+        self.window.controller.notepad.open()
+
+    def make_screenshot(self):
+        """Make screenshot"""
+        self.window.controller.drawing.make_screenshot()
+        self.window.activateWindow()
+        self.window.controller.chat.common.focus_input()
