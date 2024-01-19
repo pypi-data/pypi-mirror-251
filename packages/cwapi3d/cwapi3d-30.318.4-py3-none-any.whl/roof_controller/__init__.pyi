39ee7931fe50bad458e5c0ff855a468193b1e75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:58 +0000

    Add package cwapi3d/30.318.4/cwapi3d-30.318.4-py3-none-any.whl

---
+++ packages/cwapi3d/cwapi3d-30.318.4-py3-none-any.whl/roof_controller/__init__.pyi
@@ -0,0 +1,50 @@
+from typing import List
+
+
+def get_profile_length(element_id: int) -> float:
+    """get profile length
+    Args:
+        element_id ( int): element_id
+
+    Returns:
+        floatprofile length
+    """
+
+
+def get_edge_length(element_id: int, edge_type: str) -> float:
+    """get edge length
+    Args:
+        element_id ( int): element_id
+        edge_type ( str): edge_type
+
+    Returns:
+        floatedge length
+    """
+
+
+def get_all_caddy_element_ids() -> List[int]:
+    """get all caddy element ids
+    Args:
+
+    Returns:
+        List[int]element ID list
+    """
+
+
+def get_last_error(a0: int) -> str:
+    """get last error
+    Args:
+        a0 ( int): a0
+
+    Returns:
+        str
+    """
+
+
+def clear_errors() -> None:
+    """clear errors
+    Args:
+
+    Returns:
+        None
+    """
