9225a24d6ffb25645952267ab4f2c193da945fbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:04:37 +0000

    Add package nuclia/1.2.4/nuclia-1.2.4-py3-none-any.whl

---
+++ packages/nuclia/nuclia-1.2.4-py3-none-any.whl/nuclia/sdk/process.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+from nucliadb_protos.writer_pb2 import BrokerMessage
+
+from nuclia.data import get_auth
+from nuclia.decorators import nua
+from nuclia.lib.nua import NuaClient
+from nuclia.lib.nua_responses import LearningConfig, ProcessingStatus
+from nuclia.sdk.auth import NucliaAuth
+
+
+class NucliaProcessing:
+    @property
+    def _auth(self) -> NucliaAuth:
+        auth = get_auth()
+        return auth
+
+    @nua
+    def process_file(
+        self, path: str, config: Optional[LearningConfig] = None, **kwargs
+    ) -> Optional[BrokerMessage]:
+        nc: NuaClient = kwargs["nc"]
+        response = nc.process_file(path, config)
+        payload = nc.wait_for_processing(response)
+        return payload
+
+    @nua
+    def status(self, **kwargs) -> ProcessingStatus:
+        nc: NuaClient = kwargs["nc"]
+        return nc.processing_status()
