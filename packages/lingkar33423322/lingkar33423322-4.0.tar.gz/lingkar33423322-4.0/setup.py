96a98cd5b53a35324f12fbd098cc7bbf22fda60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:06:11 +0000

    Add package lingkar33423322/4.0/lingkar33423322-4.0.tar.gz

---
+++ packages/lingkar33423322/lingkar33423322-4.0.tar.gz/lingkar33423322-4.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+VERSION = '4.0'
+DESCRIPTION = 'Menghitung luas lingkaran'
+
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / 'README.md').read_text()
+
+# Setting up
+setup(
+    name="lingkar33423322",
+    version=VERSION,
+    author="SabillillahX",
+    author_email="<sabillillahxtkj@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url='https://github.com/SabillillahX/lingkar.git',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[],
+    keywords=['luas', 'lingkaran'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+    ],
+)
