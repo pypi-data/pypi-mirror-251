e21073b856794de44eadcfdfe0607fc30ec6353f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 06:05:39 +0000

    Add package pysp2/1.4.0/pysp2-1.4.0.tar.gz

---
+++ packages/pysp2/pysp2-1.4.0.tar.gz/pysp2-1.4.0/pysp2/__init__.py
@@ -0,0 +1,12 @@
+"""
+==========================
+Python SP2 data processing
+==========================
+"""
+
+from . import io
+from . import util
+from . import vis
+from . import testing
+
+__version__ = "1.4.0"
