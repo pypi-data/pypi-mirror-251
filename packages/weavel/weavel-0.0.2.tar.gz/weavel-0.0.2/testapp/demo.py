94390139981d98f479ffe29eb445520df1e9ff0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:05:42 +0000

    Add package weavel/0.0.2/weavel-0.0.2.tar.gz

---
+++ packages/weavel/weavel-0.0.2.tar.gz/weavel-0.0.2/testapp/demo.py
@@ -0,0 +1,42 @@
+from weavel import create_client
+
+client = create_client()
+
+user_uuid = client.create_user_uuid()
+
+trace_id = client.start_trace(user_uuid)
+
+second_trace_id = client.start_trace(user_uuid)
+
+from openai import OpenAI
+openai_client = OpenAI()
+
+user_message = "what can you do for me?"
+client.log.user_message(trace_id, user_message, unit_name="testapp")
+
+import asyncio
+
+print("sleeping...")
+asyncio.sleep(5)
+print("awake!")
+
+client.log.user_message(second_trace_id, "hello!", unit_name="second_testapp")
+client.log.assistant_message(second_trace_id, "I can help you with a variety of tasks.", unit_name="second_testapp")
+
+# res = openai_client.chat.completions.create(
+#     model="gpt-3.5-turbo",
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant"},
+#         {"role": "user", "content": user_message}
+#     ]
+# )
+
+res = "I can assist you with a variety of tasks. I can help answer questions, provide information, give suggestions, assist with research, set reminders, manage your schedule, make reservations, provide translations, and much more. Just let me know what you need help with!"
+
+# print(res.choices[0].message.content)
+
+client.log.assistant_message(trace_id, res, unit_name="test_assistant")
+
+client.add_metadata_to_trace(trace_id, {"user_name": "John Doe"})
+
+client.close()
