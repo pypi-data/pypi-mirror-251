79c4fb901d39907bb4cdf0d4e622fe1eed211079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 05:05:42 +0000

    Add package openactive/1.0.1/openactive-1.0.1-py3-none-any.whl

---
+++ packages/openactive/openactive-1.0.1-py3-none-any.whl/openactive/__init__.py
@@ -0,0 +1,14 @@
+__author__ = 'Darren Temple'
+__version__ = '1.0.1'
+__copyright__ = 'Copyright (c) 2024 OpenActive'
+__license__ = 'MIT'
+
+from .openactive import (
+    get_catalogue_urls,
+    get_dataset_urls,
+    get_feeds,
+    get_opportunities,
+    get_item_kinds,
+    get_item_data_types,
+    get_partner_url,
+)
