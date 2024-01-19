461595c8e2a571445a54894a486d2cd66e0e576e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 06:05:12 +0000

    Add package djangoldp-resource/3.1.1/djangoldp_resource-3.1.1.tar.gz

---
+++ packages/djangoldp-resource/djangoldp_resource-3.1.1.tar.gz/djangoldp_resource-3.1.1/djangoldp_resource/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from guardian.admin import GuardedModelAdmin
+from djangoldp.admin import DjangoLDPAdmin
+from .models import Resource
+
+class ResourceAdmin(DjangoLDPAdmin):
+    list_display = ('name', 'shortdesc', 'circle', 'type', 'creationDate', 'get_user_name')
+    exclude = ('urlid', 'slug', 'is_backlink', 'allow_create_backlink')
+    search_fields = ['urlid', 'name', 'shortdesc', 'longdesc']
+    ordering = ['id', 'name', 'type']
+
+    def get_user_name(self, obj):
+            return obj.user.name()
+
+    get_user_name.admin_order_field  = 'user'  #Allows column order sorting
+    get_user_name.short_description = 'Author Name'  #Renames column head
+
+admin.site.register(Resource, ResourceAdmin)
