472c964cf18a4f031701e768e9e7b9c1b5e621d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:05:02 +0000

    Add package kruksik-hello/0.0.3/kruksik_hello-0.0.3.tar.gz

---
+++ packages/kruksik-hello/kruksik_hello-0.0.3.tar.gz/kruksik_hello-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def load_req(r_file: str) -> list[str]:
+    with open(os.path.join(os.getcwd(), r_file)) as f:
+        return [
+            r for r in (line.split("#", 1)[0].strip() for line in f.readlines()) if r
+        ]
+
+
+with open("readme.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="kruksik_hello",
+    version="0.0.3",
+    packages=find_packages(),
+    install_requires=load_req("requirements.txt"),
+    entry_points={
+        "console_scripts": [
+            "kruksik-hello = kruksik_hello:hello",
+        ]
+    },
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
