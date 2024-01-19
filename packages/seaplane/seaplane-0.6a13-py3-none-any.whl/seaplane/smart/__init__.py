24e8169bd7ec5c84e3bffdb34302496cf189008e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:16 +0000

    Add package seaplane/0.6a13/seaplane-0.6a13-py3-none-any.whl

---
+++ packages/seaplane/seaplane-0.6a13-py3-none-any.whl/seaplane/smart/__init__.py
@@ -0,0 +1,10 @@
+from seaplane.pipes.executor import Message, Result
+from seaplane.pipes import Dag, EdgeTo, EdgeFrom
+
+for_export = [
+    Dag,
+    EdgeFrom,
+    EdgeTo,
+    Message,
+    Result,
+]
