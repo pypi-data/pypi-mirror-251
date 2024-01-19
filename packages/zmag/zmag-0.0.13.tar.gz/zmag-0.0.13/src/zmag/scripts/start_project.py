3dcee19db5628d9555231a1516af9225b8144741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 14:06:00 +0000

    Add package zmag/0.0.13/zmag-0.0.13.tar.gz

---
+++ packages/zmag/zmag-0.0.13.tar.gz/zmag-0.0.13/src/zmag/scripts/start_project.py
@@ -0,0 +1,20 @@
+"""
+    Start A New Fastberry Project
+"""
+
+import os
+import pathlib
+
+import click
+
+from .shell import unzip, shell_print
+
+TEMPLATES_DIR = pathlib.Path(__file__).parents[0] / "templates"
+
+
+@click.command()
+def cli():
+    """Zmag Start-Project."""
+
+    shell_print("""* Starting-Project! ...\n""")
+    unzip(TEMPLATES_DIR / "project-template.zip", pathlib.Path(os.getcwd()))
---
+++ packages/zmag/zmag-0.0.13.tar.gz/zmag-0.0.13/tests/config/settings.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+    { Settings }
+"""
+
+import pathlib
+
+# Base Directory
+BASE_DIR = pathlib.Path(__file__).parents[1]
