1c848bb5df592ad727633b965bd6b7a613ab07f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:31 +0000

    Add package mqt.problemsolver/0.3.0/mqt.problemsolver-0.3.0.tar.gz

---
+++ packages/mqt.problemsolver/mqt.problemsolver-0.3.0.tar.gz/mqt.problemsolver-0.3.0/tests/test_tsp.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+from mqt.problemsolver.tsp import TSP
+
+
+def test_kakuro() -> None:
+    tsp = TSP()
+    res = tsp.solve(1, 2, 3, 4, 5, 6, quantum_algorithm="QPE", objective_function="shortest_path")
+    assert res is not None
+    assert res == [3, 1, 2, 4]
+    res = tsp.solve(6, 9, 2, 1, 8, 4, quantum_algorithm="QPE", objective_function="shortest_path")
+    assert res is not None
+    assert res == [4, 1, 2, 3]
