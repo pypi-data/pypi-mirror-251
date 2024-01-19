5d4bb5519594d414b46561ffa0d03b1e60f67727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:55 +0000

    Add package HawaData/0.13.6/HawaData-0.13.6.tar.gz

---
+++ packages/HawaData/HawaData-0.13.6.tar.gz/HawaData-0.13.6/hawa/base/errors.py
@@ -0,0 +1,11 @@
+class NoCasesError(Exception):
+    """Raised when no cases are found in the database."""
+    pass
+
+
+class NoSchoolIdsError(Exception):
+    pass
+
+
+class NoAnswersError(Exception):
+    pass
