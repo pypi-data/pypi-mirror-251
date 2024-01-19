481ab01299e9abcce7893193b0149d90803c813c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:46 +0000

    Add package pyrabbitmq/0.0.3/pyrabbitmq-0.0.3.tar.gz

---
+++ packages/pyrabbitmq/pyrabbitmq-0.0.3.tar.gz/pyrabbitmq-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='pyrabbitmq',
+    version='0.0.3',
+    author='Xizhen Du',
+    author_email='xizhendu@gmail.com',
+    url='https://github.com/xizhendu/pyrabbitmq',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    description='Simple Python client library for RabbitMQ',
+    packages=['pyrabbitmq'],
+    install_requires=[
+        "requests",
+    ]
+)
