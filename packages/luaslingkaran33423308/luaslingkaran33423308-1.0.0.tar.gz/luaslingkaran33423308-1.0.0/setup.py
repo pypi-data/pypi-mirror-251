ed14e7dc29e46adc10135b7686d6ae1921773f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:06:28 +0000

    Add package luaslingkaran33423308/1.0.0/luaslingkaran33423308-1.0.0.tar.gz

---
+++ packages/luaslingkaran33423308/luaslingkaran33423308-1.0.0.tar.gz/luaslingkaran33423308-1.0.0/setup.py
@@ -0,0 +1,10 @@
+# File: /your_project/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='luaslingkaran33423308',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[],
+)
