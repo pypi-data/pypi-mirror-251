721049d1ac34dd1e97ca01f888fc8163e42e7f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 06:05:35 +0000

    Add package tendenci-django-tagging/0.6/tendenci-django-tagging-0.6.tar.gz

---
+++ packages/tendenci-django-tagging/tendenci-django-tagging-0.6.tar.gz/tendenci-django-tagging-0.6/tagging/__init__.py
@@ -0,0 +1,15 @@
+"""
+Django-tagging
+"""
+__version__ = '0.6'
+__license__ = 'BSD License'
+
+__author__ = 'Jonathan Buchanan'
+__author_email__ = 'jonathan.buchanan@gmail.com'
+
+__maintainer__ = 'Tendenci'
+__maintainer_email__ = 'programmers@tendenci.com'
+
+__url__ = 'https://github.com/tendenci/django-tagging'
+
+#default_app_config = 'tagging.apps.TaggingConfig'
