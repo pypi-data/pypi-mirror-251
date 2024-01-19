ad390b7d6565da2a2b93168d04ea37dc2be62fb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:18 +0000

    Add package prot2d/0.2.1/prot2d-0.2.1-py3-none-any.whl

---
+++ packages/prot2d/prot2d-0.2.1-py3-none-any.whl/prot2d/cli.py
@@ -0,0 +1,11 @@
+import typer
+from Main import check_pdb_path
+from Helper_functions import count_residues
+
+app = typer.Typer()
+
+app.command()(check_pdb_path)
+app.command()(count_residues)
+
+if __name__ == "__main__":
+    app()
