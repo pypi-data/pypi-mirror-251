8a82b63f33ed3f3f9df8086f679fca4d044812b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:16 +0000

    Add package tealogger/0.7.6/tealogger-0.7.6-py2.py3-none-any.whl

---
+++ packages/tealogger/tealogger-0.7.6-py2.py3-none-any.whl/tealogger/__init__.py
@@ -0,0 +1,20 @@
+"""Tea Logger is a simple logger package for Python."""
+
+from .logger import (
+    CRITICAL,
+    ERROR,
+    WARNING,
+    INFO,
+    DEBUG
+    )
+
+from .logger import (
+    critical,
+    error,
+    warning,
+    info,
+    debug,
+    log
+    )
+
+from .logger import Logger
