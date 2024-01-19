5ef1c40c4111c474bc04562f13515b35ea7337fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 05:05:06 +0000

    Add package matprob/1.0.8/matprob-1.0.8.tar.gz

---
+++ packages/matprob/matprob-1.0.8.tar.gz/matprob-1.0.8/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='matprob',
+      version='1.0.8',
+      description='none',
+      packages=['matprob'],
+      author_email='mhivanov4@gmail.com',
+      install_requires=['pyperclip','scikit-learn','numpy','IPython'],
+      zip_safe=False)
