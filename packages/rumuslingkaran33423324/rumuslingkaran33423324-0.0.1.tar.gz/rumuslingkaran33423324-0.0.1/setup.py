e5a0e3ee8c841841874a61cd49ac653a0a590baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:06:20 +0000

    Add package rumuslingkaran33423324/0.0.1/rumuslingkaran33423324-0.0.1.tar.gz

---
+++ packages/rumuslingkaran33423324/rumuslingkaran33423324-0.0.1.tar.gz/rumuslingkaran33423324-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# Proyek/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='rumuslingkaran33423324',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
