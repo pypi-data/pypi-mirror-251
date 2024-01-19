21db5f4ce92114cdafad39088299a8cd01ad9148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:43 +0000

    Add package mnelab/0.9.0/mnelab-0.9.0-py3-none-any.whl

---
+++ packages/mnelab/mnelab-0.9.0-py3-none-any.whl/mnelab/utils/dependencies.py
@@ -0,0 +1,21 @@
+# © MNELAB developers
+#
+# License: BSD (3-clause)
+
+from importlib import import_module
+
+required = ["mne", "numpy", "scipy", "matplotlib", "PySide6", "pyxdf"]
+optional = ["sklearn", "picard", "pyedflib", "pybv"]
+
+have = {}
+for package in required + optional:
+    try:
+        mod = import_module(package)
+    except ModuleNotFoundError:
+        have[package] = False
+    else:  # module successfully imported
+        try:
+            version = mod.__version__
+        except AttributeError:
+            version = "unknown"
+        have[package] = version
