9e2b60553285d15e4a7b866eb7c8ac7cd02501ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 06:05:23 +0000

    Add package hspylib-clitt/0.9.108/hspylib_clitt-0.9.108-py3-none-any.whl

---
+++ packages/hspylib-clitt/hspylib_clitt-0.9.108-py3-none-any.whl/build/lib/build/lib/clitt/core/tui/__init__.py
@@ -0,0 +1,19 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-clitt v0.9.108
+#
+# Package: main.build.lib.build.lib.clitt.core.tui
+"""Package initialization."""
+
+__all__ = [
+    'mchoose', 
+    'mdashboard', 
+    'menu', 
+    'minput', 
+    'mselect', 
+    'table', 
+    'tui_application', 
+    'tui_component', 
+    'tui_preferences'
+]
+__version__ = '0.9.108'
