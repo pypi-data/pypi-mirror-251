bc54707d0e91a63c69aa0287346ee5498b10bad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:23 +0000

    Add package gvgames/1.0/gvgames-1.0.tar.gz

---
+++ packages/gvgames/gvgames-1.0.tar.gz/gvgames-1.0/README.md
@@ -0,0 +1,7 @@
+These are 2 games 1st is Guess the Number and 2nd is Snake and Ladder Game. these Both are Console games.
+
+to use my package you need to import it like below and call the playGames() function.
+------------------------------
+from ggames import playGames
+playGames()
+------------------------------
---
+++ packages/gvgames/gvgames-1.0.tar.gz/gvgames-1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gvgames',
+    version='1.0',
+    author='GHANSHYAM VAJA',
+    author_email='ghanshyamvaja11@gmail.com',
+    description='These are 2 games 1st is Guess the Number and 2nd is Snake and Ladder Game. these Both are Console games.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+
+    ],
+)
