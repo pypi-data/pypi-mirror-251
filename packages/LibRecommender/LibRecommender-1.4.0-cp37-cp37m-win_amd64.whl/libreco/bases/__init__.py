c5b6e10970154c4e237bff018797d93f067bb78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:04:46 +0000

    Add package LibRecommender/1.4.0/LibRecommender-1.4.0-cp37-cp37m-win_amd64.whl

---
+++ packages/LibRecommender/LibRecommender-1.4.0-cp37-cp37m-win_amd64.whl/libreco/bases/__init__.py
@@ -0,0 +1,19 @@
+from .base import Base
+from .cf_base import CfBase
+from .dyn_embed_base import DynEmbedBase
+from .embed_base import EmbedBase
+from .gensim_base import GensimBase
+from .meta import ModelMeta
+from .sage_base import SageBase
+from .tf_base import TfBase
+
+__all__ = [
+    "Base",
+    "CfBase",
+    "DynEmbedBase",
+    "EmbedBase",
+    "GensimBase",
+    "ModelMeta",
+    "SageBase",
+    "TfBase",
+]
