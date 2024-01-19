20fe3de0428ab23d059154be773793cde88a8317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:58 +0000

    Add package cwapi3d/30.318.4/cwapi3d-30.318.4.tar.gz

---
+++ packages/cwapi3d/cwapi3d-30.318.4.tar.gz/cwapi3d-30.318.4/src/dimension_controller/__init__.py
@@ -0,0 +1,106 @@
+from typing import List
+from cadwork import point_3d
+
+def create_dimension(a0: point_3d, a1: point_3d, a2: point_3d, a3: List[point_3d]) -> int:
+    """create dimension
+    Args:
+        a0 ( point_3d): a0
+        a1 ( point_3d): a1
+        a2 ( point_3d): a2
+        a3 ( List[point_3d]): a3
+
+    Returns:
+        int
+    """
+
+def set_orientation(a0: List[int], a1: point_3d, a2: point_3d) -> None:
+    """set orientation
+    Args:
+        a0 ( List[int]): a0
+        a1 ( point_3d): a1
+        a2 ( point_3d): a2
+
+    Returns:
+        None
+    """
+
+def add_segment(a0: int, a1: point_3d) -> None:
+    """add segment
+    Args:
+        a0 ( int): a0
+        a1 ( point_3d): a1
+
+    Returns:
+        None
+    """
+
+def set_precision(a0: List[int], a1: int) -> None:
+    """set precision
+    Args:
+        a0 ( List[int]): a0
+        a1 ( int): a1
+
+    Returns:
+        None
+    """
+
+def set_text_size(a0: List[int], a1: float) -> None:
+    """set text size
+    Args:
+        a0 ( List[int]): a0
+        a1 ( float): a1
+
+    Returns:
+        None
+    """
+
+def set_line_thickness(a0: List[int], a1: float) -> None:
+    """set line thickness
+    Args:
+        a0 ( List[int]): a0
+        a1 ( float): a1
+
+    Returns:
+        None
+    """
+
+def set_total_dimension(a0: List[int], a1: bool) -> None:
+    """set total dimension
+    Args:
+        a0 ( List[int]): a0
+        a1 ( bool): a1
+
+    Returns:
+        None
+    """
+
+def set_text_color(a0: List[int], a1: int) -> None:
+    """set text color
+    Args:
+        a0 ( List[int]): a0
+        a1 ( int): a1
+
+    Returns:
+        None
+    """
+
+def set_line_color(a0: List[int], a1: int) -> None:
+    """set line color
+    Args:
+        a0 ( List[int]): a0
+        a1 ( int): a1
+
+    Returns:
+        None
+    """
+
+def set_default_anchor_length(a0: List[int], a1: float) -> None:
+    """set default anchor length
+    Args:
+        a0 ( List[int]): a0
+        a1 ( float): a1
+
+    Returns:
+        None
+    """
+
