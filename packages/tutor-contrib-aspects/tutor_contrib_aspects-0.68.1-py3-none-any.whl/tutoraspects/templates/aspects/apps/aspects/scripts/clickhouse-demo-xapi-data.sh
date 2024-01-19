0ddc8ec309de12022b2b9a2abde823b55e9f392c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:29 +0000

    Add package tutor-contrib-aspects/0.68.1/tutor_contrib_aspects-0.68.1-py3-none-any.whl

---
+++ packages/tutor-contrib-aspects/tutor_contrib_aspects-0.68.1-py3-none-any.whl/tutoraspects/templates/aspects/apps/aspects/scripts/clickhouse-demo-xapi-data.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+echo "Loading demo xAPI data..."
+xapi-db-load --backend ralph_clickhouse \
+    --num_batches $1 \
+    --batch_size $2 \
+    --db_host "{{ CLICKHOUSE_HOST }}" \
+    --db_port "{{ CLICKHOUSE_INTERNAL_HTTP_PORT }}" \
+    --db_username "{{ CLICKHOUSE_ADMIN_USER }}" \
+    --db_password "{{ CLICKHOUSE_ADMIN_PASSWORD }}" \
+    --db_name "{{ ASPECTS_XAPI_DATABASE }}" \
+    --lrs_url "{% if RUN_RALPH %}http://ralph:{{ RALPH_PORT }}{% else %}{% if RALPH_RUN_HTTPS %}https://{% else %}http://{% endif %}{{ RALPH_HOST }}{% endif %}/xAPI/statements/" \
+    --lrs_username "{{RALPH_LMS_USERNAME}}" \
+    --lrs_password "{{RALPH_LMS_PASSWORD}}"
+
+echo "Done."
