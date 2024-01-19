c602280d2786376c998f6b43cb77b8424d2b092e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:05:46 +0000

    Add package protmapper/0.0.29/protmapper-0.0.29-py3-none-any.whl

---
+++ packages/protmapper/protmapper-0.0.29-py3-none-any.whl/protmapper/__init__.py
@@ -0,0 +1,19 @@
+__version__ = '0.0.29'
+import os
+import logging
+
+logging.basicConfig(format=('%(levelname)s: [%(asctime)s] %(name)s'
+                            ' - %(message)s'),
+                    level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
+
+logging.getLogger('requests').setLevel(logging.ERROR)
+logging.getLogger('urllib3').setLevel(logging.ERROR)
+logging.getLogger('rdflib').setLevel(logging.ERROR)
+logging.getLogger('boto3').setLevel(logging.CRITICAL)
+logging.getLogger('botocore').setLevel(logging.CRITICAL)
+
+logger = logging.getLogger('protmapper')
+
+if not os.environ.get('INITIAL_RESOURCE_DOWNLOAD'):
+    from protmapper.api import ProtMapper, MappedSite
+    from protmapper.resources import resource_dir
