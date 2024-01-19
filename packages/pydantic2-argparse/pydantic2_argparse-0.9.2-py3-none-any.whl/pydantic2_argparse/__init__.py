3e159c61fc6c98c69ae2e5a3faf259b05226e069
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 05:05:50 +0000

    Add package pydantic2-argparse/0.9.2/pydantic2_argparse-0.9.2-py3-none-any.whl

---
+++ packages/pydantic2-argparse/pydantic2_argparse-0.9.2-py3-none-any.whl/pydantic2_argparse/__init__.py
@@ -0,0 +1,28 @@
+"""Declarative Typed Argument Parsing with Pydantic Models.
+
+This is the `pydantic2-argparse` package, which contains the classes, methods
+and functions required for declarative and typed argument parsing with
+`pydantic` models.
+
+The public interface exposed by this package is the declarative and typed
+`ArgumentParser` class, as well as the package "dunder" metadata.
+"""
+
+# Local
+from .argparse import ArgumentParser
+from .__metadata__ import __title__
+from .__metadata__ import __description__
+from .__metadata__ import __version__
+from .__metadata__ import __author__
+from .__metadata__ import __license__
+
+
+# Public Re-Exports
+__all__ = (
+    "ArgumentParser",
+    "__title__",
+    "__description__",
+    "__version__",
+    "__author__",
+    "__license__",
+)
