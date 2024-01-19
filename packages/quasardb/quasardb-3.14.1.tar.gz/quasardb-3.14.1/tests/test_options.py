2cc7c0eeec5f061ca86373651bd22db450c285c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:33 +0000

    Add package quasardb/3.14.1/quasardb-3.14.1.tar.gz

---
+++ packages/quasardb/quasardb-3.14.1.tar.gz/quasardb-3.14.1/tests/test_options.py
@@ -0,0 +1,34 @@
+# pylint: disable=C0103,C0111,C0302,W0212
+
+import quasardb
+import pytest
+
+
+def test_get_client_max_parallelism(qdbd_connection):
+    assert qdbd_connection.options().get_client_max_parallelism() > 0
+
+
+def test_set_client_max_parallelism_negative(qdbd_connection):
+    with pytest.raises(TypeError):
+        qdbd_connection.options().set_client_max_parallelism(-1)
+
+
+def test_set_client_max_parallelism_0(qdbd_connection):
+    qdbd_connection.options().set_client_max_parallelism(0)
+    assert qdbd_connection.options().get_client_max_parallelism() > 0
+
+
+def test_set_client_max_parallelism_1(qdbd_connection):
+    qdbd_connection.options().set_client_max_parallelism(1)
+    assert qdbd_connection.options().get_client_max_parallelism() == 1
+
+
+def test_get_client_max_parallelism_8(qdbd_connection):
+    assert qdbd_connection.options().get_client_max_parallelism() == 1
+
+def test_get_query_max_length(qdbd_connection):
+    assert qdbd_connection.options().get_query_max_length() >= 0
+
+def test_set_query_max_length(qdbd_connection):
+    qdbd_connection.options().set_query_max_length(1234)
+    assert qdbd_connection.options().get_query_max_length() == 1234
