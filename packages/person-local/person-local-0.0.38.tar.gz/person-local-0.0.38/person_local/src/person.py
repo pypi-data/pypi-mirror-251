11b678b447b41d069919e7d505bdaf1779f6b9af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 06:05:04 +0000

    Add package person-local/0.0.38/person-local-0.0.38.tar.gz

---
+++ packages/person-local/person-local-0.0.38.tar.gz/person-local-0.0.38/person_local/src/person.py
@@ -0,0 +1,28 @@
+from database_mysql_local.point import Point
+
+
+# TOOO Person -> PersonLocal
+class Person:
+    "person details class"
+
+    def __init__(self, number: int = None, last_coordinate: Point = Point(0, 0),
+                 birthday_date: str = None, day: int = None,
+                 month: int = None, year: int = None, first_name: str = None,
+                 last_name: str = None, location_id: int = None,
+                 nickname: str = None, gender_id: int = None,
+                 father_name: str = None, main_email_address: str = None, 
+                 is_test_data: bool = False) -> None:
+        self.number = number
+        self.gender_id = gender_id
+        self.last_coordinate = last_coordinate
+        self.location_id = location_id
+        self.birthday_date = birthday_date
+        self.day = day
+        self.month = month
+        self.year = year
+        self.first_name = first_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.father_name = father_name
+        self.main_email_address = main_email_address
+        self.is_test_data = is_test_data
