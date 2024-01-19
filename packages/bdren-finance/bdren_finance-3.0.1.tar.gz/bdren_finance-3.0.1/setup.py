79bade93c60b33f47733da3e1ac623f822420730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:04:16 +0000

    Add package bdren-finance/3.0.1/bdren_finance-3.0.1.tar.gz

---
+++ packages/bdren-finance/bdren_finance-3.0.1.tar.gz/bdren_finance-3.0.1/setup.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='bdren_finance',
+    version='3.0.1',
+    author='Shuvo',
+    author_email='shuvo.punam@gmail.com',
+    description='BdREN Finance',
+    packages=find_packages(
+        include=[
+            'bdren_finance',
+            'bdren_finance.*',
+        ]
+    ),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'requests',
+        'Django',
+    ],
+    python_requires='>=3.6',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
