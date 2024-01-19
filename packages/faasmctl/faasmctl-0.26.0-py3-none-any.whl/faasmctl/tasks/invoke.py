fdffc0844a494d22f79897a2fe4eed24e3a89fb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 05:05:46 +0000

    Add package faasmctl/0.26.0/faasmctl-0.26.0-py3-none-any.whl

---
+++ packages/faasmctl/faasmctl-0.26.0-py3-none-any.whl/faasmctl/tasks/invoke.py
@@ -0,0 +1,46 @@
+from base64 import b64encode
+from faasmctl.util.invoke import invoke_wasm
+from invoke import task
+
+
+@task(default=True)
+def invoke(
+    ctx,
+    user,
+    func,
+    ini_file=None,
+    cmdline=None,
+    input_data=None,
+    mpi_world_size=None,
+    single_host=False,
+):
+    """
+    Invoke the execution of a user/func pair
+
+    TODO: think how to enable all the possible command line values in a
+    scalable way.
+    """
+    num_messages = 1
+    req_dict = {"user": user, "function": func}
+    msg_dict = {"user": user, "function": func}
+
+    if cmdline is not None:
+        msg_dict["cmdline"] = cmdline
+    if input_data is not None:
+        msg_dict["input_data"] = b64encode(input_data.encode("utf-8")).decode("utf-8")
+    if mpi_world_size is not None:
+        msg_dict["mpi_world_size"] = int(mpi_world_size)
+    if single_host:
+        req_dict["singleHost"] = single_host
+
+    # Invoke WASM using main API function
+    result = invoke_wasm(
+        msg_dict,
+        num_messages=num_messages,
+        req_dict=req_dict,
+        ini_file=ini_file,
+    )
+
+    # We print the outputData of the first message for backwards compatibility.
+    # We could eventually change this behaviour
+    print(result.messageResults[0].outputData)
