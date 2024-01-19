ff13f780e6ef735e493742c40c35d727f274885d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:17 +0000

    Add package embody-file/1.0.21/embody_file-1.0.21-py3-none-any.whl

---
+++ packages/embody-file/embody_file-1.0.21-py3-none-any.whl/embodyfile/__init__.py
@@ -0,0 +1,8 @@
+"""Initialize the embodyfile package."""
+import importlib.metadata
+
+
+try:
+    __version__ = importlib.metadata.version("embody-file")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0"
