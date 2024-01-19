8e423124ebe244a7f7292154c8faa5fefb4e3d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:09 +0000

    Add package gradio-doctestaudio/0.0.3/gradio_doctestaudio-0.0.3.tar.gz

---
+++ packages/gradio-doctestaudio/gradio_doctestaudio-0.0.3.tar.gz/gradio_doctestaudio-0.0.3/backend/gradio_doctestaudio/templates/component/index.js
@@ -0,0 +1,8 @@
+import { E as s, a as t, A as i, S as o, I as r } from "./index-55115273.js";
+export {
+  s as BaseExample,
+  t as BaseInteractiveAudio,
+  i as BasePlayer,
+  o as BaseStaticAudio,
+  r as default
+};
