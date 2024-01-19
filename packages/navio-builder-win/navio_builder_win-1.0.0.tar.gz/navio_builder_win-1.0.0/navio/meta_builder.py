7514f01273bd74fb59370b25088466a7f8635e0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 05:06:03 +0000

    Add package navio-builder-win/1.0.0/navio_builder_win-1.0.0.tar.gz

---
+++ packages/navio-builder-win/navio_builder_win-1.0.0.tar.gz/navio_builder_win-1.0.0/navio/meta_builder.py
@@ -0,0 +1,4 @@
+__version__ = "1.0.0"
+__license__ = "MIT License"
+__website__ = "https://github.com/navio-online/navio-builder/"
+__download_url__ = "https://github.com/navio-online/navio-builder/archive/" "{}.tar.gz".format(__version__)
