a9e9999862655f02a49fbf84a2c3fd8bdf76c13f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:10 +0000

    Add package aeotrade-log/0.1.0/aeotrade_log-0.1.0.tar.gz

---
+++ packages/aeotrade-log/aeotrade_log-0.1.0.tar.gz/aeotrade_log-0.1.0/setup.py
@@ -0,0 +1,24 @@
+"""
+@company: 慧贸天下(北京)科技有限公司
+@author: wanghao@aeotrade.com
+@time: 2024/1/5 14:57 
+@file: setup.py.py
+@project: aeotrade_log
+@describe: None
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="aeotrade_log",
+    version="0.1.0",
+    author="Hero",
+    author_email="wanghao@aeotrade.com",
+    description="log config",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
