83c7e0f41f64b207e3adf1a5ff4ac102b35235d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 04:05:20 +0000

    Add package gen-openapi-rest/0.1/gen-openapi-rest-0.1.tar.gz

---
+++ packages/gen-openapi-rest/gen-openapi-rest-0.1.tar.gz/gen-openapi-rest-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gen-openapi-rest',
+    version='0.1',
+    packages=find_packages(),
+    description='Generate .http files from OpenAPI/Swagger JSON. to be used with VS Code REST Client.',
+    long_description='Generate .http files from OpenAPI/Swagger JSON. to be used with VS Code REST Client',
+    long_description_content_type='text/markdown',
+    author='rangulvers',
+    url='https://github.com/rangulvers/openapi-rest-client-generator',
+    license='LICENSE',
+    install_requires=[
+        'requests',
+        'json',
+        'sys',
+        'urllib'
+    ],
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.org/classifiers/
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
