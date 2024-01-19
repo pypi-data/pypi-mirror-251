aecad8b136f42a0867556dc0f0eb178511b92b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:07:04 +0000

    Add package rfidpy/0.2/rfidpy-0.2.tar.gz

---
+++ packages/rfidpy/rfidpy-0.2.tar.gz/rfidpy-0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("readme.md","r") as rd:
+    long_description = rd.read()
+
+setup(
+    name='rfidpy',
+    version='0.2',
+    author="M.Karthickraja",
+    author_email="karthickrajam8100@gmail.com",
+    description="Raspberry pi 5 interface with RFID(MFR522)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['rfid', 'raspberry-pi', 'mfrc522', 'pi5', 'python',],
+
+)
