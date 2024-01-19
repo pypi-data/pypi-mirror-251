906278a07aef55bfb6fe5beb71a6cdb917b033b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:04:28 +0000

    Add package pathway/0.7.8/pathway-0.7.8-cp310-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pathway/pathway-0.7.8-cp310-abi3-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pathway/tests/test_types.py
@@ -0,0 +1,47 @@
+# Copyright © 2024 Pathway
+
+import pathway as pw
+import pathway.internals.dtype as dt
+from pathway.tests.utils import T
+
+
+def test_date_time_naive_schema():
+    table = T(
+        """
+      |         t1          |         t2
+    0 | 2023-05-15T10:13:00 | 2023-05-15T10:13:23
+    """
+    )
+    fmt = "%Y-%m-%dT%H:%M:%S"
+    table_with_datetimes = table.select(
+        t1=table.t1.dt.strptime(fmt=fmt), t2=table.t2.dt.strptime(fmt=fmt)
+    )
+    table_with_datetimes = table_with_datetimes.with_columns(
+        diff=pw.this.t1 - pw.this.t2
+    )
+    assert table_with_datetimes.schema._dtypes() == {
+        "t1": dt.DATE_TIME_NAIVE,
+        "t2": dt.DATE_TIME_NAIVE,
+        "diff": dt.DURATION,
+    }
+
+
+def test_date_time_utc_schema():
+    table = T(
+        """
+      |            t1             |            t2
+    0 | 2023-05-15T10:13:00+01:00 | 2023-05-15T10:13:23+01:00
+    """
+    )
+    fmt = "%Y-%m-%dT%H:%M:%S%z"
+    table_with_datetimes = table.select(
+        t1=table.t1.dt.strptime(fmt=fmt), t2=table.t2.dt.strptime(fmt=fmt)
+    )
+    table_with_datetimes = table_with_datetimes.with_columns(
+        diff=pw.this.t1 - pw.this.t2
+    )
+    assert table_with_datetimes.schema._dtypes() == {
+        "t1": dt.DATE_TIME_UTC,
+        "t2": dt.DATE_TIME_UTC,
+        "diff": dt.DURATION,
+    }
