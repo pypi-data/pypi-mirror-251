e5cd483bf79355a0b3f29c6bfd602d178333ef52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 14:06:07 +0000

    Add package auto-shap/0.3.2/auto_shap-0.3.2.tar.gz

---
+++ packages/auto-shap/auto_shap-0.3.2.tar.gz/auto_shap-0.3.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="auto_shap",
+    version="0.3.2",
+    author="Micah Melling",
+    author_email="micahmelling@gmail.com",
+    description="Calculate SHAP values in parallel and automatically detect what explainer to use",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/micahmelling/auto-shap',
+    license="MIT",
+    packages=['auto_shap'],
+    install_requires=['shap>=0.35.0', 'pandas>=1.1.5', 'numpy>=1.21.6', 'matplotlib>=3.2.0', "numba>=0.56.4"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
