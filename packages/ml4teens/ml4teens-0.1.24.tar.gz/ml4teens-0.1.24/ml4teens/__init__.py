ab1fab492eedf058e244805902c81d931aa2356d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 06:05:13 +0000

    Add package ml4teens/0.1.24/ml4teens-0.1.24.tar.gz

---
+++ packages/ml4teens/ml4teens-0.1.24.tar.gz/ml4teens-0.1.24/ml4teens/__init__.py
@@ -0,0 +1,5 @@
+#!/bin/env python3
+
+from . import core;
+from . import blocks;
+from . import tools;
---
+++ packages/ml4teens/ml4teens-0.1.24.tar.gz/ml4teens-0.1.24/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml4teens',
+    version='0.1.24',
+    packages=find_packages(),
+    install_requires=[ "ipython", 
+                       "opencv-python", 
+                       "numpy",
+                       "requests", 
+                       "ultralytics", 
+                       "Pillow", 
+                       "ipycanvas==0.11",
+                       "ipyevents",
+                       "pandas>=1.3.4",
+                       "transformers",
+                       "face_recognition",
+                     ],
+    # metadata para publicar en PyPI
+    author='Francisco Puentes',
+    author_email='fran@puentescalvo.com',
+    description='Machine Learning para Adolescentes',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/FranPuentes/ML4Teens',
+)
