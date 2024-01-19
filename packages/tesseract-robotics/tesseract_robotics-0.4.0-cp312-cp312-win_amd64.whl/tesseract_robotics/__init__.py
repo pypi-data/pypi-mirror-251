adcf110ea0fee7178dcd5b566bd063775df8418d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 06:05:32 +0000

    Add package tesseract-robotics/0.4.0/tesseract_robotics-0.4.0-cp312-cp312-win_amd64.whl

---
+++ packages/tesseract-robotics/tesseract_robotics-0.4.0-cp312-cp312-win_amd64.whl/tesseract_robotics/__init__.py
@@ -0,0 +1,23 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_5_2():
+    import os
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'tesseract_robotics.libs'))
+    if os.path.isdir(libs_dir):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_5_2()
+del _delvewheel_patch_1_5_2
+# end delvewheel patch
+
+def _tesseract_dll_path_():
+    import sys
+    import os
+    if sys.platform == "win32" and sys.version_info[:2] >= (3, 8):
+        tesseract_env_path = os.environ.get("TESSERACT_PYTHON_DLL_PATH",None)
+        if tesseract_env_path:
+            for p in tesseract_env_path.split(os.pathsep):
+                os.add_dll_directory(p)
+
+_tesseract_dll_path_()
+del _tesseract_dll_path_
