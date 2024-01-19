c14615f03c0e72e5ac1b05366c87a5ede6e823c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 14:05:54 +0000

    Add package combo-nlp/3.0.4/combo-nlp-3.0.4.tar.gz

---
+++ packages/combo-nlp/combo-nlp-3.0.4.tar.gz/combo-nlp-3.0.4/combo/data/__init__.py
@@ -0,0 +1,13 @@
+from .vocabulary import Vocabulary
+from .samplers import TokenCountBatchSampler
+from .instance import Instance
+from .token_indexers import (SingleIdTokenIndexer, TokenIndexer, TokenFeatsIndexer)
+from .tokenizers import (Tokenizer, Token, CharacterTokenizer, PretrainedTransformerTokenizer,
+                         SpacyTokenizer, WhitespaceTokenizer, LamboTokenizer)
+from .dataset_readers import (ConllDatasetReader, DatasetReader,
+                              TextClassificationJSONReader, UniversalDependenciesDatasetReader)
+from .api import (Sentence, tokens2conllu, conllu2sentence, sentence2conllu)
+
+
+class TokenCharactersIndexer:
+    pass
