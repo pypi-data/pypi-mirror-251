a7d54d59c67acb9a21a74f572e7e1efa112a656b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:06:24 +0000

    Add package travo/0.2.8/travo-0.2.8-py3-none-any.whl

---
+++ packages/travo/travo-0.2.8-py3-none-any.whl/travo/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.2.8"
+
+from .gitlab import Forge, GitLab
+from .assignment import Assignment
+from .course import Course
+from .homework import Homework
---
+++ packages/travo/travo-0.2.8-py3-none-any.whl/travo/tests/test_utils.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+import pytest
+
+from travo.utils import git_get_origin
+
+
+def test_get_origin_error(tmp_path: str) -> None:
+    os.chdir(tmp_path)
+    with pytest.raises(RuntimeError, match="fatal: not a git repository"):
+        # The directory is not a git repository
+        git_get_origin()
+
+    subprocess.run(["git", "init", "--quiet"], cwd=tmp_path)
+    with pytest.raises(RuntimeError, match="error: No such remote 'origin'"):
+        # The remote origin is not defined
+        git_get_origin()
