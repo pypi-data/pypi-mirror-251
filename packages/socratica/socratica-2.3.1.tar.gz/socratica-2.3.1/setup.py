bb24c828ee77ac05895cf49c172c9d8a074797e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:06:23 +0000

    Add package socratica/2.3.1/socratica-2.3.1.tar.gz

---
+++ packages/socratica/socratica-2.3.1.tar.gz/socratica-2.3.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='socratica',
+    version='2.3.1',
+    description='''\
+    Uma biblioteca Python abrangente que oferece módulos para automação web, criação de GUI, tratamento de dados, testes e segue estritamente as diretrizes da PEP 8.
+    ''',
+    author='Andrey Ruben Ribeiro Bessa',
+    author_email='andrey.bessa@gestao.gov.br',
+    packages=find_packages(),
+)
