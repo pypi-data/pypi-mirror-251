84bee7f483fe1996460729f44bc9f54e62b87dcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:05:04 +0000

    Add package rgle-arapm/0.0.2/rgle_arapm-0.0.2.tar.gz

---
+++ packages/rgle-arapm/rgle_arapm-0.0.2.tar.gz/rgle_arapm-0.0.2/src/rgle_arapm/arapm.py
@@ -0,0 +1,8 @@
+def area():
+    length = float(input('请输入矩形的长：'))
+    width = float(input('请输入矩形的宽：'))
+    print(f'矩形的长是：{length}n矩形的宽是：{width}n矩形的面积是：{length * width}')
+def perimeter():
+    length = float(input('请输入矩形的长：'))
+    width = float(input('请输入矩形的宽：'))
+    print(f'矩形的长是：{length}n矩形的宽是：{width}n矩形的面积是：{2 * (length + width)}')
