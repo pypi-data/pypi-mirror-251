16e5199f8adef306a5149c06f051a8bac9ec36fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:07:08 +0000

    Add package msscrawler/1.1.8/msscrawler-1.1.8.tar.gz

---
+++ packages/msscrawler/msscrawler-1.1.8.tar.gz/msscrawler-1.1.8/src/msscrawler/items/website_item.py
@@ -0,0 +1,12 @@
+import scrapy
+
+from .base import BaseItem
+
+
+class WebsiteItem(BaseItem):
+    name = scrapy.Field()
+    css_selector = scrapy.Field()
+    categories_num = scrapy.Field()
+    category_urls = scrapy.Field()
+    total_categories = scrapy.Field()
+    errors = scrapy.Field()
