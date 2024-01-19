26e61a1abd07adf6e5487383e8d34fdc53eca228
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 03:04:54 +0000

    Add package kruksik-hello/0.0.1/kruksik_hello-0.0.1.tar.gz

---
+++ packages/kruksik-hello/kruksik_hello-0.0.1.tar.gz/kruksik_hello-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import os
+
+from setuptools import find_packages, setup
+
+
+def load_req(r_file: str) -> list[str]:
+    with open(os.path.join(os.getcwd(), r_file)) as f:
+        return [
+            r for r in (line.split("#", 1)[0].strip() for line in f.readlines()) if r
+        ]
+
+
+setup(
+    name="kruksik_hello",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=load_req("requirements.txt"),
+    entry_points={
+        "console_scripts": [
+            "kruksik-hello = kruksik_hello:hello",
+        ]
+    },
+)
