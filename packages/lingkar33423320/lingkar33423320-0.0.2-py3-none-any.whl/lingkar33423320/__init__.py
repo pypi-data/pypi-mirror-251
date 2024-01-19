58874273ea09018985cf679519688e45c6182943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:06:20 +0000

    Add package lingkar33423320/0.0.2/lingkar33423320-0.0.2-py3-none-any.whl

---
+++ packages/lingkar33423320/lingkar33423320-0.0.2-py3-none-any.whl/lingkar33423320/__init__.py
@@ -0,0 +1,1 @@
+from lingkar33423320.luaslingkar import LingkaranCalculator
---
+++ packages/lingkar33423320/lingkar33423320-0.0.2-py3-none-any.whl/lingkar33423320/luaslingkar.py
@@ -0,0 +1,7 @@
+class LingkaranCalculator:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def hitung_luas(self):
+        luas = 3.141592653589793 * (self.radius ** 2)
+        return luas
