2d8f94c9bdf091e57ca7b0171e2ad765fbe6f53f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:07:07 +0000

    Add package webq/0.0.2/webq-0.0.2-py3-none-any.whl

---
+++ packages/webq/webq-0.0.2-py3-none-any.whl/webq/main.py
@@ -0,0 +1,36 @@
+import fire
+
+
+def start(c: str):
+    import uvicorn
+    from .context import get_context, init
+
+    init(c)
+    ctx = get_context()
+    config = ctx.config
+    host, port = config.get_host_port()
+
+    uvicorn.run("webq.app:app",
+                host=host,
+                port=port,
+                log_level=config.data.log_level,
+                )
+
+
+def db_init(c: str):
+    from .context import get_context, init
+    init(c)
+    ctx = get_context()
+    ctx.db.create_tables()
+    ctx.user_service.create_admin()
+
+
+def main():
+    fire.Fire({
+        'start': start,
+        'db-init': db_init,
+    })
+
+
+if __name__ == '__main__':
+    main()
