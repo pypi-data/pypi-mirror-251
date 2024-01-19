473dcff99d0b39284acacb94c4efe159bf249172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 16:06:18 +0000

    Add package liechains/0.2.7/liechains-0.2.7.tar.gz

---
+++ packages/liechains/liechains-0.2.7.tar.gz/liechains-0.2.7/ether/uniswapv3/chains.py
@@ -0,0 +1,11 @@
+chains = {
+    "ethereum": {
+        "init_code": "0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54",
+        "factory": "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+        "router": "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+    },
+    "manta": {
+        "factory": "0x56c2162254b0E4417288786eE402c2B41d4e181e",
+        "router": "0xfdE3eaC61C5Ad5Ed617eB1451cc7C3a0AC197564"
+    }
+}
