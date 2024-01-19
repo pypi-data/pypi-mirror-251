9cc3c2d82fdc373df23dbcebd94f6b17040f7716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-19 15:06:27 +0000

    Add package rumus33423324/0.0.1/rumus33423324-0.0.1.tar.gz

---
+++ packages/rumus33423324/rumus33423324-0.0.1.tar.gz/rumus33423324-0.0.1/setup.py
@@ -0,0 +1,12 @@
+# Proyek/setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='rumus33423324',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
