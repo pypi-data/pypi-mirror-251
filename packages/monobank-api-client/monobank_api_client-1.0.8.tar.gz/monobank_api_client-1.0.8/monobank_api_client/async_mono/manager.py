cb48fca482203e32ca14fcc48876c506b0c2bdc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:04:49 +0000

    Add package monobank-api-client/1.0.8/monobank_api_client-1.0.8.tar.gz

---
+++ packages/monobank-api-client/monobank_api_client-1.0.8.tar.gz/monobank_api_client-1.0.8/monobank_api_client/async_mono/manager.py
@@ -0,0 +1,120 @@
+import aiohttp
+from typing import Dict
+from mono_config.manager import BaseMonoManager
+from mono_config.exceptions import MonoException
+
+
+class AsyncMonoManager(BaseMonoManager, MonoException):
+    @classmethod
+    async def session(cls) -> aiohttp.client.ClientSession:
+        return aiohttp.ClientSession()
+
+    async def get_currencies(self) -> Dict:
+        try:
+            session = await self.session()
+            async with session:
+                uri = self.mono_currencies_uri
+                async with session.get(uri) as response:
+                    try:
+                        code = response.status
+                        response.raise_for_status()
+                        detail = await response.json()
+                        payload = {"code": code, "detail": detail}
+                        return payload
+                    except aiohttp.ClientResponseError as exc:
+                        error_response = {"code": code, "detail": str(exc.message)}
+                        return error_response
+        except Exception as exc:
+            error = {"datail": str(exc)}
+            return error
+
+    async def get_currency(self, ccy_pair: str) -> Dict:
+        try:
+            pair = self.mono_currencies.get(ccy_pair)
+            if pair is not None:
+                currencies = await self.get_currencies()
+                response = self.currency(ccy_pair, pair, currencies)
+                return response
+            list_ccy = [key for key in self.mono_currencies.keys()]
+            error_response = self.currency_error(list_ccy)
+            return error_response
+        except Exception as exc:
+            exception = {"detail": str(exc)}
+            return exception
+
+    async def get_client_info(self) -> Dict:
+        try:
+            session = await self.session()
+            async with session:
+                uri = self.mono_client_info_uri
+                token = self.token
+                headers = {"X-Token": token}
+                async with session.get(uri, headers=headers) as response:
+                    try:
+                        code = response.status
+                        response.raise_for_status()
+                        detail = await response.json()
+                        payload = {"code": code, "detail": detail}
+                        return payload
+                    except aiohttp.ClientResponseError as exc:
+                        error_response = {"code": code, "detail": str(exc.message)}
+                        return error_response
+        except Exception as exc:
+            error = {"detail": str(exc)}
+            return error
+
+    async def get_balance(self) -> Dict:
+        try:
+            info = await self.get_client_info()
+            code = info.get("code")
+            data = info.get("detail")
+            balance = {"balance": data["accounts"][0]["balance"] / 100}
+            payload = {"code": code, "detail": balance}
+            return payload
+        except Exception:
+            return info
+
+    async def get_statement(self, period: int) -> Dict:
+        try:
+            session = await self.session()
+            async with session:
+                token = self.token
+                uri = self.mono_statement_uri
+                headers = {"X-Token": token}
+                time_delta = self.date(period).get("time_delta")
+                async with session.get(
+                    f"{uri}{time_delta}/", headers=headers
+                ) as response:
+                    try:
+                        code = response.status
+                        response.raise_for_status()
+                        detail = await response.json()
+                        payload = {"code": code, "detail": detail}
+                        return payload
+                    except aiohttp.ClientResponseError as exc:
+                        error_response = {"code": code, "detail": str(exc.message)}
+                        return error_response
+        except Exception as exc:
+            exception = {"detail": str(exc)}
+            return exception
+
+    async def create_webhook(self, webhook: str) -> Dict:
+        try:
+            session = await self.session()
+            async with session:
+                token = self.token
+                uri = self.mono_webhook_uri
+                headers = {"X-Token": token}
+                async with session.post(uri, headers=headers, data=webhook) as response:
+                    try:
+                        code = response.status
+                        response.raise_for_status()
+                        detail = await response.json()
+                        payload = {"code": code, "detail": detail}
+                        return payload
+                    except aiohttp.ClientResponseError as exc:
+                        error_response = {"code": code, "detail": str(exc.message)}
+                        return error_response
+        except Exception as exc:
+            error = {"detail": str(exc)}
+            return error
