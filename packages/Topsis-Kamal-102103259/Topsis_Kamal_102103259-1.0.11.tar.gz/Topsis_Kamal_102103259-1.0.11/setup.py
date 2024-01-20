d52cf0835a9594716ecd8a9fe4d3d20ab154ba01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:04:57 +0000

    Add package Topsis-Kamal-102103259/1.0.11/Topsis_Kamal_102103259-1.0.11.tar.gz

---
+++ packages/Topsis-Kamal-102103259/Topsis_Kamal_102103259-1.0.11.tar.gz/Topsis_Kamal_102103259-1.0.11/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Topsis_Kamal_102103259",
+    version="1.0.11",
+    author="Kamalpreet Kaur",
+    author_email="kkamal101203@gmail.com",
+    url="https://github.com/kkamal2003/Topsis_Kamal_102103259",
+    description="A python package for implementing topsis",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pandas", "numpy"],
+    entry_points={"console_scripts": ["Topsis_Kamal_102103259 = Topsis_Kamal_102103259.main:main"]},
+)
