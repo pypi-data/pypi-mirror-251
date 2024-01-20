b098126f67242a51da2de4ac2a6cb0fc07a7ee22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:05:17 +0000

    Add package adc-dku-utils/0.0.0/adc-dku-utils-0.0.0.tar.gz

---
+++ packages/adc-dku-utils/adc-dku-utils-0.0.0.tar.gz/adc-dku-utils-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='adc-dku-utils',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='',
+    author='Konner Winkley',
+    author_email='kmwinkley@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
+
