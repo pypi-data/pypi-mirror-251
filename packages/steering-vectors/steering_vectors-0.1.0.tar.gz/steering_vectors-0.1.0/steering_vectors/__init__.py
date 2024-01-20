52847518fe8171a73c5a527d9d6ee2a396c36a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:05:46 +0000

    Add package steering-vectors/0.1.0/steering_vectors-0.1.0.tar.gz

---
+++ packages/steering-vectors/steering_vectors-0.1.0.tar.gz/steering_vectors-0.1.0/steering_vectors/__init__.py
@@ -0,0 +1,30 @@
+__version__ = "0.1.0"
+
+from .layer_matching import (
+    LayerMatcher,
+    LayerType,
+    ModelLayerConfig,
+    get_num_matching_layers,
+    guess_and_enhance_layer_config,
+)
+from .record_activations import record_activations
+from .steering_vector import (
+    PatchOperator,
+    SteeringPatchHandle,
+    SteeringVector,
+)
+from .train_steering_vector import SteeringVectorTrainingSample, train_steering_vector
+
+__all__ = [
+    "LayerType",
+    "LayerMatcher",
+    "ModelLayerConfig",
+    "get_num_matching_layers",
+    "guess_and_enhance_layer_config",
+    "PatchOperator",
+    "record_activations",
+    "SteeringVector",
+    "SteeringPatchHandle",
+    "train_steering_vector",
+    "SteeringVectorTrainingSample",
+]
