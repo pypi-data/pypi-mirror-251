a81ce810b973f17d2542878e35a31604cd2d993b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:05:21 +0000

    Add package clear-skies-auth-server/0.12.0/clear_skies_auth_server-0.12.0.tar.gz

---
+++ packages/clear-skies-auth-server/clear_skies_auth_server-0.12.0.tar.gz/clear_skies_auth_server-0.12.0/src/clearskies_auth_server/input_requirements/__init__.py
@@ -0,0 +1,26 @@
+from clearskies.binding_config import BindingConfig
+from .letters_digits import LettersDigits
+from .letters_digits_special_characters import LettersDigitsSpecialCharacters
+from .password_validation import PasswordValidation
+
+
+def letters_digits():
+    return BindingConfig(LettersDigits)
+
+
+def letters_digits_special_characters(special_characters="!@#$%^&*()<>,.?~`"):
+    return BindingConfig(LettersDigitsSpecialCharacters, special_characters=special_characters)
+
+
+def password_validation(password_column_name):
+    return BindingConfig(PasswordValidation, password_column_name=password_column_name)
+
+
+__all__ = [
+    "letters_digits",
+    "letters_digits_special_characters",
+    "LettersDigits",
+    "LettersDigitsSpecialCharacters",
+    "password_validation",
+    "PasswordValidation",
+]
