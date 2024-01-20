90de5c9f711699d7c56f7217891b91b9ef6687d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:05:36 +0000

    Add package aprsd-borat-plugin/0.1.1.dev1/aprsd_borat_plugin-0.1.1.dev1-py3-none-any.whl

---
+++ packages/aprsd-borat-plugin/aprsd_borat_plugin-0.1.1.dev1-py3-none-any.whl/aprsd_borat_plugin/quotes.py
@@ -0,0 +1,67 @@
+# All banned words are replaced with *** by aprsd by default here:
+# https://github.com/craigerl/aprsd/blob/master/aprsd/packets/core.py#L334
+
+BORAT_QUOTES = [
+    "Kazakhstan is more civilised now. Women can now travel on inside of bus, "
+    "and homosexuals no longer have to wear blue hat.",
+    "America national sport is called baseballs. It very similar to our sport, "
+    "shurik, where we take dogs, shoot them in a field and then have a party.",
+    "In U.S. and A. they treat horses like we in Kazakhstan treat our women. "
+    "They feed them two times a day. They have them sleep on straw in a small "
+    "box. And for entertainment, they make them jump over fences while being whipped.",
+    "American wine is like Kazakhstani wine, but not made from fermented horse urine.",
+    "Very sorry to interrupt politic. Might I make a shit in your house?",
+    "Yakshemash! In US of A, democracy is very different from Kazakhstan. "
+    "In America, woman *can* vote, but horse - *cannot*!",
+    "My wife she is dead...she die in a field...she die from work, an "
+    "accident, but is not important, I have a new wife.",
+    "Every Englishman must have a hobby. Some like to collect the stamp, "
+    "some like to make the jam, but the most fun is to a kill a little "
+    "animal with a shotgun or rip them up with wild dog.",
+    "In Kazakhstan we have many hobbies: disco dancing, archery, rape, and table tennis.",
+    "There are many job opportunities in the US and of A. For men, construction "
+    "worker, taxi driver or accountant. For woman, prostitute.",
+    "My wife, she is scared of men with chocolate face, there won't be any around here?",
+    "Throw the Jew down the well, So my country can be free, You must grab him by the"
+    "horns, Then we have a big party.",
+    "I hope you kill every man, woman and child in Iraq, down to the lizards. And may "
+    "George W. Bush drink the blood of every man, woman and child in Iraq.",
+    "My country send me to United States to make movie-film. Please, come and see my "
+    "film. If it not success, I will be execute.",
+    "We support your war of terror.",
+    "Although Kazakhstan a glorious country, it have a problem, too: economic, social and Jew.",
+    "You telling me the man who try to put a rubber fist in my anus was a homosexual?",
+    "Pamela! I no find you attractive anymore!... NOT!",
+    "Why you call police? The retard escape?",
+    "Quick children, smash the Jew egg!",
+    "He insist we not fly, in case the Jews repeated their attack of 9/11.",
+    "He is my neighbor, Nushuktan Tulyiagby, he is pain in my assholes. I get a window "
+    "from a glass, he must get a window from a glass. I get a step, he must get a step. "
+    "I get a clock-radio, he cannot afford. Great success!",
+    "I will look on your treasures, gypsy. Is this understood?",
+    "Gypsy, who is this woman you have shrunk?",
+    "I arrived in America's airport with clothings, U.S dollars and a jar of gypsy tears "
+    "to protect me from AIDS.",
+    "Her vagine hang like sleeve of wizard.",
+    "I feel like American movie star Dirty Harold...",
+    "Whoawhoawewa!",
+    "Kazakstan is the greatest country in the world, all other countries are run by little girls.",
+    "Kazakhstan is number one exporter of potassium, Other Central Asian countries have inferior "
+    "potassium.",
+    "I want to buy a car with pussy magnet.",
+    "I like to make sexy time!",
+    "This suit is NOT BLACK!",
+    "What's up, vanilla face?",
+    "Jak sie masz? My name Borat. I like you. I like sex. Is nice!",
+    "In my country, they would go crazy for these two ...not so much... this one",
+    "High five!",
+    "Yakshemash!",
+    "My wife is dead? …. High five!",
+    "Is Nice!",
+    "Gypsy! Give me your tears! If you will not give them to me, I will take them from you!",
+    "This is Natalya. She is my sister. She is number-four prostitute in whole of Kazakhstan. "
+    "Niiice!",
+    "My moustache still tastes of your testes!",
+    "This my mother. She is oldest woman in ALL of Kuzek! She is 42!Uhh this my wife, Oxanna "
+    "she is boring.",
+]
