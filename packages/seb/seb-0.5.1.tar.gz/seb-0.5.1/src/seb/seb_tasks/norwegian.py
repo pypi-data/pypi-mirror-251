366aa3945acfbc24690ccbb5331582a918ba3486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:05:19 +0000

    Add package seb/0.5.1/seb-0.5.1.tar.gz

---
+++ packages/seb/seb-0.5.1.tar.gz/seb-0.5.1/src/seb/seb_tasks/norwegian.py
@@ -0,0 +1,30 @@
+from seb.mteb_tasks import (NorwegianCourtsBitextMining,
+                            NorwegianParliamentClassification)
+from seb.registries import tasks
+from seb.tasks_interface import MTEBTask, Task
+
+
+@tasks.register("NoReC")
+def create_norec() -> Task:
+    from mteb import NoRecClassification
+
+    task = MTEBTask(NoRecClassification())
+    task.name = "NoReC"
+    task.domain = ["reviews"]
+    return task
+
+
+@tasks.register("Norwegian parliament")
+def create_norwegian_parliament() -> Task:
+    task = MTEBTask(NorwegianParliamentClassification())
+    task.name = "Norwegian parliament"
+    task.domain = ["spoken"]
+    return task
+
+
+# @tasks.register("Norwegian courts") # noqa
+def create_norwegian_courts() -> Task:
+    task = MTEBTask(NorwegianCourtsBitextMining())
+    task.name = "Norwegian courts"
+    task.domain = ["legal", "non-ficiton"]
+    return task
