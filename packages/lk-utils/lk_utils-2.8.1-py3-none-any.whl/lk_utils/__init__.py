6a1717cb473779eb80cf3edc4f92a37ac4aa2f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:05:26 +0000

    Add package lk-utils/2.8.1/lk_utils-2.8.1-py3-none-any.whl

---
+++ packages/lk-utils/lk_utils-2.8.1-py3-none-any.whl/lk_utils/__init__.py
@@ -0,0 +1,42 @@
+if 1:
+    import lk_logger
+    lk_logger.setup(quiet=True)
+
+from . import common_typing as t
+from . import common_typing as typing
+from . import filesniff
+from . import filesniff as fs
+from . import read_and_write
+from . import read_and_write as rw
+from . import subproc
+from . import textwrap
+from . import time_utils
+from .filesniff import cd_current_dir
+from .filesniff import find_dir_names
+from .filesniff import find_dir_paths
+from .filesniff import find_dirs
+from .filesniff import find_file_names
+from .filesniff import find_file_paths
+from .filesniff import find_files
+from .filesniff import findall_dir_names
+from .filesniff import findall_dir_paths
+from .filesniff import findall_dirs
+from .filesniff import findall_file_names
+from .filesniff import findall_file_paths
+from .filesniff import findall_files
+from .filesniff import get_current_dir
+from .filesniff import make_link as mklink
+from .filesniff import make_links as mklinks
+from .filesniff import normpath
+from .filesniff import xpath
+from .filesniff import xpath as relpath  # backward compatible
+from .read_and_write import dumps
+from .read_and_write import loads
+from .read_and_write import ropen
+from .read_and_write import wopen
+from .subproc import new_thread
+from .subproc import run_cmd_args
+from .subproc import run_cmd_line
+from .subproc import run_new_thread
+
+__version__ = '2.8.1'
