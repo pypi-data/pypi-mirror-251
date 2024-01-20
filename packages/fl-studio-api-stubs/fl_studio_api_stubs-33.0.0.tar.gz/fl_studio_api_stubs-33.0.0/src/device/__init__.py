4ae337dcd5dddeebcf9c2a225a06f5295d1163ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:06:45 +0000

    Add package fl-studio-api-stubs/33.0.0/fl_studio_api_stubs-33.0.0.tar.gz

---
+++ packages/fl-studio-api-stubs/fl_studio_api_stubs-33.0.0.tar.gz/fl_studio_api_stubs-33.0.0/src/device/__init__.py
@@ -0,0 +1,78 @@
+"""
+# Device
+
+FL Studio built-in module
+
+Handles the way that devices connect to FL Studio's MIDI interface, and how
+scripts communicate with each other.
+"""
+
+__all__ = [
+    'isAssigned',
+    'getPortNumber',
+    'getName',
+    'midiOutMsg',
+    'midiOutNewMsg',
+    'midiOutSysex',
+    'sendMsgGeneric',
+    'directFeedback',
+    'repeatMidiEvent',
+    'stopRepeatMidiEvent',
+    'setMasterSync',
+    'getMasterSync',
+    'processMIDICC',
+    'forwardMIDICC',
+    'findEventID',
+    'getLinkedValue',
+    'getLinkedValueString',
+    'getLinkedParamName',
+    'getLinkedInfo',
+    'dispatch',
+    'dispatchReceiverCount',
+    'dispatchGetReceiverPortNumber',
+    'createRefreshThread',
+    'destroyRefreshThread',
+    'fullRefresh',
+    'isDoubleClick',
+    'getDeviceId',
+    'getLinkedChannel',
+    'linkToLastTweaked',
+]
+
+from .__device import (
+    isAssigned,
+    getPortNumber,
+    getName,
+    midiOutMsg,
+    midiOutNewMsg,
+    midiOutSysex,
+    sendMsgGeneric,
+    directFeedback,
+    repeatMidiEvent,
+    stopRepeatMidiEvent,
+    setMasterSync,
+    getMasterSync,
+    getDeviceId,
+)
+from .__fl import (
+    processMIDICC,
+    forwardMIDICC,
+    findEventID,
+    getLinkedValue,
+    getLinkedValueString,
+    getLinkedParamName,
+    getLinkedInfo,
+    getLinkedChannel,
+    linkToLastTweaked,
+)
+from .__dispatch import (
+    dispatch,
+    dispatchReceiverCount,
+    dispatchGetReceiverPortNumber,
+)
+from .__util import (
+    createRefreshThread,
+    destroyRefreshThread,
+    fullRefresh,
+    isDoubleClick,
+)
