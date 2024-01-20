a56ec71eac2330451f2777cb501c79f42873fb4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:08:57 +0000

    Add package modopt/1.7.1/modopt-1.7.1.tar.gz

---
+++ packages/modopt/modopt-1.7.1.tar.gz/modopt-1.7.1/modopt/math/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""MATHEMATICS ROUTINES.
+
+This module contains submodules for mathematical applications.
+
+:Author: Samuel Farrens <samuel.farrens@cea.fr>
+
+"""
+
+__all__ = ['convolve', 'matrix', 'stats', 'metrics']
