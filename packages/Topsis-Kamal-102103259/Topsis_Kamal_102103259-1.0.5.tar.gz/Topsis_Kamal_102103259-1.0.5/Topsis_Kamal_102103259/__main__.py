1bb5eecdc383a54ab4e87dcbd6dc1010fe600c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:05:15 +0000

    Add package Topsis-Kamal-102103259/1.0.5/Topsis_Kamal_102103259-1.0.5.tar.gz

---
+++ packages/Topsis-Kamal-102103259/Topsis_Kamal_102103259-1.0.5.tar.gz/Topsis_Kamal_102103259-1.0.5/Topsis_Kamal_102103259/__main__.py
@@ -0,0 +1,14 @@
+from .Topsis_102103259 import TOPSIS
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("incorrect number of parameters")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    weights = sys.argv[2]
+    impacts = sys.argv[3]
+    output_file = sys.argv[4]
+
+    TOPSIS(input_file, weights, impacts, output_file)
