7b87504f5d8779d4eb1f2bb0e1c4401d3bad9272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 04:05:34 +0000

    Add package intel-extension-for-transformers/1.3.1/intel_extension_for_transformers-1.3.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/intel-extension-for-transformers/intel_extension_for_transformers-1.3.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/intel_extension_for_transformers/neural_chat/chatbot.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Neural Chat Chatbot API."""
+
+from intel_extension_for_transformers.llm.quantization.optimization import Optimization
+from .config import PipelineConfig
+from .config import BaseFinetuningConfig
+from .plugins import plugins
+
+from .errorcode import ErrorCodes
+from .utils.error_utils import set_latest_error, get_latest_error, clear_latest_error
+from .config_logging import configure_logging
+logger = configure_logging()
+
+
+def build_chatbot(config: PipelineConfig=None):
+    """Build the chatbot with a given configuration.
+
+    Args:
+        config (PipelineConfig): Configuration for building the chatbot.
+
+    Returns:
+        adapter: The chatbot model adapter.
+
+    Example:
+        from intel_extension_for_transformers.neural_chat import build_chatbot
+        pipeline = build_chatbot()
+        response = pipeline.predict(query="Tell me about Intel Xeon Scalable Processors.")
+    """
+    global plugins
+    clear_latest_error()
+    if not config:
+        config = PipelineConfig()
+
+    # create model adapter
+    if "llama" in config.model_name_or_path.lower():
+        from .models.llama_model import LlamaModel
+        adapter = LlamaModel()
+    elif "mpt" in config.model_name_or_path.lower():
+        from .models.mpt_model import MptModel
+        adapter = MptModel()
+    elif "neural-chat" in config.model_name_or_path.lower():
+        from .models.neuralchat_model import NeuralChatModel
+        adapter = NeuralChatModel()
+    elif "chatglm" in config.model_name_or_path.lower():
+        from .models.chatglm_model import ChatGlmModel
+        adapter = ChatGlmModel()
+    elif "qwen" in config.model_name_or_path.lower():
+        from .models.qwen_model import QwenModel
+        adapter = QwenModel()
+    elif "mistral" in config.model_name_or_path.lower():
+        from .models.mistral_model import MistralModel
+        adapter = MistralModel()
+    elif "solar" in config.model_name_or_path.lower():
+        from .models.solar_model import SolarModel
+        adapter = SolarModel()
+    elif "opt" in config.model_name_or_path.lower() or \
+         "gpt" in config.model_name_or_path.lower() or \
+         "flan-t5" in config.model_name_or_path.lower() or \
+         "bloom" in config.model_name_or_path.lower() or \
+         "starcoder" in config.model_name_or_path.lower() or \
+         "codegen" in config.model_name_or_path.lower() or \
+         "magicoder" in config.model_name_or_path.lower() or \
+         "mixtral" in config.model_name_or_path.lower() or \
+         "phi-2" in config.model_name_or_path.lower():
+        from .models.base_model import BaseModel
+        adapter = BaseModel()
+    else:
+        set_latest_error(ErrorCodes.ERROR_MODEL_NOT_SUPPORTED)
+        logger.error("build_chatbot: unknown model")
+        return
+
+    # register plugin instance in model adaptor
+    if config.plugins:
+        for plugin_name, plugin_value in config.plugins.items():
+            enable_plugin = plugin_value.get('enable', False)
+            if enable_plugin:
+                if plugin_name == "tts":
+                    from .pipeline.plugins.audio.tts import TextToSpeech
+                    plugins[plugin_name]['class'] = TextToSpeech
+                elif plugin_name == "tts_chinese":
+                    from .pipeline.plugins.audio.tts_chinese import ChineseTextToSpeech
+                    plugins[plugin_name]['class'] = ChineseTextToSpeech
+                elif plugin_name == "asr":
+                    from .pipeline.plugins.audio.asr import AudioSpeechRecognition
+                    plugins[plugin_name]['class'] = AudioSpeechRecognition
+                elif plugin_name == "retrieval":
+                    from .pipeline.plugins.retrieval.retrieval_agent import Agent_QA
+                    plugins[plugin_name]['class'] = Agent_QA
+                elif plugin_name == "cache":
+                    from .pipeline.plugins.caching.cache import ChatCache
+                    plugins[plugin_name]['class'] = ChatCache
+                elif plugin_name == "safety_checker":
+                    from .pipeline.plugins.security.safety_checker import SafetyChecker
+                    plugins[plugin_name]['class'] = SafetyChecker
+                elif plugin_name == "ner":
+                    from .pipeline.plugins.ner.ner import NamedEntityRecognition
+                    plugins[plugin_name]['class'] = NamedEntityRecognition
+                elif plugin_name == "face_animation":
+                    from .pipeline.plugins.video.face_animation.sadtalker import SadTalker
+                    plugins[plugin_name]['class'] = SadTalker
+                elif plugin_name == "image2image": # pragma: no cover
+                    from .pipeline.plugins.image2image.image2image import Image2Image
+                    plugins[plugin_name]['class'] = Image2Image
+                else:
+                    set_latest_error(ErrorCodes.ERROR_PLUGIN_NOT_SUPPORTED)
+                    logger.error("build_chatbot: unknown plugin")
+                    return
+                print(f"create {plugin_name} plugin instance...")
+                print(f"plugin parameters: ", plugin_value['args'])
+                try:
+                    plugins[plugin_name]["instance"] = plugins[plugin_name]['class'](**plugin_value['args'])
+                except Exception as e:
+                    if "[Rereieval ERROR] Document format not supported" in str(e):
+                        set_latest_error(ErrorCodes.ERROR_RETRIEVAL_DOC_FORMAT_NOT_SUPPORTED)
+                        logger.error("build_chatbot: retrieval plugin init failed")
+                    elif "[SafetyChecker ERROR] Sensitive check file not found" in str(e):
+                        set_latest_error(ErrorCodes.ERROR_SENSITIVE_CHECK_FILE_NOT_FOUND)
+                        logger.error("build_chatbot: safety checker plugin init failed")
+                    else:
+                        set_latest_error(ErrorCodes.ERROR_GENERIC)
+                        logger.error("build_chatbot: plugin init failed")
+                    return
+                adapter.register_plugin_instance(plugin_name, plugins[plugin_name]["instance"])
+
+    parameters = {}
+    parameters["model_name"] = config.model_name_or_path
+    if config.tokenizer_name_or_path:
+        parameters["tokenizer_name"] = config.tokenizer_name_or_path
+    else:
+        parameters["tokenizer_name"] = config.model_name_or_path
+    parameters["device"] = config.device
+    parameters["use_hpu_graphs"] = config.loading_config.use_hpu_graphs
+    parameters["cpu_jit"] = config.loading_config.cpu_jit
+    parameters["ipex_int8"] = config.loading_config.ipex_int8
+    parameters["use_cache"] = config.loading_config.use_cache
+    parameters["peft_path"] = config.loading_config.peft_path
+    parameters["use_deepspeed"] = config.loading_config.use_deepspeed
+    parameters["use_llm_runtime"] = config.loading_config.use_llm_runtime
+    parameters["optimization_config"] = config.optimization_config
+    parameters["hf_access_token"] = config.hf_access_token
+    parameters["assistant_model"] = config.assistant_model
+    if config.serving_config and config.serving_config.framework == "vllm":
+        parameters["use_vllm"] = True
+        parameters["vllm_engine_params"] = config.serving_config.framework_config
+    else:
+        parameters["use_vllm"] = False
+        parameters["vllm_engine_params"] = None
+    adapter.load_model(parameters)
+    if get_latest_error():
+        return
+    else:
+        return adapter
+
+def finetune_model(config: BaseFinetuningConfig):
+    """Finetune the model based on the provided configuration.
+
+    Args:
+        config (BaseFinetuningConfig): Configuration for finetuning the model.
+    """
+    clear_latest_error()
+    assert config is not None, "BaseFinetuningConfig is needed for finetuning."
+    from intel_extension_for_transformers.llm.finetuning.finetuning import Finetuning
+    finetuning = Finetuning(config)
+    try:
+        finetuning.finetune()
+    except FileNotFoundError as e:
+        logger.error(f"Exception: {e}")
+        if "Couldn't find a dataset script" in str(e):
+            set_latest_error(ErrorCodes.ERROR_DATASET_NOT_FOUND)
+    except ValueError as e:
+        logger.error(f"Exception: {e}")
+        if "--do_eval requires a validation dataset" in str(e):
+            set_latest_error(ErrorCodes.ERROR_VALIDATION_FILE_NOT_FOUND)
+        elif "--do_train requires a train dataset" in str(e):
+            set_latest_error(ErrorCodes.ERROR_TRAIN_FILE_NOT_FOUND)
+    except Exception as e:
+        logger.error(f"Exception: {e}")
+        if "Permission denied" in str(e):
+            set_latest_error(ErrorCodes.ERROR_DATASET_CACHE_DIR_NO_WRITE_PERMISSION)
+        elif config.finetune_args.peft == "lora":
+            set_latest_error(ErrorCodes.ERROR_LORA_FINETUNE_FAIL)
+        elif config.finetune_args.peft == "llama_adapter":
+            set_latest_error(ErrorCodes.ERROR_LLAMA_ADAPTOR_FINETUNE_FAIL)
+        elif config.finetune_args.peft == "ptun":
+            set_latest_error(ErrorCodes.ERROR_PTUN_FINETUNE_FAIL)
+        elif config.finetune_args.peft == "prefix":
+            set_latest_error(ErrorCodes.ERROR_PREFIX_FINETUNE_FAIL)
+        elif config.finetune_args.peft == "prompt":
+            set_latest_error(ErrorCodes.ERROR_PROMPT_FINETUNE_FAIL)
+        else:
+            set_latest_error(ErrorCodes.ERROR_GENERIC)
+
+def optimize_model(model, config, use_llm_runtime=False):
+    """Optimize the model based on the provided configuration.
+
+    Args:
+        model: large language model
+        config (OptimizationConfig): The configuration required for optimizing the model.
+        use_llm_runtime (bool): A boolean indicating whether to use the LLM runtime graph optimization.
+    """
+    clear_latest_error()
+    optimization = Optimization(optimization_config=config)
+    try:
+        model = optimization.optimize(model, use_llm_runtime)
+    except Exception as e:
+        logger.error(f"Exception: {e}")
+        from intel_extension_for_transformers.transformers import (
+            MixedPrecisionConfig,
+            WeightOnlyQuantConfig,
+            BitsAndBytesConfig
+        )
+        if type(config) == MixedPrecisionConfig:
+            set_latest_error(ErrorCodes.ERROR_AMP_OPTIMIZATION_FAIL)
+        elif type(config) == WeightOnlyQuantConfig:
+            set_latest_error(ErrorCodes.ERROR_WEIGHT_ONLY_QUANT_OPTIMIZATION_FAIL)
+        elif type(config) == BitsAndBytesConfig:
+            set_latest_error(ErrorCodes.ERROR_BITS_AND_BYTES_OPTIMIZATION_FAIL)
+    return model
---
+++ packages/intel-extension-for-transformers/intel_extension_for_transformers-1.3.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/intel_extension_for_transformers/neural_chat/server/restful/voicechat_api.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from fastapi import APIRouter, Request
+from fastapi.responses import StreamingResponse
+from typing import Optional
+from ...cli.log import logger
+from fastapi import File, UploadFile, Form
+from pydub import AudioSegment
+from ...config import GenerationConfig
+from ...plugins import plugins
+import base64
+import torch
+
+class VoiceChatAPIRouter(APIRouter):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.chatbot = None
+
+    def set_chatbot(self, chatbot, use_deepspeed=False, world_size=1, host="0.0.0.0", port=80) -> None:
+        self.chatbot = chatbot
+        self.use_deepspeed = use_deepspeed
+        self.world_size = world_size
+        self.host = host
+        self.port = port
+
+    def get_chatbot(self):
+        if self.chatbot is None:
+            logger.error("Chatbot instance is not found.")
+            raise RuntimeError("Chatbot instance has not been set.")
+        return self.chatbot
+
+    def handle_voice_asr_request(self, filename: str) -> str:
+        chatbot = self.get_chatbot()
+        try:
+            return chatbot.asr.audio2text(filename)
+        except Exception as e:
+            raise Exception(e)
+
+    async def handle_voice_chat_request(self, prompt: str, voice: str, audio_output_path: Optional[str]=None) -> str:
+        chatbot = self.get_chatbot()
+        try:
+            plugins.tts.args["voice"] = voice
+            config = GenerationConfig(audio_output_path=audio_output_path)
+            result, link = chatbot.chat_stream(query=prompt, config=config)
+            def audio_file_generate(result):
+                for path in result:
+                    with open(path,mode="rb") as file:
+                        bytes = file.read()
+                        data = base64.b64encode(bytes)
+                    yield f"data: {data}\n\n"
+                yield f"data: [DONE]\n\n"
+            return StreamingResponse(audio_file_generate(result), media_type="text/event-stream")
+        except Exception as e:
+            raise Exception(e)
+
+    async def handle_create_speaker_embedding(self, spk_id):
+        chatbot = self.get_chatbot()
+        try:
+            spk_embedding = chatbot.tts.create_speaker_embedding(spk_id)
+            torch.save(spk_embedding, f'../../../../speaker_embeddings/spk_embed_{spk_id}.pt')
+        except Exception as e:
+            logger.info(f"create spk embedding failes! {e}")
+            return {"create_spk": "fail"}
+        return {"create_spk": "success"}
+
+
+router = VoiceChatAPIRouter()
+
+# We deliver an audio stream to the frontend which necessitating the use of SSE to manage real-time audio streaming.
+# However, SSE isn't suitable for transmitting audio files directly.
+# As a solution, we should split the API into /v1/voicechat/asr and /v1/voicechat/llm_tts.
+
+@router.post("/v1/talkingbot/asr")
+async def handle_talkingbot_asr(file: UploadFile = File(...)):
+    file_name = file.filename
+    logger.info(f'Received file: {file_name}')
+    with open("tmp_audio_bytes", 'wb') as fout:
+        content = await file.read()
+        fout.write(content)
+    audio = AudioSegment.from_file("tmp_audio_bytes")
+    audio = audio.set_frame_rate(16000)
+    # bytes to wav
+    file_name = file_name +'.wav'
+    audio.export(f"{file_name}", format="wav")
+    asr_result = router.handle_voice_asr_request(file_name)
+    return {"asr_result": asr_result}
+
+
+@router.post("/v1/talkingbot/llm_tts")
+async def talkingbot(request: Request):
+    data = await request.json()
+    text = data["text"]
+    voice = data["voice"]
+    knowledge_id = data["knowledge_id"]
+    audio_output_path = data["audio_output_path"] if "audio_output_path" in data else "output_audio"
+
+    logger.info(f'Received prompt: {text}, and use voice: {voice} knowledge_id: {knowledge_id}')
+
+    return await router.handle_voice_chat_request(text, voice, audio_output_path)
+
+@router.post("/v1/talkingbot/create_embedding")
+async def create_speaker_embedding(file: UploadFile = File(...)):
+    print(dir(file))
+    file_name = file.filename
+    # generate a unique id
+    import uuid
+    spk_id = f"spk_{str(uuid.uuid1())[:8]}"
+    with open(f"tmp_spk_{file_name}", 'wb') as fout:
+        content = await file.read()
+        fout.write(content)
+    audio = AudioSegment.from_file(f"tmp_spk_{file_name}")
+    audio.export(f"{spk_id}", format="mp3")
+
+    await router.handle_create_speaker_embedding(spk_id)
+    return {"spk_id": spk_id}
