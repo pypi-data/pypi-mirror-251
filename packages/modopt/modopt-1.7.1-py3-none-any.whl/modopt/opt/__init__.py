fb6484d768ecb19f8cad1d22bcb421b658544144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:08:57 +0000

    Add package modopt/1.7.1/modopt-1.7.1-py3-none-any.whl

---
+++ packages/modopt/modopt-1.7.1-py3-none-any.whl/modopt/opt/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""OPTIMISATION PROBLEM MODULES.
+
+This module contains submodules for solving optimisation problems.
+
+:Author: Samuel Farrens <samuel.farrens@cea.fr>
+
+"""
+
+__all__ = ['cost', 'gradient', 'linear', 'algorithms', 'proximity', 'reweight']
---
+++ packages/modopt/modopt-1.7.1-py3-none-any.whl/modopt/plot/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""PLOTTING ROUTINES.
+
+This module contains submodules for plotting applications.
+
+:Author: Samuel Farrens <samuel.farrens@cea.fr>
+
+"""
+
+__all__ = ['cost_plot']
