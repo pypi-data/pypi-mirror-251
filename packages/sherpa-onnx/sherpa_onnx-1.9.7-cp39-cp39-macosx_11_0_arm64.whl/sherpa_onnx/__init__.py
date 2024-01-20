3643ae0fcc6b4d310a8156a85e19ab16d584870a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:06:43 +0000

    Add package sherpa-onnx/1.9.7/sherpa_onnx-1.9.7-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/sherpa-onnx/sherpa_onnx-1.9.7-cp39-cp39-macosx_11_0_arm64.whl/sherpa_onnx/__init__.py
@@ -0,0 +1,20 @@
+from _sherpa_onnx import (
+    CircularBuffer,
+    Display,
+    OfflineStream,
+    OfflineTts,
+    OfflineTtsConfig,
+    OfflineTtsModelConfig,
+    OfflineTtsVitsModelConfig,
+    OnlineStream,
+    SileroVadModelConfig,
+    SpeechSegment,
+    VadModel,
+    VadModelConfig,
+    VoiceActivityDetector,
+)
+
+from .offline_recognizer import OfflineRecognizer
+from .online_recognizer import OnlineRecognizer
+from .utils import text2token
+__version__ = '1.9.7'
