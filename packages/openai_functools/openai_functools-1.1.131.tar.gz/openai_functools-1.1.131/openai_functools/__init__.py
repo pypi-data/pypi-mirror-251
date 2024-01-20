f953fdc59466f84d1dc3b7524220c413ad65b5d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:06:43 +0000

    Add package openai_functools/1.1.131/openai_functools-1.1.131.tar.gz

---
+++ packages/openai_functools/openai_functools-1.1.131.tar.gz/openai_functools-1.1.131/openai_functools/__init__.py
@@ -0,0 +1,10 @@
+from .function_spec import FunctionSpec
+from .functions_orchestrator import FunctionsOrchestrator
+from .metadata_generator import extract_openai_function_metadata, openai_function
+
+__all__ = [
+    "openai_function",
+    "extract_openai_function_metadata",
+    "FunctionsOrchestrator",
+    "FunctionSpec",
+]
