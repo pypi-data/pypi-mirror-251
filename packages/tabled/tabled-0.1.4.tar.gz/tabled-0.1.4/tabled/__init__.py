2c99f517584e439cd131c6d9035f74199b5184ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:08:13 +0000

    Add package tabled/0.1.4/tabled-0.1.4.tar.gz

---
+++ packages/tabled/tabled-0.1.4.tar.gz/tabled-0.1.4/tabled/__init__.py
@@ -0,0 +1,16 @@
+"""
+A  (key-value) data-object-layer to get (pandas) tables from a variety of sources with ease.
+
+"""
+from tabled.html import get_tables_from_url
+from tabled.base import (
+    DfLocalFileReader,
+    KeyFuncReader,
+    dflt_ext_mapping,
+    df_from_data_according_to_key,
+    get_ext,
+    identity,
+    key_func_mapping,
+    split_keys,
+)
+from tabled.multi import ColumnOrientedMapping
