44e619146c2fd4aa6f73ab32d49605c6f510a7d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:06:37 +0000

    Add package ai-logicplum/1.1.8/ai-logicplum-1.1.8.tar.gz

---
+++ packages/ai-logicplum/ai-logicplum-1.1.8.tar.gz/ai-logicplum-1.1.8/logicplum/aicloud/users/client.py
@@ -0,0 +1,39 @@
+import requests
+from ..config import base_url, api_token
+
+# Creates an account  Organization/Client
+def create_account(name,email,username,password):
+    url = f"{base_url}/client/" 
+    data = {        
+    "name": name,
+    "email":email,
+    "username": username,       
+    "password": password
+    }
+    headers = {"Authorization":api_token}
+    response = requests.post(url,data=data,headers=headers)
+    return response.text
+
+
+def create_app(name,client_id):
+    data = {
+    "name": name,                  
+    "client": client_id  
+    }
+    url = f"{base_url}/app/"
+    headers = {"Authorization":api_token}
+    response = requests.post(url,json=data,headers=headers)
+    return response.json()
+
+
+def login(username,password):
+    data = {
+    "username": username,                  
+    "password": password  
+    }
+    url = f"{base_url}/login"
+    headers = {"Authorization":api_token}
+    response = requests.post(url,json=data,headers=headers)
+    output = response.json()
+    del output["refresh"]
+    return output
