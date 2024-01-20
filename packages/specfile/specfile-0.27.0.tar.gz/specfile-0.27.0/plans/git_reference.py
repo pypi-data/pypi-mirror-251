b2ee96cb6121babe439f0bb5f445a32a11f4f6bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-20 03:08:04 +0000

    Add package specfile/0.27.0/specfile-0.27.0.tar.gz

---
+++ packages/specfile/specfile-0.27.0.tar.gz/specfile-0.27.0/plans/git_reference.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import subprocess
+from pathlib import Path
+
+import fmf
+
+# Set discover of specfile tests to a fixed commit
+tree_root = Path.cwd().absolute()
+tree = fmf.Tree(tree_root)
+main_node = tree.find("/plans")
+with main_node as data:
+    data["discover"]["url"] = "https://github.com/packit/specfile.git"
+    data["discover"]["ref"] = (
+        subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
+    )
+
+
+# Set discover of packit integration tests to the current main
+packit_node = tree.find("/plans/packit-integration")
+with packit_node as data:
+    data["discover"]["ref"] = (
+        subprocess.check_output(
+            ["git", "ls-remote", "https://github.com/packit/packit", "main"]
+        )
+        .decode()
+        .strip()
+        .split()[0]
+    )
